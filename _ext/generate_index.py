53f015f5c7055cdef3935fed8e397f1e419e8dba
Karthik Venkateswaran <vasanthivenkateswaran@gmail.com>
2017-07-07 13:30:02 +0530

    WIP search pagination

--- _ext/generate_index.py
+++ _ext/generate_index.py
@@ -17,77 +17,17 @@ from bs4 import BeautifulSoup
 
 indexObjs = []
 
-def init_generate_index(app):
-
-    def _get_generate_index(docname, **kwds):
-        print ('\n')
-        print (docname)
-        doctree = app.env.get_doctree(docname)
-        if 'maxdepth' not in kwds:
-            kwds['maxdepth'] = 0
-        toctrees = []
-
-        for toctreenode in doctree.traverse(addnodes.toctree):
-            toctreenode.parent.remove(toctreenode)
-
-        for pending_xhrefnode in doctree.traverse(addnodes.pending_xref ):
-            pending_xhrefnode.parent.remove(pending_xhrefnode)
-
-        doctree = app.env.get_and_resolve_doctree(docname, app.builder, doctree)
-
-        htmlString = docutils.core.publish_from_doctree(doctree, writer_name='html')
-        soup = BeautifulSoup(htmlString, 'html.parser')
-
-        docpath = app.env.doc2path(docname, None)
-
-        imgs = soup.findAll("img", { "class" : "featured-image" })
-
-        # if the imgs has something  return else return default image
-
-
-        # title = soup.findAll('title')[0].text
-        content = soup.findAll('body')[0].text
-        url = '/' + docpath.split('.rst')[0] + '.html'
-        category = docpath.split('/')[0]
-        image = ''
-        title = 'Hasura Authentication'
-        description = 'Hasura authentication helps you implement sign/signup'
-
-        if ( len(imgs) > 0 ):
-            image = imgs[0]['src'].split('/')[-1]
-            indexObj = { "title": title, "content": content, "url": url, "category": category, "image": image, "description": description }
-
-            indexObjs.append(indexObj)
-        else:
-            indexObj = { "title": title, "content": content, "url": url, "category": category, "image": image, "description": description }
-
-            indexObjs.append(indexObj)
-
-    ctx = app.env.config['html_context']
-    if 'generate_index' not in ctx:
-        ctx['generate_index'] = _get_generate_index
-
-
 def callme(app, exception):
     f = open('./sample.json', 'w+')
     f.write(json.dumps(indexObjs))
     print('I am called')
 
-def callmeAgain(app, pagename, templatename, context, doctree):
-    # print ('\n Values')
-    # print ('\n')
-    # print (pagename)
-    # print (templatename)
-    # print (context)
-    # print (doctree)
+def generateIndexFile(app, pagename, templatename, context, doctree):
 
     title = ''
-
     if ( 'title' in context ):
         title = context['title']
-
     content = ''
-
     image = ''
 
     if ( 'body' in context ):
@@ -109,6 +49,5 @@ def callmeAgain(app, pagename, templatename, context, doctree):
     indexObjs.append(indexObj)
 
 def setup(app):
-    app.connect('builder-inited', init_generate_index)
     app.connect('build-finished', callme)
-    app.connect('html-page-context', callmeAgain)
+    app.connect('html-page-context', generateIndexFile)
