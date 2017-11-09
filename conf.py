75a5682d4cbfdc7c0be62f166b370b42d654b758
Tanmai Gopal <tanmaig@34cross.in>
2017-11-10 00:37:05 +0530

    removes guides, tutorials, ref as build sources

--- conf.py
+++ conf.py
@@ -134,7 +134,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'venv', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'venv', 'Thumbs.db', '.DS_Store', 'ref', 'guides', 'tutorials']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
