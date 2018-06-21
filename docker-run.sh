0c9d9f5ad62039fbc77175df0fca2c697ad38c57
wawhal <rishichandra.wawhal@gmail.com>
2018-06-21 17:12:36 +0530

    Fixed entrypoint command

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 docker run -p 8080:8080 \
        hasura/graphql-engine:latest \
-       raven \
+       graphql-engine \
        --database-url postgres://username:password@hostname:port/dbname \
        serve
