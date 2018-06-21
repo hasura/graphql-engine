12721809bd08daa3b5a9560960972eb405e7d402
Tanmai Gopal <tanmaig@34cross.in>
2018-06-21 00:30:04 -0700

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 docker run -p 8080:8080 \
-       hasuranightly/raven:8df5234 \
+       hasura/graphql-engine:latest \
        raven \
        --database-url postgres://username:password@hostname:port/dbname \
        serve
