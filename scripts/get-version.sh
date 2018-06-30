51c19a984593c972a801878b3ba3cac9ddd8d476
Shahidh <shahidhkmuhammed@gmail.com>
2018-06-30 20:34:01 +0530

    add build system components

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 
 GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-GIT_SHA="$(git rev-parse --short HEAD)"
-GIT_TAG="$(git describe --tags 2>/dev/null)"
-GIT_TAG_EXACT="$(git describe --tags --exact-only 2>/dev/null)"
-GIT_DIRTY=$(test -n "`git status --porcelain`" && echo "-dirty" || echo "")
+GIT_TAG="$(git describe --tags --dirty 2>/dev/null)"
+GIT_TAG_EXACT="$(git describe --tags --exact-match --dirty 2>/dev/null)"
+
 VERSION="${GIT_TAG_EXACT}"
-
-if [ -z "$GIT_TAG" ]; then
-    VERSION="${GIT_BRANCH}-${GIT_SHA}${GIT_DIRTY}"
-fi
-
-if [ -z "$GIT_TAG_EXACT" ]; then
-    VERSION="${GIT_TAG}-${GIT_BRANCH}${GIT_DIRTY}"
-fi
+test -n "$VERSION" || VERSION="${GIT_TAG}-${GIT_BRANCH}"
 
 echo "$VERSION"
