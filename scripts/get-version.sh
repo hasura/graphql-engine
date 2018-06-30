007377bf30d898d0c79865891b4826925113f0ff
Shahidh <shahidhkmuhammed@gmail.com>
2018-06-30 19:31:51 +0530

    update get-version script

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -1,46 +1,18 @@
-#!/usr/bin/env sh
-set -e
+#!/usr/bin/env bash
 
-get_changes_hash()
-{
-    # We use this to determine if there are any new additions
-    local GIT_STATUS="$(git status --porcelain)"
-    # To determine if there are any changes
-    local GIT_DIFF_INDEX="$(git diff-index -p HEAD --)"
-    # Whether anything changed in the repo
-    export GIT_DIRTY="$GIT_STATUS$GIT_DIFF_INDEX"
-    if [ -n "$GIT_DIRTY" ]; then
-        DIRTY_HASH_SHORT="$(echo $GIT_DIRTY | sha256sum | awk '{print $1}' | tail -c 9)"
-        echo -dirty-$DIRTY_HASH_SHORT
-    else
-        echo ''
-    fi
-}
+GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+GIT_SHA="$(git rev-parse --short HEAD)"
+GIT_TAG="$(git describe --tags 2>/dev/null)"
+GIT_TAG_EXACT="$(git describe --tags --exact-only 2>/dev/null)"
+GIT_DIRTY=$(test -n "`git status --porcelain`" && echo "-dirty" || echo "")
+VERSION="${GIT_TAG_EXACT}"
 
-get_main_version()
-{
-    # Get the branch name
-    local GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-    # Get the current commit id
-    local COMMIT_HASH_SHORT="$(git rev-parse --short HEAD)"
+if [ -z "$GIT_TAG" ]; then
+    VERSION="${GIT_BRANCH}-${GIT_SHA}${GIT_DIRTY}"
+fi
 
-    case "$GIT_BRANCH" in
+if [ -z "$GIT_TAG_EXACT" ]; then
+    VERSION="${GIT_TAG}-${GIT_BRANCH}${GIT_DIRTY}"
+fi
 
-        # The master branch
-        'master')
-            echo $COMMIT_HASH_SHORT;;
-
-        # The release branches
-        release-*)
-            local RELEASE_VER="$(git describe --match "v[0-9]*" HEAD 2>/dev/null)"
-            test -n "$RELEASE_VER" ||
-                RELEASE_VER="$(expr "$GIT_BRANCH" : release-*'\(.*\)')"-$COMMIT_HASH_SHORT
-            echo $RELEASE_VER;;
-
-        # Everything else
-        *)
-            echo $GIT_BRANCH-$COMMIT_HASH_SHORT;;
-    esac
-}
-
-echo "$(get_main_version)$(get_changes_hash)"
+echo "$VERSION"
