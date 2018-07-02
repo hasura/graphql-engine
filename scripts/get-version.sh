5ba9a16b7170469407c84ddbca00a381c77e32ec
Shahidh <shahidhkmuhammed@gmail.com>
2018-07-02 15:23:47 +0530

    update versioning format

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-GIT_TAG="$(git describe --tags --dirty 2>/dev/null)"
+GIT_SHA="$(git rev-parse --short HEAD)"
 GIT_TAG_EXACT="$(git describe --tags --exact-match --dirty 2>/dev/null)"
+GIT_DIRTY=$(test -n "`git status --porcelain`" && echo "-dirty" || echo "")
 
 VERSION="${GIT_TAG_EXACT}"
-test -n "$VERSION" || VERSION="${GIT_TAG}-${GIT_BRANCH}"
+test -n "$VERSION" || VERSION="${GIT_BRANCH}-${GIT_SHA}${GIT_DIRTY}"
 
 echo "$VERSION"
