2c8c289973268b337c24279032b10e672a9d1cbf
shahidhk <shahidh@hasura.io>
2018-07-06 10:47:27 +0530

    wip: get process pid

--- .circleci/test-cli.sh
+++ .circleci/test-cli.sh
@@ -9,6 +9,8 @@ cd "$CLI_ROOT"
 # start graphql-engine
 /build/_server_output/graphql-engine \
     --database-url postgres://gql_test@localhost:5432/gql_test serve &
+#PID=$!
 
 # test cli
 HASURA_GRAPHQL_TEST_ENDPOINT="http://localhost:8080" make test
+#kill $PID
