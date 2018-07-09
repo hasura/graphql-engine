4820303ff8559279687caf82297c2603b59ae410
Shahidh K Muhammed <shahidh@hasura.io>
2018-07-09 19:59:55 +0530

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 docker run -p 8080:8080 \
-       hasura/graphql-engine:v1.0.0-alpha01 \
+       hasura/graphql-engine:v1.0.0-alpha04 \
        graphql-engine \
        --database-url postgres://username:password@hostname:port/dbname \
        serve --enable-console
