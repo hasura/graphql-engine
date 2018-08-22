0eae77fb39af2228605e49a7abdfae1d6dc404ff
Aswin M Prabhu <aswinmprabhu@gmail.com>
2018-08-22 22:32:43 +0530

    add login functionality

--- src/components/SubscriptionExample.js
+++ src/components/SubscriptionExample.js
@@ -18,34 +18,56 @@ const fetchMessages = gql`
 `;
 
 export default class ChatComponent extends React.Component {
- 
+
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      isLoggedIn: false,
+      username:""
+    };
   } 
-  
+
   render() {
     return (
- 	 		<Query
-		    query={fetchMessages}
-		  >
-		    {({ data, loading, error, refetch }) => {
-		    	if (loading) {
-		    		return (<h2> Loading </h2>);
-		    	}
-          if (error) {
-            return (JSON.stringify(error, null, 2));
-          }
-          if (!this.state.refetch) {
-            this.setState({refetch});
-          }
-		    	return (
-		    		<div className="App">
-		    			<RenderMessages messages={data.message} refetch={refetch}/>
-		    		</div>
-		    	)
-		    }}
-		  </Query>
- 	 	)
+      <div className="App">
+        {
+          !this.state.isLoggedIn ? (
+            <div className="login">
+              <input type="text" id="username" placeholder="Username" value={this.state.username} onChange={e => {
+                this.setState({username:e.target.value});
+              }}/>
+            <button onClick={e => {
+              e.preventDefault();
+              if(this.state.username != ""){
+                this.setState({isLoggedIn: true});
+              }
+            }} 
+          >
+            Enter
+          </button> 
+        </div>
+          ) : (
+            <Query
+              query={fetchMessages}
+            >
+              {({ data, loading, error, refetch }) => {
+                if (loading) {
+                  return (<h2> Loading </h2>);
+                }
+                if (error) {
+                  return (JSON.stringify(error, null, 2));
+                }
+                if (!this.state.refetch) {
+                  this.setState({refetch});
+                }
+                return (
+                  <RenderMessages messages={data.message} refetch={refetch}/>
+                )
+              }}
+            </Query>
+          )
+        }
+      </div>
+    )
   }
 }
