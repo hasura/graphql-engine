0d35e1a7eb733aa690d28d68d5ce2ecaa17da09f
wawhal <rishichandra.wawhal@gmail.com>
2018-08-24 03:39:53 +0530

    fix login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,20 +6,24 @@ const LoginComponent = (props) => {
       <h2 className="loginHeading"> Welcome to sample chat app made with Hasura GraphQL Engine </h2>
       <div className="login">
         <div>
-          <input
-            type="text"
-            id="username"
-            className="loginTextbox"
-            placeholder="Username"
-            value={props.username}
-            onChange={(e) => props.setUsername(e.target.value)}
-          />
-          <button
-            className="loginButton"
-            onClick={props.login}
+          <form
+            submit={props.login}
           >
-            Enter
-          </button> 
+            <input
+              type="text"
+              id="username"
+              className="loginTextbox"
+              placeholder="Username"
+              value={props.username}
+              onChange={(e) => props.setUsername(e.target.value)}
+            />
+            <button
+              className="loginButton"
+              onClick={props.login}
+            >
+              Enter
+            </button> 
+          </form>
         </div>
       </div>
     </div>
