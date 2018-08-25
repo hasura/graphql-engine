b9f211693840215fe33c96291a22a79ec7b46c09
rakeshkky <rakesh.emmadi21c@gmail.com>
2018-08-25 12:56:14 +0530

    do not allow creating permissions for admin role, fix #310

--- server/test/Spec.hs
+++ server/test/Spec.hs
@@ -42,6 +42,7 @@ querySpecFiles =
   , "create_author_article_permissions.yaml"
   , "create_address_resident_relationship_error.yaml"
   , "create_user_permission_address.yaml"
+  , "create_author_permission_role_admin_error.yaml"
   ]
 
 gqlSpecFiles :: [FilePath]
