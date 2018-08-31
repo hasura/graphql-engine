0f364248d716d7c2c707236fc6dda26a389de966
Tirumarai Selvan A <tiru@hasura.io>
2018-08-31 19:09:01 +0530

    send event id and trigger name in payload

--- server/src-rsr/trigger.sql
+++ server/src-rsr/trigger.sql
@@ -4,25 +4,28 @@ CREATE OR REPLACE function hdb_views.notify_skor_{{NAME}}_{{OPERATION}}() RETURN
    DECLARE
    payload json;
    data json;
+   id text;
    BEGIN
-   raise notice 'pushing to event log';
-   data := json_build_object(
-     'old', {{OLD_DATA_EXPRESSION}},
-     'new', {{NEW_DATA_EXPRESSION}}
-   );
-   payload := json_build_object(
+     raise notice 'pushing to event log';
+     id := gen_random_uuid();
+     data := json_build_object(
+       'old', {{OLD_DATA_EXPRESSION}},
+       'new', {{NEW_DATA_EXPRESSION}}
+     );
+     payload := json_build_object(
                         'table', TG_TABLE_NAME,
                         'schema', TG_TABLE_SCHEMA,
+                        'trigger_name', '{{NAME}}',
                         'op', TG_OP,
+                        'id', id,
                         'data', data
                         )::text;
-   INSERT INTO
-   hdb_catalog.event_log (schema_name, table_name, trigger_name, payload)
-   VALUES
-     (TG_TABLE_SCHEMA, TG_TABLE_NAME, '{{NAME}}', payload);
-   RETURN NULL;
+     INSERT INTO
+     hdb_catalog.event_log (id, schema_name, table_name, trigger_name, payload)
+     VALUES
+     (id, TG_TABLE_SCHEMA, TG_TABLE_NAME, '{{NAME}}', payload);
+     RETURN NULL;
    END;
    $$;
    DROP TRIGGER IF EXISTS notify_skor_{{NAME}}_{{OPERATION}} ON {{SCHEMA_NAME}}.{{TABLE_NAME}};
    CREATE TRIGGER notify_skor_{{NAME}}_{{OPERATION}} AFTER {{OPERATION}} ON {{SCHEMA_NAME}}.{{TABLE_NAME}} FOR EACH ROW EXECUTE PROCEDURE hdb_views.notify_skor_{{NAME}}_{{OPERATION}}();
-
