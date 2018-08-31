6f9a2c6124094e4e55030d47120e6cdb304e028f
Tirumarai Selvan A <tiru@hasura.io>
2018-08-31 16:22:01 +0530

    add old and new row updates in payload

--- server/src-rsr/trigger.sql
+++ server/src-rsr/trigger.sql
@@ -3,13 +3,18 @@ CREATE OR REPLACE function hdb_views.notify_skor_{{NAME}}_{{OPERATION}}() RETURN
    AS $$
    DECLARE
    payload json;
+   data json;
    BEGIN
    raise notice 'pushing to event log';
+   data := json_build_object(
+     'old', {{OLD_DATA_EXPRESSION}},
+     'new', {{NEW_DATA_EXPRESSION}}
+   );
    payload := json_build_object(
                         'table', TG_TABLE_NAME,
                         'schema', TG_TABLE_SCHEMA,
                         'op', TG_OP,
-                        'data', {{DATA_EXPRESSION}}
+                        'data', data
                         )::text;
    INSERT INTO
    hdb_catalog.event_log (schema_name, table_name, trigger_name, payload)
