4c2422ecd1e7b69f223c5be535784aeaca162323
Anon Ray <rayanon004@gmail.com>
2018-09-06 14:46:42 +0530

    fix help text for jwt config

--- server/src-lib/Hasura/Server/Init.hs
+++ server/src-lib/Hasura/Server/Init.hs
@@ -181,8 +181,8 @@ parseJwtSecret =
 
 jwtSecretHelp :: String
 jwtSecretHelp = "The JSON containing type and the JWK used for verifying. e.g: "
-              <> "`{\"type\": \"HS256\", \"key\": \"<your-hmac-shared-secret>\"}`,"
-              <> "`{\"type\": \"RS256\", \"key\": \"<your-PEM-RSA-public-key>\"}`"
+              <> "`{\"type\": \"HS256\", \"key\": \"<your-hmac-shared-secret>\", \"claims_namespace\": \"<optional-custom-claims-key-name>\"}`,"
+              <> "`{\"type\": \"RS256\", \"key\": \"<your-PEM-RSA-public-key>\", \"claims_namespace\": \"<optional-custom-claims-key-name>\"}`"
 
 
 parseCorsConfig :: Parser CorsConfigFlags
