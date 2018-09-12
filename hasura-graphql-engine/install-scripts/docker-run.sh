97de7ef07846e893fd1dd72ee94352749954dd67
Shahidh K Muhammed <shahidh@hasura.io>
2018-09-12 19:51:00 +0530

    restructure

--- hasura-graphql-engine/install-scripts/docker-run.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-docker run -p 8080:8080 \
-       hasura/graphql-engine:c7905f9 \
-       raven \
-       --database-url postgres://username:password@hostname:port/dbname \
-       serve
--- todo-app/src/history.js
+++
@@ -1,3 +0,0 @@
-import createHistory from 'history/createBrowserHistory'
-
-export default createHistory()
