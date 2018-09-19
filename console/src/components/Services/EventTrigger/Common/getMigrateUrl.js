4fa140c8dfb26a8601477af3b4ed9c1b1842aaf4
Karthik Venkateswaran <karthik@hasura.io>
2018-09-19 20:31:09 +0530

    Remove devDataApiUrl from env and from places where it is used

--- console/src/components/Services/EventTrigger/Common/getMigrateUrl.js
+++ console/src/components/Services/EventTrigger/Common/getMigrateUrl.js
@@ -5,12 +5,7 @@ const returnMigrateUrl = mode => {
   if (globals.consoleMode === 'cli') {
     return mode ? Endpoints.hasuractlMigrate : Endpoints.hasuractlMetadata;
   } else if (globals.consoleMode === 'hasuradb') {
-    let finalUrl;
-    if (globals.nodeEnv === 'development') {
-      finalUrl = globals.devDataApiUrl + '/v1/query';
-    } else {
-      finalUrl = Endpoints.query;
-    }
+    const finalUrl = Endpoints.query;
     return finalUrl;
   }
 };
