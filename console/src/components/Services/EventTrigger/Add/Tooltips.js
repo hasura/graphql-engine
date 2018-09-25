42177360e3fa50f775e53b8b149268cb5c85572d
Karthik Venkateswaran <karthikvt26@users.noreply.github.com>
2018-09-25 22:11:56 +0530

    console fixes (close #406) (close #491) (#522)
    Fetches event information on click on the redeliver event icon and refreshes every 5 seconds to display the recent invocation data

--- console/src/components/Services/EventTrigger/Add/Tooltips.js
+++ console/src/components/Services/EventTrigger/Add/Tooltips.js
@@ -13,6 +13,18 @@ export const operationsDescription = (
   </Tooltip>
 );
 
+export const webhookUrlDescription = (
+  <Tooltip id="tooltip-webhook-description">
+    POST endpoint which will be triggered with payload on configured events
+  </Tooltip>
+);
+
+export const advancedOperationDescription = (
+  <Tooltip id="tooltip-advanced-operation-description">
+    Columns to be sent in the payload of webhook
+  </Tooltip>
+);
+
 export const postgresDescription = (
   <Tooltip id="tooltip-postgres-description">
     Select the postgres schema and table
