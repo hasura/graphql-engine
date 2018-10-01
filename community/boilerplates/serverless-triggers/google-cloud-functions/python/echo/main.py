bb56329c7824882a8742edc45fdd1bad059805be
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2018-10-02 00:52:01 +0200

    use spaces instead of tabs for indentation on python echo

--- community/boilerplates/serverless-triggers/google-cloud-functions/python/echo/main.py
+++ community/boilerplates/serverless-triggers/google-cloud-functions/python/echo/main.py
@@ -3,10 +3,10 @@ import json
 
 def function(request):
     request_json = request.json
-	op = request_json.get('event/op')
-	data = request_json.get('event/data')
-	name = request_json.get('table/name')
-	schema = request_json.get('table/schema')
-	response = json.dumps({'message': 'received event', 'data': {op, data, name, schema}})
-	print(response)
-	return response
+    op = request_json.get('event/op')
+    data = request_json.get('event/data')
+    name = request_json.get('table/name')
+    schema = request_json.get('table/schema')
+    response = json.dumps({'message': 'received event', 'data': {op, data, name, schema}})
+    print(response)
+    return response
