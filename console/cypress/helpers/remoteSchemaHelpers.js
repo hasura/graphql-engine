90db553a6ea4260655235ece2373db8107dff3d0
Karthik Venkateswaran <karthik@hasura.io>
2018-11-13 20:07:34 +0530

    Change test url to heroku for now

--- console/cypress/helpers/remoteSchemaHelpers.js
+++ console/cypress/helpers/remoteSchemaHelpers.js
@@ -1,7 +1,8 @@
 export const baseUrl = Cypress.config('baseUrl');
 export const getRemoteSchemaName = (i, schemaName) =>
   `test-remote-schema-${schemaName}-${i}`;
-export const getRemoteGraphQLURL = () => 'https://countries.trevorblades.com/';
+export const getRemoteGraphQLURL = () =>
+  'https://hasura-realtime-poll.herokuapp.com/v1alpha1/graphql';
 export const getRemoteGraphQLURLFromEnv = () => 'GRAPHQL_URL';
 export const getInvalidRemoteSchemaUrl = () => 'http://httpbin.org/post';
 export const getHeaderAccessKey = i => `ACCESS_KEY-${i}`;
