e2dc647975dad90823926942b93f91e879204d34
Aravind Shankar <aravind@hasura.io>
2018-11-16 18:21:41 +0530

    WIP

--- .circleci/community-apps-test.py
+++ .circleci/community-apps-test.py
@@ -7,8 +7,7 @@ with open(".circleci/community-apps.yaml", 'r') as stream:
         APPS_DATA = yaml.load(stream)
     except yaml.YAMLError as exc:
         print(exc)
-print(APPS_DATA)
 
 for app in APPS_DATA['apps']:
-    print(app)
+    print("Running test for app {}".format(app['name']))
     subprocess.call(['.circleci/community-apps-test.sh', '{}'.format(app['name']), app['path'], app['dockerfile']])
--- community/boilerplates/auth-webhooks/passport-js/test.sh
+++ community/boilerplates/auth-webhooks/passport-js/test.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 set -evo pipefail
-export DATABASE_URL=postgres://${DATABASE_USER}:${DATABASE_PASS}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}
-echo "${DATABASE_URL}"
+export DATABASE_URL=postgres://${DATABASE_USER}:${DATABASE_PASS}@${DATABASE_HOST}ss:${DATABASE_PORT}/${DATABASE_NAME}
 echo "Migrate database"
 knex migrate:latest
 echo "Migrated database"
