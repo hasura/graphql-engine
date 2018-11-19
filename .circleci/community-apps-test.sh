23f41b04df914f16cf2146479fb1026a74c6d2df
Aravind Shankar <aravind@hasura.io>
2018-11-19 15:13:37 +0530

    WIP

--- .circleci/community-apps-test.sh
+++ .circleci/community-apps-test.sh
@@ -6,7 +6,7 @@ PGPASSWORD=gql_test psql -h localhost -p 5432 -U gql_test -c "CREATE DATABASE ${
 PGPASSWORD=gql_test psql -h localhost -p 5432 -U gql_test -c "ALTER DATABASE ${1} OWNER TO gql_test;"
 docker ps
 docker network ls
-docket network inspect bridge
+docker network inspect bridge
 docker network inspect host
 docker build -t ${1} -f ${2}/${3} ${2}
 ( docker run -e "DATABASE_NAME=${1}" -e "DATABASE_USER=gql_test" -e "DATABASE_HOST=localhost" -e "DATABASE_PORT=5432" -e "DATABASE_PASS=gql_test" --net host ${1} ) || true
