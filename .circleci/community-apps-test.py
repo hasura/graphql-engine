2024e536b3abdfa6037d7e3a3b548ebb3bf646ce
Aravind Shankar <aravind@hasura.io>
2018-11-19 19:07:17 +0530

    WIP

--- .circleci/community-apps-test.py
+++ .circleci/community-apps-test.py
@@ -9,10 +9,10 @@ def default_env(path, app_name):
     set_key(path, 'DATABASE_USER', 'gql_test')
     set_key(path, 'DATABASE_HOST', 'localhost')
     set_key(path, 'DATABASE_PORT', '5432')
-    set_key(path, 'DATABASE_PASS', 'gql_test')
-    set_key(path, 'DATABASE_URL', 'postgres://gql_test:gql_test@localhost:5432/{}'.format(app_name))
+    set_key(path, 'DATABASE_PASS', '')
+    set_key(path, 'DATABASE_URL', 'postgres://gql_test@localhost:5432/{}'.format(app_name))
 
-POSTGRES_DEFAULT_CMD=['PGPASSWORD=gql_test', 'psql', '-h', 'localhost', '-p', '5432', '-U', 'gql_test', '-c']
+POSTGRES_DEFAULT_CMD=['psql', '-h', 'localhost', '-p', '5432', '-U', 'gql_test', '-c']
 DEFAULT_DOCKER_CMD=['docker', 'run', '--env-file', '{}/.env.list', '--net', 'host']
 APPS_DATA = {}
 with open(".circleci/community-apps.yaml", 'r') as stream:
@@ -23,13 +23,11 @@ with open(".circleci/community-apps.yaml", 'r') as stream:
 
 for app in APPS_DATA['apps']:
     print("Running test for app {}".format(app['name']))
-    global POSTGRES_DEFAULT_CMD
     subprocess.call(POSTGRES_DEFAULT_CMD + ['"CREATE DATABASE {};"'.format(app['name'])])
     subprocess.call(POSTGRES_DEFAULT_CMD + ['"ALTER DATABASE {} OWNER TO gql_test;"'.format(app['name'])])
     subprocess.call(['docker', 'build', '-t', app['name'], '-f', '{}/{}'.format(app['path'], app['dockerfile']), app['path']])
     file_path='{}/.env.list'.format(app['path'])
     default_env(file_path, app['name'])
-    global DEFAULT_DOCKER_CMD
     docker_cmd = DEFAULT_DOCKER_CMD.copy()
     for port_mapping in app['port_mappings']:
         docker_cmd = docker_cmd + ['-p', port_mapping]
