086e4ac6214de2de9c83f41cc18f003b2c710800
Aravind Shankar <aravind@hasura.io>
2018-11-21 16:08:24 +0530

    WIP

--- .circleci/community-apps-test.py
+++ .circleci/community-apps-test.py
@@ -1,5 +1,6 @@
 import yaml
 import subprocess
+import sys
 from dotenv import set_key, load_dotenv
 
 def default_env(path, app_name):
@@ -13,6 +14,7 @@ def default_env(path, app_name):
     set_key(path, 'HASURA_GRAPHQL_DATABASE_URL', 'postgres://gql_test@localhost:5432/{}'.format(app_name), quote_mode="never")
     set_key(path, 'HASURA_GRAPHQL_SERVER_URL', 'http://localhost:8080', quote_mode="never")
 
+EXIT_FAILURE=False
 POSTGRES_DEFAULT_CMD=['psql', '-h', 'localhost', '-p', '5432', '-U', 'gql_test', '-c']
 DEFAULT_DOCKER_CMD=['docker', 'run', '--net', 'host']
 APPS_DATA = {}
@@ -26,12 +28,15 @@ for app in APPS_DATA['apps']:
     print("Running test for app {}".format(app['name']))
     exit_code = subprocess.call(POSTGRES_DEFAULT_CMD + ['CREATE DATABASE {};'.format(app['name'])])
     if exit_code:
+        EXIT_FAILURE=True
         continue
     exit_code = subprocess.call(POSTGRES_DEFAULT_CMD + ['ALTER DATABASE {} OWNER TO gql_test;'.format(app['name'])])
     if exit_code:
+        EXIT_FAILURE=True
         continue
     exit_code = subprocess.call(['docker', 'build', '-t', app['name'], '-f', '{}/{}'.format(app['path'], app['dockerfile']), app['path']])
     if exit_code:
+        EXIT_FAILURE=True
         continue
     file_path='{}/.env.list'.format(app['path'])
     default_env(file_path, app['name'])
@@ -41,6 +46,16 @@ for app in APPS_DATA['apps']:
         set_key(file_path, 'HASURA_GRAPHQL_AUTH_HOOK', app['webhook_url'], quote_mode="never")
     if 'jwt_secret' in app and app['jwt_secret']:
         set_key(file_path, 'HASURA_GRAPHQL_JWT_SECRET', app['jwt_secret'], quote_mode="auto")
+    docker_server_cmd = DEFAULT_DOCKER_CMD[:]
+    docker_server_cmd = docker_server_cmd + ['--env-file', '{}/.env.list'.format(app['path'])]
+    docker_server_cmd = docker_server_cmd + ['-p', '8080:8080']
+    if 'migrations_dir' in app and app['migrations_dir']:
+        docker_server_cmd = docker_server_cmd + ['-v', '{}/{}:/hasura-migrations'.format(app['path'], app['migrations_dir'])]
+    docker_server_cmd.append('server:latest')
+    exit_code = subprocess.call(docker_server_cmd)
+    if exit_code:
+        EXIT_FAILURE=True
+        continue
     docker_community_cmd = DEFAULT_DOCKER_CMD[:]
     docker_community_cmd = docker_community_cmd + ['--env-file', '{}/.env.list'.format(app['path'])]
     for port_mapping in app['port_mappings']:
@@ -50,5 +65,11 @@ for app in APPS_DATA['apps']:
         docker_community_cmd.append(app['command'])
     exit_code = subprocess.call(docker_community_cmd)
     if exit_code:
+        EXIT_FAILURE=True
         continue
-    subprocess.call(POSTGRES_DEFAULT_CMD + ['DROP DATABASE {};'.format(app['name'])])
+    exit_code = subprocess.call(POSTGRES_DEFAULT_CMD + ['DROP DATABASE {};'.format(app['name'])])
+    if exit_code:
+        EXIT_FAILURE=True
+
+if EXIT_FAILURE:
+    sys.exit(1)
