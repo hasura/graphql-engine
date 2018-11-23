3d6fcd552e2930578f15d2f2a4504249b36f4d54
wawhal <rishichandra.wawhal@gmail.com>
2018-11-23 17:02:20 +0530

    make chat app responsive

--- community/examples/realtime-chat/src/components/OnlineUsers.js
+++ community/examples/realtime-chat/src/components/OnlineUsers.js
@@ -20,47 +20,65 @@ class OnlineUsers extends React.Component {
     super(props);
     this.state = {
       time: moment().subtract(10, 'seconds').format(),
-      refetch: null
+      refetch: null,
+      showMobileView: false
     }
   }
 
+  toggleMobileView = () => {
+    this.setState({
+      ...this.state,
+      showMobileView: !this.state.showMobileView
+    });
+  }
+
   render() {
-    return (
-      <div>
-        <div className="onlineUsers hidden-xs">
-          <Subscription
-            subscription={fetchOnlineUsersSubscription}
-          >
-            {
-              ({data, error, loading }) => {
-                if (loading) {
-                  return null;
-                }
-                if (error) { return "Error loading online users"; }
-                return (
-                  <div>
-                   <p className="userListHeading"> Online Users ({!data.user_online ? 0 : data.user_online.length})</p>
-                    <ul className="userList">
+
+    const subscriptionData = (isMobileView) => (
+      <Subscription
+        subscription={fetchOnlineUsersSubscription}
+      >
+        {
+          ({data, error, loading }) => {
+            if (loading) {
+              return null;
+            }
+            if (error) { return "Error loading online users"; }
+            return (
+              <div>
+                <p
+                  className={ isMobileView ? "mobileuserListHeading" : "userListHeading"}
+                  onClick={this.toggleMobileView}
+                >
+                  Online Users ({!data.user_online ? 0 : data.user_online.length}) { isMobileView && (<i className="fa fa-angle-up"></i>)}
+                </p>
+                {
+                  ((isMobileView && this.state.showMobileView) || !isMobileView) &&
+                  (
+                    <ul className={isMobileView ? "mobileUserList" : "userList"}>
                       {
                         data.user_online.map((u) => {
                           return <li key={u.id}>{u.username}</li>
                         })
                       }
                     </ul>
-                  </div>
-                );
-              }
-            }
-          </Subscription>
+                  )
+                }
+                
+              </div>
+            );
+          }
+        }
+      </Subscription>
+    );
+
+    return (
+      <div>
+        <div className="onlineUsers hidden-xs">
+          {subscriptionData(false)}
         </div>
         <div className="mobileonlineUsers visible-xs">
-          <p className="mobileuserListHeading"> Online Users <i className="fa fa-angle-up"></i></p>
-          <ul className="mobileUserList">
-            <li>User 1</li>
-            <li>User 2</li>
-            <li>User 3</li>
-            <li>User 4</li>
-          </ul>
+          {subscriptionData(true)}
         </div>
       </div>
     );
