4b5139cd99dc49f078e5f85ef63d06de28b46a9c
Aravind Shankar <aravind@hasura.io>
2018-12-05 17:29:28 +0530

    WIP

--- .circleci/test-console.sh
+++ .circleci/test-console.sh
@@ -21,9 +21,9 @@ if [ "$TEST_SUITE_NUMBER" = "1" ]; then
   docker run --name test2 -e TEST_ENV=MIGRATE_URL=http://${CONSOLE_HOST}:9693/apis/migrate -e TEST_CONFIG=baseUrl=http://${CONSOLE_HOST}:3000 -e 'TEST_SPECS=cypress/integration/data/insert-browse/test.js,cypress/integration/data/views/test.js' -d -v /home/circleci/graphql-engine:/root/graphql-engine hasura/graphql-engine-console-builder:v0.3 /bin/bash -c "cd /root/graphql-engine && ./.circleci/run-console-test.sh"
   docker wait test1
   docker wait test2
-  mkdir -p /home/circleci/build/docker_logs
-  docker logs test1 > /home/circleci/build/docker_logs/1.log
-  docker logs test2 > /home/circleci/build/docker_logs/2.log
+  mkdir -p /home/circleci/build/_console_test_logs
+  docker logs test1 > /home/circleci/build/_console_test_logs/1.log
+  docker logs test2 > /home/circleci/build/_console_test_logs/2.log
   docker stop test1
   docker rm test1
   docker stop test2
@@ -34,9 +34,9 @@ if [ "$TEST_SUITE_NUMBER" = "2" ]; then
   docker run --name test4 -e TEST_ENV=MIGRATE_URL=http://${CONSOLE_HOST}:9693/apis/migrate -e TEST_CONFIG=baseUrl=http://${CONSOLE_HOST}:3000 -e 'TEST_SPECS=cypress/integration/data/migration-mode/test.js,cypress/integration/api-explorer/graphql/test.js,cypress/integration/data/create-table/test.js,cypress/integration/data/modify/test.js,cypress/integration/data/404/test.js' -d -v /home/circleci/graphql-engine:/root/graphql-engine hasura/graphql-engine-console-builder:v0.3 /bin/bash -c "cd /root/graphql-engine && ./.circleci/run-console-test.sh"
   docker wait test3
   docker wait test4
-  mkdir -p /home/circleci/build/docker_logs
-  docker logs test3 > /home/circleci/build/docker_logs/3.log
-  docker logs test4 > /home/circleci/build/docker_logs/4.log
+  mkdir -p /home/circleci/build/_console_test_logs
+  docker logs test3 > /home/circleci/build/_console_test_logs/3.log
+  docker logs test4 > /home/circleci/build/_console_test_logs/4.log
   docker stop test3
   docker rm test3
   docker stop test4
