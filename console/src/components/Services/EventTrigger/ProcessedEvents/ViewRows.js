432a151f62503bc6df0bd445ae5dae33660cd1a9
Aravind Shankar <face11301@gmail.com>
2018-12-15 10:36:25 +0530

    show response status code in event trigger events tab (close #1022) (#1203)

--- console/src/components/Services/EventTrigger/ProcessedEvents/ViewRows.js
+++ console/src/components/Services/EventTrigger/ProcessedEvents/ViewRows.js
@@ -19,6 +19,8 @@ import {
 } from './FilterActions';
 import { ordinalColSort } from '../utils';
 import '../TableCommon/ReactTableFix.css';
+import * as tooltip from '../Common/Tooltips';
+import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 
 const ViewRows = ({
   curTriggerName,
@@ -404,8 +406,57 @@ const ViewRows = ({
                                 </div>
                               ) : null}
                               <div className={styles.add_mar_top}>
-                                <div className={styles.subheading_text}>
-                                  Payload
+                                <div
+                                  className={
+                                    styles.subheading_text +
+                                    ' col-md-6 ' +
+                                    styles.padd_remove
+                                  }
+                                >
+                                  {finalResponse.status_code
+                                    ? 'Payload'
+                                    : 'Error'}
+                                </div>
+                                <div
+                                  className={
+                                    styles.status_code_right +
+                                    ' col-md-6 ' +
+                                    styles.padd_remove
+                                  }
+                                >
+                                  {finalResponse.status_code
+                                    ? [
+                                      'Status Code: ',
+                                      finalResponse.status_code === 200 ? (
+                                        <i
+                                          className={
+                                            styles.invocationSuccess +
+                                              ' fa fa-check'
+                                          }
+                                        />
+                                      ) : (
+                                        <i
+                                          className={
+                                            styles.invocationFailure +
+                                              ' fa fa-times'
+                                          }
+                                        />
+                                      ),
+                                      finalResponse.status_code,
+                                      ' ',
+                                      <OverlayTrigger
+                                        placement="top"
+                                        overlay={
+                                          tooltip.statusCodeDescription
+                                        }
+                                      >
+                                        <i
+                                          className="fa fa-question-circle"
+                                          aria-hidden="true"
+                                        />
+                                      </OverlayTrigger>,
+                                    ]
+                                    : null}
                                 </div>
                                 <AceEditor
                                   mode="json"
