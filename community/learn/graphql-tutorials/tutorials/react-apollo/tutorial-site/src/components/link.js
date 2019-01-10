68f129a64eb4eb51090d02c1e05b095c4c98fbac
rikinsk <rikin.kachhia@gmail.com>
2019-01-10 19:39:20 +0530

    fix link generation

--- community/learn/graphql-tutorials/tutorials/react-apollo/tutorial-site/src/components/link.js
+++ community/learn/graphql-tutorials/tutorials/react-apollo/tutorial-site/src/components/link.js
@@ -6,7 +6,7 @@ const Link = ({ to, ...props }) =>
   isAbsoluteUrl(to) ? (
     <a href={to} {...props} />
   ) : (
-    <GatsbyLink to={withPrefix(to)} {...props} />
+    <GatsbyLink to={to} {...props} />
   );
 
 export default Link;
