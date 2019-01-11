ad859a671a1baee59bea44612b6a706a906e6926
Shahidh K Muhammed <shahidh@hasura.io>
2019-01-11 16:14:07 +0530

    console: update link

--- console/src/telemetry/Notifications.js
+++ console/src/telemetry/Notifications.js
@@ -1,8 +1,10 @@
 import Notifications from 'react-notification-system-redux';
 import { setNotificationShownInDB } from './Actions';
 
-const message =
-  'Help us improve Hasura! The console collects anonymized usage stats which allows us to keep improving Hasura at warp speed. Click read more to opt-out or read more';
+const message = `Help us improve Hasura!
+The console collects anonymized usage stats which allows us to keep improving Hasura at warp speed.
+<a href="https://docs.hasura.io/1.0/graphql/manual/guides/telemetry.html"
+target="_blank">Click here</a> to read more or to opt-out.`;
 
 const onRemove = () => {
   return dispatch => {
@@ -15,7 +17,7 @@ const showTelemetryNotification = () => {
     dispatch(
       Notifications.show({
         position: 'tr',
-        autoDismiss: 15,
+        autoDismiss: 10,
         level: 'info',
         title: 'Telemetry',
         message: message,
