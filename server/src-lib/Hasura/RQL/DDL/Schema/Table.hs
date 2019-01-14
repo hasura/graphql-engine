0411ca99de1000b3dda983e68500bae754122851
rakeshkky <rakesh.emmadi21c@gmail.com>
2019-01-14 20:36:04 +0530

    optimise run_sql query, close #1362

--- server/src-lib/Hasura/RQL/DDL/Schema/Table.hs
+++ server/src-lib/Hasura/RQL/DDL/Schema/Table.hs
@@ -12,6 +12,7 @@ import           Hasura.RQL.DDL.Schema.Diff
 import           Hasura.RQL.DDL.Subscribe
 import           Hasura.RQL.DDL.Utils
 import           Hasura.RQL.Types
+import           Hasura.Server.Utils                (matchRegex)
 import           Hasura.SQL.Types
 
 import qualified Database.PG.Query                  as Q
@@ -375,8 +376,9 @@ buildSchemaCache = do
 
 data RunSQL
   = RunSQL
-  { rSql     :: T.Text
-  , rCascade :: !(Maybe Bool)
+  { rSql               :: T.Text
+  , rCascade           :: !(Maybe Bool)
+  , rEnforceStateCheck :: !(Maybe Bool)
   } deriving (Show, Eq, Lift)
 
 $(deriveJSON (aesonDrop 1 snakeCase){omitNothingFields=True} ''RunSQL)
@@ -389,10 +391,18 @@ data RunSQLRes
 
 $(deriveJSON (aesonDrop 2 snakeCase){omitNothingFields=True} ''RunSQLRes)
 
+execRawSQL :: (MonadTx m) => T.Text -> m RunSQLRes
+execRawSQL =
+  liftTx . Q.multiQE rawSqlErrHandler . Q.fromText
+  where
+    rawSqlErrHandler txe =
+      let e = err400 PostgresError "query execution failed"
+      in e {qeInternal = Just $ toJSON txe}
+
 runSqlP2
   :: (QErrM m, CacheRWM m, MonadTx m, MonadIO m, HasHttpManager m)
-  => RunSQL -> m RespBody
-runSqlP2 (RunSQL t cascade) = do
+  => RunSQL -> m RunSQLRes
+runSqlP2 (RunSQL t cascade _) = do
 
   -- Drop hdb_views so no interference is caused to the sql query
   liftTx $ Q.catchE defaultTxErrorHandler clearHdbViews
@@ -401,7 +411,7 @@ runSqlP2 (RunSQL t cascade) = do
   oldMetaU <- liftTx $ Q.catchE defaultTxErrorHandler fetchTableMeta
 
   -- Run the SQL
-  res <- liftTx $ Q.multiQE rawSqlErrHandler $ Q.fromText t
+  res <- execRawSQL t
 
   -- Get the metadata after the sql query
   newMeta <- liftTx $ Q.catchE defaultTxErrorHandler fetchTableMeta
@@ -440,19 +450,26 @@ runSqlP2 (RunSQL t cascade) = do
   -- refresh the gCtxMap in schema cache
   refreshGCtxMapInSchema
 
-  return $ encode (res :: RunSQLRes)
+  return res
 
+isAltrOrDrop :: QErrM m => T.Text -> m Bool
+isAltrOrDrop = either throwErr return . isMatchedE
   where
-    rawSqlErrHandler :: Q.PGTxErr -> QErr
-    rawSqlErrHandler txe =
-      let e = err400 PostgresError "query execution failed"
-      in e {qeInternal = Just $ toJSON txe}
+    throwErr s = throw500 $ "compiling regex failed: " <> T.pack s
+    isMatchedE = matchRegex regex False
+    regex = "alter table|drop table|alter view|drop view|replace view"
 
 runRunSQL
   :: (QErrM m, UserInfoM m, CacheRWM m, MonadTx m, MonadIO m, HasHttpManager m)
   => RunSQL -> m RespBody
-runRunSQL q =
-  adminOnly >> runSqlP2 q
+runRunSQL q@(RunSQL t _ mEnfStChk) = do
+  adminOnly
+  encode <$> bool withoutStChk runP2 enfStChk
+  where
+    enfStChk = fromMaybe False mEnfStChk
+    withoutStChk =
+      bool (execRawSQL t) runP2 =<< isAltrOrDrop t
+    runP2 = runSqlP2 q
 
 -- Should be used only after checking the status
 resToCSV :: PQ.Result -> ExceptT T.Text IO [[T.Text]]
