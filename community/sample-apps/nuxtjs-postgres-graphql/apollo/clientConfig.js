9b193e9bee8df33090f0ee30bd3a65d524bb92aa
Kematzy <kematzy@gmail.com>
2019-02-12 16:22:29 +0800

    spaces instead of tabs

--- community/sample-apps/nuxtjs-postgres-graphql/apollo/clientConfig.js
+++ community/sample-apps/nuxtjs-postgres-graphql/apollo/clientConfig.js
@@ -1,16 +1,16 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
 export default function(context){
   return {
-  	httpLinkOptions: {
+    httpLinkOptions: {
       // uri: 'https://my-app.herokuapp.com/v1alpha1/graphql',
       uri: 'http://localhost:8080/v1alpha1/graphql',
       // headers: {
       //   'x-hasura-access-key': '<YOUR_SECRET_KEY>'
       // },
-    	credentials: 'same-origin'
-  	},
-  	cache: new InMemoryCache(),
+      credentials: 'same-origin'
+    },
+    cache: new InMemoryCache(),
     // wsEndpoint: 'wss://my-app.herokuapp.com/v1alpha1/graphql',
-	  wsEndpoint: 'ws://localhost:8080/v1alpha1/graphql',
+    wsEndpoint: 'ws://localhost:8080/v1alpha1/graphql',
   }
 }
