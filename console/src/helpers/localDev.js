ad7a1733c70a2da82d39caf7e2750dcab5b11e15
wawhal <rishichandra.wawhal@gmail.com>
2019-02-15 17:03:57 +0530

    remove dev data api url from localDev env

--- console/src/helpers/localDev.js
+++ console/src/helpers/localDev.js
@@ -1,61 +1,35 @@
-let envObj;
+const envObj = `apiHost: '${process.env.API_HOST}',
+        apiPort: '${process.env.API_PORT}',
+        dataApiUrl: '${process.env.DATA_API_URL}',
+        adminSecret: '${process.env.ADMIN_SECRET}',
+        consoleMode: '${process.env.CONSOLE_MODE}',
+        nodeEnv: '${process.env.NODE_ENV}',
+        urlPrefix: '${process.env.URL_PREFIX}',
+        enableTelemetry: ${process.env.ENABLE_TELEMETRY},
+`;
+
+let appendObj;
 
 if (process.env.ADMIN_SECRET !== undefined) {
-  envObj = `window.__env={
-    apiHost: '${process.env.API_HOST}',
-    apiPort: '${process.env.API_PORT}',
-    dataApiUrl: '${process.env.DATA_API_URL}',
-    devDataApiUrl: '${process.env.DEV_DATA_API_URL}',
-    adminSecret: '${process.env.ADMIN_SECRET}',
-    consoleMode: '${process.env.CONSOLE_MODE}',
-    nodeEnv: '${process.env.NODE_ENV}',
-    urlPrefix: '${process.env.URL_PREFIX}',
-    enableTelemetry: ${process.env.ENABLE_TELEMETRY}
-  };`;
+  appendObj = `
+  adminSecret: '${process.env.ADMIN_SECRET}`;
 } else {
   // ADMIN_SECRET is undefined
   if (process.env.IS_ADMIN_SECRET_SET !== undefined) {
-    envObj = `window.__env={
-      apiHost: '${process.env.API_HOST}',
-      apiPort: '${process.env.API_PORT}',
-      dataApiUrl: '${process.env.DATA_API_URL}',
-      devDataApiUrl: '${process.env.DEV_DATA_API_URL}',
-      isAdminSecretSet: ${process.env.IS_ADMIN_SECRET_SET},
-      consoleMode: '${process.env.CONSOLE_MODE}',
-      nodeEnv: '${process.env.NODE_ENV}',
-      urlPrefix: '${process.env.URL_PREFIX}',
-      enableTelemetry: ${process.env.ENABLE_TELEMETRY}
-    };`;
+    appendObj = `isAdminSecretSet: ${process.env.IS_ADMIN_SECRET_SET}`;
   } else {
     // Both ADMIN_SECRET and IS_ADMIN_SECRET_SET is undefined
     if (process.env.ACCESS_KEY !== undefined) {
-      envObj = `window.__env={
-        apiHost: '${process.env.API_HOST}',
-        apiPort: '${process.env.API_PORT}',
-        dataApiUrl: '${process.env.DATA_API_URL}',
-        devDataApiUrl: '${process.env.DEV_DATA_API_URL}',
-        accessKey: ${process.env.ACCESS_KEY},
-        consoleMode: '${process.env.CONSOLE_MODE}',
-        nodeEnv: '${process.env.NODE_ENV}',
-        urlPrefix: '${process.env.URL_PREFIX}',
-        enableTelemetry: ${process.env.ENABLE_TELEMETRY}
-      };`;
+      appendObj = `accessKey: ${process.env.ACCESS_KEY}`;
     } else {
-      envObj = `window.__env={
-        apiHost: '${process.env.API_HOST}',
-        apiPort: '${process.env.API_PORT}',
-        dataApiUrl: '${process.env.DATA_API_URL}',
-        devDataApiUrl: '${process.env.DEV_DATA_API_URL}',
-        isAccessKeySet: ${process.env.IS_ACCESS_KEY_SET},
-        consoleMode: '${process.env.CONSOLE_MODE}',
-        nodeEnv: '${process.env.NODE_ENV}',
-        urlPrefix: '${process.env.URL_PREFIX}',
-        enableTelemetry: ${process.env.ENABLE_TELEMETRY}
-      };`;
+      appendObj = `isAccessKeySet: ${process.env.IS_ACCESS_KEY_SET}`;
     }
   }
 }
 
-const env = envObj;
+const env = `
+    window.__env={\n\t\t${envObj}\t\t${appendObj}
+    };
+`;
 
 export { env };
