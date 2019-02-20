a179b15a7f9bbe1694d87dda62220395051e65ce
rikinsk <rikin.kachhia@gmail.com>
2019-02-21 00:28:28 +0530

    add blank sort option in console brose rows

--- console/src/components/Services/Data/TableBrowseRows/FilterQuery.js
+++ console/src/components/Services/Data/TableBrowseRows/FilterQuery.js
@@ -139,6 +139,7 @@ const renderSorts = (orderBy, tableSchema, dispatch) => {
         />
       );
     }
+
     return (
       <div key={i} className={`${styles.inputRow} row`}>
         <div className="col-xs-6">
@@ -146,13 +147,14 @@ const renderSorts = (orderBy, tableSchema, dispatch) => {
         </div>
         <div className="col-xs-5">
           <select
-            value={c.type}
+            value={c.column ? c.type : ''}
             className="form-control"
             onChange={e => {
               dispatch(setOrderType(e.target.value, i));
             }}
             data-test={`sort-order-${i}`}
           >
+            <option disabled value="">--</option>
             <option value="asc">Asc</option>
             <option value="desc">Desc</option>
           </select>
