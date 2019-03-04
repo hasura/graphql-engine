20749064c2a775f83462450bba87dab4b744693e
Anon Ray <rayanon004@gmail.com>
2019-03-04 18:35:15 +0530

    fix generate introspection for remote subscriptions

--- server/src-lib/Hasura/GraphQL/RemoteServer.hs
+++ server/src-lib/Hasura/GraphQL/RemoteServer.hs
@@ -14,8 +14,8 @@ import qualified Data.HashMap.Strict           as Map
 import qualified Data.HashSet                  as Set
 import qualified Data.Text                     as T
 import qualified Data.Text.Encoding            as T
-import qualified Language.GraphQL.Draft.Syntax as G
 import qualified Language.GraphQL.Draft.Parser as G
+import qualified Language.GraphQL.Draft.Syntax as G
 import qualified Network.HTTP.Client           as HTTP
 import qualified Network.Wreq                  as Wreq
 
@@ -117,7 +117,7 @@ mergeGCtx gCtx rmMergedGCtx = do
   let newQR = mergeQueryRoot gCtx rmMergedGCtx
       newMR = mergeMutRoot gCtx rmMergedGCtx
       newSR = mergeSubRoot gCtx rmMergedGCtx
-      newTyMap = mergeTyMaps hsraTyMap rmTypes newQR newMR
+      newTyMap = mergeTyMaps hsraTyMap rmTypes newQR newMR newSR
       updatedGCtx = gCtx { GS._gTypes = newTyMap
                          , GS._gQueryRoot = newQR
                          , GS._gMutRoot = newMR
@@ -177,18 +177,21 @@ mkNewEmptySubRoot = VT.ObjTyInfo (Just "subscription root")
 
 
 mergeTyMaps
-  :: VT.TypeMap
+  :: VT.TypeMap         -- hasura type map
+  -> VT.TypeMap         -- remote type map
+  -> VT.ObjTyInfo       -- merged query root
+  -> Maybe VT.ObjTyInfo -- merged mutation root
+  -> Maybe VT.ObjTyInfo -- merged subscription root
   -> VT.TypeMap
-  -> VT.ObjTyInfo
-  -> Maybe VT.ObjTyInfo
-  -> VT.TypeMap
-mergeTyMaps hTyMap rmTyMap newQR newMR =
-  let newTyMap  = hTyMap <> rmTyMap
-      newTyMap' = Map.insert (G.NamedType "query_root") (VT.TIObj newQR) $
-                  newTyMap
-  in maybe newTyMap' (\mr -> Map.insert
-                              (G.NamedType "mutation_root")
-                              (VT.TIObj mr) newTyMap') newMR
+mergeTyMaps hTyMap rmTyMap newQR newMR newSR =
+  let newTyMap = hTyMap <> rmTyMap
+      qRoot   = mk "query_root" newQR
+      mutRoot = mk "mutation_root" <$> newMR
+      subRoot = mk "subscription_root" <$> newSR
+  in ins subRoot $ ins mutRoot $ ins (Just qRoot) newTyMap
+  where
+    ins obj m = maybe m (\x -> uncurry Map.insert x m) obj
+    mk n v = (G.NamedType n, VT.TIObj v)
 
 
 -- parsing the introspection query result
