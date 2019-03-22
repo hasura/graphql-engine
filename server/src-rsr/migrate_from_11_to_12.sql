7ae3314e5cda4cb9302404a001bba99aa30de6b7
Tirumarai Selvan A <tiru@hasura.io>
2019-03-22 16:17:17 +0530

    drop id from event_triggers table

---
+++ server/src-rsr/migrate_from_11_to_12.sql
@@ -0,0 +1,13 @@
+ALTER TABLE hdb_catalog.event_triggers
+  DROP CONSTRAINT event_triggers_pkey;
+
+ALTER TABLE hdb_catalog.event_triggers
+  DROP CONSTRAINT event_triggers_name_key;
+
+ALTER TABLE hdb_catalog.event_triggers
+  ADD PRIMARY KEY (name);
+
+ALTER TABLE hdb_catalog.event_log
+  DROP COLUMN trigger_id;
+
+CREATE INDEX ON hdb_catalog.event_log (trigger_name)
