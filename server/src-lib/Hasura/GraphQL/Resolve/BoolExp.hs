b8700cce70d39f069015624ea30a006f8d0a4a70
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2019-03-25 17:59:52 +0530

    add spatial predicates for geography columns (close #1674) (#1735)
    This PR adds support for PostGIS spatial predicates on geography columns. The predicates are _st_d_within and _st_intersects.

--- server/src-lib/Hasura/GraphQL/Resolve/BoolExp.hs
+++ server/src-lib/Hasura/GraphQL/Resolve/BoolExp.hs
@@ -15,36 +15,38 @@ import           Hasura.GraphQL.Resolve.InputValue
 import           Hasura.GraphQL.Validate.Types
 import           Hasura.RQL.Types
 import           Hasura.SQL.Value
+import           Hasura.SQL.Types
 
 type OpExp = OpExpG AnnPGVal
 
 parseOpExps
   :: (MonadError QErr m)
-  => AnnInpVal -> m [OpExp]
-parseOpExps annVal = do
+  => PGColType -> AnnInpVal -> m [OpExp]
+parseOpExps colTy annVal = do
   opExpsM <- flip withObjectM annVal $ \nt objM -> forM objM $ \obj ->
-    forM (OMap.toList obj) $ \(k, v) -> case k of
-      "_eq"           -> fmap (AEQ True) <$> asPGColValM v
-      "_ne"           -> fmap (ANE True) <$> asPGColValM v
-      "_neq"          -> fmap (ANE True) <$> asPGColValM v
-      "_is_null"      -> resolveIsNull v
+    forM (OMap.toList obj) $ \(k, v) -> do
+    case k of
+      "_eq"       -> fmap (AEQ True) <$> asPGColValM v
+      "_ne"       -> fmap (ANE True) <$> asPGColValM v
+      "_neq"      -> fmap (ANE True) <$> asPGColValM v
+      "_is_null"  -> resolveIsNull v
 
-      "_in"           -> fmap (AIN . catMaybes) <$> parseMany asPGColValM v
-      "_nin"          -> fmap (ANIN . catMaybes) <$> parseMany asPGColValM v
+      "_in"       -> fmap (AIN . catMaybes) <$> parseMany asPGColValM v
+      "_nin"      -> fmap (ANIN . catMaybes) <$> parseMany asPGColValM v
 
-      "_gt"           -> fmap AGT <$> asPGColValM v
-      "_lt"           -> fmap ALT <$> asPGColValM v
-      "_gte"          -> fmap AGTE <$> asPGColValM v
-      "_lte"          -> fmap ALTE <$> asPGColValM v
+      "_gt"       -> fmap AGT <$> asPGColValM v
+      "_lt"       -> fmap ALT <$> asPGColValM v
+      "_gte"      -> fmap AGTE <$> asPGColValM v
+      "_lte"      -> fmap ALTE <$> asPGColValM v
 
-      "_like"         -> fmap ALIKE <$> asPGColValM v
-      "_nlike"        -> fmap ANLIKE <$> asPGColValM v
+      "_like"     -> fmap ALIKE <$> asPGColValM v
+      "_nlike"    -> fmap ANLIKE <$> asPGColValM v
 
-      "_ilike"        -> fmap AILIKE <$> asPGColValM v
-      "_nilike"       -> fmap ANILIKE <$> asPGColValM v
+      "_ilike"    -> fmap AILIKE <$> asPGColValM v
+      "_nilike"   -> fmap ANILIKE <$> asPGColValM v
 
-      "_similar"      -> fmap ASIMILAR <$> asPGColValM v
-      "_nsimilar"     -> fmap ANSIMILAR <$> asPGColValM v
+      "_similar"  -> fmap ASIMILAR <$> asPGColValM v
+      "_nsimilar" -> fmap ANSIMILAR <$> asPGColValM v
 
       -- jsonb related operators
       "_contains"     -> fmap AContains <$> asPGColValM v
@@ -53,15 +55,15 @@ parseOpExps annVal = do
       "_has_keys_any" -> fmap AHasKeysAny <$> parseMany asPGColText v
       "_has_keys_all" -> fmap AHasKeysAll <$> parseMany asPGColText v
 
-      -- geometry type related operators
-      "_st_contains"    -> fmap ASTContains <$> asPGColValM v
-      "_st_crosses"     -> fmap ASTCrosses <$> asPGColValM v
-      "_st_equals"      -> fmap ASTEquals <$> asPGColValM v
-      "_st_intersects"  -> fmap ASTIntersects <$> asPGColValM v
-      "_st_overlaps"    -> fmap ASTOverlaps <$> asPGColValM v
-      "_st_touches"     -> fmap ASTTouches <$> asPGColValM v
-      "_st_within"      -> fmap ASTWithin <$> asPGColValM v
-      "_st_d_within"    -> asObjectM v >>= mapM parseAsSTDWithinObj
+      -- geometry/geography type related operators
+      "_st_contains"   -> fmap ASTContains <$> asPGColValM v
+      "_st_crosses"    -> fmap ASTCrosses <$> asPGColValM v
+      "_st_equals"     -> fmap ASTEquals <$> asPGColValM v
+      "_st_intersects" -> fmap ASTIntersects <$> asPGColValM v
+      "_st_overlaps"   -> fmap ASTOverlaps <$> asPGColValM v
+      "_st_touches"    -> fmap ASTTouches <$> asPGColValM v
+      "_st_within"     -> fmap ASTWithin <$> asPGColValM v
+      "_st_d_within"   -> asObjectM v >>= mapM parseAsSTDWithinObj
 
       _ ->
         throw500
@@ -80,12 +82,20 @@ parseOpExps annVal = do
 
     parseAsSTDWithinObj obj = do
       distanceVal <- onNothing (OMap.lookup "distance" obj) $
-                 throw500 "expected \"distance\" input field in st_d_within_input ty"
+                throw500 "expected \"distance\" input field in st_d_within"
       dist <- asPGColVal distanceVal
       fromVal <- onNothing (OMap.lookup "from" obj) $
-                 throw500 "expected \"from\" input field in st_d_within_input ty"
+                throw500 "expected \"from\" input field in st_d_within"
       from <- asPGColVal fromVal
-      return $ ASTDWithin $ WithinOp dist from
+      case colTy of
+        PGGeography -> do
+          useSpheroidVal <- onNothing (OMap.lookup "use_spheroid" obj) $
+                    throw500 "expected \"use_spheroid\" input field in st_d_within"
+          useSpheroid <- asPGColVal useSpheroidVal
+          return $ ASTDWithinGeog $ DWithinGeogOp dist from useSpheroid
+        PGGeometry -> do
+          return $ ASTDWithinGeom $ DWithinGeomOp dist from
+        _ -> throw500 "expected PGGeometry/PGGeography column for st_d_within"
 
 parseAsEqOp
   :: (MonadError QErr m)
@@ -101,8 +111,8 @@ parseColExp
 parseColExp f nt n val = do
   fldInfo <- getFldInfo nt n
   case fldInfo of
-    Left  pgColInfo -> do
-      opExps <- parseOpExps val
+    Left pgColInfo -> do
+      opExps <- parseOpExps (pgiType pgColInfo) val
       AVCol pgColInfo <$> traverse (traverse f) opExps
     Right (relInfo, _, permExp, _) -> do
       relBoolExp <- parseBoolExp f val
