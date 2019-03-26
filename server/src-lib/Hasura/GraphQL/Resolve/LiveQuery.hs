hasura/graphql-engine

commit 739ff80a51038f4ca6de74a7ca1c2ad7c61aa250

refactor graphql query execution logic (#1855)

server/src-lib/Hasura/GraphQL/Transport/WebSocket.hs
@@ -26,22 +26,18 @@ import           Control.Concurrent                          (threadDelay)
 import           Data.ByteString                             (ByteString)
 import qualified Data.IORef                                  as IORef
 
+import           Hasura.EncJSON
 import           Hasura.GraphQL.Context                      (GCtx)
-import           Hasura.GraphQL.Resolve                      (resolveSelSet)
+import qualified Hasura.GraphQL.Execute                      as E
+import qualified Hasura.GraphQL.Resolve                      as R
 import           Hasura.GraphQL.Resolve.Context              (LazyRespTx)
 import qualified Hasura.GraphQL.Resolve.LiveQuery            as LQ
-import           Hasura.GraphQL.Schema                       (getGCtx)
-import qualified Hasura.GraphQL.Transport.HTTP               as TH
 import           Hasura.GraphQL.Transport.HTTP.Protocol
 import           Hasura.GraphQL.Transport.WebSocket.Protocol
 import qualified Hasura.GraphQL.Transport.WebSocket.Server   as WS
-import           Hasura.GraphQL.Validate                     (QueryParts (..),
-                                                              getQueryParts,
-                                                              validateGQ)
-import qualified Hasura.GraphQL.Validate.Types               as VT
+import qualified Hasura.GraphQL.Validate                     as V
 import qualified Hasura.Logging                              as L
 import           Hasura.Prelude
-import           Hasura.EncJSON
 import           Hasura.RQL.Types
 import           Hasura.Server.Auth                          (AuthMode,
                                                               getUserInfo)
@@ -224,49 +220,40 @@ onStart serverEnv wsConn (StartMsg opId q) msgRaw = catchAndIgnore $ do
       let connErr = "start received before the connection is initialised"
       withComplete $ sendConnErr connErr
 
-  -- validate and build tx
   sc <- liftIO $ IORef.readIORef gCtxMapRef
-  (gCtx, _) <- flip runStateT sc $ getGCtx (userRole userInfo) (scGCtxMap sc)
-
-  eQueryParts <- runExceptT $ runReaderT (getQueryParts q) gCtx
-  queryParts <- either (withComplete . preExecErr) return eQueryParts
-
-  let opDef = qpOpDef queryParts
-      topLevelNodes = TH.getTopLevelNodes opDef
-      typeLocs = TH.gatherTypeLocs gCtx topLevelNodes
-
-  res <- runExceptT $ TH.assertSameLocationNodes typeLocs
-  either (withComplete . preExecErr) return res
-
-  case typeLocs of
-    []          -> runHasuraQ userInfo gCtx queryParts
-    (typeLoc:_) -> case typeLoc of
-      VT.HasuraType       -> runHasuraQ userInfo gCtx queryParts
-      VT.RemoteType _ rsi -> runRemoteQ userInfo reqHdrs opDef rsi
-
+  execPlanE <- runExceptT $ E.getExecPlan userInfo sc q
+  execPlan <- either (withComplete . preExecErr) return execPlanE
+  case execPlan of
+    E.GExPHasura gCtx rootSelSet ->
+      runHasuraGQ userInfo gCtx rootSelSet
+    E.GExPRemote rsi opDef  ->
+      runRemoteGQ userInfo reqHdrs opDef rsi
   where
-    runHasuraQ :: UserInfo -> GCtx -> QueryParts -> ExceptT () IO ()
-    runHasuraQ userInfo gCtx queryParts = do
-      (opTy, fields) <- either (withComplete . preExecErr) return $
-                        runReaderT (validateGQ queryParts) gCtx
-      let qTx = withUserInfo userInfo $ resolveSelSet userInfo gCtx sqlGenCtx opTy fields
-      case opTy of
-        G.OperationTypeSubscription -> do
+    runHasuraGQ :: UserInfo -> GCtx -> V.RootSelSet -> ExceptT () IO ()
+    runHasuraGQ userInfo gCtx rootSelSet =
+      case rootSelSet of
+        V.RQuery selSet ->
+          execQueryOrMut $ R.resolveQuerySelSet userInfo gCtx sqlGenCtx selSet
+        V.RMutation selSet ->
+          execQueryOrMut $ R.resolveMutSelSet userInfo gCtx sqlGenCtx selSet
+        V.RSubscription fld -> do
+          let tx = R.resolveSubsFld userInfo gCtx sqlGenCtx fld
           let lq = LQ.LiveQuery userInfo q
           liftIO $ STM.atomically $ STMMap.insert lq opId opMap
           liftIO $ LQ.addLiveQuery runTx lqMap lq
-            qTx (wsId, opId) liveQOnChange
+            tx (wsId, opId) liveQOnChange
           logOpEv ODStarted
-        _ ->  do
-          logOpEv ODStarted
-          resp <- liftIO $ runTx qTx
-          either postExecErr sendSuccResp resp
-          sendCompleted
 
-    runRemoteQ :: UserInfo -> [H.Header]
-               -> G.TypedOperationDefinition -> RemoteSchemaInfo
-               -> ExceptT () IO ()
-    runRemoteQ userInfo reqHdrs opDef rsi = do
+    execQueryOrMut tx = do
+      logOpEv ODStarted
+      resp <- liftIO $ runTx tx
+      either postExecErr sendSuccResp resp
+      sendCompleted
+
+    runRemoteGQ :: UserInfo -> [H.Header]
+                -> G.TypedOperationDefinition -> RemoteSchemaInfo
+                -> ExceptT () IO ()
+    runRemoteGQ userInfo reqHdrs opDef rsi = do
       when (G._todType opDef == G.OperationTypeSubscription) $
         withComplete $ preExecErr $
         err400 NotSupported "subscription to remote server is not supported"
@@ -279,7 +266,7 @@ onStart serverEnv wsConn (StartMsg opId q) msgRaw = catchAndIgnore $ do
         const $ withComplete $ preExecErr $
         err500 Unexpected "invalid websocket payload"
       let payload = J.encode $ _wpPayload sockPayload
-      resp <- runExceptT $ TH.runRemoteGQ httpMgr userInfo reqHdrs
+      resp <- runExceptT $ E.execRemoteGQ httpMgr userInfo reqHdrs
               payload rsi opDef
       either postExecErr sendSuccResp resp
       sendCompleted

server/graphql-engine.cabal
@@ -208,8 +208,9 @@ library
                      , Hasura.GraphQL.Validate.Context
                      , Hasura.GraphQL.Validate.Field
                      , Hasura.GraphQL.Validate.InputValue
-                     , Hasura.GraphQL.Resolve
                      , Hasura.GraphQL.Explain
+                     , Hasura.GraphQL.Execute
+                     , Hasura.GraphQL.Resolve
                      , Hasura.GraphQL.Resolve.LiveQuery
                      , Hasura.GraphQL.Resolve.BoolExp
                      , Hasura.GraphQL.Resolve.Context

server/src-lib/Hasura/GraphQL/Execute.hs
@@ -1,0 +1,130 @@
+module Hasura.GraphQL.Execute
+  ( GQExecPlan(..)
+  , getExecPlan
+  , execRemoteGQ
+  ) where
+
+import           Control.Exception                      (try)
+import           Control.Lens
+
+import qualified Data.ByteString.Lazy                   as BL
+import qualified Data.CaseInsensitive                   as CI
+import qualified Data.HashMap.Strict                    as Map
+import qualified Data.HashSet                           as Set
+import qualified Data.String.Conversions                as CS
+import qualified Data.Text                              as T
+import qualified Language.GraphQL.Draft.Syntax          as G
+import qualified Network.HTTP.Client                    as HTTP
+import qualified Network.HTTP.Types                     as N
+import qualified Network.Wreq                           as Wreq
+
+import           Hasura.EncJSON
+import           Hasura.GraphQL.Schema
+import           Hasura.GraphQL.Transport.HTTP.Protocol
+import           Hasura.HTTP
+import           Hasura.Prelude
+import           Hasura.RQL.DDL.Headers
+import           Hasura.RQL.Types
+
+import qualified Hasura.GraphQL.Validate                as VQ
+import qualified Hasura.GraphQL.Validate.Types          as VT
+
+data GQExecPlan
+  = GExPHasura !GCtx !VQ.RootSelSet
+  | GExPRemote !RemoteSchemaInfo !G.TypedOperationDefinition
+
+getExecPlan
+  :: (MonadError QErr m)
+  => UserInfo
+  -> SchemaCache
+  -> GraphQLRequest
+  -> m GQExecPlan
+getExecPlan userInfo sc req = do
+
+  (gCtx, _) <- flip runStateT sc $ getGCtx (userRole userInfo) gCtxRoleMap
+  queryParts <- flip runReaderT gCtx $ VQ.getQueryParts req
+
+  let opDef = VQ.qpOpDef queryParts
+      topLevelNodes = getTopLevelNodes opDef
+      -- gather TypeLoc of topLevelNodes
+      typeLocs = gatherTypeLocs gCtx topLevelNodes
+
+  -- see if they are all the same
+  typeLoc <- assertSameLocationNodes typeLocs
+
+  case typeLoc of
+    VT.HasuraType ->
+      GExPHasura gCtx <$> runReaderT (VQ.validateGQ queryParts) gCtx
+    VT.RemoteType _ rsi ->
+      return $ GExPRemote rsi opDef
+  where
+    gCtxRoleMap = scGCtxMap sc
+
+execRemoteGQ
+  :: (MonadIO m, MonadError QErr m)
+  => HTTP.Manager
+  -> UserInfo
+  -> [N.Header]
+  -> BL.ByteString
+  -- ^ the raw request string
+  -> RemoteSchemaInfo
+  -> G.TypedOperationDefinition
+  -> m EncJSON
+execRemoteGQ manager userInfo reqHdrs q rsi opDef = do
+  let opTy = G._todType opDef
+  when (opTy == G.OperationTypeSubscription) $
+    throw400 NotSupported "subscription to remote server is not supported"
+  hdrs <- getHeadersFromConf hdrConf
+  let confHdrs   = map (\(k, v) -> (CI.mk $ CS.cs k, CS.cs v)) hdrs
+      clientHdrs = bool [] filteredHeaders fwdClientHdrs
+      options    = wreqOptions manager (userInfoToHdrs ++ clientHdrs ++ confHdrs)
+
+  res  <- liftIO $ try $ Wreq.postWith options (show url) q
+  resp <- either httpThrow return res
+  return $ encJFromLBS $ resp ^. Wreq.responseBody
+
+  where
+    RemoteSchemaInfo url hdrConf fwdClientHdrs = rsi
+    httpThrow :: (MonadError QErr m) => HTTP.HttpException -> m a
+    httpThrow err = throw500 $ T.pack . show $ err
+
+    userInfoToHdrs = map (\(k, v) -> (CI.mk $ CS.cs k, CS.cs v)) $
+                 userInfoToList userInfo
+    filteredHeaders = flip filter reqHdrs $ \(n, _) ->
+      n `notElem` [ "Content-Length", "Content-MD5", "User-Agent", "Host"
+                  , "Origin", "Referer" , "Accept", "Accept-Encoding"
+                  , "Accept-Language", "Accept-Datetime"
+                  , "Cache-Control", "Connection", "DNT"
+                  ]
+
+assertSameLocationNodes
+  :: (MonadError QErr m) => [VT.TypeLoc] -> m VT.TypeLoc
+assertSameLocationNodes typeLocs =
+  case Set.toList (Set.fromList typeLocs) of
+    -- this shouldn't happen
+    []    -> return VT.HasuraType
+    [loc] -> return loc
+    _     -> throw400 NotSupported msg
+  where
+    msg = "cannot mix top level fields from two different graphql servers"
+
+-- TODO: we should fix this function asap
+-- as this will fail when there is a fragment at the top level
+getTopLevelNodes :: G.TypedOperationDefinition -> [G.Name]
+getTopLevelNodes opDef =
+  mapMaybe f $ G._todSelectionSet opDef
+  where
+    f = \case
+      G.SelectionField fld        -> Just $ G._fName fld
+      G.SelectionFragmentSpread _ -> Nothing
+      G.SelectionInlineFragment _ -> Nothing
+
+gatherTypeLocs :: GCtx -> [G.Name] -> [VT.TypeLoc]
+gatherTypeLocs gCtx nodes =
+  catMaybes $ flip map nodes $ \node ->
+    VT._fiLoc <$> Map.lookup node schemaNodes
+  where
+    schemaNodes =
+      let qr = VT._otiFields $ _gQueryRoot gCtx
+          mr = VT._otiFields <$> _gMutRoot gCtx
+      in maybe qr (Map.union qr) mr

server/src-lib/Hasura/GraphQL/Explain.hs
@@ -13,21 +13,19 @@ import qualified Database.PG.Query                      as Q
 import qualified Language.GraphQL.Draft.Syntax          as G
 import qualified Text.Builder                           as TB
 
-import           Hasura.GraphQL.Context
 import           Hasura.EncJSON
+import           Hasura.GraphQL.Context
 import           Hasura.GraphQL.Resolve.Context
-import           Hasura.GraphQL.Schema
 import           Hasura.GraphQL.Validate.Field
 import           Hasura.Prelude
 import           Hasura.RQL.DML.Internal
 import           Hasura.RQL.Types
 import           Hasura.SQL.Types
 
+import qualified Hasura.GraphQL.Execute                 as E
 import qualified Hasura.GraphQL.Resolve.Select          as RS
-import qualified Hasura.GraphQL.Transport.HTTP          as TH
 import qualified Hasura.GraphQL.Transport.HTTP.Protocol as GH
 import qualified Hasura.GraphQL.Validate                as GV
-import qualified Hasura.GraphQL.Validate.Types          as VT
 import qualified Hasura.RQL.DML.Select                  as RS
 
 data GQLExplain
@@ -128,30 +126,23 @@ explainGQLQuery
   -> GQLExplain
   -> m EncJSON
 explainGQLQuery pool iso sc sqlGenCtx (GQLExplain query userVarsRaw)= do
-  (gCtx, _) <- flip runStateT sc $ getGCtx (userRole userInfo) gCtxMap
-  queryParts <- runReaderT (GV.getQueryParts query) gCtx
-  let topLevelNodes = TH.getTopLevelNodes (GV.qpOpDef queryParts)
-
-  unless (allHasuraNodes gCtx topLevelNodes) $
-    throw400 InvalidParams "only hasura queries can be explained"
-
-  (opTy, selSet) <- runReaderT (GV.validateGQ queryParts) gCtx
-  unless (opTy == G.OperationTypeQuery) $
-    throw400 InvalidParams "only queries can be explained"
-  let tx = mapM (explainField userInfo gCtx sqlGenCtx) (toList selSet)
-  plans <- liftIO (runExceptT $ runTx tx) >>= liftEither
-  return $ encJFromJValue plans
-
+  execPlan <- E.getExecPlan userInfo sc query
+  (gCtx, rootSelSet) <- case execPlan of
+    E.GExPHasura gCtx rootSelSet ->
+      return (gCtx, rootSelSet)
+    E.GExPRemote _ _  ->
+      throw400 InvalidParams "only hasura queries can be explained"
+  case rootSelSet of
+    GV.RQuery selSet -> do
+      let tx = mapM (explainField userInfo gCtx sqlGenCtx) (toList selSet)
+      plans <- liftIO (runExceptT $ runTx tx) >>= liftEither
+      return $ encJFromJValue plans
+    GV.RMutation _ ->
+      throw400 InvalidParams "only queries can be explained"
+    GV.RSubscription _ ->
+      throw400 InvalidParams "only queries can be explained"
   where
-    gCtxMap = scGCtxMap sc
     usrVars = mkUserVars $ maybe [] Map.toList userVarsRaw
     userInfo = mkUserInfo (fromMaybe adminRole $ roleFromVars usrVars) usrVars
 
     runTx tx = runLazyTx pool iso $ withUserInfo userInfo tx
-
-    allHasuraNodes gCtx nodes =
-      let typeLocs = TH.gatherTypeLocs gCtx nodes
-          isHasuraNode = \case
-            VT.HasuraType     -> True
-            VT.RemoteType _ _ -> False
-      in all isHasuraNode typeLocs

server/src-lib/Hasura/GraphQL/Resolve.hs
@@ -1,5 +1,7 @@
 module Hasura.GraphQL.Resolve
-  ( resolveSelSet
+  ( resolveQuerySelSet
+  , resolveMutSelSet
+  , resolveSubsFld
   ) where
 
 import           Hasura.Prelude
@@ -8,8 +10,8 @@ import qualified Data.HashMap.Strict               as Map
 import qualified Database.PG.Query                 as Q
 import qualified Language.GraphQL.Draft.Syntax     as G
 
-import           Hasura.GraphQL.Context
 import           Hasura.EncJSON
+import           Hasura.GraphQL.Context
 import           Hasura.GraphQL.Resolve.Context
 import           Hasura.GraphQL.Resolve.Introspect
 import           Hasura.GraphQL.Validate.Field
@@ -20,83 +22,106 @@ import qualified Hasura.GraphQL.Resolve.Insert     as RI
 import qualified Hasura.GraphQL.Resolve.Mutation   as RM
 import qualified Hasura.GraphQL.Resolve.Select     as RS
 
--- {-# SCC buildTx #-}
-buildTx :: UserInfo -> GCtx -> SQLGenCtx -> Field -> Q.TxE QErr EncJSON
-buildTx userInfo gCtx sqlCtx fld = do
+validateHdrs
+  :: (Foldable t, QErrM m) => UserInfo -> t Text -> m ()
+validateHdrs userInfo hdrs = do
+  let receivedVars = userVars userInfo
+  forM_ hdrs $ \hdr ->
+    unless (isJust $ getVarVal hdr receivedVars) $
+    throw400 NotFound $ hdr <<> " header is expected but not found"
+
+resolvePGFld
+  :: UserInfo
+  -> GCtx
+  -> SQLGenCtx
+  -> Field
+  -> Q.TxE QErr EncJSON
+resolvePGFld userInfo gCtx sqlCtx fld = do
   opCxt <- getOpCtx $ _fName fld
-  join $ fmap fst $ runConvert ( fldMap
-                               , orderByCtx
-                               , insCtxMap
-                               , sqlCtx
-                               ) $ case opCxt of
-
-    OCSelect ctx ->
-      validateHdrs (_socHeaders ctx) >> RS.convertSelect ctx fld
-
-    OCSelectPkey ctx ->
-      validateHdrs (_spocHeaders ctx) >> RS.convertSelectByPKey ctx fld
-
-    OCSelectAgg ctx ->
-      validateHdrs (_socHeaders ctx) >> RS.convertAggSelect ctx fld
-
-    OCFuncQuery ctx ->
-      validateHdrs (_fqocHeaders ctx) >> RS.convertFuncQuery ctx False fld
-
-    OCFuncAggQuery ctx ->
-      validateHdrs (_fqocHeaders ctx) >> RS.convertFuncQuery ctx True fld
-
-    OCInsert ctx    ->
-      validateHdrs (_iocHeaders ctx) >> RI.convertInsert roleName (_iocTable ctx) fld
-
-    OCUpdate ctx ->
-      validateHdrs (_uocHeaders ctx) >> RM.convertUpdate ctx fld
-
-    OCDelete ctx ->
-      validateHdrs (_docHeaders ctx) >> RM.convertDelete ctx fld
+  join $ runConvert (fldMap, orderByCtx, insCtxMap, sqlCtx) $ case opCxt of
+    OCSelect ctx -> do
+      validateHdrs userInfo (_socHeaders ctx)
+      RS.convertSelect ctx fld
+    OCSelectPkey ctx -> do
+      validateHdrs userInfo (_spocHeaders ctx)
+      RS.convertSelectByPKey ctx fld
+    OCSelectAgg ctx -> do
+      validateHdrs userInfo (_socHeaders ctx)
+      RS.convertAggSelect ctx fld
+    OCFuncQuery ctx -> do
+      validateHdrs userInfo (_fqocHeaders ctx)
+      RS.convertFuncQuery ctx False fld
+    OCFuncAggQuery ctx -> do
+      validateHdrs userInfo (_fqocHeaders ctx)
+      RS.convertFuncQuery ctx True fld
+    OCInsert ctx -> do
+      validateHdrs userInfo (_iocHeaders ctx)
+      RI.convertInsert roleName (_iocTable ctx) fld
+    OCUpdate ctx -> do
+      validateHdrs userInfo (_uocHeaders ctx)
+      RM.convertUpdate ctx fld
+    OCDelete ctx -> do
+      validateHdrs userInfo (_docHeaders ctx)
+      RM.convertDelete ctx fld
   where
-    roleName = userRole userInfo
     opCtxMap = _gOpCtxMap gCtx
     fldMap = _gFields gCtx
     orderByCtx = _gOrdByCtx gCtx
     insCtxMap = _gInsCtxMap gCtx
+    roleName = userRole userInfo
 
     getOpCtx f =
       onNothing (Map.lookup f opCtxMap) $ throw500 $
       "lookup failed: opctx: " <> showName f
 
-    validateHdrs hdrs = do
-      let receivedVars = userVars userInfo
-      forM_ hdrs $ \hdr ->
-        unless (isJust $ getVarVal hdr receivedVars) $
-        throw400 NotFound $ hdr <<> " header is expected but not found"
+mkRootTypeName :: G.OperationType -> Text
+mkRootTypeName = \case
+  G.OperationTypeQuery        -> "query_root"
+  G.OperationTypeMutation     -> "mutation_root"
+  G.OperationTypeSubscription -> "subscription_root"
 
--- {-# SCC resolveFld #-}
-resolveFld
+resolveQuerySelSet
   :: (MonadTx m)
-  => UserInfo -> GCtx -> SQLGenCtx
-  -> G.OperationType
-  -> Field
-  -> m EncJSON
-resolveFld userInfo gCtx sqlGenCtx opTy fld =
-  case _fName fld of
-    "__type"     -> encJFromJValue <$> runReaderT (typeR fld) gCtx
-    "__schema"   -> encJFromJValue <$> runReaderT (schemaR fld) gCtx
-    "__typename" -> return $ encJFromJValue $ mkRootTypeName opTy
-    _            -> liftTx $ buildTx userInfo gCtx sqlGenCtx fld
-  where
-    mkRootTypeName :: G.OperationType -> Text
-    mkRootTypeName = \case
-      G.OperationTypeQuery        -> "query_root"
-      G.OperationTypeMutation     -> "mutation_root"
-      G.OperationTypeSubscription -> "subscription_root"
-
-resolveSelSet
-  :: (MonadTx m)
-  => UserInfo -> GCtx -> SQLGenCtx
-  -> G.OperationType
+  => UserInfo
+  -> GCtx
+  -> SQLGenCtx
   -> SelSet
   -> m EncJSON
-resolveSelSet userInfo gCtx sqlGenCtx opTy fields =
+resolveQuerySelSet userInfo gCtx sqlGenCtx fields =
   fmap encJFromAssocList $ forM (toList fields) $ \fld -> do
-    fldResp <- resolveFld userInfo gCtx sqlGenCtx opTy fld
+    fldResp <- case _fName fld of
+      "__type"     -> encJFromJValue <$> runReaderT (typeR fld) gCtx
+      "__schema"   -> encJFromJValue <$> runReaderT (schemaR fld) gCtx
+      "__typename" -> return $ encJFromJValue $
+                      mkRootTypeName G.OperationTypeQuery
+      _            -> liftTx $ resolvePGFld userInfo gCtx sqlGenCtx fld
     return (G.unName $ G.unAlias $ _fAlias fld, fldResp)
+
+resolveMutSelSet
+  :: (MonadTx m)
+  => UserInfo
+  -> GCtx
+  -> SQLGenCtx
+  -> SelSet
+  -> m EncJSON
+resolveMutSelSet userInfo gCtx sqlGenCtx fields =
+  fmap encJFromAssocList $ forM (toList fields) $ \fld -> do
+    fldResp <- case _fName fld of
+      "__typename" -> return $ encJFromJValue $
+                      mkRootTypeName G.OperationTypeMutation
+      _            -> liftTx $ resolvePGFld userInfo gCtx sqlGenCtx fld
+    return (G.unName $ G.unAlias $ _fAlias fld, fldResp)
+
+resolveSubsFld
+  :: (MonadTx m)
+  => UserInfo
+  -> GCtx
+  -> SQLGenCtx
+  -> Field
+  -> m EncJSON
+resolveSubsFld userInfo gCtx sqlGenCtx fld = do
+  resp <- case _fName fld of
+    "__typename" -> return $ encJFromJValue $
+                    mkRootTypeName G.OperationTypeSubscription
+    _            -> liftTx $ resolvePGFld userInfo gCtx sqlGenCtx fld
+  return $ encJFromAssocList [(G.unName $ G.unAlias $ _fAlias fld, resp)]

server/src-lib/Hasura/GraphQL/Resolve/Context.hs
@@ -24,6 +24,7 @@ module Hasura.GraphQL.Resolve.Context
   , PrepArgs
   , Convert
   , runConvert
+  , withPrepArgs
   , prepare
   , prepareColVal
   , txtConverter
@@ -138,12 +139,12 @@ withArgM args arg f = prependArgsInPath $ nameAsPath arg $
 type PrepArgs = Seq.Seq Q.PrepArg
 
 type Convert =
-  StateT PrepArgs (ReaderT ( FieldMap
-                           , OrdByCtx
-                           , InsCtxMap
-                           , SQLGenCtx
-                           ) (Except QErr)
-                  )
+  (ReaderT ( FieldMap
+           , OrdByCtx
+           , InsCtxMap
+           , SQLGenCtx
+           ) (Except QErr)
+  )
 
 prepare
   :: (MonadState PrepArgs m) => PrepFn m
@@ -163,11 +164,14 @@ txtConverter :: Monad m => PrepFn m
 txtConverter (AnnPGVal _ _ a b) =
   return $ toTxtValue a b
 
+withPrepArgs :: StateT PrepArgs Convert a -> Convert (a, PrepArgs)
+withPrepArgs m = runStateT m Seq.empty
+
 runConvert
   :: (MonadError QErr m)
   => (FieldMap, OrdByCtx, InsCtxMap, SQLGenCtx)
   -> Convert a
-  -> m (a, PrepArgs)
+  -> m a
 runConvert ctx m =
   either throwError return $
-  runExcept $ runReaderT (runStateT m Seq.empty) ctx
+  runExcept $ runReaderT m ctx

commit ada35c2236f89e0901522b87054055dbde17d461

add user information to transactions in ws protocol (fix #1867) (#1869)
This bug introduced with the refactor in 739ff80a51038f4ca6de74a7ca1c2ad7c61aa250.

server/src-lib/Hasura/GraphQL/Resolve/LiveQuery.hs
@@ -3,6 +3,7 @@ module Hasura.GraphQL.Resolve.LiveQuery
   , LiveQueryMap
   , newLiveQueryMap
   , addLiveQuery
+  , TxRunner
   , removeLiveQuery
   ) where
 

server/src-lib/Hasura/GraphQL/Transport/WebSocket.hs
@@ -30,7 +30,6 @@ import           Hasura.EncJSON
 import           Hasura.GraphQL.Context                      (GCtx)
 import qualified Hasura.GraphQL.Execute                      as E
 import qualified Hasura.GraphQL.Resolve                      as R
-import           Hasura.GraphQL.Resolve.Context              (LazyRespTx)
 import qualified Hasura.GraphQL.Resolve.LiveQuery            as LQ
 import           Hasura.GraphQL.Transport.HTTP.Protocol
 import           Hasura.GraphQL.Transport.WebSocket.Protocol
@@ -46,8 +45,6 @@ import           Hasura.Server.Utils                         (bsToTxt)
 
 -- uniquely identifies an operation
 type GOperationId = (WS.WSId, OperationId)
-
-type TxRunner = LazyRespTx -> IO (Either QErr EncJSON)
 
 type OperationMap
   = STMMap.Map OperationId LQ.LiveQuery
@@ -124,7 +121,7 @@ data WSServerEnv
   = WSServerEnv
   { _wseLogger     :: !L.Logger
   , _wseServer     :: !WSServer
-  , _wseRunTx      :: !TxRunner
+  , _wseRunTx      :: !LQ.TxRunner
   , _wseLiveQMap   :: !LiveQueryMap
   , _wseGCtxMap    :: !(IORef.IORef SchemaCache)
   , _wseHManager   :: !H.Manager
@@ -233,11 +230,14 @@ onStart serverEnv wsConn (StartMsg opId q) msgRaw = catchAndIgnore $ do
     runHasuraGQ userInfo gCtx rootSelSet =
       case rootSelSet of
         V.RQuery selSet ->
-          execQueryOrMut $ R.resolveQuerySelSet userInfo gCtx sqlGenCtx selSet
+          execQueryOrMut $ withUserInfo userInfo $
+          R.resolveQuerySelSet userInfo gCtx sqlGenCtx selSet
         V.RMutation selSet ->
-          execQueryOrMut $ R.resolveMutSelSet userInfo gCtx sqlGenCtx selSet
+          execQueryOrMut $ withUserInfo userInfo $
+          R.resolveMutSelSet userInfo gCtx sqlGenCtx selSet
         V.RSubscription fld -> do
-          let tx = R.resolveSubsFld userInfo gCtx sqlGenCtx fld
+          let tx = withUserInfo userInfo $
+                   R.resolveSubsFld userInfo gCtx sqlGenCtx fld
           let lq = LQ.LiveQuery userInfo q
           liftIO $ STM.atomically $ STMMap.insert lq opId opMap
           liftIO $ LQ.addLiveQuery runTx lqMap lq
@@ -270,7 +270,6 @@ onStart serverEnv wsConn (StartMsg opId q) msgRaw = catchAndIgnore $ do
               payload rsi opDef
       either postExecErr sendSuccResp resp
       sendCompleted
-
 
     WSServerEnv logger _ runTx lqMap gCtxMapRef httpMgr _ sqlGenCtx = serverEnv
 
@@ -416,11 +415,12 @@ onClose logger lqMap _ wsConn = do
 createWSServerEnv
   :: L.Logger
   -> H.Manager -> SQLGenCtx -> IORef.IORef SchemaCache
-  -> TxRunner -> CorsPolicy -> IO WSServerEnv
+  -> LQ.TxRunner -> CorsPolicy -> IO WSServerEnv
 createWSServerEnv logger httpManager sqlGenCtx cacheRef runTx corsPolicy = do
   (wsServer, lqMap) <-
     STM.atomically $ (,) <$> WS.createWSServer logger <*> LQ.newLiveQueryMap
-  return $ WSServerEnv logger wsServer runTx lqMap cacheRef httpManager corsPolicy sqlGenCtx
+  return $ WSServerEnv logger wsServer runTx lqMap cacheRef
+    httpManager corsPolicy sqlGenCtx
 
 createWSServerApp :: AuthMode -> WSServerEnv -> WS.ServerApp
 createWSServerApp authMode serverEnv =