267069d7f8670008159c4d83bb176270385912ea
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2019-03-28 14:41:29 +0530

    Update cli/commands/metadata_clear.go

--- cli/commands/metadata_clear.go
+++ cli/commands/metadata_clear.go
@@ -18,7 +18,7 @@ func newMetadataClearCmd(ec *cli.ExecutionContext) *cobra.Command {
 		Use:     "clear",
 		Aliases: []string{"reset"},
 		Short:   "Clear Hasura GraphQL Engine metadata on the database",
-		Example: `  # Clean all the metadata information from database:
+		Example: `  # Clear all the metadata information from database:
   hasura metadata clear`,
 		SilenceUsage: true,
 		PreRunE: func(cmd *cobra.Command, args []string) error {
