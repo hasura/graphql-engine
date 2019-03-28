520da9b37974495035bb08293db5df412f629411
rikinsk <rikin.kachhia@gmail.com>
2019-03-28 18:44:42 +0530

    refactor metadata page

--- console/src/components/Services/Data/Metadata/Metadata.js
+++ console/src/components/Services/Data/Metadata/Metadata.js
@@ -50,6 +50,95 @@ class Metadata extends Component {
   render() {
     const styles = require('../../../Common/TableCommon/Table.scss');
     const metaDataStyles = require('./Metadata.scss');
+
+    const getMetadataImportExportSection = () => {
+      return (
+        <div>
+          <div className={metaDataStyles.intro_note}>
+            <h4>Import/Export</h4>
+            <div className={metaDataStyles.content_width}>
+              Get Hasura metadata as JSON.
+            </div>
+          </div>
+
+          <div className={metaDataStyles.display_inline}>
+            <ExportMetadata {...this.props} />
+          </div>
+
+          <div className={metaDataStyles.display_inline}>
+            <ImportMetadata {...this.props} />
+          </div>
+        </div>
+      );
+    };
+
+    const getMetadataUpdateSection = () => {
+      let updateSection = null;
+
+      if (this.state.showMetadata) {
+        updateSection = (
+          <div>
+            <div key="meta_data_1" className={metaDataStyles.intro_note}>
+              <h4>Reload metadata</h4>
+              <div className={metaDataStyles.content_width}>
+                Refresh Hasura metadata, typically required if you have changed
+                the underlying postgres.
+              </div>
+            </div>
+
+            <div key="meta_data_2">
+              <ReloadMetadata {...this.props} />
+            </div>
+
+            <div key="meta_data_3" className={metaDataStyles.intro_note}>
+              <h4>Reset Metadata</h4>
+              <div className={metaDataStyles.content_width}>
+                Permanently clear GraphQL Engine's metadata and configure it
+                from scratch (tracking relevant tables and relationships). This
+                process is not reversible.
+              </div>
+            </div>
+
+            <div key="meta_data_4">
+              <ResetMetadata {...this.props} />
+            </div>
+          </div>
+        );
+      }
+
+      return updateSection;
+    };
+
+    const getClearSecretSection = () => {
+      let clearSecretSection = null;
+
+      if (window.localStorage[CONSOLE_ADMIN_SECRET]) {
+        clearSecretSection = (
+          <div>
+            <div key="access_key_reset_1" className={metaDataStyles.intro_note}>
+              <h4>Clear {globals.adminSecretLabel} (logout)</h4>
+
+              <div className={metaDataStyles.content_width}>
+                The console caches the {globals.adminSecretLabel} (
+                {globals.adminSecretLabel === 'access-key'
+                  ? 'HASURA_GRAPHQL_ACCESS_KEY'
+                  : 'HASURA_GRAPHQL_ADMIN_SECRET'}
+                ) in the browser. You can clear this cache to force a prompt for
+                the {globals.adminSecretLabel} when the console is accessed next
+                using this browser.
+              </div>
+            </div>
+
+            <div key="access_key_reset_2">
+              <ClearAdminSecret {...this.props} />
+            </div>
+          </div>
+        );
+      }
+
+      return clearSecretSection;
+    };
+
     return (
       <div
         className={`${styles.clear_fix} ${styles.padd_left} ${
@@ -64,7 +153,8 @@ class Metadata extends Component {
           <div className="clearfix" />
           <div className={metaDataStyles.content_width}>
             Hasura metadata stores information about your tables, relationships,
-            and permissions that is used to generate the GraphQL schema and API.{' '}
+            permissions, etc. that is used to generate the GraphQL schema and
+            API.{' '}
             <a
               href="https://docs.hasura.io/1.0/graphql/manual/engine-internals/index.html"
               target="_blank"
@@ -75,67 +165,11 @@ class Metadata extends Component {
             .
           </div>
         </div>
-        <div className={metaDataStyles.intro_note}>
-          <h4>Import/Export</h4>
-          <div className={metaDataStyles.content_width}>
-            Get Hasura metadata as JSON.
-          </div>
-        </div>
-        <div className={metaDataStyles.display_inline}>
-          <ExportMetadata {...this.props} />
-        </div>
-        <div className={metaDataStyles.display_inline}>
-          <ImportMetadata {...this.props} />
-        </div>
+        {getMetadataImportExportSection()}
 
-        {this.state.showMetadata
-          ? [
-            <div key="meta_data_1" className={metaDataStyles.intro_note}>
-              <h4>Reload metadata</h4>
-              <div className={metaDataStyles.content_width}>
-                  Refresh Hasura metadata, typically required if you have
-                  changed the underlying postgres.
-              </div>
-            </div>,
-            <div key="meta_data_2">
-              <ReloadMetadata {...this.props} />
-            </div>,
-            <div key="meta_data_3" className={metaDataStyles.intro_note}>
-              <h4>Reset Metadata</h4>
-              <div className={metaDataStyles.content_width}>
-                  Permanently clear GraphQL Engine's metadata and configure it
-                  from scratch (tracking relevant tables and relationships).
-                  This process is not reversible.
-              </div>
-            </div>,
-            <div key="meta_data_4">
-              <ResetMetadata {...this.props} />
-            </div>,
-          ]
-          : null}
+        {getMetadataUpdateSection()}
 
-        {window.localStorage[CONSOLE_ADMIN_SECRET]
-          ? [
-            <div
-              key="access_key_reset_1"
-              className={metaDataStyles.intro_note}
-            >
-              <h4>Clear {globals.adminSecretLabel} (logout)</h4>
-              <div className={metaDataStyles.content_width}>
-                  The console caches the {globals.adminSecretLabel} (
-                {globals.adminSecretLabel === 'access-key'
-                  ? 'HASURA_GRAPHQL_ACCESS_KEY'
-                  : 'HASURA_GRAPHQL_ADMIN_SECRET'}
-                  ) in the browser. You can clear this cache to force a prompt
-                  for the {globals.adminSecretLabel} when the console is
-                  accessed next using this browser.
-              </div>
-            </div>,
-            <div key="access_key_reset_2">
-              <ClearAdminSecret {...this.props} />
-            </div>,
-          ]
-          : null}
+        {getClearSecretSection()}
       </div>
     );
   }
