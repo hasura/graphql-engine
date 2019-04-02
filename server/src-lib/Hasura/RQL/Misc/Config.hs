92dd8ce5c5f6f1dfe2e5e6e0b63c26b6fea3cb31
Shahidh K Muhammed <shahidh@hasura.io>
2019-04-02 14:13:42 +0530

    wip: add actual values to get_config response

--- server/src-lib/Hasura/RQL/Misc/Config.hs
+++ server/src-lib/Hasura/RQL/Misc/Config.hs
@@ -12,7 +12,7 @@ import           Language.Haskell.TH.Syntax (Lift)
 import           Hasura.EncJSON
 import           Hasura.Prelude
 import           Hasura.RQL.Types
-import Hasura.Server.Auth (AuthMode)
+import qualified Hasura.Server.Auth         as A
 import qualified Hasura.Server.Auth.JWT     as J
 import qualified Hasura.Server.Version      as V
 
@@ -27,7 +27,7 @@ $(deriveToJSON defaultOptions ''GetConfig)
 
 data JWTInfo
   = JWTInfo
-  { jwtiClaimNamespace :: !Text
+  { jwtiClaimNamespace :: !(Maybe Text)
   , jwtiClaimsFormat   :: !J.JWTClaimsFormat
   } deriving (Show, Eq)
 
@@ -37,22 +37,42 @@ data ServerConfig
   = ServerConfig
   { scfgVersion          :: !Text
   , scfgIsAdminSecretSet :: !Bool
-  , scfgIsAccessKeySet   :: !Bool
   , scfgIsAuthHookSet    :: !Bool
   , scfgIsJwtSet         :: !Bool
-  , scfgJwt              :: !JWTInfo
+  , scfgJwt              :: !(Maybe JWTInfo)
   } deriving (Show, Eq)
 
 $(deriveToJSON (aesonDrop 4 snakeCase) ''ServerConfig)
 
-runGetConfig :: (UserInfoM m, MonadTx m) => AuthMode -> m EncJSON
+runGetConfig :: (UserInfoM m, MonadTx m) => A.AuthMode -> m EncJSON
 runGetConfig am = do
   adminOnly
   return $ encJFromJValue $ ServerConfig
-    -- TODO(shahidhk): need to populate this object with actual values
     V.currentVersion
-    True
-    True
-    True
-    True
-    (JWTInfo "ns" J.JCFJson)
+    (isAdminSecretSet am)
+    (isAuthHookSet am)
+    (isJWTSet am)
+    (getJWTInfo am)
+
+isAdminSecretSet :: A.AuthMode -> Bool
+isAdminSecretSet = \case
+  AMNoAuth -> False
+  _        -> True
+
+isAuthHookSet :: A.AuthMode -> Bool
+isAuthHookSet = \case
+  AMAdminSecretAndHook _ _ -> True
+  _                        -> False
+
+isJWTSet :: A.AuthMode -> Bool
+isJWTSet = \case
+  AMAdminSecretAndJWT -> True
+  _                   -> False
+
+getJWTInfo :: A.AuthMode -> Maybe JWTInfo
+getJWTInfo AMAdminSecretAndJWT _ jwtCtx _ =
+  Just $ JWTInfo ns format
+  where
+    ns = J.jcxClaimNs jwtCtx
+    format = J.jcxClaimsFormat jwtCtx
+getJWTInfo _ = Nothing
