7687fbab01c0672dcdb1d4b18c13dae8f6c273e6
Shahidh K Muhammed <shahidh@hasura.io>
2019-04-02 14:29:31 +0530

    remove qualified imports for auth and jwt

--- server/src-lib/Hasura/RQL/Misc/Config.hs
+++ server/src-lib/Hasura/RQL/Misc/Config.hs
@@ -12,8 +12,8 @@ import           Language.Haskell.TH.Syntax (Lift)
 import           Hasura.EncJSON
 import           Hasura.Prelude
 import           Hasura.RQL.Types
-import qualified Hasura.Server.Auth         as A
-import qualified Hasura.Server.Auth.JWT     as J
+import           Hasura.Server.Auth
+import           Hasura.Server.Auth.JWT
 import qualified Hasura.Server.Version      as V
 
 data GetConfig
@@ -28,7 +28,7 @@ $(deriveToJSON defaultOptions ''GetConfig)
 data JWTInfo
   = JWTInfo
   { jwtiClaimNamespace :: !(Maybe Text)
-  , jwtiClaimsFormat   :: !J.JWTClaimsFormat
+  , jwtiClaimsFormat   :: !JWTClaimsFormat
   } deriving (Show, Eq)
 
 $(deriveToJSON (aesonDrop 4 snakeCase) ''JWTInfo)
@@ -44,7 +44,7 @@ data ServerConfig
 
 $(deriveToJSON (aesonDrop 4 snakeCase) ''ServerConfig)
 
-runGetConfig :: (UserInfoM m, MonadTx m) => A.AuthMode -> m EncJSON
+runGetConfig :: (UserInfoM m, MonadTx m) => AuthMode -> m EncJSON
 runGetConfig am = do
   adminOnly
   return $ encJFromJValue $ ServerConfig
@@ -54,25 +54,25 @@ runGetConfig am = do
     (isJWTSet am)
     (getJWTInfo am)
 
-isAdminSecretSet :: A.AuthMode -> Bool
+isAdminSecretSet :: AuthMode -> Bool
 isAdminSecretSet = \case
   AMNoAuth -> False
   _        -> True
 
-isAuthHookSet :: A.AuthMode -> Bool
+isAuthHookSet :: AuthMode -> Bool
 isAuthHookSet = \case
   AMAdminSecretAndHook _ _ -> True
   _                        -> False
 
-isJWTSet :: A.AuthMode -> Bool
+isJWTSet :: AuthMode -> Bool
 isJWTSet = \case
   AMAdminSecretAndJWT -> True
   _                   -> False
 
-getJWTInfo :: A.AuthMode -> Maybe JWTInfo
+getJWTInfo :: AuthMode -> Maybe JWTInfo
 getJWTInfo AMAdminSecretAndJWT _ jwtCtx _ =
   Just $ JWTInfo ns format
   where
-    ns = J.jcxClaimNs jwtCtx
-    format = J.jcxClaimsFormat jwtCtx
+    ns = jcxClaimNs jwtCtx
+    format = jcxClaimsFormat jwtCtx
 getJWTInfo _ = Nothing
