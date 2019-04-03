e2ed2451c4bd62fae037a322455aff742b785971
rikinsk <rikin.kachhia@gmail.com>
2019-04-04 00:19:48 +0530

    update tooltips + rel def

--- console/src/components/Services/Data/TableRelationships/utils.js
+++ console/src/components/Services/Data/TableRelationships/utils.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 /* This function sets the styling to the way the relationship looks, for eg: article.id -> user.user_id */
-export const getRelDef = (isObjRel, lcol, rcol, lTable, _rTable) => {
+export const getRelDef = (isObjRel, _lCol, _rCol, lTable, _rTable) => {
   const rTable = _rTable.name ? _rTable.name : _rTable;
+
+  const lCol = _lCol.length > 1 ? '( ' + _lCol.join(', ') + ' )' : _lCol[0];
+  const rCol = _rCol.length > 1 ? '( ' + _rCol.join(', ') + ' )' : _rCol[0];
 
   return isObjRel ? (
     <span>
-      {lTable} . {lcol.join(', ')} &nbsp;&rarr;&nbsp;
-      {_rTable} . {rcol.join(', ')}
+      {lTable} . {lCol} &nbsp;&rarr;&nbsp;
+      {rTable} . {rCol}
     </span>
   ) : (
     <span>
-      {rTable} . {rcol.join(', ')} &nbsp;&rarr;&nbsp;
-      {lTable} . {lcol.join(', ')}
+      {rTable} . {rCol} &nbsp;&rarr;&nbsp;
+      {lTable} . {lCol}
     </span>
   );
 };
