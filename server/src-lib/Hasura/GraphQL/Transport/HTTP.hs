fd38afdeb0b1b9eaf35dad3563c8cc16e3292089
Anon Ray <rayanon004@gmail.com>
2019-04-09 15:03:57 +0530

    fix hasura execution when plan is empty

--- server/src-lib/Hasura/GraphQL/Transport/HTTP.hs
+++ server/src-lib/Hasura/GraphQL/Transport/HTTP.hs
@@ -2,6 +2,11 @@ module Hasura.GraphQL.Transport.HTTP
   ( runGQ
   ) where
 
+--import           Control.Exception                      (catch, try)
+
+--import qualified Control.Concurrent.Async               as A
+import qualified Data.Aeson                             as J
+import qualified Data.HashMap.Lazy                      as Map
 import qualified Database.PG.Query                      as Q
 import qualified Network.HTTP.Client                    as HTTP
 import qualified Network.HTTP.Types                     as N
@@ -12,8 +17,6 @@ import           Hasura.GraphQL.Transport.HTTP.Protocol
 import           Hasura.Prelude
 import           Hasura.RQL.Types
 
-import qualified Data.Aeson                             as J
-import qualified Data.HashMap.Lazy                      as Map
 import qualified Hasura.GraphQL.Execute                 as E
 import qualified Hasura.GraphQL.Resolve                 as R
 import qualified Hasura.GraphQL.Validate                as V
@@ -55,7 +58,9 @@ runMixedGQ pool isoL userInfo sqlGenCtx manager reqHdrs plan = do
   let interimResBS = map encJToLBS allRes
   interimRes <- forM interimResBS $ \res -> do
     let obj = J.decode res :: (Maybe J.Object)
-    onNothing obj $ throw500 "could not parse response as JSON"
+    onNothing obj $ do
+      liftIO $ print res
+      throw500 "could not parse response as JSON"
 
   -- TODO: the order is not guaranteed! should we have a orderedmap?
   let datas = onlyObjs $ mapMaybe (Map.lookup "data") interimRes
@@ -68,14 +73,14 @@ runMixedGQ pool isoL userInfo sqlGenCtx manager reqHdrs plan = do
     -- TODO: use async
     run (hasuraPlan, remotePlans) = do
       hasuraRes <- case hasuraPlan of
-        Nothing -> return mempty
-        Just (E.GExPHasura gCtx rootSelSet) ->
-          runHasuraGQ pool isoL userInfo sqlGenCtx gCtx rootSelSet
-
+        Nothing -> return []
+        Just (E.GExPHasura gCtx rootSelSet) -> do
+          res <- runHasuraGQ pool isoL userInfo sqlGenCtx gCtx rootSelSet
+          return [res]
       remoteRes <- forM remotePlans $ \(E.GExPRemote rsi newq rs) ->
         E.execRemoteGQ manager userInfo reqHdrs newq rsi rs
 
-      return (hasuraRes:remoteRes)
+      return $ hasuraRes ++ remoteRes
 
     -- TODO: should validate response and throw error?
     onlyObjs jVals =
