7b5bbb758a14d775f4ca2d1b5f9f8570972f6624
Rakesh Emmadi <rakesh.emmadi21c@gmail.com>
2019-04-09 21:41:06 +0530

    fix excessive memory consumption with bulk queries (#1972)
    Use 'Strict' State monad instead of 'Lazy' to avoid unevaluated memory thunks

--- server/src-lib/Hasura/Prelude.hs
+++ server/src-lib/Hasura/Prelude.hs
@@ -5,32 +5,34 @@ module Hasura.Prelude
   , onLeft
   ) where
 
-import           Control.Applicative    as M ((<|>))
-import           Control.Monad          as M (void, when)
-import           Control.Monad.Base     as M
-import           Control.Monad.Except   as M
-import           Control.Monad.Fail     as M (MonadFail)
-import           Control.Monad.Identity as M
-import           Control.Monad.Reader   as M
-import           Control.Monad.State    as M
-import           Data.Bool              as M (bool)
-import           Data.Either            as M (lefts, partitionEithers, rights)
-import           Data.Foldable          as M (foldrM, toList)
-import           Data.Hashable          as M (Hashable)
-import           Data.List              as M (find, foldl', group, intercalate,
-                                              intersect, lookup, sort, sortBy,
-                                              sortOn, union, unionBy, (\\))
-import           Data.Maybe             as M (catMaybes, fromMaybe, isJust,
-                                              isNothing, listToMaybe, mapMaybe,
-                                              maybeToList)
-import           Data.Ord               as M (comparing)
-import           Data.Semigroup         as M (Semigroup (..))
-import           Data.String            as M (IsString)
-import           Data.Text              as M (Text)
-import           Data.Word              as M (Word64)
-import           GHC.Generics           as M (Generic)
-import           Prelude                as M hiding (fail, init, lookup)
-import           Text.Read              as M (readEither, readMaybe)
+import           Control.Applicative        as M ((<|>))
+import           Control.Monad              as M (void, when)
+import           Control.Monad.Base         as M
+import           Control.Monad.Except       as M
+import           Control.Monad.Fail         as M (MonadFail)
+import           Control.Monad.Identity     as M
+import           Control.Monad.Reader       as M
+import           Control.Monad.State.Strict as M
+import           Data.Bool                  as M (bool)
+import           Data.Either                as M (lefts, partitionEithers,
+                                                  rights)
+import           Data.Foldable              as M (foldrM, toList)
+import           Data.Hashable              as M (Hashable)
+import           Data.List                  as M (find, foldl', group,
+                                                  intercalate, intersect,
+                                                  lookup, sort, sortBy, sortOn,
+                                                  union, unionBy, (\\))
+import           Data.Maybe                 as M (catMaybes, fromMaybe, isJust,
+                                                  isNothing, listToMaybe,
+                                                  mapMaybe, maybeToList)
+import           Data.Ord                   as M (comparing)
+import           Data.Semigroup             as M (Semigroup (..))
+import           Data.String                as M (IsString)
+import           Data.Text                  as M (Text)
+import           Data.Word                  as M (Word64)
+import           GHC.Generics               as M (Generic)
+import           Prelude                    as M hiding (fail, init, lookup)
+import           Text.Read                  as M (readEither, readMaybe)
 
 onNothing :: (Monad m) => Maybe a -> m a -> m a
 onNothing m act = maybe act return m
