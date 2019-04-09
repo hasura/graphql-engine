860f5c71351d57e337ac952ab00d386da67861ec
Tanmai Gopal <tanmaig@34cross.in>
2019-04-09 10:25:22 +0530

    WIP: updates boilerplate components from final

--- community/learn/graphql-tutorials/tutorials/react-apollo/app-boilerplate/src/components/Todo/TodoPublicWrapper.js
+++ community/learn/graphql-tutorials/tutorials/react-apollo/app-boilerplate/src/components/Todo/TodoPublicWrapper.js
@@ -6,7 +6,7 @@ import TodoPublicList from './TodoPublicList';
 const TodoPublicWrapper = () => {
   return (
     <div className="todoWrapper">
-      <div className="sectionHeader">Public feed (realtime) </div>
+      <div className="sectionHeader">Public feed (realtime)</div>
 
       <TodoInput isPublic />
       <TodoPublicList />
