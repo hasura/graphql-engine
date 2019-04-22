d500b33f84c6f0194d92bb4a5729501d5e9930ec
Praveen D <praveen@hasura.io>
2019-04-22 18:21:26 +0530

    update robots.txt, graphiql urls

--- community/learn/graphql-tutorials/backend-services/homepage/gatsby-config.js
+++ community/learn/graphql-tutorials/backend-services/homepage/gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
-	"plugins" : ["gatsby-plugin-sass",
-	"gatsby-plugin-react-helmet",
-	{
-	    "resolve": "gatsby-plugin-favicon",
-	    "options": {
-	      "logo": "./src/favicon.png",
-	    }
-  	}]
+	"plugins" : [
+		"gatsby-plugin-sass",
+		"gatsby-plugin-react-helmet",
+		{
+				"resolve": "gatsby-plugin-favicon",
+				"options": {
+					"logo": "./src/favicon.png",
+				}
+		},
+		{
+			resolve: 'gatsby-plugin-robots-txt',
+			options: {
+				policy: [{ userAgent: '*', disallow: ['/'] }]
+			}
+		}
+	]
 };
