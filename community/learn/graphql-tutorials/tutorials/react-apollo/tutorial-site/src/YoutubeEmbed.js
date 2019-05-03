529fb456bf94848697ba6446b8f0e1dda4cb1137
Praveen D <praveen@hasura.io>
2019-05-03 20:51:38 +0530

    update youtube mobile responsive

--- community/learn/graphql-tutorials/tutorials/react-apollo/tutorial-site/src/YoutubeEmbed.js
+++ community/learn/graphql-tutorials/tutorials/react-apollo/tutorial-site/src/YoutubeEmbed.js
@@ -3,7 +3,7 @@ import './components/styles.css';
 
 const YoutubeEmbed = ({link}) => {
 	return (
-		<div>
+		<div className="video-responsive">
 			<iframe width="750" height="422" src={link} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
 			</iframe>
 		</div>
