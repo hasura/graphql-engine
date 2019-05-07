c2fd6dd1462e894f7741658da72c86938557ab25
Praveen D <praveen@hasura.io>
2019-05-07 15:04:01 +0530

    custom jwt-server wip

---
+++ community/learn/graphql-tutorials/backend-services/auth-server/db/schema.js
@@ -0,0 +1,49 @@
+const { promisify } = require('util');
+const Knex = require('knex');
+const connection = require('../knexfile');
+const { Model } = require('objection');
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+
+const knexConnection = Knex(connection);
+
+Model.knex(knexConnection);
+
+class User extends Model {
+  static get tableName () {
+    return 'users'
+  }
+
+  static get idColumn() {
+    return 'id';
+  }
+
+  getUser() {
+    return {
+      'id': this.id,
+      'email': this.email,
+    }
+  }
+
+  async $beforeInsert () {
+    const salt = bcrypt.genSaltSync();
+    this.password = await bcrypt.hash(this.password, salt)
+  }
+
+  verifyPassword (password, callback) {
+    bcrypt.compare(password, this.password, callback)
+  };
+
+  static get jsonSchema () {
+    return {
+      type: 'object',
+      required: ['email'],
+      properties: {
+        id: {type: 'integer'},
+        email: {type: 'string', minLength: 1, maxLength: 255},
+      }
+    }
+  }
+}
+
+module.exports = { User }
