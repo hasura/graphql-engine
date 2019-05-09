7ebc2d9f02e64adf8ad80fcbb3a107339d811c04
wawhal <rishichandra.wawhal@gmail.com>
2019-05-09 19:06:03 +0530

    bootstrap custom login ui

---
+++ community/learn/graphql-tutorials/tutorials/react-native-apollo/app-final/src/screens/components/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Login = () => {
+  return <Text>Login screen</Text>
+};
+
+export default Login;
---
+++ community/learn/graphql-tutorials/tutorials/react-native-apollo/app-final/src/screens/components/Signup.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Signup = () => {
+  return <Text>Signup screen</Text>
+}
+
+export default Signup;
