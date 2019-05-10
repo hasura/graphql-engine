111fa55386c96476bbca26a3df2dafead9421d37
Shahidh K Muhammed <shahidh@hasura.io>
2019-05-10 20:38:32 +0530

    wip: static console assets

--- server/src-lib/Hasura/Server/Version.hs
+++ server/src-lib/Hasura/Server/Version.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
 module Hasura.Server.Version
   ( currentVersion
   , consoleVersion
@@ -7,29 +8,40 @@ where
 
 import           Control.Lens        ((^.))
 
-import qualified Data.SemVer         as V
 import qualified Data.Text           as T
+import qualified Salve               as V
 
 import           Hasura.Prelude
 import           Hasura.Server.Utils (getValFromEnvOrScript)
 
 version :: T.Text
-version = T.dropWhileEnd (== '\n') $(getValFromEnvOrScript "VERSION" "../scripts/get-version.sh")
+version = T.dropWhileEnd (== '\n') $(getValFromEnvOrScript "VERSION1" "../scripts/get-version.sh")
 
-consoleVersion :: T.Text
-consoleVersion = case V.fromText $ T.dropWhile (== 'v') version of
-  Right ver -> mkVersion ver
-  Left _    -> version
-
-mkVersion :: V.Version -> T.Text
-mkVersion ver = T.pack $ "v" ++ show major ++ "." ++ show minor
-  where
-    major = ver ^. V.major
-    minor = ver ^. V.minor
+parsedVersion :: Maybe V.Version
+parsedVersion = V.parseVersion $ T.unpack $ T.dropWhile (== 'v') version
 
 currentVersion :: T.Text
 currentVersion = version
 
 isDevVersion :: Bool
-isDevVersion = either (const True) (const False) $
-               V.fromText $ T.dropWhile (== 'v') version
+isDevVersion = case parsedVersion of
+  Just _  -> False
+  Nothing -> True
+
+consoleVersion :: T.Text
+consoleVersion = case parsedVersion of
+  Nothing -> version
+  Just v  -> mkConsoleV v
+
+mkConsoleV :: V.Version -> T.Text
+mkConsoleV v = T.pack $ release <> "/v" <> show major <> "." <> show minor
+  where
+    major = v ^. V.majorLens
+    minor = v ^. V.minorLens
+    release = case v ^. V.preReleasesLens of
+      [] -> "stable"
+      (r:_) -> case V.renderPreRelease r of
+        ('a':'l':'p':'h':'a':_) -> "alpha"
+        ('b':'e':'t':'a':_) -> "beta"
+        ('r':'c':_) -> "rc"
+        _ -> "unknown"
