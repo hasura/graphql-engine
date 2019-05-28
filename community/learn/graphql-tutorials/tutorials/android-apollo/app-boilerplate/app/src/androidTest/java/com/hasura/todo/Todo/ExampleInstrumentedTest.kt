685c33fd9abfb2a78411fc0d413928a44c406d12
Manishalexin <Manishalexin@gmail.com>
2019-05-28 20:42:36 +0530

    Update community/learn/graphql-tutorials/tutorials/android-apollo/app-boilerplate/app/src/androidTest/java/com/hasura/todo/Todo/ExampleInstrumentedTest.kt
    fixed bundle id in test file
    
    Co-Authored-By: Shahidh K Muhammed <muhammedshahid.k@gmail.com>

--- community/learn/graphql-tutorials/tutorials/android-apollo/app-boilerplate/app/src/androidTest/java/com/hasura/todo/Todo/ExampleInstrumentedTest.kt
+++ community/learn/graphql-tutorials/tutorials/android-apollo/app-boilerplate/app/src/androidTest/java/com/hasura/todo/Todo/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mk.todo.Todo", appContext.packageName)
+        assertEquals("com.hasura.todo.Todo", appContext.packageName)
     }
 }
