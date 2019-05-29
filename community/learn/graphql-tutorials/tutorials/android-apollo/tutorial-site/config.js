213a42ae3693c0fb0159705f9edb72148c9e60d7
Praveen D <praveen@hasura.io>
2019-05-29 11:06:31 +0530

    update android manifests

--- community/learn/graphql-tutorials/tutorials/android-apollo/tutorial-site/config.js
+++ community/learn/graphql-tutorials/tutorials/android-apollo/tutorial-site/config.js
@@ -9,7 +9,7 @@ const config = {
 		"title": "/ graphql / android",
 		"githubUrl": "https://github.com/hasura/graphql-engine",
 		"helpUrl": "https://discordapp.com/invite/vBPpJkS",
-		"tweetText": "Check out this GraphQL course for android developers by @HasuraHQ https://learn.hasura.io/graphql/android",
+		"tweetText": "Check out this GraphQL course for Android Developers by @HasuraHQ https://learn.hasura.io/graphql/android",
 		"links": [{
 			"text": "hasura.io",
 			"link": "https://hasura.io"
@@ -40,8 +40,8 @@ const config = {
 		]
 	},
 	"siteMetadata": {
-		"title": "2 hour GraphQL course for android developers | Hasura",
-		"description": "A concise and powerful tutorial that covers fundamental concepts of both GraphQL and using GraphQL in android",
+		"title": "2 hour GraphQL course for Android Developers | Hasura",
+		"description": "A concise and powerful tutorial that covers fundamental concepts of both GraphQL and using GraphQL in Android",
 		"ogImage": "https://storage.googleapis.com/graphql-engine-cdn.hasura.io/learn-hasura/assets/twitter-card-android.png",
 		"docsLocation": "https://github.com/hasura/graphql-engine/tree/master/community/learn/graphql-tutorials/tutorials/android-apollo/tutorial-site/content",
 		"favicon": "https://graphql-engine-cdn.hasura.io/learn-hasura/assets/graphql-android/android-favicon.png"
