d25479a0f6c0a0390ab8a213ee85fad622a660c4
Ajeet D'Souza <98ajeet@gmail.com>
2019-05-30 11:53:42 +0530

    Fix FromJSONKey instance of NEText

--- server/src-lib/Hasura/RQL/Types/Common.hs
+++ server/src-lib/Hasura/RQL/Types/Common.hs
@@ -31,6 +31,7 @@ import           Hasura.SQL.Types
 import           Data.Aeson
 import           Data.Aeson.Casing
 import           Data.Aeson.TH
+import           Data.Aeson.Types
 import qualified Data.HashMap.Strict        as HM
 import qualified Data.Text                  as T
 import qualified Database.PG.Query          as Q
@@ -54,13 +55,16 @@ mkNEText :: T.Text -> Maybe NEText
 mkNEText ""   = Nothing
 mkNEText text = Just $ NEText text
 
-instance FromJSON NEText where
-  parseJSON = withText "String"
-    $ \text -> case mkNEText text of
-      Nothing     -> fail "empty string not allowed"
-      Just neText -> return neText
+parseNEText :: T.Text -> Parser NEText
+parseNEText text = case mkNEText text of
+  Nothing     -> fail "empty string not allowed"
+  Just neText -> return neText
 
-instance FromJSONKey NEText
+instance FromJSON NEText where
+  parseJSON = withText "String" parseNEText
+
+instance FromJSONKey NEText where
+  fromJSONKey = FromJSONKeyTextParser parseNEText
 
 instance Q.FromCol NEText where
   fromCol bs = mkNEText <$> Q.fromCol bs
