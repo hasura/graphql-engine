de3bd50c25d32d762db493e8c997db1fd6912c59
KC Erb <iamkcerb@gmail.com>
2019-06-03 13:16:21 +0200

    initialize migration driver correctly in cli settings command

--- cli/commands/settings.go
+++ cli/commands/settings.go
@@ -4,7 +4,6 @@ import (
 	"strings"
 
 	"github.com/hasura/graphql-engine/cli"
-	"github.com/hasura/graphql-engine/cli/migrate"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -25,8 +24,9 @@ func NewSettingsCmd(ec *cli.ExecutionContext) *cobra.Command {
   # Set migration_mode to false:
   hasura settings --set migration_mode=false`,
 		SilenceUsage: true,
-		PreRun: func(cmd *cobra.Command, args []string) {
+		PreRunE: func(cmd *cobra.Command, args []string) error {
 			ec.Viper = v
+			return ec.Validate()
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return opts.run()
@@ -46,16 +46,14 @@ type settingsOptions struct {
 }
 
 func (o *settingsOptions) run() error {
-	// dbURL := getDataPath(o.EC.ServerConfig.ParsedEndpoint, getAdminSecretHeaderName(o.EC.Version), o.EC.ServerConfig.AdminSecret)
-	// fileURL := getFilePath(o.EC.MigrationDir)
-	t, err := migrate.New(fileURL.String(), dbURL.String(), false, o.EC.Logger)
+	migrateDrv, err := newMigrate(o.EC.MigrationDir, o.EC.ServerConfig.ParsedEndpoint, o.EC.ServerConfig.AdminSecret, o.EC.Logger, o.EC.Version)
 	if err != nil {
 		o.EC.Logger.WithError(err).Errorf("cannot create migrate instance")
 		return err
 	}
 
 	if o.getKey != "" {
-		setting, err := t.GetSetting(o.getKey)
+		setting, err := migrateDrv.GetSetting(o.getKey)
 		if err != nil {
 			o.EC.Logger.WithError(err).Errorf("cannot get value for key `%s`", o.getKey)
 			return err
@@ -65,8 +63,12 @@ func (o *settingsOptions) run() error {
 
 	if o.setKeyValue != "" {
 		arr := strings.Split(o.setKeyValue, "=")
+		if len(arr) != 2 {
+			o.EC.Logger.WithError(err).Errorf("`set` must be in format `key=value`")
+			return err
+		}
 		key, value := arr[0], arr[1]
-		err = t.UpdateSetting(key, value)
+		err = migrateDrv.UpdateSetting(key, value)
 		if err != nil {
 			o.EC.Logger.WithError(err).Errorf("cannot set value `%s` for key `%s`", key, value)
 			return err
