5ae5030192d7e9ce92fcc7ab04dc47b2d696e19b
strongSoda <strongsoda2@gmail.com>
2019-06-06 15:34:58 +0530

    display loading while data is fetched

--- console/src/components/Services/Metadata/About/About.js
+++ console/src/components/Services/Metadata/About/About.js
@@ -7,22 +7,65 @@ import Star from '../../../Common/Icons/Star';
 class About extends Component {
   state = {
     serverVersion: '',
+    serverVersionLoading: true,
     latestVersionAvailable: '',
+    latestVersionAvailableLoading: true,
   };
 
   componentDidMount() {
     fetch(Endpoints.version)
       .then(response => response.json())
       .then(serverVersion =>
-        this.setState({ serverVersion: serverVersion.version })
+        this.setState({
+          serverVersion: serverVersion.version,
+          serverVersionLoading: false,
+        })
       );
 
     fetch(Endpoints.updateCheck)
       .then(response => response.json())
-      .then(latest => this.setState({ latestVersionAvailable: latest.latest }));
+      .then(latest =>
+        this.setState({
+          latestVersionAvailable: latest.latest,
+          latestVersionAvailableLoading: false,
+        })
+      );
   }
 
   render() {
+    if (
+      this.state.serverVersionLoading &&
+      this.state.latestVersionAvailableLoading
+    ) {
+      return (
+        <div
+          className={`${styles.clear_fix} ${styles.padd_left} ${
+            styles.padd_top
+          } ${styles.metadata_wrapper} container-fluid`}
+        >
+          <div className={styles.subHeader}>
+            <h2
+              className={`${styles.heading_text} ${styles.remove_pad_bottom}`}
+            >
+              Server Version
+            </h2>
+            <div className={styles.add_mar_top + ' ' + styles.wd60}>
+              <Star /> <em>...loading</em>
+            </div>
+            <hr />
+            <h2
+              className={`${styles.heading_text} ${styles.remove_pad_bottom}`}
+            >
+              Latest Server Version Available
+            </h2>
+            <div className={styles.add_mar_top + ' ' + styles.wd60}>
+              <Star /> <em>...loading</em>
+            </div>
+            <hr />
+          </div>
+        </div>
+      );
+    }
     return (
       <div
         className={`${styles.clear_fix} ${styles.padd_left} ${
@@ -30,15 +73,19 @@ class About extends Component {
         } ${styles.metadata_wrapper} container-fluid`}
       >
         <div className={styles.subHeader}>
-          <h2 className={`${styles.heading_text} ${styles.remove_pad_bottom}`}>
-            Server Version
+          <h2
+            className={`${styles.heading_text} ${styles.remove_pad_bottom}`}
+          >
+              Server Version
           </h2>
           <div className={styles.add_mar_top + ' ' + styles.wd60}>
             <Star /> {this.state.serverVersion}
           </div>
           <hr />
-          <h2 className={`${styles.heading_text} ${styles.remove_pad_bottom}`}>
-            Latest Server Version Available
+          <h2
+            className={`${styles.heading_text} ${styles.remove_pad_bottom}`}
+          >
+              Latest Server Version Available
           </h2>
           <div className={styles.add_mar_top + ' ' + styles.wd60}>
             <Star /> {this.state.latestVersionAvailable}
