a30814f01cfd1bbad0768867f694e35b6d06143a
Praveen Durairaj <praveen@hasura.io>
2019-06-10 18:57:38 +0530

    community: add gridsome sample app (#2363)

---
+++ community/sample-apps/gridsome-postgres-graphql/gridsome.server.js
@@ -0,0 +1,16 @@
+// Server API makes it possible to hook into various parts of Gridsome
+// on server-side and add custom data to the GraphQL data layer.
+// Learn more: https://gridsome.org/docs/server-api
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = function (api) {
+  api.loadSource(({ addContentType }) => {
+    // Use the Data Store API here: https://gridsome.org/docs/data-store-api
+  })
+
+  api.createPages(({ createPage }) => {
+    // Use the Pages API here: https://gridsome.org/docs/pages-api
+  })
+}
