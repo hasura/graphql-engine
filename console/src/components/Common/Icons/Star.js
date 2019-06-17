43559ba58a033806cc82dd9c3e119f811327dd54
strongSoda <strongsoda2@gmail.com>
2019-06-17 15:46:40 +0530

    ui change

--- console/src/components/Common/Icons/Star.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Star = ({ className }) => {
-  return <i className={`fa fa-certificate ${className}`} aria-hidden="true" />;
-};
-
-export default Star;
--- console/src/components/Services/Metadata/About/About.js
+++ console/src/components/Services/Metadata/About/About.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import styles from './About.scss';
 
 import Endpoints from '../../../../Endpoints';
-import Star from '../../../Common/Icons/Star';
 import Spinner from '../../../Common/Spinner/Spinner';
 
 import globals from '../../../../Globals';
@@ -47,59 +46,59 @@ class About extends Component {
           {this.state.serverVersion && this.state.latestVersionAvailable ? (
             <div>
               <div className={styles.add_mar_top + ' ' + styles.wd60}>
-                <Star /> <b>Console asset version: </b>
+                <b>Console asset version: </b>
                 <span className={styles.add_mar_left_mid}>
                   {this.state.consoleAssetVersion}
                 </span>
               </div>
               <hr />
               <div className={styles.add_mar_top + ' ' + styles.wd60}>
-                <Star /> <b>Server version: </b>
+                <b>Server version: </b>
                 <span className={styles.add_mar_left_mid}>
                   {this.state.serverVersion}
                 </span>
               </div>
               <hr />
               <div className={styles.add_mar_top + ' ' + styles.wd60}>
-                <Star /> <b>Latest available server version: </b>
+                <b>Latest available server version: </b>
                 <span className={styles.add_mar_left_mid}>
                   {this.state.latestVersionAvailable}
                 </span>
+                {this.state.serverVersion !==
+                this.state.latestVersionAvailable ? (
+                  <span className={styles.add_mar_left_mid}>
+                    <a
+                      href={
+                        'https://github.com/hasura/graphql-engine/releases/tag/' +
+                        this.state.latestVersionAvailable
+                      }
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <span>
+                        <i>View Changelog</i>
+                      </span>
+                    </a>
+                    <span className={styles.middot}>
+                      {' '}
+                      <b>&middot;</b>{' '}
+                    </span>
+                    <a
+                      className={styles.updateLink}
+                      href="https://docs.hasura.io/1.0/graphql/manual/deployment/updating.html"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <span>
+                        <i>Update Now</i>
+                      </span>
+                    </a>
+                  </span>
+                ) : (
+                  ''
+                )}
               </div>
               <hr />
-              {this.state.serverVersion !==
-              this.state.latestVersionAvailable ? (
-                <div>
-                  <a
-                    href={
-                      'https://github.com/hasura/graphql-engine/releases/tag/' +
-                      this.state.latestVersionAvailable
-                    }
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span>
-                      <i>View Changelog</i>
-                    </span>
-                  </a>
-                  <span className={styles.middot}>
-                    {' '}
-                    <b>&middot;</b>{' '}
-                  </span>
-                  <a
-                    className={styles.updateLink}
-                    href="https://docs.hasura.io/1.0/graphql/manual/deployment/updating.html"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <span>
-                      <i>Update Now</i>
-                    </span>
-                  </a>
-                </div>
-              ) : (
-                ''
-              )}
             </div>
           ) : (
             <Spinner />
