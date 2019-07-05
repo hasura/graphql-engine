2958188a955919719bffefecec2385aa35d67da9
Ajeet D'Souza <98ajeet@gmail.com>
2019-07-05 14:37:22 +0530

    optimize hdb_primary_key view

--- server/src-rsr/migrate_from_17_to_18.sql
+++ server/src-rsr/migrate_from_17_to_18.sql
@@ -1,120 +1,24 @@
 CREATE OR REPLACE VIEW hdb_catalog.hdb_primary_key AS
 SELECT
-  tc.table_schema,
-  tc.table_name,
-  tc.constraint_name,
-  json_agg(constraint_column_usage.column_name) AS columns
+  nr.nspname as table_schema,
+  r.relname as table_name,
+  c.conname as constraint_name,
+  json_agg(a.attname) as "columns"
 FROM
-  (
-    (
-      SELECT
-        nr.nspname::information_schema.sql_identifier AS table_schema,
-        r.relname::information_schema.sql_identifier AS table_name,
-        c.conname::information_schema.sql_identifier AS constraint_name,
-        c.contype AS constraint_type
-      FROM
-        pg_namespace nr,
-        pg_constraint c,
-        pg_class r
-      WHERE
-        nr.oid = r.relnamespace
-        AND c.conrelid = r.oid
-        AND c.contype = 'p'
-        AND (r.relkind = ANY (ARRAY['r'::"char", 'p'::"char"]))
-        AND NOT pg_is_other_temp_schema(nr.oid)
-    ) tc
-    JOIN (
-      SELECT
-        x.tblschema AS table_schema,
-        x.tblname AS table_name,
-        x.colname AS column_name,
-        x.cstrname AS constraint_name
-      FROM
-        (
-          SELECT
-            DISTINCT nr.nspname,
-            r.relname,
-            a.attname,
-            c.conname
-          FROM
-            pg_namespace nr,
-            pg_class r,
-            pg_attribute a,
-            pg_depend d,
-            pg_namespace nc,
-            pg_constraint c
-          WHERE
-            (
-              (nr.oid = r.relnamespace)
-              AND (r.oid = a.attrelid)
-              AND (d.refclassid = ('pg_class' :: regclass) :: oid)
-              AND (d.refobjid = r.oid)
-              AND (d.refobjsubid = a.attnum)
-              AND (d.classid = ('pg_constraint' :: regclass) :: oid)
-              AND (d.objid = c.oid)
-              AND (c.connamespace = nc.oid)
-              AND (c.contype = 'c' :: "char")
-              AND (
-                r.relkind = ANY (ARRAY ['r'::"char", 'p'::"char"])
-              )
-              AND (NOT a.attisdropped)
-            )
-          UNION ALL
-          SELECT
-            nr.nspname,
-            r.relname,
-            a.attname,
-            c.conname
-          FROM
-            pg_namespace nr,
-            pg_class r,
-            pg_attribute a,
-            pg_namespace nc,
-            pg_constraint c
-          WHERE
-            (
-              (nr.oid = r.relnamespace)
-              AND (r.oid = a.attrelid)
-              AND (nc.oid = c.connamespace)
-              AND (
-                r.oid = CASE
-                  c.contype
-                  WHEN 'f' :: "char" THEN c.confrelid
-                  ELSE c.conrelid
-                END
-              )
-              AND (
-                a.attnum = ANY (
-                  CASE
-                    c.contype
-                    WHEN 'f' :: "char" THEN c.confkey
-                    ELSE c.conkey
-                  END
-                )
-              )
-              AND (NOT a.attisdropped)
-              AND (
-                c.contype = ANY (ARRAY ['p'::"char", 'u'::"char", 'f'::"char"])
-              )
-              AND (
-                r.relkind = ANY (ARRAY ['r'::"char", 'p'::"char"])
-              )
-            )
-        ) x (
-          tblschema,
-          tblname,
-          colname,
-          cstrname
-        )
-    ) constraint_column_usage ON (
-      (
-        (tc.constraint_name) :: text = (constraint_column_usage.constraint_name) :: text
-        AND (tc.table_schema) :: text = (constraint_column_usage.table_schema) :: text
-        AND (tc.table_name) :: text = (constraint_column_usage.table_name) :: text
-      )
-    )
-  )
-GROUP BY
-  tc.table_schema,
-  tc.table_name,
-  tc.constraint_name;
+  pg_namespace nr,
+  pg_class r,
+  pg_attribute a,
+  pg_namespace nc,
+  pg_constraint c
+WHERE
+  nr.oid = r.relnamespace
+  AND (r.oid = a.attrelid)
+  AND (nc.oid = c.connamespace)
+  -- only primary key constraint
+  AND (c.contype = 'p')
+  AND (r.oid = c.conrelid)
+  AND (a.attnum = ANY (c.conkey))
+  AND (NOT a.attisdropped)
+  -- 'normal table or partitioned table'
+  AND (r.relkind = 'r' OR r.relkind = 'p')
+GROUP BY (nr.nspname, r.relname, c.conname);
