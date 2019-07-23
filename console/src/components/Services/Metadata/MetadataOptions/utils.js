a6c2a33389fb8266549ba8bbcb3eb775b795dfa3
rikinsk <rikin.kachhia@gmail.com>
2019-07-23 17:38:01 +0530

    prettier sucks

--- console/src/components/Services/Metadata/MetadataOptions/utils.js
+++ console/src/components/Services/Metadata/MetadataOptions/utils.js
@@ -6,23 +6,29 @@ export const getCurrTimeForFileName = () => {
     .getFullYear()
     .toString()
     .padStart(4, '0');
+
   const month = (currTime.getMonth() + 1).toString().padStart(2, '0');
+
   const date = currTime
     .getDate()
     .toString()
     .padStart(2, '0');
+
   const hours = currTime
     .getHours()
     .toString()
     .padStart(2, '0');
+
   const minutes = currTime
     .getMinutes()
     .toString()
     .padStart(2, '0');
+
   const seconds = currTime
     .getSeconds()
     .toString()
     .padStart(2, '0');
+
   const milliSeconds = currTime
     .getMilliseconds()
     .toString()
