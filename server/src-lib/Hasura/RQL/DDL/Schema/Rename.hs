372608cbc904553395489a863be8667c04f25653
rakeshkky <12475069+rakeshkky@users.noreply.github.com>
2019-08-08 18:04:31 +0530

    account table and column renames in '_exists' fields in permission

--- server/src-lib/Hasura/RQL/DDL/Schema/Rename.hs
+++ server/src-lib/Hasura/RQL/DDL/Schema/Rename.hs
@@ -37,6 +37,11 @@ data RenameField
 
 type RenameTable = (QualifiedTable, QualifiedTable)
 
+data Rename
+  = RTable !RenameTable
+  | RField !RenameField
+  deriving (Show, Eq)
+
 otherDeps :: QErrM m => Text -> SchemaObjId -> m ()
 otherDeps errMsg d =
   throw500 $ "unexpected dependancy "
@@ -52,8 +57,8 @@ renameTableInCatalog newQT oldQT = do
   forM_ allDeps $ \case
     SOTableObj refQT (TORel rn) ->
       updateRelDefs refQT rn (oldQT, newQT)
-    -- table names are not specified in permission definitions
-    SOTableObj _ (TOPerm _ _)   -> return ()
+    SOTableObj refQT (TOPerm rn pt)   ->
+      updatePermFlds refQT rn pt $ RTable (oldQT, newQT)
     -- A trigger's definition is not dependent on the table directly
     SOTableObj _ (TOTrigger _)   -> return ()
     d -> otherDeps errMsg d
@@ -83,7 +88,7 @@ renameColInCatalog oCol nCol qt ti = do
   -- Update dependent objects
   forM_ depObjs $ \case
     SOTableObj refQT (TOPerm role pt) ->
-      updatePermFlds refQT role pt renameFld
+      updatePermFlds refQT role pt $ RField renameFld
     SOTableObj refQT (TORel rn) ->
       updateColInRel refQT rn $ RenameItem qt oCol nCol
     SOTableObj _ (TOTrigger triggerName) ->
@@ -109,7 +114,7 @@ renameRelInCatalog qt oldRN newRN = do
 
   forM_ depObjs $ \case
     SOTableObj refQT (TOPerm role pt) ->
-      updatePermFlds refQT role pt renameFld
+      updatePermFlds refQT role pt $ RField renameFld
     d -> otherDeps errMsg d
   liftTx updateRelName
   where
@@ -169,62 +174,75 @@ updateArrRelDef qt rn (oldQT, newQT) = do
 
 -- | update fields in premissions
 updatePermFlds :: (MonadTx m, CacheRM m)
-  => QualifiedTable -> RoleName -> PermType -> RenameField -> m ()
-updatePermFlds refQT rn pt rf = do
+  => QualifiedTable -> RoleName -> PermType -> Rename -> m ()
+updatePermFlds refQT rn pt rename = do
   pDef <- fmap fst $ liftTx $ fetchPermDef refQT rn pt
   case pt of
     PTInsert -> do
       perm <- decodeValue pDef
-      updateInsPermFlds refQT rf rn perm
+      updateInsPermFlds refQT rename rn perm
     PTSelect -> do
       perm <- decodeValue pDef
-      updateSelPermFlds refQT rf rn perm
+      updateSelPermFlds refQT rename rn perm
     PTUpdate -> do
       perm <- decodeValue pDef
-      updateUpdPermFlds refQT rf rn perm
+      updateUpdPermFlds refQT rename rn perm
     PTDelete -> do
       perm <- decodeValue pDef
-      updateDelPermFlds refQT rf rn perm
+      updateDelPermFlds refQT rename rn perm
 
 updateInsPermFlds
   :: (MonadTx m, CacheRM m)
-  => QualifiedTable -> RenameField -> RoleName -> InsPerm -> m ()
-updateInsPermFlds qt rf rn (InsPerm chk preset cols) = do
-  updBoolExp <- updateBoolExp qt rf chk
-  liftTx $ updatePermDefInCatalog PTInsert qt rn $
-    InsPerm updBoolExp updPresetM updColsM
-  where
-    updPresetM = updatePreset qt rf <$> preset
-    updColsM = updateCols qt rf <$> cols
+  => QualifiedTable -> Rename -> RoleName -> InsPerm -> m ()
+updateInsPermFlds refQT rename rn (InsPerm chk preset cols) = do
+  updatedPerm <- case rename of
+    RTable rt -> do
+      let updChk = updateTableInBoolExp rt chk
+      return $ InsPerm updChk preset cols
+    RField rf -> do
+      updChk <- updateFieldInBoolExp refQT rf chk
+      let updPresetM = updatePreset refQT rf <$> preset
+          updColsM = updateCols refQT rf <$> cols
+      return $ InsPerm updChk updPresetM updColsM
+  liftTx $ updatePermDefInCatalog PTInsert refQT rn updatedPerm
 
 updateSelPermFlds
   :: (MonadTx m, CacheRM m)
-  => QualifiedTable -> RenameField -> RoleName -> SelPerm -> m ()
-updateSelPermFlds refQT rf rn (SelPerm cols fltr limit aggAllwd) = do
-  updBoolExp <- updateBoolExp refQT rf fltr
-  liftTx $ updatePermDefInCatalog PTSelect refQT rn $
-      SelPerm updCols updBoolExp limit aggAllwd
-  where
-    updCols = updateCols refQT rf cols
+  => QualifiedTable -> Rename -> RoleName -> SelPerm -> m ()
+updateSelPermFlds refQT rename rn (SelPerm cols fltr limit aggAllwd) = do
+  updatedPerm <- case rename of
+    RTable rt -> do
+      let updFltr = updateTableInBoolExp rt fltr
+      return $ SelPerm cols updFltr limit aggAllwd
+    RField rf -> do
+      updFltr <- updateFieldInBoolExp refQT rf fltr
+      let updCols = updateCols refQT rf cols
+      return $ SelPerm updCols updFltr limit aggAllwd
+  liftTx $ updatePermDefInCatalog PTSelect refQT rn updatedPerm
 
 updateUpdPermFlds
   :: (MonadTx m, CacheRM m)
-  => QualifiedTable -> RenameField -> RoleName -> UpdPerm -> m ()
-updateUpdPermFlds refQT rf rn (UpdPerm cols preset fltr) = do
-  updBoolExp <- updateBoolExp refQT rf fltr
-  liftTx $ updatePermDefInCatalog PTUpdate refQT rn $
-      UpdPerm updCols updPresetM updBoolExp
-  where
-    updCols = updateCols refQT rf cols
-    updPresetM = updatePreset refQT rf <$> preset
+  => QualifiedTable -> Rename -> RoleName -> UpdPerm -> m ()
+updateUpdPermFlds refQT rename rn (UpdPerm cols preset fltr) = do
+  updatedPerm <- case rename of
+    RTable rt -> do
+      let updFltr = updateTableInBoolExp rt fltr
+      return $ UpdPerm cols preset updFltr
+    RField rf -> do
+      updFltr <- updateFieldInBoolExp refQT rf fltr
+      let updCols = updateCols refQT rf cols
+          updPresetM = updatePreset refQT rf <$> preset
+      return $ UpdPerm updCols updPresetM updFltr
+  liftTx $ updatePermDefInCatalog PTUpdate refQT rn updatedPerm
 
 updateDelPermFlds
   :: (MonadTx m, CacheRM m)
-  => QualifiedTable -> RenameField -> RoleName -> DelPerm -> m ()
-updateDelPermFlds refQT rf rn (DelPerm fltr) = do
-  updBoolExp <- updateBoolExp refQT rf fltr
-  liftTx $ updatePermDefInCatalog PTDelete refQT rn $
-    DelPerm updBoolExp
+  => QualifiedTable -> Rename -> RoleName -> DelPerm -> m ()
+updateDelPermFlds refQT rename rn (DelPerm fltr) = do
+  updFltr <- case rename of
+    RTable rt -> return $ updateTableInBoolExp rt fltr
+    RField rf -> updateFieldInBoolExp refQT rf fltr
+  liftTx $ updatePermDefInCatalog PTDelete refQT rn $ DelPerm updFltr
 
 updatePreset
   :: QualifiedTable -> RenameField -> ColVals -> ColVals
@@ -259,11 +277,38 @@ updateCols qt rf permSpec =
       PCCols c -> PCCols $ flip map c $
         \col -> if col == oCol then nCol else col
 
-updateBoolExp
+updateTableInBoolExp :: RenameTable -> BoolExp -> BoolExp
+updateTableInBoolExp rt@(oldQT, newQT) (BoolExp bExp) = BoolExp $
+  case bExp of
+    BoolAnd exps -> BoolAnd $ procExps exps
+    BoolOr  exps -> BoolOr $ procExps exps
+    BoolNot e    -> BoolNot $ updateTableInBoolExp' e
+    BoolFld fld  -> BoolFld fld
+    BoolExists (GExists refqt wh) ->
+      let updWhereExp = updateTableInBoolExp' wh
+      in BoolExists $
+         if refqt == oldQT then GExists newQT updWhereExp
+         else GExists refqt updWhereExp
+  where
+    updateTableInBoolExp' = unBoolExp . updateTableInBoolExp rt . BoolExp
+    procExps = map updateTableInBoolExp'
+
+updateFieldInBoolExp
   :: (QErrM m, CacheRM m)
   => QualifiedTable -> RenameField -> BoolExp -> m BoolExp
-updateBoolExp qt rf =
-  fmap BoolExp . traverse (updateColExp qt rf) . unBoolExp
+updateFieldInBoolExp qt rf be = BoolExp <$>
+  case unBoolExp be of
+    BoolAnd exps -> BoolAnd <$> procExps exps
+    BoolOr  exps -> BoolOr <$> procExps exps
+    BoolNot e    -> BoolNot <$> updateBoolExp' e
+    BoolExists (GExists refqt wh) ->
+      (BoolExists . GExists refqt . unBoolExp)
+      <$> updateFieldInBoolExp refqt rf (BoolExp wh)
+    BoolFld fld  -> BoolFld <$> updateColExp qt rf fld
+  where
+    procExps = mapM updateBoolExp'
+    updateBoolExp' =
+      fmap unBoolExp . updateFieldInBoolExp qt rf . BoolExp
 
 updateColExp
   :: (QErrM m, CacheRM m)
@@ -280,7 +325,7 @@ updateColExp qt rf (ColExp fld val) =
         FIRelationship ri -> do
           let remTable = riRTable ri
           be <- decodeValue val
-          ube <- updateBoolExp remTable rf be
+          ube <- updateFieldInBoolExp remTable rf be
           return $ toJSON ube
 
     (oFld, nFld, opQT) = case rf of
