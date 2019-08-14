16d48bee5309eda605c38ded37b69b5cb8797c6f
rikinsk <rikin.kachhia@gmail.com>
2019-08-14 17:15:50 +0530

    make common Modal component

---
+++ console/src/components/Common/Modal/Modal.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import BootstrapModal from 'react-bootstrap/lib/Modal';
+import BootstrapModalButton from 'react-bootstrap/lib/Button';
+
+const Modal = ({
+  show = true,
+  title,
+  onClose,
+  onSubmit,
+  onCancel = null,
+  submitText = null,
+  children,
+}) => {
+  return (
+    <BootstrapModal show={show} onHide={onClose}>
+      <BootstrapModal.Header closeButton>
+        <BootstrapModal.Title>{title}</BootstrapModal.Title>
+      </BootstrapModal.Header>
+      <BootstrapModal.Body>{children}</BootstrapModal.Body>
+      <BootstrapModal.Footer>
+        <BootstrapModalButton onClick={onCancel || onClose}>
+          Cancel
+        </BootstrapModalButton>
+        <BootstrapModalButton onClick={onSubmit} bsStyle="primary">
+          {submitText || 'Submit'}
+        </BootstrapModalButton>
+      </BootstrapModal.Footer>
+    </BootstrapModal>
+  );
+};
+
+export default Modal;
