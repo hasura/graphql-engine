fb0ebd8603f08f134b75b111d695dce5aa606caf
Tirumarai Selvan A <tiru@hasura.io>
2019-09-04 16:52:53 +0530

    change hdb_permission_agg to account for remote schema perms

--- server/src-rsr/initialise.sql
+++ server/src-rsr/initialise.sql
@@ -47,17 +47,6 @@ CREATE TABLE hdb_catalog.hdb_permission
     FOREIGN KEY (table_schema, table_name) REFERENCES hdb_catalog.hdb_table(table_schema, table_name) ON UPDATE CASCADE
 );
 
-CREATE VIEW hdb_catalog.hdb_permission_agg AS
-SELECT
-    table_schema,
-    table_name,
-    role_name,
-    json_object_agg(perm_type, perm_def) as permissions
-FROM
-    hdb_catalog.hdb_permission
-GROUP BY
-    table_schema, table_name, role_name;
-
 CREATE VIEW hdb_catalog.hdb_foreign_key_constraint AS
 SELECT
     q.table_schema :: text,
@@ -609,10 +598,33 @@ CREATE TABLE hdb_catalog.hdb_allowlist
 );
 
 CREATE TABLE hdb_catalog.remote_schema_permissions
-  (
-    remote_schema TEXT,
-    role TEXT,
-    definition JSONB NOT NULL,
-    PRIMARY KEY (remote_schema, role),
-    FOREIGN KEY (remote_schema) REFERENCES hdb_catalog.remote_schemas(name) ON UPDATE CASCADE
+(
+  remote_schema TEXT,
+  role TEXT,
+  definition JSONB NOT NULL,
+  PRIMARY KEY (remote_schema, role),
+  FOREIGN KEY (remote_schema) REFERENCES hdb_catalog.remote_schemas(name) ON UPDATE CASCADE
+);
+
+CREATE VIEW hdb_catalog.hdb_permission_agg AS
+( SELECT
+    table_schema,
+    table_name,
+    null as remote_schema,
+    role_name,
+    json_object_agg(perm_type, perm_def) as permissions
+  FROM
+    hdb_catalog.hdb_permission
+  GROUP BY
+    table_schema, table_name, role_name
+)
+UNION ALL
+( SELECT
+    null as table_schema,
+    null as table_name,
+    remote_schema,
+    role as role_name,
+    definition::json as permissions
+  FROM
+    hdb_catalog.remote_schema_permissions
 );
