35e44836543464ba951c74ee2922c74db7ffade6
Rishichandra Wawhal <rishi@hasura.io>
2019-09-17 11:18:29 +0530

    add disabled component

--- console/src/components/Services/RemoteSchema/Permissions/Disabled.js
+++ console/src/components/Services/RemoteSchema/Permissions/Disabled.js
@@ -4,7 +4,13 @@ const Disabled = () => {
   return (
     <div>
       This feature is disabled.{' '}
-      <a href="https://docs.hasura.io">Read more...</a>
+      <a
+        href="https://docs.hasura.io"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Read more <i className={'fa fa-external-link'} aria-hidden="true" />
+      </a>
     </div>
   );
 };
