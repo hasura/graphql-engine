5aa3974be1c4628831230b8d5fad3d345e01d993
Rishichandra Wawhal <rishi@hasura.io>
2019-10-11 08:22:58 +0530

    disable unnecessary notif

--- console/src/components/Main/utils.js
+++ console/src/components/Main/utils.js
@@ -1,7 +1,7 @@
 const notificationVersionBlacklist = ['v1.0.0-beta.7'];
 
 export const showVersionUpdate = version => {
-  if (notificationVersionBlacklist.find(version)) {
+  if (notificationVersionBlacklist.find((v) => v !== version)) {
     return true;
   }
   return false;
