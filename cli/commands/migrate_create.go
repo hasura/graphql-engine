d41031a8de1067183001dcf77f11c9cc408b2a38
Aravind Shankar <aravind@hasura.io>
2019-11-20 15:28:00 +0530

    remove --from-server,--metadata-from-file,--metadata-from-server from migrate create command

--- cli/commands/migrate_create.go
+++ cli/commands/migrate_create.go
@@ -1,11 +1,8 @@
 package commands
 
 import (
-	"io/ioutil"
-	"os"
 	"time"
 
-	"github.com/ghodss/yaml"
 	"github.com/hasura/graphql-engine/cli"
 	"github.com/hasura/graphql-engine/cli/migrate"
 	"github.com/pkg/errors"
@@ -18,7 +15,7 @@ import (
 )
 
 const migrateCreateCmdExamples = `  # Setup migration files for the first time by introspecting a server:
-  hasura migrate create "init" --from-server`
+  hasura migrate create "init" --sql-from-server`
 
 func newMigrateCreateCmd(ec *cli.ExecutionContext) *cobra.Command {
 	v := viper.New()
@@ -54,12 +51,9 @@ func newMigrateCreateCmd(ec *cli.ExecutionContext) *cobra.Command {
 	}
 	f := migrateCreateCmd.Flags()
 	opts.flags = f
-	f.BoolVar(&opts.fromServer, "from-server", false, "get SQL statements and hasura metadata from the server")
 	f.StringVar(&opts.sqlFile, "sql-from-file", "", "path to an sql file which contains the SQL statements")
 	f.BoolVar(&opts.sqlServer, "sql-from-server", false, "take pg_dump from server and save it as a migration")
 	f.StringArrayVar(&opts.schemaNames, "schema", []string{"public"}, "name of Postgres schema to export as migration")
-	f.StringVar(&opts.metaDataFile, "metadata-from-file", "", "path to a hasura metadata file to be used for up actions")
-	f.BoolVar(&opts.metaDataServer, "metadata-from-server", false, "take metadata from the server and write it as an up migration file")
 	f.String("endpoint", "", "http(s) endpoint for Hasura GraphQL Engine")
 	f.String("admin-secret", "", "admin secret for Hasura GraphQL Engine")
 	f.String("access-key", "", "access key for Hasura GraphQL Engine")
@@ -83,32 +77,21 @@ type migrateCreateOptions struct {
 	flags *pflag.FlagSet
 
 	// Flags
-	fromServer     bool
-	sqlFile        string
-	sqlServer      bool
-	metaDataFile   string
-	metaDataServer bool
-	schemaNames    []string
+	sqlFile     string
+	sqlServer   bool
+	schemaNames []string
 }
 
 func (o *migrateCreateOptions) run() (version int64, err error) {
 	timestamp := getTime()
 	createOptions := mig.New(timestamp, o.name, o.EC.MigrationDir)
 
-	if o.fromServer {
-		o.sqlServer = true
-		o.metaDataServer = true
-	}
-
-	if o.flags.Changed("metadata-from-file") && o.sqlServer {
+	if o.flags.Changed("sql-from-file") && o.sqlServer {
 		return 0, errors.New("only one sql type can be set")
-	}
-	if o.flags.Changed("metadata-from-file") && o.metaDataServer {
-		return 0, errors.New("only one metadata type can be set")
 	}
 
 	var migrateDrv *migrate.Migrate
-	if o.sqlServer || o.metaDataServer {
+	if o.sqlServer {
 		migrateDrv, err = newMigrate(o.EC.MigrationDir, o.EC.ServerConfig.ParsedEndpoint, o.EC.ServerConfig.AdminSecret, o.EC.Logger, o.EC.Version, true)
 		if err != nil {
 			return 0, errors.Wrap(err, "cannot create migrate instance")
@@ -130,48 +113,10 @@ func (o *migrateCreateOptions) run() (version int64, err error) {
 		createOptions.SetSQLUp(string(data))
 	}
 
-	if o.flags.Changed("metadata-from-file") {
-		// metadata-file flag is set
-		err := createOptions.SetMetaUpFromFile(o.metaDataFile)
-		if err != nil {
-			return 0, errors.Wrap(err, "cannot set metadata file")
-		}
-	}
-
-	if o.metaDataServer {
-		// fetch metadata from server
-		metaData, err := migrateDrv.ExportMetadata()
-		if err != nil {
-			return 0, errors.Wrap(err, "cannot fetch metadata from server")
-		}
-
-		tmpfile, err := ioutil.TempFile("", "metadata")
-		if err != nil {
-			return 0, errors.Wrap(err, "cannot create tempfile")
-		}
-		defer os.Remove(tmpfile.Name())
-
-		t, err := yaml.Marshal(metaData)
-		if err != nil {
-			return 0, errors.Wrap(err, "cannot marshal metadata")
-		}
-		if _, err := tmpfile.Write(t); err != nil {
-			return 0, errors.Wrap(err, "cannot write to temp file")
-		}
-		if err := tmpfile.Close(); err != nil {
-			return 0, errors.Wrap(err, "cannot close tmp file")
-		}
-
-		err = createOptions.SetMetaUpFromFile(tmpfile.Name())
-		if err != nil {
-			return 0, errors.Wrap(err, "cannot parse metadata from the server")
-		}
-	}
-
-	if !o.flags.Changed("sql-from-file") && !o.flags.Changed("metadata-from-file") && !o.metaDataServer && !o.sqlServer {
+	if !o.flags.Changed("sql-from-file") && !o.sqlServer {
 		// Set empty data for [up|down].yaml
-		createOptions.MetaUp = []byte(`[]`)
-		createOptions.MetaDown = []byte(`[]`)
+		createOptions.SQLUp = []byte(``)
+		createOptions.SQLDown = []byte(``)
 	}
 
 	defer func() {
