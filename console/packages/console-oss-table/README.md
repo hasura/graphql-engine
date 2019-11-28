8ab76e5f46bdcd5b13707e5a80ba6a8bd4b9e610
Karthik Venkateswaran <karthik@hasura.io>
2019-11-28 17:53:19 +0530

    Added few modules to table package and released oss-table

---
+++ console/packages/console-oss-table/README.md
@@ -0,0 +1,6 @@
+0.0.6 - Contains
+
+- Editor
+  0.0.5 - Contains
+
+---
--- console/pro-table.js
+++ console/pro-table.js
@@ -3,9 +3,13 @@ import DragFoldTable from './src/components/Common/TableCommon/DragFoldTable';
 
 const tableScss = require('./src/components/Common/TableCommon/Table.scss');
 
+import Editor from './src/components/Common/Layout/ExpandableEditor/Editor';
+
 // Export telemetry stuff
 
 export { DragFoldTable };
 
 // export styles
 export { tableScss };
+
+export { Editor };
