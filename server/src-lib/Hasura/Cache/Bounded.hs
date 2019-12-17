6f1578e133baddb5de3c129850d3054ad75aca5f
Brandon Simmons <brandon.m.simmons@gmail.com>
2019-12-17 11:41:41 -0500

    Fix comment for haddock

--- server/src-lib/Hasura/Cache/Bounded.hs
+++ server/src-lib/Hasura/Cache/Bounded.hs
@@ -120,7 +120,7 @@ clearLocal (LocalCacheRef ref)=
 -- | lookup for a key in the local cache
 lookupLocal :: (Hashable k, Ord k) => LocalCacheRef k v -> k -> IO (Maybe v)
 lookupLocal (LocalCacheRef ref) k =
-  -- | Return the result and replace the cache if needed
+  -- Return the result and replace the cache if needed
   IORef.atomicModifyIORef' ref $ \currentCache ->
     case lookupPure k currentCache of
       Just (v, newCache) -> (newCache, Just v)
