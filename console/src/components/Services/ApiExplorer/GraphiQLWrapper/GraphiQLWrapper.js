405d6450f918f9b1d1fe1fc51612e2a08f631002
Rishichandra Wawhal <rishi@hasura.io>
2020-01-31 17:24:19 +0530

    console: replace graphiql fork with pkg + update explorer + replace uglify with terser (#3073)

--- console/src/components/Services/ApiExplorer/GraphiQLWrapper/GraphiQLWrapper.js
+++ console/src/components/Services/ApiExplorer/GraphiQLWrapper/GraphiQLWrapper.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import GraphiQL from 'hasura-console-graphiql';
+import GraphiQL from 'graphiql';
 import PropTypes from 'prop-types';
 import GraphiQLErrorBoundary from './GraphiQLErrorBoundary';
 import OneGraphExplorer from '../OneGraphExplorer/OneGraphExplorer';
+import AnalyzeButton from '../Analyzer/AnalyzeButton';
 
-import { clearCodeMirrorHints, setQueryVariableSectionHeight } from './utils';
+import {
+  clearCodeMirrorHints,
+  setQueryVariableSectionHeight,
+  copyToClipboard,
+} from './utils';
 import { analyzeFetcher, graphQLFetcherFinal } from '../Actions';
+import { parse, print } from 'graphql';
 
+import 'graphiql/graphiql.css';
 import './GraphiQL.css';
 
 class GraphiQLWrapper extends Component {
@@ -17,6 +24,7 @@ class GraphiQLWrapper extends Component {
       error: false,
       noSchema: false,
       onBoardingEnabled: false,
+      copyButtonText: 'Copy',
     };
   }
 
@@ -50,16 +58,96 @@ class GraphiQLWrapper extends Component {
       graphqlNetworkData.headers
     );
 
+    let graphiqlContext;
+
+    const handleClickPrettifyButton = () => {
+      const editor = graphiqlContext.getQueryEditor();
+      const currentText = editor.getValue();
+      const prettyText = print(parse(currentText));
+      editor.setValue(prettyText);
+    };
+
+    const handleCopyQuery = () => {
+      const editor = graphiqlContext.getQueryEditor();
+      const query = editor.getValue();
+
+      if (!query) {
+        return;
+      }
+      copyToClipboard(query);
+      this.setState({ copyButtonText: 'Copied' });
+      setTimeout(() => {
+        this.setState({ copyButtonText: 'Copy' });
+      }, 1500);
+    };
+
+    const handleToggleHistory = () => {
+      graphiqlContext.setState(prevState => ({
+        historyPaneOpen: !prevState.historyPaneOpen,
+      }));
+    };
+
     const renderGraphiql = graphiqlProps => {
       const voyagerUrl = graphqlNetworkData.consoleUrl + '/voyager-view';
+      let analyzerProps = {};
+      if (graphiqlContext) {
+        analyzerProps = graphiqlContext.state;
+      }
+
+      // get toolbar buttons
+      const getGraphiqlButtons = () => {
+        const buttons = [];
+        buttons.push({
+          label: 'Prettify',
+          title: 'Prettify Query (Shift-Ctrl-P)',
+          onClick: handleClickPrettifyButton,
+        });
+        buttons.push({
+          label: 'History',
+          title: 'Show History',
+          onClick: handleToggleHistory,
+        });
+        buttons.push({
+          label: this.state.copyButtonText,
+          title: 'Copy Query',
+          onClick: handleCopyQuery,
+        });
+        buttons.push({
+          label: 'Explorer',
+          title: 'Toggle Explorer',
+          onClick: graphiqlProps.toggleExplorer,
+        });
+        buttons.push({
+          label: 'Voyager',
+          title: 'GraphQL Voyager',
+          onClick: () => window.open(voyagerUrl, '_blank'),
+          icon: <i className="fa fa-external-link" aria-hidden="true" />,
+        });
+        return buttons.map(b => {
+          return <GraphiQL.Button key={b.label} {...b} />;
+        });
+      };
+
       return (
         <GraphiQL
+          ref={c => {
+            graphiqlContext = c;
+          }}
           fetcher={graphQLFetcher}
-          supportAnalyze
           voyagerUrl={voyagerUrl}
           analyzeFetcher={analyzeFetcherInstance}
           {...graphiqlProps}
-        />
+        >
+          <GraphiQL.Logo>GraphiQL</GraphiQL.Logo>
+          <GraphiQL.Toolbar>
+            {getGraphiqlButtons()}
+            <AnalyzeButton
+              operations={graphiqlContext && graphiqlContext.state.operations}
+              analyzeFetcher={analyzeFetcherInstance}
+              {...analyzerProps}
+            />
+          </GraphiQL.Toolbar>
+        </GraphiQL>
       );
     };
 
