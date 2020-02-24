062f3cf75db94a09da8413fd9831693581349db4
rikinsk <rikin.kachhia@gmail.com>
2020-02-24 12:17:48 +0530

    fix new release notification logic

--- console/src/components/Main/Main.js
+++ console/src/components/Main/Main.js
@@ -86,7 +86,7 @@ class Main extends React.Component {
   setShowUpdateNotification() {
     const {
       latestStableServerVersion,
-      latestServerVersion,
+      latestPreReleaseServerVersion,
       serverVersion,
       console_opts,
     } = this.props;
@@ -94,9 +94,17 @@ class Main extends React.Component {
     const allowPreReleaseNotifications =
       !console_opts || !console_opts.disablePreReleaseUpdateNotifications;
 
-    const latestServerVersionToCheck = allowPreReleaseNotifications
-      ? latestServerVersion
-      : latestStableServerVersion;
+    let latestServerVersionToCheck;
+    if (allowPreReleaseNotifications) {
+      latestServerVersionToCheck = versionGT(
+        latestPreReleaseServerVersion,
+        latestStableServerVersion
+      )
+        ? latestPreReleaseServerVersion
+        : latestStableServerVersion;
+    } else {
+      latestServerVersionToCheck = latestStableServerVersion;
+    }
 
     try {
       const lastUpdateCheckClosed = getLocalStorageItem(
