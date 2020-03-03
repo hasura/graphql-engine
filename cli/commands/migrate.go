ef4d614bf1298c95167856d4f577cd7f91673ad9
Aravind Shankar <aravind@hasura.io>
2020-03-03 12:36:59 +0530

    cli: fix flags in actions, migrate and metadata cmd (fix #3982) (#3991)

--- cli/commands/migrate.go
+++ cli/commands/migrate.go
@@ -31,12 +31,12 @@ import (
 // NewMigrateCmd returns the migrate command
 func NewMigrateCmd(ec *cli.ExecutionContext) *cobra.Command {
 	v := viper.New()
-	ec.Viper = v
 	migrateCmd := &cobra.Command{
 		Use:          "migrate",
 		Short:        "Manage migrations on the database",
 		SilenceUsage: true,
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			ec.Viper = v
 			err := ec.Prepare()
 			if err != nil {
 				return err
