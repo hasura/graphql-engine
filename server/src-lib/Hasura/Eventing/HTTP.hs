8e66df1cf49fda63c0f7e7e6f381a57af5647bc0
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2020-03-20 13:57:59 +0530

    add module documentation for Hasura.Eventing.HTTP

--- server/src-lib/Hasura/Eventing/HTTP.hs
+++ server/src-lib/Hasura/Eventing/HTTP.hs
@@ -1,3 +1,13 @@
+{-|
+  = Hasura.Eventing.HTTP
+
+  This module is an utility module providing HTTP utilities for Hasura.Eventing.EventTriggers and
+  Hasura.Eventing.ScheduledTriggers.
+
+  The EventTriggers and ScheduledTriggers share the event delivery mechanism using the `tryWebhook`
+  function defined in this module.
+
+-}
 module Hasura.Eventing.HTTP
   ( HTTPErr(..)
   , HTTPResp(..)
