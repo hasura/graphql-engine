f3bf2db67a777d6d118ec07433d834b78c80ad7b
lastnamearya <arya.jigyasu6815@gmail.com>
2020-03-21 18:46:17 +0530

    console: BredCrumb refactor to Functional & Icon Component integration

--- console/src/components/Common/Layout/BreadCrumb/BreadCrumb.js
+++ console/src/components/Common/Layout/BreadCrumb/BreadCrumb.js
@@ -2,48 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 
-class BreadCrumb extends React.Component {
-  render() {
-    const { breadCrumbs } = this.props;
-    const styles = require('../../TableCommon/Table.scss');
+import { Icon } from '../../../UIKit/atoms';
+import styles from '../../TableCommon/Table.scss';
 
-    let bC = null;
+const BreadCrumb = ({ breadCrumbs }) => {
+  let bC = null;
 
-    if (breadCrumbs && breadCrumbs.length > 0) {
-      bC = breadCrumbs.map((b, i) => {
-        let bCElem;
+  if (breadCrumbs && breadCrumbs.length > 0) {
+    bC = breadCrumbs.map((b, i) => {
+      let bCElem;
 
-        const Sp = () => {
-          const space = ' ';
-          return space;
-        };
+      const Sp = () => {
+        const space = ' ';
+        return space;
+      };
 
-        const addArrow = () => [
-          <Sp key={'breadcrumb-space-before' + i} />,
-          <i key={'l' + i} className="fa fa-angle-right" aria-hidden="true" />,
-          <Sp key={'breadcrumb-space-after' + i} />,
+      const addArrow = () => [
+        <Sp key={'breadcrumb-space-before' + i} />,
+        <Icon key={'l' + i} type="right" />,
+
+        <Sp key={'breadcrumb-space-after' + i} />,
+      ];
+
+      const isLastElem = i === breadCrumbs.length - 1;
+
+      if (!isLastElem) {
+        bCElem = [
+          <Link key={'l' + i} to={`${b.url}`}>
+            {b.title}
+          </Link>,
+          addArrow(),
         ];
+      } else {
+        bCElem = [b.title];
+      }
 
-        const isLastElem = i === breadCrumbs.length - 1;
-
-        if (!isLastElem) {
-          bCElem = [
-            <Link key={'l' + i} to={`${b.url}`}>
-              {b.title}
-            </Link>,
-            addArrow(),
-          ];
-        } else {
-          bCElem = [b.title];
-        }
-
-        return bCElem;
-      });
-    }
-
-    return <div className={styles.dataBreadCrumb}>You are here: {bC}</div>;
+      return bCElem;
+    });
   }
-}
+
+  return <div className={styles.dataBreadCrumb}>You are here: {bC}</div>;
+};
 
 BreadCrumb.propTypes = {
   breadCrumbs: PropTypes.array.isRequired,
