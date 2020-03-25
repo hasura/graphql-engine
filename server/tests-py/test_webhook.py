156fd340d67f1edae9400ecdb09d9349d097ae44
Antoine Leblanc <antoine@hasura.io>
2020-03-25 21:18:00 +0000

    tests: simple test for webhook connection expiry

---
+++ server/tests-py/test_webhook.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+import math
+import json
+import time
+import base64
+
+import ruamel.yaml as yaml
+import pytest
+from test_subscriptions import init_ws_conn
+from context import PytestConf
+
+
+if not PytestConf.config.getoption('--hge-webhook'):
+    pytest.skip('--hge-webhook is missing, skipping webhook expiration tests', allow_module_level=True)
+
+class TestSubscriptionWebhookExpiry(object):
+
+    def test_webhook_expiry_with_expires_header(self, hge_ctx, ws_client):
+        exp = datetime.now() + timedelta(seconds=2)
+        headers = {
+            'Expires': exp.strftime('%a, %d %b %Y %T GMT'),
+            'X-Hasura-Role': 'user',
+            'X-Hasura-User-Id': '1234321',
+            'X-Hasura-Auth-Mode': 'webhook',
+        }
+        token = base64.b64encode(json.dumps(headers).encode('utf-8')).decode('utf-8')
+        headers['Authorization'] = 'Bearer ' + token
+        init_ws_conn(hge_ctx, ws_client, {'headers': headers})
+        time.sleep(5)
+        assert ws_client.remote_closed == True, ws_client.remote_closed
