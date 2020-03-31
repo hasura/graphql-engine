3b39708ab5bf9006fc24ca1c9f267eb79f920fb9
Alexis King <lexi.lambda@gmail.com>
2020-03-30 19:16:50 -0500

    Remove dependency on string-conversions in favor of text-conversions
    string-conversions silently uses UTF8 instead of being explicit about
    it, and it uses lenientDecode when decoding ByteStrings when it’s
    usually better to reject invalid UTF8 input outright. text-conversions
    solves both those problems.

--- server/src-lib/Data/Parser/Expires.hs
+++ server/src-lib/Data/Parser/Expires.hs
@@ -3,9 +3,7 @@ module Data.Parser.Expires
   ) where
 
 import           Control.Monad.Except
-import           Data.Maybe              (maybe)
-import qualified Data.String.Conversions as CS
-import qualified Data.Text               as T
+import           Data.Text.Conversions
 import           Data.Time.Clock
 import           Data.Time.Format        (defaultTimeLocale, parseTimeM)
 
@@ -13,9 +11,7 @@ import           Hasura.Prelude
 
 
 -- | Extracts an absolute expiration time from a Expires header.
-parseExpirationTime :: MonadError String m => T.Text -> m UTCTime
-parseExpirationTime header = maybe (throwError errorMessage) return
-                           $ parseTimeM True defaultTimeLocale timeFormat
-                           $ CS.cs header
-  where timeFormat = "%a, %d %b %Y %T GMT"
-        errorMessage = "Value of Expires header is not a valid timestamp"
+parseExpirationTime :: MonadError String m => Text -> m UTCTime
+parseExpirationTime = fromText
+  >>> parseTimeM True defaultTimeLocale "%a, %d %b %Y %T GMT"
+  >>> (`onNothing` throwError "Value of Expires header is not a valid timestamp")
