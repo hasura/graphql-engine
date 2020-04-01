969592f0e60f345080a19a797cc50f528b73522e
Shahidh K Muhammed <shahidh@hasura.io>
2020-04-01 10:25:57 +0530

    tag release v1.2.0-beta.3

--- install-manifests/docker-run/docker-run.sh
+++ install-manifests/docker-run/docker-run.sh
@@ -2,4 +2,4 @@
 docker run -d -p 8080:8080 \
        -e HASURA_GRAPHQL_DATABASE_URL=postgres://username:password@hostname:port/dbname \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
-       hasura/graphql-engine:v1.1.1
+       hasura/graphql-engine:v1.2.0-beta.3
