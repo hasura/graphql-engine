ddf53fabfb23c86154d31c37fe33d6e94fef8184
Alexis King <lexi.lambda@gmail.com>
2020-04-01 15:16:26 -0500

    server: Rewrite/refactor JSONPath parser
    The previous implementation was written in a very explicitly “recursive
    descent” style, but the whole point of using attoparsec is to be able to
    backtrack! Taking advantage of the combinators makes for a much simpler
    parser.

--- server/src-lib/Data/Parser/JSONPath.hs
+++ server/src-lib/Data/Parser/JSONPath.hs
@@ -4,79 +4,47 @@ module Data.Parser.JSONPath
   , JSONPath
   ) where
 
-import           Control.Applicative  ((<|>))
+import           Prelude
+
+import qualified Data.Text            as T
+
+import           Control.Applicative
 import           Data.Aeson.Internal  (JSONPath, JSONPathElement (..))
 import           Data.Attoparsec.Text
-import           Data.Bool            (bool)
-import qualified Data.Text            as T
-import           Prelude              hiding (takeWhile)
-import           Text.Read            (readMaybe)
-
-
-parseKey :: Parser JSONPathElement
-parseKey = do
-  firstChar <- letter
-           <|> char '_'
-           <?> "the first character of property name must be a letter or underscore."
-  name <- many' (letter
-           <|> digit
-           <|> satisfy (`elem` ("-_" :: String))
-            )
-  return $ Key $ T.pack (firstChar:name)
-
--- parse json property key or index in squared bracket format
--- this parser support special characters
-parseBracketElement :: Parser JSONPathElement
-parseBracketElement = skip (== '[') *> anyChar >>= parseContent
-  where
-    parseContent :: Char -> Parser JSONPathElement
-    parseContent ']'  = fail "empty array index"
-    parseContent '\'' = parseTextContent '\''
-    parseContent '"'  = parseTextContent '"'
-    parseContent c    = parseIntContent c
-
-    parseTextContent :: Char -> Parser JSONPathElement
-    parseTextContent quote = do
-      remain <- many' (notChar quote)
-      skip (== quote)
-      skip (== ']')
-      return $ Key $ T.pack remain
-
-    parseIntContent :: Char -> Parser JSONPathElement
-    parseIntContent c = do
-      remain <- many' (notChar ']')
-      skip (== ']')
-      let content = c:remain
-      case (readMaybe content :: Maybe Int) of
-        Nothing -> fail $ "invalid array index: " ++ content
-        Just v  -> return $ Index v
-
-parseElement :: Parser JSONPathElement
-parseElement = do
-  dotLen <- T.length <$> takeWhile (== '.')
-  if dotLen > 1
-    then fail "multiple dots in json path"
-    else peekChar >>= \case
-      Nothing ->  fail "empty json path"
-      Just '[' -> parseBracketElement
-      _ -> parseKey
-
-parseElements :: Parser JSONPath
-parseElements = skipWhile (== '$') *> many1 parseElement
 
 parseJSONPath :: T.Text -> Either String JSONPath
-parseJSONPath = parseResult . parse parseElements
-  where
-    parseResult = \case
-      Fail _ pos err ->
-        Left $ bool (head pos) err (null pos)
-      Partial p ->  parseResult $ p ""
-      Done remain r ->
-        if not $ T.null remain then
-          Left $ invalidMessage remain
-        else
-          Right r
+parseJSONPath = parseOnly (optional (char '$') *> many1' element <* endOfInput)
 
-    invalidMessage s = "invalid property name: "  ++ T.unpack s
-      ++ "Accept letters, digits, underscore (_) or hyphen (-) only. "
-      ++ "You should use bracket notation (e.g ['Hello!']) for special characters"
+element :: Parser JSONPathElement
+element = Key <$> (optional (char '.') *> name)   -- field or .field
+      <|> bracketElement                          -- [42] or ['field']
+
+name :: Parser T.Text
+name = go <?> "property name" where
+  go = do
+    firstChar <- letter
+             <|> char '_'
+             <|> fail "first character of property name must be a letter or underscore"
+    otherChars <- many' (letter <|> digit <|> satisfy (inClass "-_"))
+    pure $ T.pack (firstChar:otherChars)
+
+-- | Parses a JSON property key or index in square bracket format, e.g.
+-- > [42]
+-- > ["hello"]
+-- > ['你好']
+bracketElement :: Parser JSONPathElement
+bracketElement = do
+  char '['
+  result <- Index <$> decimal
+        <|>   Key <$> quotedString '"'
+        <|>   Key <$> quotedString '\''
+  char ']'
+  pure result
+  where
+    quotedString delimiter = do
+      char delimiter
+      result <- T.pack <$> many' (charOrEscape delimiter)
+      char delimiter
+      pure result
+
+    charOrEscape delimiter = (char '\\' *> anyChar) <|> notChar delimiter
