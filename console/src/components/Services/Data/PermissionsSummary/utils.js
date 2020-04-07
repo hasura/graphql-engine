7580c1522fe67936e263381f96d8671551d5a552
Aleksandra Sikora <ola.zxcvbnm@gmail.com>
2020-04-07 20:12:53 +0200

    Remove redundant function

--- console/src/components/Services/Data/PermissionsSummary/utils.js
+++ console/src/components/Services/Data/PermissionsSummary/utils.js
@@ -1,3 +1,5 @@
+import { getDefaultFilterType } from '../TablePermissions/utils';
+
 export const getAllRoles = allTableSchemas => {
   const _allRoles = [];
 
@@ -26,17 +28,13 @@ export const getTablePermissionsByRoles = tableSchema => {
   return tablePermissionsByRoles;
 };
 
-const getQueryFilterKey = query => {
-  return query === 'insert' ? 'check' : 'filter';
-};
-
 export const getPermissionFilterString = (
   permission,
   query,
   pretty = false,
   filterType
 ) => {
-  const filterKey = filterType || getQueryFilterKey(query);
+  const filterKey = filterType || getDefaultFilterType(query);
 
   let filterString = '';
   if (permission && permission[filterKey]) {
