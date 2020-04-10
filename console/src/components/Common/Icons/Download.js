1769ba1c2b6d45b65d79343e7324320757400bfc
Rishichandra Wawhal <rishi@hasura.io>
2020-04-10 17:08:27 +0530

    remove unnecessary files

--- console/src/components/Common/Icons/Download.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Download = ({ className }) => {
-  return (
-    <i className={`fa fa-download ${className || ''}`} aria-hidden="true" />
-  );
-};
-
-export default Download;
--- console/src/components/Common/Icons/Github.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Github = ({ className }) => {
-  return <i className={`fa fa-github ${className || ''}`} aria-hidden="true" />;
-};
-
-export default Github;
