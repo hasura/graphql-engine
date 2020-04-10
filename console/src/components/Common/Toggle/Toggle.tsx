26482bb1b5f0a70d7ab913e2f835cfbef50ba4be
Rishichandra Wawhal <rishi@hasura.io>
2020-04-10 18:10:48 +0530

    remove unnecessary import, export Toggle as is

--- console/src/components/Common/Toggle/Toggle.tsx
+++ console/src/components/Common/Toggle/Toggle.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import Toggle, { ToggleProps } from 'react-toggle';
+import Toggle from 'react-toggle';
 import './Toggle.css';
 import 'react-toggle/style.css';
 
-const ToggleComponent = (props: ToggleProps) => <Toggle {...props} />;
-
-export default ToggleComponent;
+export default Toggle;
