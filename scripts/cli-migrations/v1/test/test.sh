fabd78dccc8588dd8b8938621763b28f36a0505b
Aravind Shankar <aravind@hasura.io>
2020-04-14 04:35:31 +0530

    WIP

---
+++ scripts/cli-migrations/v1/test/test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -evo pipefail
+IFS=$'\n\t'
+ROOT="$(readlink -f ${BASH_SOURCE[0]%/*}/../../)"
+
+fail_if_port_busy() {
+    local PORT=$1
+    if nc -z localhost $PORT ; then
+        echo "Port $PORT is busy. Exiting"
+        exit 1
+    fi
+}
+
+wait_for_port() {
+    local PORT=$1
+    echo "waiting for $PORT"
+    for _ in $(seq 1 60);
+    do
+      nc -z localhost $PORT && echo "port $PORT is ready" && return
+      echo -n .
+      sleep 0.2
+    done
+    echo "Failed waiting for $PORT" && exit 1
+}
+
+fail_if_port_busy 8080
+docker-compose up -d
+docker run --network container:server appropriate/curl --retry 10 --retry-connrefused http://localhost:8080/v1/version
+wait_for_port 8080
+docker-compose down -v
