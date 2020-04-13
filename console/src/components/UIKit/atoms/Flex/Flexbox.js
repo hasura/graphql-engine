822d9ad5084592862640179c71df555678a49186
lastnamearya <arya.jigyasu6815@gmail.com>
2020-04-14 03:45:27 +0530

    console: UIKit add - Flex component

---
+++ console/src/components/UIKit/atoms/Flex/Flexbox.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { flexbox } from 'styled-system';
+
+import { Box } from '../Box';
+
+export const Flex = styled(Box)`
+  ${flexbox}
+`;
+
+Flex.defaultProps = {
+  alignItems: 'center',
+  justifyContent: 'center'
+};
