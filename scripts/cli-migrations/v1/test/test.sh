4c96f38970c20d0ef9226e0779bac99dc8a919a6
Aravind Shankar <aravind@hasura.io>
2020-04-14 05:23:27 +0530

    WIP

--- scripts/cli-migrations/v1/test/test.sh
+++ scripts/cli-migrations/v1/test/test.sh
@@ -25,7 +25,8 @@ wait_for_port() {
 }
 
 fail_if_port_busy 8080
-set -x
-docker-compose up -d
-docker run --network container:server appropriate/curl --retry 10 --retry-delay 6 --retry-connrefused http://localhost:8080/v1/version
+docker-compose up --no-start graphql-engine
+docker cp ./migrations/. graphql-engine:/hasura-migrations
+docker-compose up -d --no-recreate graphql-engine
+docker run --network container:graphql-engine appropriate/curl --retry 10 --retry-delay 6 --retry-connrefused http://localhost:8080/v1/version
 docker-compose down -v
