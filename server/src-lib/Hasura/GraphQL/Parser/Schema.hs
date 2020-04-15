c7387b5e972e2aaac61712d4cbd8fc2e446818f8
Alexis King <lexi.lambda@gmail.com>
2020-04-15 11:50:56 -0500

    [skip ci] Add function to recursively collect types for introspection

--- server/src-lib/Hasura/GraphQL/Parser/Schema.hs
+++ server/src-lib/Hasura/GraphQL/Parser/Schema.hs
@@ -5,8 +5,11 @@ module Hasura.GraphQL.Parser.Schema where
 
 import           Hasura.Prelude
 
+import qualified Data.HashMap.Strict.Extended  as Map
+
 import           Control.Lens.Extended
 import           Control.Monad.Unique
+import           Data.Functor.Classes
 import           Data.Void
 import           Language.GraphQL.Draft.Syntax (Description (..), Name (..), Value (..))
 
@@ -73,6 +76,15 @@ data Type k
   = NonNullable (NonNullableType k)
   | Nullable (NonNullableType k)
 
+instance Eq (Type k) where
+  (==) = eqType
+
+-- | Like '==', but can compare 'Type's of different kinds.
+eqType :: Type k1 -> Type k2 -> Bool
+eqType (NonNullable a) (NonNullable b) = eqNonNullableType a b
+eqType (Nullable    a) (Nullable    b) = eqNonNullableType a b
+eqType _               _               = False
+
 instance HasName (Type k) where
   getName = getName . discardNullability
 
@@ -88,6 +100,15 @@ data NonNullableType k
   = TNamed (Definition (TypeInfo k))
   | TList (Type k)
 
+instance Eq (NonNullableType k) where
+  (==) = eqNonNullableType
+
+-- | Like '==', but can compare 'NonNullableType's of different kinds.
+eqNonNullableType :: NonNullableType k1 -> NonNullableType k2 -> Bool
+eqNonNullableType (TNamed a) (TNamed b) = liftEq eqTypeInfo a b
+eqNonNullableType (TList  a) (TList  b) = eqType a b
+eqNonNullableType _          _          = False
+
 instance HasName (NonNullableType k) where
   getName (TNamed definition) = getName definition
   getName (TList t)           = getName t
@@ -102,6 +123,17 @@ data TypeInfo k where
   TIInputObject :: [Definition (FieldInfo 'Input)] -> TypeInfo 'Input
   TIObject :: [Definition (FieldInfo 'Output)] -> TypeInfo 'Output
 
+instance Eq (TypeInfo k) where
+  (==) = eqTypeInfo
+
+-- | Like '==', but can compare 'TypeInfo's of different kinds.
+eqTypeInfo :: TypeInfo k1 -> TypeInfo k2 -> Bool
+eqTypeInfo TIScalar                TIScalar                = True
+eqTypeInfo (TIEnum values1)        (TIEnum values2)        = values1 == values2
+eqTypeInfo (TIInputObject fields1) (TIInputObject fields2) = fields1 == fields2
+eqTypeInfo (TIObject fields1)      (TIObject fields2)      = fields1 == fields2
+eqTypeInfo _                       _                       = False
+
 data Definition a = Definition
   { dName        :: Name
   , dUnique      :: Maybe Unique
@@ -114,10 +146,23 @@ data Definition a = Definition
   , dDescription :: Maybe Description
   , dInfo        :: ~a
   -- ^ Lazy to allow mutually-recursive type definitions.
-  }
+  } deriving (Functor)
 
 mkDefinition :: Name -> Maybe Description -> a -> Definition a
 mkDefinition name description info = Definition name Nothing description info
+
+instance Eq a => Eq (Definition a) where
+  (==) = eq1
+
+instance Eq1 Definition where
+  liftEq eq (Definition name1 maybeUnique1 _ info1)
+            (Definition name2 maybeUnique2 _ info2)
+    | Just unique1 <- maybeUnique1
+    , Just unique2 <- maybeUnique2
+    , unique1 == unique2
+    = True
+    | otherwise
+    = name1 == name2 && eq info1 info2
 
 instance HasName (Definition a) where
   getName = dName
@@ -137,6 +182,7 @@ addDefinitionUnique unique = over definitionLens \definition ->
 -- all definitions, so this is just a placeholder for use as @'Definition'
 -- 'EnumValueInfo'@.
 data EnumValueInfo = EnumValueInfo
+  deriving (Eq)
 
 data family FieldInfo (k :: Kind)
 
@@ -153,10 +199,18 @@ data instance FieldInfo 'Input
   -- completely absent; see <http://spec.graphql.org/June2018/#CoerceArgumentValues()>.
   | forall k. ('Input <: k) => IFOptional (NonNullableType k) (Maybe (Value Void))
 
+instance Eq (FieldInfo 'Input) where
+  IFRequired t1    == IFRequired t2    = eqNonNullableType t1 t2
+  IFOptional t1 v1 == IFOptional t2 v2 = eqNonNullableType t1 t2 && v1 == v2
+  _                == _                = False
+
 data instance FieldInfo 'Output = forall k. ('Output <: k) => FieldInfo
   { fArguments :: [Definition (FieldInfo 'Input)]
   , fType :: Type k
   }
+
+instance Eq (FieldInfo 'Output) where
+  FieldInfo args1 t1 == FieldInfo args2 t2 = args1 == args2 && eqType t1 t2
 
 data Variable = Variable
   { vDefinition :: Definition (FieldInfo 'Input)
@@ -167,3 +221,91 @@ data Variable = Variable
 
 instance HasName Variable where
   getName = getName . vDefinition
+
+-- -----------------------------------------------------------------------------
+-- support for introspection queries
+
+-- | This type contains all the information needed to efficiently serve GraphQL
+-- introspection queries. It corresponds to the GraphQL @__Schema@ type defined
+-- in <§ 4.5 Schema Introspection http://spec.graphql.org/June2018/#sec-Introspection>.
+data Schema = Schema
+  { sTypes            :: HashMap Name (Definition SomeTypeInfo)
+  , sQueryType        :: Definition (TypeInfo 'Output)
+  , sMutationType     :: Definition (TypeInfo 'Output)
+  , sSubscriptionType :: Definition (TypeInfo 'Output)
+  }
+
+data SomeTypeInfo = forall k. SomeTypeInfo (TypeInfo k)
+
+instance Eq SomeTypeInfo where
+  SomeTypeInfo a == SomeTypeInfo b = eqTypeInfo a b
+
+-- | Recursively collects all type definitions accessible from the given value.
+collectTypeDefinitions
+  :: (HasTypeDefinitions a, MonadError ConflictingDefinitions m)
+  => a -> m (HashMap Name (Definition SomeTypeInfo))
+collectTypeDefinitions = flip execStateT Map.empty . accumulateTypeDefinitions
+
+data ConflictingDefinitions
+  = ConflictingDefinitions (Definition SomeTypeInfo) (Definition SomeTypeInfo)
+
+class HasTypeDefinitions a where
+  -- | Recursively accumulates all type definitions accessible from the given
+  -- value. This is done statefully to avoid infinite loops arising from
+  -- recursive type definitions; see Note [Tying the knot] in Hasura.GraphQL.Parser.Class.
+  accumulateTypeDefinitions
+    :: ( MonadError ConflictingDefinitions m
+       , MonadState (HashMap Name (Definition SomeTypeInfo)) m )
+    => a -> m ()
+
+instance HasTypeDefinitions (Definition (TypeInfo k)) where
+  accumulateTypeDefinitions definition = do
+    -- This is the important case! We actually have a type definition, so we
+    -- need to add it to the state.
+    definitions <- get
+    let new = SomeTypeInfo <$> definition
+    case Map.lookup (dName new) definitions of
+      Nothing -> do
+        put $! Map.insert (dName new) new definitions
+        -- This type definition might reference other type definitions, so we
+        -- still need to recur.
+        accumulateTypeDefinitions (dInfo definition)
+      Just old
+        -- It’s important we /don’t/ recur if we’ve already seen this definition
+        -- before to avoid infinite loops; see Note [Tying the knot] in Hasura.GraphQL.Parser.Class.
+        | old == new -> pure ()
+        | otherwise  -> throwError $ ConflictingDefinitions old new
+
+instance HasTypeDefinitions a => HasTypeDefinitions [a] where
+  accumulateTypeDefinitions = traverse_ accumulateTypeDefinitions
+
+instance HasTypeDefinitions (Type k) where
+  accumulateTypeDefinitions = \case
+    NonNullable t -> accumulateTypeDefinitions t
+    Nullable    t -> accumulateTypeDefinitions t
+
+instance HasTypeDefinitions (NonNullableType k) where
+  accumulateTypeDefinitions = \case
+    TNamed d -> accumulateTypeDefinitions d
+    TList  t -> accumulateTypeDefinitions t
+
+instance HasTypeDefinitions (TypeInfo k) where
+  accumulateTypeDefinitions = \case
+    TIScalar             -> pure ()
+    TIEnum _             -> pure ()
+    TIInputObject fields -> accumulateTypeDefinitions fields
+    TIObject fields      -> accumulateTypeDefinitions fields
+
+instance HasTypeDefinitions (FieldInfo k)
+      => HasTypeDefinitions (Definition (FieldInfo k)) where
+  accumulateTypeDefinitions = accumulateTypeDefinitions . dInfo
+
+instance HasTypeDefinitions (FieldInfo 'Input) where
+  accumulateTypeDefinitions = \case
+    IFRequired t   -> accumulateTypeDefinitions t
+    IFOptional t _ -> accumulateTypeDefinitions t
+
+instance HasTypeDefinitions (FieldInfo 'Output) where
+  accumulateTypeDefinitions (FieldInfo args t) = do
+    accumulateTypeDefinitions args
+    accumulateTypeDefinitions t
