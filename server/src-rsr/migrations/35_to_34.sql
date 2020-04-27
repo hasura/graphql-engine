7d1f3a66af829e8a1efc0d763fad86acf727e5c9
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2020-04-27 15:37:32 +0530

    delete the ST related tables while downgrading

--- server/src-rsr/migrations/35_to_34.sql
+++ server/src-rsr/migrations/35_to_34.sql
@@ -2,3 +2,11 @@ DROP VIEW hdb_catalog.hdb_scheduled_events_stats;
 DROP TABLE hdb_catalog.hdb_scheduled_event_invocation_logs;
 DROP TABLE hdb_catalog.hdb_scheduled_events;
 DROP TABLE hdb_catalog.hdb_scheduled_trigger;
+
+DELETE FROM hdb_catalog.hdb_relationship
+where table_schema = 'hdb_catalog' and
+table_name in ('hdb_scheduled_event_invocation_logs','hdb_scheduled_events','hdb_scheduled_trigger');
+
+DELETE FROM hdb_catalog.hdb_table
+where table_schema = 'hdb_catalog' and
+table_name in ('hdb_scheduled_event_invocation_logs','hdb_scheduled_events','hdb_scheduled_trigger');
