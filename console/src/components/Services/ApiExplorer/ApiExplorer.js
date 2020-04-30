97e8d0a204a0009e451245e097c842f223423ac5
lastnamearya <arya.jigyasu6815@gmail.com>
2020-05-01 01:15:17 +0530

    console: change ApiExplorer to functional component

--- console/src/components/Services/ApiExplorer/ApiExplorer.js
+++ console/src/components/Services/ApiExplorer/ApiExplorer.js
@@ -1,101 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
 import ApiRequestWrapper from './ApiRequestWrapper';
-
 import globals from '../../../Globals';
+import styles from './ApiExplorer.scss';
 
-/*
-import ApiCollectionPanel from './ApiCollectionPanel';
+const ApiExplorer = props => {
+  const {
+    displayedApi,
+    credentials,
+    explorerData,
+    route,
+    dataHeaders,
+    tables,
+    headerFocus,
+    location,
+    serverVersion,
+    serverConfig,
+    dispatch,
+  } = props;
 
-import {
-  changeTabSelection,
-  changeApiSelection,
-  expandAuthApi,
-  clearHistory,
-  // changeRequestParams,
-} from './Actions';
+  const consoleUrl =
+    window.location.protocol + '//' + window.location.host + globals.urlPrefix;
 
-import {triggerOnBoarding} from '../Main/Actions';
-*/
-
-class ApiExplorer extends Component {
-  /*
-  onTabSelectionChanged = tabIndex => {
-    this.props.dispatch(changeTabSelection(tabIndex));
-  };
-
-  onApiSelectionChanged = (selectedApi, authApiExpanded) => {
-    this.props.dispatch(changeApiSelection(selectedApi, authApiExpanded));
-  };
-
-  onAuthApiExpanded = index => {
-    this.props.dispatch(expandAuthApi(index));
-  };
-
-  onClearHistoryClicked = () => {
-    this.props.dispatch(clearHistory());
-  };
-
-  getDQBQuery(propsObj) {
-    const { type, args } = propsObj;
-    const _query = {};
-    _query.type = type;
-    _query.args = JSON.parse(JSON.stringify(args));
-    return _query;
-  }
-
-  updateDQBState(data) {
-    this.props.dispatch(hydrateDQBData(data));
-  }
-  */
-
-  render() {
-    const {
-      displayedApi,
-      credentials,
-      explorerData,
-      route,
-      dataHeaders,
-      tables,
-      headerFocus,
-      location,
-      serverVersion,
-      serverConfig,
-    } = this.props;
-
-    const styles = require('./ApiExplorer.scss');
-    const consoleUrl =
-      window.location.protocol +
-      '//' +
-      window.location.host +
-      globals.urlPrefix;
-
-    return (
-      <div className={'container-fluid ' + styles.padd_remove}>
-        <Helmet title="API Explorer | Hasura" />
-        <div className={styles.apiExplorerWrapper}>
-          <ApiRequestWrapper
-            dispatch={this.props.dispatch}
-            credentials={credentials}
-            explorerData={explorerData}
-            details={displayedApi.details}
-            request={displayedApi.request}
-            route={route}
-            dataHeaders={dataHeaders}
-            numberOfTables={tables.length}
-            headerFocus={headerFocus}
-            urlParams={location.query}
-            serverVersion={serverVersion}
-            consoleUrl={consoleUrl}
-            serverConfig={serverConfig}
-          />
-        </div>
+  return (
+    <div className={'container-fluid ' + styles.padd_remove}>
+      <Helmet title="API Explorer | Hasura" />
+      <div className={styles.apiExplorerWrapper}>
+        <ApiRequestWrapper
+          dispatch={dispatch}
+          credentials={credentials}
+          explorerData={explorerData}
+          details={displayedApi.details}
+          request={displayedApi.request}
+          route={route}
+          dataHeaders={dataHeaders}
+          numberOfTables={tables.length}
+          headerFocus={headerFocus}
+          urlParams={location.query}
+          serverVersion={serverVersion}
+          consoleUrl={consoleUrl}
+          serverConfig={serverConfig}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 ApiExplorer.propTypes = {
   modalState: PropTypes.object.isRequired,
