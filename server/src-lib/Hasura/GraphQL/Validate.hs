78ef8931662d19f66738a8d11f606f55ebd8505d
Auke Booij <auke@hasura.io>
2020-04-30 14:43:48 +0200

    Slightly saner comment formatting

--- server/src-lib/Hasura/GraphQL/Validate.hs
+++ server/src-lib/Hasura/GraphQL/Validate.hs
@@ -174,9 +174,8 @@ validateGQ (QueryParts opDef opRoot fragDefsL varValsM) = do
       case selSet of
         Seq.Empty       -> throw500 "empty selset for subscription"
         (_ Seq.:<| rst) -> do
-          -- As an internal testing feature, we support subscribing to
-          -- multiple selection sets.  First check if the
-          -- corresponding directive is set.
+          -- As an internal testing feature, we support subscribing to multiple
+          -- selection sets.  First check if the corresponding directive is set.
           let multipleAllowed = elem (G.Directive "_multiple_top_level_fields" []) (G._todDirectives opDef)
           unless (multipleAllowed || null rst) $
             throwVE "subscriptions must select one top level field"
