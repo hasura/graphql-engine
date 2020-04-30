78741c136980c974241d46b40954bc1f03a2725b
lastnamearya <arya.jigyasu6815@gmail.com>
2020-05-01 02:12:17 +0530

    console: change TextAreaWithCopy to functional component

--- console/src/components/Common/TextAreaWithCopy/TextAreaWithCopy.js
+++ console/src/components/Common/TextAreaWithCopy/TextAreaWithCopy.js
@@ -3,12 +3,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Icon } from '../../UIKit/atoms';
+import style from './TextAreaWithCopy.scss';
 
-class TextAreaWithCopy extends React.Component {
-  copyToClip(id, e) {
+const TextAreaWithCopy = props => {
+  const { copyText, textLanguage, containerId, toolTipClass } = props;
+
+  const copyToClip = (id, e) => {
     e.preventDefault();
-
-    const { copyText, textLanguage, containerId } = this.props;
 
     let text = '';
     if (copyText.length > 0) {
@@ -50,111 +51,99 @@ class TextAreaWithCopy extends React.Component {
     }
 
     appendLoc.removeChild(textArea);
-  }
+  };
 
-  resetCopy(id) {
+  const resetCopy = id => {
     const tooltip = document.getElementById(id);
     tooltip.innerHTML = 'Copy';
-  }
+  };
 
-  render() {
-    const style = require('./TextAreaWithCopy.scss');
+  const renderSimpleValue = () => {
+    return (
+      <pre className={style.schemaPreWrapper}>
+        <code className={style.formattedCode}>{copyText}</code>
+      </pre>
+    );
+  };
 
-    const {
-      copyText,
-      toolTipClass,
-      id,
-      containerId,
-      textLanguage,
-    } = this.props;
-
-    const renderSimpleValue = () => {
-      return (
-        <pre className={style.schemaPreWrapper}>
-          <code className={style.formattedCode}>{copyText}</code>
-        </pre>
-      );
-    };
-
-    const renderSQLValue = () => {
-      if (!window || !window.hljs || !window.sqlFormatter) {
-        return renderSimpleValue();
-      }
-
-      return (
-        <pre>
-          <code
-            className={style.formattedCode}
-            dangerouslySetInnerHTML={{
-              __html: window.hljs.highlight(
-                'sql',
-                window.sqlFormatter.format(copyText, { language: textLanguage })
-              ).value,
-            }}
-          />
-        </pre>
-      );
-    };
-
-    const renderJSONValue = () => {
-      if (!window || !window.hljs) {
-        return renderSimpleValue();
-      }
-
-      return (
-        <pre>
-          <code
-            className={style.formattedCode}
-            dangerouslySetInnerHTML={{
-              __html: window.hljs.highlight(
-                'json',
-                JSON.stringify(JSON.parse(copyText), null, 4)
-              ).value,
-            }}
-          />
-        </pre>
-      );
-    };
-
-    const getTypeRenderer = type => {
-      let typeRenderer;
-
-      switch (type) {
-        case 'sql':
-          typeRenderer = renderSQLValue;
-          break;
-        case 'json':
-          typeRenderer = renderJSONValue;
-          break;
-        default:
-          typeRenderer = renderSimpleValue;
-      }
-
-      return typeRenderer;
-    };
+  const renderSQLValue = () => {
+    if (!window || !window.hljs || !window.sqlFormatter) {
+      return renderSimpleValue();
+    }
 
     return (
-      <div className={style.codeBlockCustom} id={containerId}>
-        <div className={style.copyGenerated}>
-          <div className={style.copyTooltip}>
-            <span
-              className={toolTipClass ? toolTipClass : style.tooltiptext}
-              id={id}
-            >
-              Copy
-            </span>
-            <Icon
-              type="copy"
-              onClick={this.copyToClip.bind(this, id)}
-              onMouseLeave={this.resetCopy.bind(this, id)}
-            />
-          </div>
-        </div>
-        {getTypeRenderer(textLanguage)()}
-      </div>
+      <pre>
+        <code
+          className={style.formattedCode}
+          dangerouslySetInnerHTML={{
+            __html: window.hljs.highlight(
+              'sql',
+              window.sqlFormatter.format(copyText, { language: textLanguage })
+            ).value,
+          }}
+        />
+      </pre>
     );
-  }
-}
+  };
+
+  const renderJSONValue = () => {
+    if (!window || !window.hljs) {
+      return renderSimpleValue();
+    }
+
+    return (
+      <pre>
+        <code
+          className={style.formattedCode}
+          dangerouslySetInnerHTML={{
+            __html: window.hljs.highlight(
+              'json',
+              JSON.stringify(JSON.parse(copyText), null, 4)
+            ).value,
+          }}
+        />
+      </pre>
+    );
+  };
+
+  const getTypeRenderer = type => {
+    let typeRenderer;
+
+    switch (type) {
+      case 'sql':
+        typeRenderer = renderSQLValue;
+        break;
+      case 'json':
+        typeRenderer = renderJSONValue;
+        break;
+      default:
+        typeRenderer = renderSimpleValue;
+    }
+
+    return typeRenderer;
+  };
+
+  return (
+    <div className={style.codeBlockCustom} id={containerId}>
+      <div className={style.copyGenerated}>
+        <div className={style.copyTooltip}>
+          <span
+            className={toolTipClass ? toolTipClass : style.tooltiptext}
+            id={props.id}
+          >
+            Copy
+          </span>
+          <Icon
+            type="copy"
+            onClick={() => copyToClip(props.id)}
+            onMouseLeave={() => resetCopy(props.id)}
+          />
+        </div>
+      </div>
+      {getTypeRenderer(textLanguage)()}
+    </div>
+  );
+};
 
 TextAreaWithCopy.propTypes = {
   copyText: PropTypes.string.isRequired,
