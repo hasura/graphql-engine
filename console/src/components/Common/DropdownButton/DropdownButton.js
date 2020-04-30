6bb22ccb59a3648441f7032599615ea55b3d363c
lastnamearya <arya.jigyasu6815@gmail.com>
2020-05-01 01:07:48 +0530

    console: change DropButton to functional component

--- console/src/components/Common/DropdownButton/DropdownButton.js
+++ console/src/components/Common/DropdownButton/DropdownButton.js
@@ -5,62 +5,61 @@ import InputGroup from 'react-bootstrap/lib/InputGroup';
 import DropdownButton from 'react-bootstrap/lib/DropdownButton';
 import MenuItem from 'react-bootstrap/lib/MenuItem';
 
-class DropButton extends React.Component {
-  render() {
-    const {
-      title,
-      dropdownOptions,
-      value,
-      required,
-      onInputChange,
-      onButtonChange,
-      dataKey,
-      dataIndex,
-      bsClass,
-      disabled,
-      inputVal,
-      inputPlaceHolder,
-      id,
-      testId,
-    } = this.props;
-    return (
-      <InputGroup className={bsClass}>
-        <DropdownButton
-          title={value || title}
-          componentClass={InputGroup.Button}
-          disabled={disabled}
-          id={id}
-          data-test={testId + '-' + 'dropdown-button'}
-        >
-          {dropdownOptions.map((d, i) => (
-            <MenuItem
-              data-index-id={dataIndex}
-              value={d.value}
-              onClick={onButtonChange}
-              eventKey={i + 1}
-              key={i}
-              data-test={testId + '-' + 'dropdown-item' + '-' + (i + 1)}
-            >
-              {d.display_text}
-            </MenuItem>
-          ))}
-        </DropdownButton>
-        <input
-          type="text"
-          data-key={dataKey}
-          data-index-id={dataIndex}
-          className={'form-control'}
-          required={required}
-          onChange={onInputChange}
-          disabled={disabled}
-          value={inputVal || ''}
-          placeholder={inputPlaceHolder}
-          data-test={testId + '-' + 'input'}
-        />
-      </InputGroup>
-    );
-  }
-}
+const DropButton = props => {
+  const {
+    title,
+    dropdownOptions,
+    value,
+    required,
+    onInputChange,
+    onButtonChange,
+    dataKey,
+    dataIndex,
+    bsClass,
+    disabled,
+    inputVal,
+    inputPlaceHolder,
+    id,
+    testId,
+  } = props;
+
+  return (
+    <InputGroup className={bsClass}>
+      <DropdownButton
+        title={value || title}
+        componentClass={InputGroup.Button}
+        disabled={disabled}
+        id={id}
+        data-test={testId + '-' + 'dropdown-button'}
+      >
+        {dropdownOptions.map((d, i) => (
+          <MenuItem
+            data-index-id={dataIndex}
+            value={d.value}
+            onClick={onButtonChange}
+            eventKey={i + 1}
+            key={i}
+            data-test={testId + '-' + 'dropdown-item' + '-' + (i + 1)}
+          >
+            {d.display_text}
+          </MenuItem>
+        ))}
+      </DropdownButton>
+      <input
+        type="text"
+        data-key={dataKey}
+        data-index-id={dataIndex}
+        className={'form-control'}
+        required={required}
+        onChange={onInputChange}
+        disabled={disabled}
+        value={inputVal || ''}
+        placeholder={inputPlaceHolder}
+        data-test={testId + '-' + 'input'}
+      />
+    </InputGroup>
+  );
+};
 
 DropButton.propTypes = {
   dispatch: PropTypes.func.isRequired,
