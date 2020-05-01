bb0d66779ed35d8d22546dfc367ca7246fa63124
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2020-05-01 19:31:52 +0530

    convert `makeRouteHooksSafe` module

--- console/src/helpers/makeRouteHooksSafe.ts
+++ console/src/helpers/makeRouteHooksSafe.ts
@@ -1,9 +1,11 @@
 import { createRoutes } from 'react-router/lib/RouteUtils';
+import { PlainRoute, EnterHook } from 'react-router';
 
 // Wrap the hooks so they don't fire if they're called before
 // the store is initialised. This only happens when doing the first
 // client render of a route that has an onEnter hook
-function makeHooksSafe(routes, store) {
+
+function makeHooksSafe(routes: PlainRoute[] | PlainRoute, store: any): any {
   if (Array.isArray(routes)) {
     return routes.map(route => makeHooksSafe(route, store));
   }
@@ -11,11 +13,12 @@ function makeHooksSafe(routes, store) {
   const onEnter = routes.onEnter;
 
   if (onEnter) {
-    routes.onEnter = function safeOnEnter(...args) {
+    // eslint-disable-next-line no-param-reassign
+    routes.onEnter = function safeOnEnter(...args: Parameters<EnterHook>) {
       try {
         store.getState();
       } catch (err) {
-        if (onEnter.length === 3) {
+        if (onEnter.length === 3 && args[2]) {
           args[2]();
         }
 
@@ -23,6 +26,7 @@ function makeHooksSafe(routes, store) {
         return;
       }
 
+      // eslint-disable-next-line prefer-spread
       onEnter.apply(null, args);
     };
   }
@@ -38,6 +42,6 @@ function makeHooksSafe(routes, store) {
   return routes;
 }
 
-export default function makeRouteHooksSafe(_getRoutes) {
-  return store => makeHooksSafe(createRoutes(_getRoutes(store)), store);
+export default function makeRouteHooksSafe(_getRoutes: any) {
+  return (store: any) => makeHooksSafe(createRoutes(_getRoutes(store)), store);
 }
