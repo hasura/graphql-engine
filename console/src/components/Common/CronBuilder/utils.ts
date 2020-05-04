97d9af44011768565c1cb0b99259bcafe9a81cb4
Rishichandra Wawhal <rishi@hasura.io>
2020-05-05 03:50:18 +0530

    remove cron generator dependencies

--- console/src/components/Common/CronBuilder/utils.ts
+++
@@ -1,7 +0,0 @@
-export const stripNonStandardElements = (cronExpression: string) => {
-  const elements = cronExpression.split(' ');
-  if (elements.length <= 5) {
-    return cronExpression;
-  }
-  return elements.join(' ');
-};
