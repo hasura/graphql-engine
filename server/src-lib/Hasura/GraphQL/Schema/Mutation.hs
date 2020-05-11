74f4325ffad32f44ae950cf4cd177a87f015c696
Antoine Leblanc <antoine@hasura.io>
2020-05-11 15:26:25 +0100

    [skip ci] fix and clean update mutation

--- server/src-lib/Hasura/GraphQL/Schema/Mutation.hs
+++ server/src-lib/Hasura/GraphQL/Schema/Mutation.hs
@@ -2,6 +2,7 @@
 
 module Hasura.GraphQL.Schema.Mutation
   ( updateTable
+  , updateTableByPk
   , deleteFromTable
   , deleteFromTableByPk
   ) where
@@ -46,44 +47,16 @@ updateTable
   -> UpdPermInfo          -- ^ update permissions of the table
   -> Bool
   -> m (Maybe (FieldsParser 'Output n (Maybe (RQL.AnnUpdG UnpreparedValue))))
-updateTable table fieldName description selectPerms updatePerms stringifyNum = do
+updateTable table fieldName description selectPerms updatePerms stringifyNum = runMaybeT $ do
   let whereName = $$(G.litName "where")
       whereDesc = G.Description "filter the rows which have to be updated"
-  whereArg  <- P.field whereName (Just whereDesc) <$> boolExp table selectPerms
-  opArgs    <- updateOperators table updatePerms
-  selection <- mutationSelectionSet table selectPerms stringifyNum
-  columns   <- tableSelectColumns table selectPerms
-  whenMaybe (not $ null columns) do
-    let argsParser = liftA2 (,) opArgs whereArg
-    pure $ P.selection fieldName description argsParser
-      (RQL.MOutMultirowFields <$> selection)
-      `P.bindFields` traverse \(_, (opExps, whereExp), mutationOutput) -> do
-        -- there needs to be at least one column in the update
-        when (null opExps) $ parseError $
-          "at lease one of " <> (T.intercalate ", " allOperators) <> " is expected"
-
-        -- no column should appear twice
-        let groupedExps   = L.groupBy ((==) `on` fst) $ sortOn (pgiName . fst) opExps
-            erroneousExps = concat $ filter ((> 1) . length) groupedExps
-        when (not $ null erroneousExps) $ parseError $ "column found in multiple operators; "
-          <> (T.intercalate ", " $ flip map erroneousExps \(columnInfo, opExp) ->
-                 G.unName (pgiName columnInfo) <> " in " <> RQL.updateOperatorText opExp)
-
-        -- FIXME: validate JSON parsing here?
-        pure $ RQL.AnnUpd { RQL.uqp1Table   = table
-                          , RQL.uqp1OpExps  = opExps
-                          , RQL.uqp1Where   = (permissionFilter, whereExp)
-                          , RQL.uqp1Check   = checkExp
-                          , RQL.uqp1Output  = mutationOutput
-                          -- TODO: is this correct?
-                          -- I'm only passing the columns that the user has SELECT access to
-                          -- while the code suggests that this should be *ALL* columns
-                          , RQL.uqp1AllCols = columns
-                          }
-  where
-    permissionFilter = fmapAnnBoolExp partialSQLExpToUnpreparedValue $ upiFilter updatePerms
-    checkExp = maybe annBoolExpTrue (fmapAnnBoolExp partialSQLExpToUnpreparedValue) $ upiCheck updatePerms
-
+  opArgs    <- MaybeT $ updateOperators table updatePerms
+  columns   <- lift $ tableSelectColumns table selectPerms
+  whereArg  <- lift $ P.field whereName (Just whereDesc) <$> boolExp table selectPerms
+  selection <- lift $ mutationSelectionSet table selectPerms stringifyNum
+  let argsParser = liftA2 (,) opArgs whereArg
+  pure $ P.selection fieldName description argsParser selection
+    `mapField` (mkUpdateObject table columns updatePerms . third RQL.MOutMultirowFields)
 
 updateTableByPk
   :: forall m n. (MonadSchema n m, MonadError QErr m)
@@ -93,33 +66,55 @@ updateTableByPk
   -> SelPermInfo          -- ^ select permissions of the table
   -> UpdPermInfo          -- ^ update permissions of the table
   -> Bool
-  -> m (FieldsParser 'Output n (Maybe (RQL.AnnUpdG UnpreparedValue)))
-updateTableByPk = undefined
+  -> m (Maybe (FieldsParser 'Output n (Maybe (RQL.AnnUpdG UnpreparedValue))))
+updateTableByPk table fieldName description selectPerms updatePerms stringifyNum = runMaybeT $ do
+  columns   <- lift   $ tableSelectColumns table selectPerms
+  pkArgs    <- MaybeT $ primaryKeysArguments table selectPerms
+  opArgs    <- MaybeT $ updateOperators table updatePerms
+  selection <- lift   $ tableFields table selectPerms stringifyNum
+  let argsParser = liftA2 (,) opArgs pkArgs
+  pure $ P.selection fieldName description argsParser selection
+    `mapField` (mkUpdateObject table columns updatePerms . third RQL.MOutSinglerowObject)
 
+mkUpdateObject
+  :: QualifiedTable
+  -> [PGColumnInfo]
+  -> UpdPermInfo
+  -> ( G.Name
+     , ( [(PGColumnInfo, RQL.UpdOpExpG UnpreparedValue)]
+       , AnnBoolExp UnpreparedValue
+       )
+     , RQL.MutationOutputG UnpreparedValue
+     )
+  -> RQL.AnnUpdG UnpreparedValue
+mkUpdateObject table columns updatePerms (_, (opExps, whereExp), mutationOutput) =
+  RQL.AnnUpd { RQL.uqp1Table   = table
+             , RQL.uqp1OpExps  = opExps
+             , RQL.uqp1Where   = (permissionFilter, whereExp)
+             , RQL.uqp1Check   = checkExp
+             , RQL.uqp1Output  = mutationOutput
+             -- TODO: is this correct?
+             -- I'm only passing the columns that the user has SELECT access to
+             -- while the code suggests that this should be *ALL* columns.
+             -- Is it ok for that list to be empty?
+             , RQL.uqp1AllCols = columns
+             }
+  where
+    permissionFilter = fmapAnnBoolExp partialSQLExpToUnpreparedValue $ upiFilter updatePerms
+    checkExp = maybe annBoolExpTrue (fmapAnnBoolExp partialSQLExpToUnpreparedValue) $ upiCheck updatePerms
 
--- FIXME: should this be somewhere else? Should it be merged with the
--- second list below?
-allOperators :: [Text]
-allOperators = [ "_set"
-               , "_inc"
-               , "_prepend"
-               , "_append"
-               , "_delete_key"
-               , "_delete_elem"
-               , "_delete_path_at"
-               ]
 
 updateOperators
   :: forall m n. (MonadSchema n m, MonadError QErr m)
   => QualifiedTable       -- ^ qualified name of the table
   -> UpdPermInfo          -- ^ update permissions of the table
-  -> m (FieldsParser 'Input n [(PGColumnInfo, RQL.UpdOpExpG UnpreparedValue)])
+  -> m (Maybe (FieldsParser 'Input n [(PGColumnInfo, RQL.UpdOpExpG UnpreparedValue)]))
 updateOperators table updatePermissions = do
   tableName <- qualifiedObjectToName table
   columns   <- tableUpdateColumns table updatePermissions
   let intCols  = onlyIntCols   columns
       jsonCols = onlyJSONBCols columns
-  fmap (concat . catMaybes) . sequenceA . catMaybes <$> sequenceA
+  parsers <- catMaybes <$> sequenceA
     [ updateOperator tableName $$(G.litName "_set")
         columnParser RQL.UpdSet columns
         (G.Description "sets the columns of the filtered rows to the given values")
@@ -155,6 +150,21 @@ updateOperators table updatePermissions = do
         (G.Description "delete the field or element with specified path (for JSON arrays, negative integers count from the end)")
         (G.Description "delete the field or element with specified path (for JSON arrays, negative integers count from the end)")
     ]
+  whenMaybe (not $ null parsers) do
+    let allowedOperators = fst <$> parsers
+    pure $ fmap (concat . catMaybes) (sequenceA $ snd <$> parsers)
+      `P.bindFields` \opExps -> do
+        -- there needs to be at least one column in the update
+        when (null opExps) $ parseError $
+          "at lease one of " <> (T.intercalate ", " allowedOperators) <> " is expected"
+        -- no column should appear twice
+        let groupedExps   = L.groupBy ((==) `on` fst) $ sortOn (pgiName . fst) opExps
+            erroneousExps = concat $ filter ((> 1) . length) groupedExps
+        when (not $ null erroneousExps) $ parseError $ "column found in multiple operators; "
+          <> (T.intercalate ", " $ flip map erroneousExps \(columnInfo, opExp) ->
+                 G.unName (pgiName columnInfo) <> " in " <> RQL.updateOperatorText opExp)
+        -- FIXME: validate JSON parsing here?
+        pure opExps
   where
     columnParser columnInfo = fmap P.mkParameter <$> P.column (pgiType columnInfo) (G.Nullability $ pgiIsNullable columnInfo)
     textParser   _          = fmap P.mkParameter <$> P.column (PGColumnScalar PGText)    (G.Nullability False)
@@ -168,9 +178,9 @@ updateOperators table updatePermissions = do
       -> [PGColumnInfo]
       -> G.Description
       -> G.Description
-      -> m (Maybe (FieldsParser 'Input n (Maybe [(PGColumnInfo, RQL.UpdOpExpG UnpreparedValue)])))
+      -> m (Maybe (Text, FieldsParser 'Input n (Maybe [(PGColumnInfo, RQL.UpdOpExpG UnpreparedValue)])))
     updateOperator tableName opName mkParser updOpExp columns opDesc objDesc =
-      if null columns then pure Nothing else do
+      whenMaybe (not $ null columns) do
         fields <- for columns \columnInfo -> do
           let fieldName = pgiName columnInfo
               fieldDesc = pgiDescription columnInfo
@@ -178,9 +188,10 @@ updateOperators table updatePermissions = do
           pure $ P.fieldOptional fieldName fieldDesc fieldParser
             `mapField` \value -> (columnInfo, updOpExp value)
         let objName = tableName <> opName <> $$(G.litName "_input")
-        pure $ Just $ P.fieldOptional opName (Just opDesc)
-                    $ P.object objName (Just objDesc)
-                    $ catMaybes <$> sequenceA fields
+        pure $ (G.unName opName,)
+             $ P.fieldOptional opName (Just opDesc)
+             $ P.object objName (Just objDesc)
+             $ catMaybes <$> sequenceA fields
 
 
 
@@ -201,9 +212,8 @@ deleteFromTable table fieldName description selectPerms deletePerms stringifyNum
   whereArg  <- P.field whereName (Just whereDesc) <$> boolExp table selectPerms
   selection <- mutationSelectionSet table selectPerms stringifyNum
   columns   <- tableSelectColumns table selectPerms
-  pure $ P.selection fieldName description whereArg (RQL.MOutMultirowFields <$> selection)
-    `mapField` mkDeleteObject table columns deletePerms
-
+  pure $ P.selection fieldName description whereArg selection
+    `mapField` (mkDeleteObject table columns deletePerms . third RQL.MOutMultirowFields)
 
 deleteFromTableByPk
   :: forall m n. (MonadSchema n m, MonadError QErr m)
@@ -214,33 +224,12 @@ deleteFromTableByPk
   -> DelPermInfo          -- ^ delete permissions of the table
   -> Bool
   -> m (Maybe (FieldsParser 'Output n (Maybe (RQL.AnnDelG UnpreparedValue))))
-deleteFromTableByPk table fieldName description selectPerms deletePerms stringifyNum = do
-  tablePrimaryKeys <- _tciPrimaryKey . _tiCoreInfo <$> askTableInfo table
-  tableColumns     <- tableSelectColumns table selectPerms
-  join <$> for tablePrimaryKeys \(_pkColumns -> columns) -> do
-    if any (\c -> not $ pgiColumn c `Set.member` spiCols selectPerms) columns
-    then pure Nothing
-    else do
-      argsParser <- fmap (BoolAnd . toList) . sequenceA <$> for columns \columnInfo -> do
-        field <- P.column (pgiType columnInfo) (G.Nullability False)
-        pure $ BoolFld . AVCol columnInfo . pure . AEQ True . mkParameter <$>
-          P.field (pgiName columnInfo) (pgiDescription columnInfo) field
-      selectionSetParser <- tableFields table selectPerms stringifyNum
-      pure $ Just $ P.selection fieldName description argsParser selectionSetParser
-        `mapField` mkDel tableColumns
-  -- WIP NOTE: I don't really like mapField and <&> to transform (Fields)Parsers...
-  -- Some of them will go away if we harmonize string types, as a lot of it is just
-  -- G.Name -> FieldName
-  -- But a lot of those functions do end up applying the same kind of functions on
-  -- the same kind of parsers, and I think there's an opportunity for some helper
-  -- functions / operators to be introduced when the code is mostly finalized,
-  -- before review.
-  -- I'd like to push as much of the piping out of those functions as possible,
-  -- to make them more readable...
-  where
-    mkDel columns (name, whereExpr, annotatedFields) = mkDeleteObject table columns deletePerms
-      (name, whereExpr, RQL.MOutMultirowFields [(FieldName $ G.unName name, RQL.MRet annotatedFields)])
-
+deleteFromTableByPk table fieldName description selectPerms deletePerms stringifyNum = runMaybeT $ do
+  columns   <- lift   $ tableSelectColumns table selectPerms
+  pkArgs    <- MaybeT $ primaryKeysArguments table selectPerms
+  selection <- lift   $ tableFields table selectPerms stringifyNum
+  pure $ P.selection fieldName description pkArgs selection
+    `mapField` (mkDeleteObject table columns deletePerms . third RQL.MOutSinglerowObject)
 
 mkDeleteObject
   :: QualifiedTable
@@ -248,17 +237,18 @@ mkDeleteObject
   -> DelPermInfo
   -> (G.Name, AnnBoolExp UnpreparedValue, RQL.MutationOutputG UnpreparedValue)
   -> RQL.AnnDelG UnpreparedValue
-mkDeleteObject table columns delPerms (_, whereExp, mutationOutput) =
+mkDeleteObject table columns deletePerms (_, whereExp, mutationOutput) =
   RQL.AnnDel { RQL.dqp1Table   = table
              , RQL.dqp1Where   = (permissionFilter, whereExp)
              , RQL.dqp1Output  = mutationOutput
              -- TODO: is this correct?
              -- I'm only passing the columns that the user has SELECT access to
-             -- while the code suggests that this should be *ALL* columns
+             -- while the code suggests that this should be *ALL* columns.
+             -- Is it ok for that list to be empty?
              , RQL.dqp1AllCols = columns
              }
   where
-    permissionFilter = fmapAnnBoolExp partialSQLExpToUnpreparedValue $ dpiFilter delPerms
+    permissionFilter = fmapAnnBoolExp partialSQLExpToUnpreparedValue $ dpiFilter deletePerms
 
 
 
@@ -270,9 +260,9 @@ mutationSelectionSet
   -> SelPermInfo
   -> Bool
   -> m (Parser 'Output n (RQL.MutFldsG UnpreparedValue))
-mutationSelectionSet table selectPermissions stringifyNum = do
+mutationSelectionSet table selectPerms stringifyNum = do
   tableName <- qualifiedObjectToName table
-  tableSet  <- tableFields table selectPermissions stringifyNum
+  tableSet  <- tableFields table selectPerms stringifyNum
   let affectedRowsName = $$(G.litName "affected_rows")
       affectedRowsDesc = G.Description "number of rows affected by the mutation"
       selectionName    = tableName <> $$(G.litName "_mutation_response")
@@ -288,3 +278,20 @@ mutationSelectionSet table selectPermissions stringifyNum = do
           `mapField` (FieldName . G.unName *** RQL.MRet)
         ]
   pure $ P.selectionSet selectionName (Just selectionDesc) typenameRepr selectionFields
+
+primaryKeysArguments
+  :: forall m n. (MonadSchema n m, MonadError QErr m)
+  => QualifiedTable
+  -> SelPermInfo
+  -> m (Maybe (FieldsParser 'Input n (AnnBoolExp UnpreparedValue)))
+primaryKeysArguments table selectPerms = runMaybeT $ do
+  primaryKeys <- MaybeT $ _tciPrimaryKey . _tiCoreInfo <$> askTableInfo table
+  let columns = _pkColumns primaryKeys
+  guard $ all (\c -> pgiColumn c `Set.member` spiCols selectPerms) columns
+  lift $ fmap (BoolAnd . toList) . sequenceA <$> for columns \columnInfo -> do
+    field <- P.column (pgiType columnInfo) (G.Nullability False)
+    pure $ BoolFld . AVCol columnInfo . pure . AEQ True . mkParameter <$>
+      P.field (pgiName columnInfo) (pgiDescription columnInfo) field
+
+third :: (c -> d) -> (a,b,c) -> (a,b,d)
+third f (a,b,c) = (a,b,f c)
