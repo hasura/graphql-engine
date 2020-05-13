e17e47ef8cdbd20c2bef3398c13d8526aee5fdc3
Auke Booij <auke@hasura.io>
2020-05-13 11:17:32 +0200

    server: avoid code duplication using type classes (#4624)
    There are two implementations of a Cache, namely a bounded and an
    unbounded variant.  This can be elegantly captured in a type class.
    In addition to reducing the amount of error-prone code in the
    definition of the cache, this version reduces the amount of
    error-prone code in usage sites of the cache, as it makes the cache
    into an abstract object, so that a calling site cannot distinguish
    between cache types.  Any decision about what should be cached should
    be made through the interface of a cache, rather than at the callsite,
    and this is captured by this variant.

--- server/src-lib/Hasura/Cache.hs
+++ server/src-lib/Hasura/Cache.hs
@@ -1,54 +1,18 @@
 module Hasura.Cache
-  ( Cache
-  , CacheOptions
-  , mkCacheOptions
-  , B.CacheSize
-  , B.mkCacheSize
-
+  ( module Hasura.Cache.Types
+  , B.CacheSize(..)
+  , B.parseCacheSize
   , initialise
-  , lookup
-  , insert
-  , clear
-  , getEntries
   ) where
 
 import           Hasura.Prelude         hiding (lookup)
 
+import           Hasura.Cache.Types
 import qualified Hasura.Cache.Bounded   as B
 import qualified Hasura.Cache.Unbounded as U
 
-data Cache k v
-  = CacheBounded !(B.BoundedCache k v)
-  | CacheUnbounded !(U.UnboundedCache k v)
-
-newtype CacheOptions
-  = CacheOptions (Maybe B.CacheSize)
-
-mkCacheOptions :: Maybe B.CacheSize -> CacheOptions
-mkCacheOptions = CacheOptions
-
-initialise :: CacheOptions -> IO (Cache k v)
-initialise (CacheOptions cacheSizeM) =
+initialise :: (Hashable k, Ord k) => Maybe B.CacheSize -> IO (Cache k v)
+initialise cacheSizeM =
   case cacheSizeM of
-    Nothing        -> CacheUnbounded <$> U.initialise
-    Just cacheSize -> CacheBounded <$> B.initialise cacheSize
-
-lookup :: (Hashable k, Ord k) => k -> Cache k v -> IO (Maybe v)
-lookup k = \case
-  CacheBounded cache   -> B.lookup k cache
-  CacheUnbounded cache -> U.lookup k cache
-
-insert :: (Hashable k, Ord k) => k -> v -> Cache k v -> IO ()
-insert k v = \case
-  CacheBounded cache   -> B.insert k v cache
-  CacheUnbounded cache -> U.insert k v cache
-
-clear :: Cache k v -> IO ()
-clear = \case
-  CacheBounded cache   -> B.clear cache
-  CacheUnbounded cache -> U.clear cache
-
-getEntries :: (Hashable k, Ord k) => Cache k v -> IO [[(k, v)]]
-getEntries = \case
-  CacheBounded cache   -> B.getEntries cache
-  CacheUnbounded cache -> U.getEntries cache
+    Nothing        -> Cache <$> U.initialise
+    Just cacheSize -> Cache <$> B.initialise cacheSize
