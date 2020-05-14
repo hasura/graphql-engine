0929f254794fba040a39190298c5d1d089248e87
Aleksandra Sikora <ola.zxcvbnm@gmail.com>
2020-05-14 22:18:59 +0200

    allow falsy values

--- console/src/components/Services/Data/Common/Components/TypedInput.js
+++ console/src/components/Services/Data/Common/Components/TypedInput.js
@@ -26,7 +26,7 @@ export const TypedInput = ({
   const isAutoIncrement = isColumnAutoIncrement(col);
   const placeHolder = hasDefault ? colDefault : getPlaceholder(colType);
   const getDefaultValue = () => {
-    if (prevValue) return prevValue;
+    if (prevValue !== undefined) return prevValue;
     if (clone && colName in clone) return clone[colName];
     return '';
   };
