ea3274ac516a5d5d15205bf1fa0213349171ea16
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2020-05-27 00:04:31 +0530

    delete from hdb_relationship and hdb_table in down migration

--- server/src-rsr/migrations/36_to_35.sql
+++ server/src-rsr/migrations/36_to_35.sql
@@ -1,1 +1,7 @@
 DROP TABLE hdb_catalog.hdb_remote_relationship;
+
+DELETE FROM hdb_catalog.hdb_relationship
+where table_schema = 'hdb_catalog' and table_name = 'hdb_remote_relationship';
+
+DELETE FROM hdb_catalog.hdb_table
+where table_schema = 'hdb_catalog' and table_name = 'hdb_remote_relationship';
