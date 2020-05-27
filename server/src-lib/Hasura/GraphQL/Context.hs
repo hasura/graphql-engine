c311201c6ea4a4affeebe1213ec4c38579065c80
Antoine Leblanc <antoine@hasura.io>
2020-05-27 19:00:39 +0100

    [skip ci] add MutationRootField

--- server/src-lib/Hasura/GraphQL/Context.hs
+++ server/src-lib/Hasura/GraphQL/Context.hs
@@ -4,6 +4,7 @@ module Hasura.GraphQL.Context
   ( GQLContext(..)
   , ParserFn
   , QueryRootField(..)
+  , MutationRootField(..)
   ) where
 
 import           Hasura.Prelude
@@ -13,9 +14,12 @@ import qualified Language.GraphQL.Draft.Syntax as G
 
 import           Data.HashMap.Strict.InsOrd    (InsOrdHashMap)
 
+import qualified Hasura.RQL.DML.Delete.Types   as RQL
 import qualified Hasura.RQL.DML.Select.Types   as RQL
+import qualified Hasura.RQL.DML.Update.Types   as RQL
 
 import           Hasura.GraphQL.Parser
+import           Hasura.GraphQL.Schema.Insert  (AnnMultiInsert)
 
 data GQLContext = GQLContext
   { gqlQueryParser :: ParserFn (InsOrdHashMap G.Name (QueryRootField UnpreparedValue))
@@ -35,6 +39,13 @@ data QueryRootField v
   | QRFPrimaryKey  (RQL.AnnSimpleSelG v)
   | QRFAggregation (RQL.AnnAggSelG    v)
   | QRFRaw         J.Value
+
+data MutationRootField v
+  = MRFInsert      (AnnMultiInsert v)
+  | MRFUpdate      (RQL.AnnUpdG    v)
+  | MRFDelete      (RQL.AnnDelG    v)
+  | MRFRaw         J.Value
+
 
 {-
 
