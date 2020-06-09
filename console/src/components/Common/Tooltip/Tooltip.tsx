c8a9145bace165142e4f394b919863dc01f394a4
soorajshankar <soorajshankar@users.noreply.github.com>
2020-06-09 20:40:26 +0530

    console: fix enum tables reload data button UI (close #4647) (#4877)

--- console/src/components/Common/Tooltip/Tooltip.tsx
+++ console/src/components/Common/Tooltip/Tooltip.tsx
@@ -10,19 +10,23 @@ export interface TooltipProps extends React.ComponentProps<'i'> {
   message: string;
   placement?: 'right' | 'left' | 'top' | 'bottom';
   className?: string;
+  tooltipStyle?: string;
 }
+
 const ToolTip: React.FC<TooltipProps> = ({
   message,
   placement = 'right',
+  tooltipStyle = '',
   children,
 }) => (
   <OverlayTrigger placement={placement} overlay={tooltipGen(message)}>
     {children || (
       <i
-        className={`fa fa-question-circle ${styles.tooltipIcon}`}
+        className={`fa fa-info-circle ${styles.tooltipIcon} ${tooltipStyle}`}
         aria-hidden="true"
       />
     )}
   </OverlayTrigger>
 );
+
 export default ToolTip;
