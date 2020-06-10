fb605d685ee9dcf669eb1d13d0f6e5f297ebd1d1
Sameer Kolhar <sameer@hasura.io>
2020-06-10 15:51:57 +0530

    fix formatting and resolve conflict

--- console/src/components/Services/Data/PermissionsSummary/Header.tsx
+++ console/src/components/Services/Data/PermissionsSummary/Header.tsx
@@ -2,64 +2,65 @@ import React from 'react';
 import styles from './PermissionsSummary.scss';
 
 type HeaderProps = {
-    content: string;
-    selectable: boolean;
-    isSelected: boolean;
-    onClick: () => void;
-    actionButtons: Array<HTMLButtonElement>;
-    key: string | null;
+  content: string;
+  selectable: boolean;
+  isSelected: boolean;
+  onClick: () => void;
+  actionButtons: Array<HTMLButtonElement>;
+  key: string | null;
 };
 
 type HeaderContentProps = {
-    content: string;
-    actionButtons: Array<HTMLButtonElement>;
+  content: string;
+  actionButtons: Array<HTMLButtonElement>;
 };
 
-const HeaderContent: React.FC<HeaderContentProps> = ({ content, actionButtons }) => {
-    let headerContent = <></>;
+const HeaderContent: React.FC<HeaderContentProps> = ({
+  content,
+  actionButtons,
+}) => {
+  let headerContent = <></>;
 
-    if (!actionButtons.length) {
-        headerContent = <>{content}</>;
-    } else {
-        headerContent = (
-            <div
-                className={`${styles.actionCell} ${styles.display_flex} ${styles.flex_space_between}`}
-            >
-                <div>{content}</div>
-                <div
-                    className={`${styles.tableHeaderActions} ${styles.display_flex}`}
-                >
-                    {actionButtons.map((actionButton, i) => (
-                        <div key={`${content}-action-btn-${i}`}>{actionButton}</div>
-                    ))}
-                </div>
-            </div>
-        );
-    }
+  if (!actionButtons.length) {
+    headerContent = <>{content}</>;
+  } else {
+    headerContent = (
+      <div
+        className={`${styles.actionCell} ${styles.display_flex} ${styles.flex_space_between}`}
+      >
+        <div>{content}</div>
+        <div className={`${styles.tableHeaderActions} ${styles.display_flex}`}>
+          {actionButtons.map((actionButton, i) => (
+            <div key={`${content}-action-btn-${i}`}>{actionButton}</div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 
-    return headerContent;
+  return headerContent;
 };
 
 const Header: React.FC<HeaderProps> = ({
-    content,
-    selectable,
-    isSelected,
-    onClick,
-    actionButtons,
-    key
+  content,
+  selectable,
+  isSelected,
+  onClick,
+  actionButtons,
+  key,
 }) => {
-    const selectableClassName = selectable ? styles.cursorPointer : "";
-    const isSelectedClassName = isSelected ? styles.selected : "";
+  const selectableClassName = selectable ? styles.cursorPointer : '';
+  const isSelectedClassName = isSelected ? styles.selected : '';
 
-    return (
-        <th
-            key={key || content}
-            onClick={selectable ? onClick : undefined}
-            className={`${selectableClassName} ${isSelectedClassName}`}
-        >
-            <HeaderContent content={content} actionButtons={actionButtons} />
-        </th>
-    );
+  return (
+    <th
+      key={key || content}
+      onClick={selectable ? onClick : undefined}
+      className={`${selectableClassName} ${isSelectedClassName}`}
+    >
+      <HeaderContent content={content} actionButtons={actionButtons} />
+    </th>
+  );
 };
 
 export default Header;
