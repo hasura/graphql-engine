fd99db9f2976220f66f607ae3074b01621df6829
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2020-06-11 22:40:38 +0530

    [skip ci] add parsers for a remote field

--- server/src-lib/Hasura/GraphQL/Schema/Remote.hs
+++ server/src-lib/Hasura/GraphQL/Schema/Remote.hs
@@ -3,43 +3,40 @@ module Hasura.GraphQL.Schema.Remote where
 import           Hasura.Prelude
 
 import           Language.GraphQL.Draft.Syntax       as G
-import qualified Language.GraphQL.Draft.Printer      as GP
-import qualified Language.GraphQL.Draft.Printer.Text as GPT
-import qualified Data.Aeson                          as J
-import qualified Data.HashMap.Strict                 as Map
-import qualified Data.HashMap.Strict.InsOrd          as OMap
-import qualified Data.Vector                         as V
+import qualified Data.List.NonEmpty                  as NE
 
 import           Hasura.GraphQL.Parser               as P
 
-import           Hasura.GraphQL.Parser          (FieldParser, Kind (..), Parser, Schema (..))
-import           Hasura.GraphQL.Parser.Class
-
--- IntrospectionResult
+import           Hasura.GraphQL.Parser          (FieldParser, Kind (..), Parser)
 
 remoteSchemaObject
   :: forall n
    . MonadParse n
   => SchemaDocument
-  -> ObjectTypeDefinition
+  -> G.ObjectTypeDefinition
   -> Parser 'Output n () -- (G.SelectionSet NoFragments Variable)
-remoteSchemaObject sdoc objType =
+remoteSchemaObject schemaDoc (G.ObjectTypeDefinition description name _ _ subFields) =
   let
-    name = G._otdName objType
-    description = G._otdDescription objType
-    subfields = G._otdFieldsDefinition objType -- [G.FieldDefinition], need [FieldParser n a]
     convert :: G.FieldDefinition -> FieldParser n ()
-    convert fieldDef =
-      -- TODO add arguments and directives
-      case G._fldType fieldDef of
+    convert (G.FieldDefinition _ _ argsDefinition gType _) =
+      -- TODO add directives
+      case gType of
         G.TypeNamed (Nullability True) fieldTypeName ->
-          remoteFieldFromName sdoc (G._fldName fieldDef) fieldTypeName
-        _ -> _todo
+          remoteFieldFromName schemaDoc name fieldTypeName argsDefinition
   in
     -- TODO return the parsed selection set.
-    () <$ (P.selectionSet name description $ map convert subfields)
+    () <$ (P.selectionSet name description $ map convert subFields)
 
-lookupType :: SchemaDocument -> G.Name -> Maybe TypeDefinition
+remoteSchemaInputObject
+  :: forall n
+  .  MonadParse n
+  => SchemaDocument
+  -> G.InputObjectTypeDefinition
+  -> Parser 'Input n ()
+remoteSchemaInputObject schemaDoc (G.InputObjectTypeDefinition desc name _ valueDefns) =
+  P.object name desc $ argumentsParser valueDefns schemaDoc
+
+lookupType :: SchemaDocument -> G.Name -> Maybe G.TypeDefinition
 lookupType (SchemaDocument types) name = find (\tp -> getNamedTyp tp == name) types
   where
     getNamedTyp :: G.TypeDefinition -> G.Name
@@ -57,38 +54,112 @@ remoteFieldFromName
   => SchemaDocument
   -> G.Name
   -> G.Name
+  -> G.ArgumentsDefinition
   -> FieldParser n ()
-remoteFieldFromName sdoc fieldName fieldTypeName =
+remoteFieldFromName sdoc fieldName fieldTypeName argsDefns =
   case lookupType sdoc fieldTypeName of
-    Nothing -> _todo
-    Just typeDef -> remoteField sdoc fieldName typeDef
+    Nothing -> _throwNameDoesNotExistError
+    Just typeDef -> remoteField sdoc fieldName argsDefns typeDef
+
+inputValueDefinitionParser
+  :: (MonadParse n)
+  => G.SchemaDocument
+  -> G.InputValueDefinition
+  -> InputFieldsParser n ()
+inputValueDefinitionParser schemaDoc (G.InputValueDefinition desc name fieldType maybeDefaultVal)  =
+  case maybeDefaultVal of
+    Nothing ->
+      case fieldType of
+        G.TypeNamed _ typeName ->
+          case lookupType schemaDoc typeName of
+            Nothing -> _throwNotFoundNameInSchemaDoc
+            Just typeDef ->
+              case typeDef of
+                G.TypeDefinitionScalar _ -> field name desc $ remoteFieldScalarParser name
+                G.TypeDefinitionEnum defn -> field name desc $ remoteFieldEnumParser name defn
+                G.TypeDefinitionObject _ -> _throwInvalidKindError
+                G.TypeDefinitionInputObject defn -> field name desc $ remoteSchemaInputObject schemaDoc defn
+                G.TypeDefinitionUnion _ -> _throwInvalidKindError
+                G.TypeDefinitionInterface _ -> _throwInvalidKindError
+        G.TypeList _ _ -> _todo
+    Just defaultVal ->
+      case fieldType of
+        G.TypeNamed _ typeName ->
+          case lookupType schemaDoc typeName of
+            Nothing -> _throwNotFoundNameInSchemaDoc
+            Just typeDef ->
+              case typeDef of
+                G.TypeDefinitionScalar _ -> fieldWithDefault name desc defaultVal $ remoteFieldScalarParser name
+                G.TypeDefinitionEnum defn -> fieldWithDefault name desc defaultVal $ remoteFieldEnumParser name defn
+                G.TypeDefinitionObject _ -> _throwInvalidKindError
+                G.TypeDefinitionInputObject defn -> fieldWithDefault name desc defaultVal $ remoteSchemaInputObject schemaDoc defn
+                G.TypeDefinitionUnion _ -> _throwInvalidKindError
+                G.TypeDefinitionInterface _ -> _throwInvalidKindError
+        G.TypeList _ _ -> _todo
+
+argumentsParser
+  :: (MonadParse n)
+  => G.ArgumentsDefinition
+  -> G.SchemaDocument
+  -> InputFieldsParser n ()
+argumentsParser args schemaDoc = do
+  () <$ traverse (inputValueDefinitionParser schemaDoc) args
 
 remoteField
   :: forall n
    . MonadParse n
   => SchemaDocument
   -> G.Name
-  -> TypeDefinition
-  -> FieldParser n () -- TODO return something useful, maybe?
-remoteField sdoc fieldName (TypeDefinitionObject objType) =
-  -- TODO add arguments and directivez
-  P.subselection_ fieldName Nothing (remoteSchemaObject sdoc objType)
+  -> G.ArgumentsDefinition
+  -> G.TypeDefinition
+  -> FieldParser n ()-- TODO return something useful, maybe?
+remoteField sdoc fieldName argsDefn typeDefn =
+  -- TODO add arguments and directives
+  let argsParser = argumentsParser argsDefn sdoc
+  in
+    case typeDefn of
+    G.TypeDefinitionObject objTypeDefn -> do
+      P.subselection fieldName (G._otdDescription objTypeDefn) argsParser $ remoteSchemaObject sdoc objTypeDefn
+      pure ()
+    G.TypeDefinitionScalar (G.ScalarTypeDefinition desc _ _) ->
+      P.selection fieldName desc argsParser $
+        case G.unName fieldName of
+          "Boolean" -> P.boolean $> ()
+          "Int" -> P.int $> ()
+          "Float" -> P.float $> ()
+          "String" -> P.string $> ()
+            --        _ -> P.selection_ fieldName desc $                          TODO: Unknown Scalar
 
-{-
-data FieldDefinition = FieldDefinition
-  { _fldDescription         :: Maybe Description
-  , _fldName                :: Name
-  , _fldArgumentsDefinition :: ArgumentsDefinition
-  , _fldType                :: GType
-  , _fldDirectives          :: [Directive Void]
-  } deriving (Ord, Show, Eq, Lift, Generic)
--}
+    G.TypeDefinitionEnum (G.EnumTypeDefinition desc _ _ valueDefns) ->
+      let enumValDefns = map (\(G.EnumValueDefinition enumDesc enumName _) ->
+                                ((mkDefinition (G.unEnumValue enumName) enumDesc EnumValueInfo),()))
+                         $ valueDefns
+      in
+        P.selection fieldName desc argsParser $ P.enum fieldName desc $ NE.fromList enumValDefns
 
-{-
-query {
-  requestWeather (city: "Berlin") {
-    temperature
-  }
-  currentTime
-}
--}
+    G.TypeDefinitionInputObject inpObjTypDefn@(G.InputObjectTypeDefinition desc name directives valueDefns) ->
+      -- not needed,as it is an input type, but handle it properly
+      _todo
+
+remoteFieldScalarParser
+  :: MonadParse n
+  => G.Name
+  -> Parser 'Both n ()
+remoteFieldScalarParser name =
+  case G.unName name of
+    "Boolean" -> P.boolean $> ()
+    "Int" -> P.int $> ()
+    "Float" -> P.float $> ()
+    "String" -> P.string $> ()
+    --        _ -> TODO: Unknown Scalar
+
+remoteFieldEnumParser
+  :: MonadParse n
+  => G.Name
+  -> G.EnumTypeDefinition
+  -> Parser 'Both n ()
+remoteFieldEnumParser name (G.EnumTypeDefinition desc _ _ valueDefns) =
+  let enumValDefns = map (\(G.EnumValueDefinition enumDesc enumName _) ->
+                            ((mkDefinition (G.unEnumValue enumName) enumDesc EnumValueInfo),()))
+                         $ valueDefns
+  in P.enum name desc $ NE.fromList enumValDefns
