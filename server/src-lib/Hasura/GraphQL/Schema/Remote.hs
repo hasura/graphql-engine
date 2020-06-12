d9280ad80affde0ceb6fe5a62225d126c2b454ba
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2020-06-12 20:53:43 +0530

    [skip ci] change the context of the parsers to be able to use memoizeOn

--- server/src-lib/Hasura/GraphQL/Schema/Remote.hs
+++ server/src-lib/Hasura/GraphQL/Schema/Remote.hs
@@ -9,69 +9,71 @@ import           Hasura.GraphQL.Parser               as P
 import qualified Hasura.GraphQL.Parser.Internal.Parser as P
 
 import           Hasura.GraphQL.Parser          (FieldParser, Kind (..), Parser)
+import qualified GHC.Base                            as B
 
 remoteFieldFullSchema
-  :: forall n
-   . MonadParse n
+  :: forall n m
+   . (MonadSchema n m, MonadError Text m)
   => SchemaDocument
   -> G.Name
-  -> Parser 'Output n (G.SelectionSet NoFragments Variable)
+  -> m (Parser 'Output n (G.SelectionSet NoFragments Variable))
 remoteFieldFullSchema sdoc name =
-  let fieldObjectType = case lookupType sdoc name of
-                          Just (G.TypeDefinitionObject o) -> o
-                          _ -> _errorNoSuchObjectInSchema
-      fieldParser = remoteSchemaObject sdoc fieldObjectType
-  in unsafeRawParser (pType fieldParser)
-
-{-
-query {
-  currentWeather (city:"berlin") {
-    temperature
-  }
-}
--}
+  P.memoizeOn 'remoteFieldFullSchema name do
+  fieldObjectType <-
+    case lookupType sdoc name of
+      Just (G.TypeDefinitionObject o) -> pure o
+      _ -> throwError $ "object with " <> G.unName name <> " not found"
+  fieldParser <- remoteSchemaObject sdoc fieldObjectType
+  pure $ P.unsafeRawParser (P.pType fieldParser)
 
 remoteSchemaObject
-  :: forall n
-   . MonadParse n
+  :: forall n m
+  . (MonadSchema n m, MonadError Text m)
   => SchemaDocument
   -> G.ObjectTypeDefinition
-  -> Parser 'Output n ()
-remoteSchemaObject schemaDoc (G.ObjectTypeDefinition description name _interfaces _directives subFields) =
+  -> m (Parser 'Output n ())
+remoteSchemaObject schemaDoc defn@(G.ObjectTypeDefinition description name _interfaces _directives subFields) =
+  P.memoizeOn 'remoteSchemaObject defn do
   let
-    convert :: G.FieldDefinition -> FieldParser n ()
+    convert :: G.FieldDefinition -> m (FieldParser n ())
     convert (G.FieldDefinition _ _ argsDefinition gType _) =
       let
         addNullableList :: FieldParser n () -> FieldParser n ()
-        addNullableList (P.FieldParser (Definition name desc un (FieldInfo args typ)) parser)
-          = P.FieldParser (Definition name desc un (FieldInfo args (Nullable (TList typ)))) parser
+        addNullableList (P.FieldParser (Definition name' desc un (FieldInfo args typ)) parser)
+          = P.FieldParser (Definition name' desc un (FieldInfo args (Nullable (TList typ)))) parser
+
         addNonNullableList :: FieldParser n () -> FieldParser n ()
-        addNonNullableList (P.FieldParser (Definition name desc un (FieldInfo args typ)) parser)
-          = P.FieldParser (Definition name desc un (FieldInfo args (NonNullable (TList typ)))) parser
+        addNonNullableList (P.FieldParser (Definition name' desc un (FieldInfo args typ)) parser)
+          = P.FieldParser (Definition name' desc un (FieldInfo args (NonNullable (TList typ)))) parser
+
         -- TODO add directives, deprecation
-        convertType :: G.GType -> FieldParser n ()
-        convertType gType' =
-          case gType' of
-            G.TypeNamed (Nullability True) fieldTypeName ->
-              P.nullableField $ remoteFieldFromName schemaDoc name fieldTypeName argsDefinition
-            G.TypeList (Nullability True) gType'' ->
-              addNullableList $ convertType gType''
-            G.TypeNamed (Nullability False) fieldTypeName ->
-              P.nonNullableField $ remoteFieldFromName schemaDoc name fieldTypeName argsDefinition
-            G.TypeList (Nullability False) gType'' ->
-              addNonNullableList $ convertType gType''
+        convertType :: G.GType -> m (FieldParser n ())
+        convertType gType' = do
+            case gType' of
+              G.TypeNamed (Nullability True) fieldTypeName -> do
+                remoteFld <- remoteFieldFromName schemaDoc name fieldTypeName argsDefinition
+                pure . P.nullableField $ remoteFld
+              G.TypeList (Nullability True) gType'' ->
+                pure .addNullableList =<< convertType gType''
+              G.TypeNamed (Nullability False) fieldTypeName -> do
+                remoteFld <- remoteFieldFromName schemaDoc name fieldTypeName argsDefinition
+                pure . P.nonNullableField $ remoteFld
+              G.TypeList (Nullability False) gType'' ->
+                pure . addNonNullableList =<< convertType gType''
       in convertType gType
-  in
-    () <$ (P.selectionSet name description $ map convert subFields)
+  subFieldParsers <- B.mapM convert subFields
+  pure $ () <$ (P.selectionSet name description subFieldParsers)
 
 remoteSchemaInputObject
-  :: forall n
-  .  MonadParse n
+  :: forall n m
+  .  (MonadSchema n m, MonadError Text m)
   => SchemaDocument
   -> G.InputObjectTypeDefinition
-  -> Parser 'Input n ()
-remoteSchemaInputObject schemaDoc (G.InputObjectTypeDefinition desc name _ valueDefns) =
-  P.object name desc $ argumentsParser valueDefns schemaDoc
+  -> m (Parser 'Input n ())
+remoteSchemaInputObject schemaDoc defn@(G.InputObjectTypeDefinition desc name _ valueDefns) =
+  P.memoizeOn 'remoteSchemaInputObject defn do
+  argsParser <- argumentsParser valueDefns schemaDoc
+  pure $ P.object name desc argsParser
 
 lookupType :: SchemaDocument -> G.Name -> Maybe G.TypeDefinition
 lookupType (SchemaDocument types) name = find (\tp -> getNamedTyp tp == name) types
@@ -86,24 +88,24 @@ lookupType (SchemaDocument types) name = find (\tp -> getNamedTyp tp == name) ty
       G.TypeDefinitionInputObject t -> G._iotdName t
 
 remoteFieldFromName
-  :: forall n
-   . MonadParse n
+  :: forall n m
+   . (MonadSchema n m, MonadError Text m)
   => SchemaDocument
   -> G.Name
   -> G.Name
   -> G.ArgumentsDefinition
-  -> FieldParser n ()
+  -> m (FieldParser n ())
 remoteFieldFromName sdoc fieldName fieldTypeName argsDefns =
   case lookupType sdoc fieldTypeName of
-    Nothing -> _throwNameDoesNotExistError
+    Nothing -> throwError $ "Could not find type with name " <> G.unName fieldName
     Just typeDef -> remoteField sdoc fieldName argsDefns typeDef
 
 inputValueDefinitionParser
-  :: forall n
-   . MonadParse n
+  :: forall n m
+   . (MonadSchema n m, MonadError Text m)
   => G.SchemaDocument
   -> G.InputValueDefinition
-  -> InputFieldsParser n ()
+  -> m (InputFieldsParser n ())
 inputValueDefinitionParser schemaDoc (G.InputValueDefinition desc name fieldType maybeDefaultVal) =
   let fieldConstructor :: forall k . 'Input <: k => Parser k n () -> InputFieldsParser n ()
       fieldConstructor = case maybeDefaultVal of
@@ -112,51 +114,52 @@ inputValueDefinitionParser schemaDoc (G.InputValueDefinition desc name fieldType
       buildField
         :: G.GType
         -> (forall k . 'Input <: k => Parser k n () -> InputFieldsParser n ())
-        -> InputFieldsParser n ()
+        -> m (InputFieldsParser n ())
       buildField fieldType' fieldConstructor' = case fieldType' of
        G.TypeNamed _ typeName ->
          case lookupType schemaDoc typeName of
-           Nothing -> _throwNotFoundNameInSchemaDoc
+           Nothing -> throwError $ "Could not find type with name " <> G.unName typeName
            Just typeDef ->
              case typeDef of
-               G.TypeDefinitionScalar _ -> fieldConstructor' $ remoteFieldScalarParser name
-               G.TypeDefinitionEnum defn -> fieldConstructor' $ remoteFieldEnumParser name defn
-               G.TypeDefinitionObject _ -> _throwInvalidKindError
-               G.TypeDefinitionInputObject defn -> fieldConstructor' $ remoteSchemaInputObject schemaDoc defn
-               G.TypeDefinitionUnion _ -> _throwInvalidKindError
-               G.TypeDefinitionInterface _ -> _throwInvalidKindError
+               G.TypeDefinitionScalar _ -> pure $ fieldConstructor' $ remoteFieldScalarParser name
+               G.TypeDefinitionEnum defn -> pure $ fieldConstructor' $ remoteFieldEnumParser name defn
+               G.TypeDefinitionObject _ -> throwError $ "expected input type, but got output type" -- couldn't find the equivalent error in Validate/Types.hs, so using a new error message
+               G.TypeDefinitionInputObject defn ->
+                 pure . fieldConstructor' =<< remoteSchemaInputObject schemaDoc defn
+               G.TypeDefinitionUnion _ -> throwError $ "expected input type, but got output type"
+               G.TypeDefinitionInterface _ -> throwError $ "expected input type, but got output type"
        G.TypeList _ subType -> buildField subType (fieldConstructor' . void . P.list)
   in buildField fieldType fieldConstructor
 
 argumentsParser
-  :: (MonadParse n)
+  :: forall n m
+  .  (MonadSchema n m, MonadError Text m)
   => G.ArgumentsDefinition
   -> G.SchemaDocument
-  -> InputFieldsParser n ()
+  -> m (InputFieldsParser n ())
 argumentsParser args schemaDoc = do
-  () <$ traverse (inputValueDefinitionParser schemaDoc) args
+  pure () <$ mapM (inputValueDefinitionParser schemaDoc) args
 
 remoteField
-  :: forall n
-   . MonadParse n
+  :: forall n m
+   . (MonadSchema n m, MonadError Text m)
   => SchemaDocument
   -> G.Name
   -> G.ArgumentsDefinition
   -> G.TypeDefinition
-  -> FieldParser n () -- TODO return something useful, maybe?
-remoteField sdoc fieldName argsDefn typeDefn =
+  -> m (FieldParser n ()) -- TODO return something useful, maybe?
+remoteField sdoc fieldName argsDefn typeDefn = do
   -- TODO add directives
-  let argsParser = argumentsParser argsDefn sdoc
-  in
-    case typeDefn of
+  argsParser <- argumentsParser argsDefn sdoc
+  case typeDefn of
     G.TypeDefinitionObject objTypeDefn -> do
-      P.subselection fieldName (G._otdDescription objTypeDefn) argsParser $ remoteSchemaObject sdoc objTypeDefn
-      pure ()
-    G.TypeDefinitionScalar scalarTypeDefn@(G.ScalarTypeDefinition desc _ _) ->
-      P.selection fieldName desc argsParser $ remoteFieldScalarParser fieldName
-    G.TypeDefinitionEnum enumTypeDefn@(G.EnumTypeDefinition desc _ _ valueDefns) ->
-      P.selection fieldName desc argsParser $ remoteFieldEnumParser fieldName enumTypeDefn
-    _ -> _errorInputTypeInOutputPosition
+      remoteSchemaObj <- remoteSchemaObject sdoc objTypeDefn
+      pure $ () <$ P.subselection fieldName (G._otdDescription objTypeDefn) argsParser remoteSchemaObj
+    G.TypeDefinitionScalar (G.ScalarTypeDefinition desc _ _) ->
+      pure $ P.selection fieldName desc argsParser $ remoteFieldScalarParser fieldName
+    G.TypeDefinitionEnum enumTypeDefn@(G.EnumTypeDefinition desc _ _ _) ->
+      pure $ P.selection fieldName desc argsParser $ remoteFieldEnumParser fieldName enumTypeDefn
+    _ -> throwError $ "expected input type, but got output type"
 
 remoteFieldScalarParser
   :: MonadParse n
@@ -177,6 +180,6 @@ remoteFieldEnumParser
   -> Parser 'Both n ()
 remoteFieldEnumParser name (G.EnumTypeDefinition desc _ _ valueDefns) =
   let enumValDefns = map (\(G.EnumValueDefinition enumDesc enumName _) ->
-                            ((mkDefinition (G.unEnumValue enumName) enumDesc EnumValueInfo),()))
+                            ((mkDefinition (G.unEnumValue enumName) enumDesc P.EnumValueInfo),()))
                          $ valueDefns
   in P.enum name desc $ NE.fromList enumValDefns
