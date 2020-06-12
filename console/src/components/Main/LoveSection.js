c9579a626c62e47c336209af90e9611723983e59
Sameer Kolhar <sameer@hasura.io>
2020-06-12 21:25:42 +0530

    clean up components related to love hasura

--- console/src/components/Main/LoveSection.js
+++ console/src/components/Main/LoveSection.js
@@ -1,103 +1,22 @@
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import { css } from 'styled-components';
 
-import globals from '../../Globals';
 import pixHeart from './images/pix-heart.svg';
 import consoleLogo from './images/console-logo.svg';
-import { Icon, Box, Flex, Heading, Text, Badge } from '../UIKit/atoms';
+import { Box, Flex, Heading, Text, Badge } from '../UIKit/atoms';
 import styles from './Main.scss';
-
-// eslint-disable-next-line no-unused-vars
-const OldLoveSection = ({ toggleDropdown }) => (
-  <ul className={'dropdown-menu ' + styles.dropdown_menu}>
-    <div className={styles.dropdown_menu_container}>
-      <Icon
-        type="close"
-        position="absolute"
-        pointer
-        color="black.secondary"
-        top="10px"
-        left="20px"
-        onClick={toggleDropdown}
-      />
-      <div className={styles.displayFlex}>
-        <li className={styles.pixelText1}>
-          Roses are red, <br />
-          Violets are blue;
-          <br />
-          Star us on GitHub,
-          <br />
-          To make our <Icon type="love" size={10} mx="xs" /> go wooooo!
-        </li>
-        <li className={'dropdown-item'}>
-          <a
-            href="https://github.com/hasura/graphql-engine"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className={styles.socialIcon}>
-              <img
-                className="img img-responsive"
-                src={`${globals.assetsPath}/common/img/githubicon.png`}
-                alt={'GitHub'}
-              />
-            </div>
-            <div className={styles.pixelText}>
-              <Icon type="star" size={12} mr="5px" />
-              Star
-            </div>
-          </a>
-        </li>
-        <li className={'dropdown-item '}>
-          <a
-            href="https://twitter.com/intent/tweet?hashtags=graphql,postgres&text=Just%20deployed%20a%20GraphQL%20backend%20with%20@HasuraHQ!%20%E2%9D%A4%EF%B8%8F%20%F0%9F%9A%80%0Ahttps://github.com/hasura/graphql-engine%0A"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className={styles.socialIcon}>
-              <img
-                className="img img-responsive"
-                src={`${globals.assetsPath}/common/img/twittericon.png`}
-                alt={'Twitter'}
-              />
-            </div>
-            <div className={styles.pixelText}>
-              <Icon type="twitter" size={12} mr="5px" />
-              Tweet
-            </div>
-          </a>
-        </li>
-      </div>
-    </div>
-  </ul>
-);
-
-const updateData = [
-  {
-    title: 'HasuraCon, Online',
-    time: '12 May, 2020',
-    description: 'HasuraCon is happening at the Hasura office this month.',
-    badge: 'event',
-  },
-  {
-    title: 'GraphQL Asia, Bengaluru',
-    time: '20 Feb, 2020',
-    description:
-      "GraphQL Asia is the largest gathering of the GraphQL community in Asia with over 500 people expected at 2020 edition, you'll meet the creators of leading GraphQL tooling, talk to the experts implementing GraphQL in their companies as well as practicioners and enthusiasts from Asia and the rest of the world.",
-    badge: 'community',
-  },
-  {
-    title: 'v1.2.0-beta.5',
-    time: '24 April, 2020',
-    description:
-      'We fixed some major bugs, added some new features and made life generally easier. Check it out!',
-    badge: 'update',
-  },
-];
+import fetch from 'isomorphic-fetch';
+import Endpoints from '../../Endpoints';
 
 const Update = ({ title, time, description, badge }) => (
-  <Box borderBottom={1}>
-    <Flex height="55px" justifyContent="space-between" px="25px" pt="5px">
+  <Box>
+    <Flex
+      height="55px"
+      justifyContent="space-between"
+      px="25px"
+      pt="5px"
+      borderBottom={'1px solid #e1e1e1'}
+    >
       <Flex>
         <Badge type={badge} mr="12px" />
         <Heading as="h4" color="#1cd3c6" fontSize="16px">
@@ -114,13 +33,13 @@ const Update = ({ title, time, description, badge }) => (
         {time}
       </Text>
     </Flex>
-    <Text fontSize="15px" fontWeight="normal" px="25px" pb="20px">
+    <Text fontSize="15px" fontWeight="normal" px="25px" py="8px">
       {description}
     </Text>
   </Box>
 );
 
-const NewLoveSection = ({ toggleDropdown }) => (
+const Notifications = ({ data }) => (
   <Box
     className="dropdown-menu"
     border={0}
@@ -156,51 +75,54 @@ const NewLoveSection = ({ toggleDropdown }) => (
         Latest updates
         <img src={consoleLogo} alt="hasura-console" id="console-logo" />
       </Heading>
-      <Icon
-        type="close"
-        color="#5a5a5a"
-        id="close-icon"
-        size={16}
-        pointer
-        onClick={toggleDropdown}
-      />
     </Flex>
-    {updateData &&
-      updateData.length >= 1 &&
-      updateData.map(({ title, time, description, badge }) => (
-        <Update
-          key={title}
-          title={title}
-          time={time}
-          description={description}
-          badge={badge}
-        />
-      ))}
-    {/* <Flex px="25px" py="20px" color="red.primary">
-      <TextLink href="#" id="update-link" color="red.primary">
-        View all updates{' '}
-        <img className={styles.arrow} src={arrowForwardRed} alt={'Arrow'} />
-      </TextLink>
-    </Flex> */}
+    {data && data.length
+      ? data.map(({ title, time, description, badge }) => (
+          <Update
+            key={title}
+            title={title}
+            time={time}
+            description={description}
+            badge={badge}
+          />
+        ))
+      : 'No Notifications!'}
   </Box>
 );
 
+// TODO: handle case where information is not avaialble...check the response type correctly
+
 const LoveSection = () => {
-  const toggleDropdown = () => {
+  const [open, toggleLove] = useState(false);
+  const [notificationData, setData] = useState([
+    {
+      title: 'No Notifications yet!',
+      time: 'n/a',
+      description: 'some text here',
+      badge: 'NO',
+    },
+  ]);
+
+  useLayoutEffect(() => {
+    if (open) {
+      fetch(Endpoints.checkNotifications)
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(err => console.error(err));
+    }
     document.getElementById('dropdown_wrapper').classList.toggle('open');
-  };
+  }, [open]);
 
   return (
     <>
       <div
         className={styles.shareSection + ' dropdown-toggle'}
         aria-expanded="false"
-        onClick={toggleDropdown}
+        onClick={() => toggleLove(!open)}
       >
-        <img className={'img-responsive'} src={pixHeart} alt={'pix Heart'} />
+        <img className="img-responsive" src={pixHeart} alt={'pix Heart'} />
       </div>
-      {/* <OldLoveSection toggleDropdown={toggleDropdown} /> */}
-      <NewLoveSection toggleDropdown={toggleDropdown} />
+      <Notifications data={notificationData} />
     </>
   );
 };
--- console/src/components/UIKit/atoms/Flex/index.js
+++ console/src/components/UIKit/atoms/Flex/index.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 import { Box } from '../Box';
 
+// TODO: use styled-system
 export const Flex = styled(Box)`
   cursor: ${props => (props.pointer ? 'pointer' : '')};
   display: flex;
@@ -9,5 +10,5 @@ export const Flex = styled(Box)`
   justify-content: ${props =>
     props.justify ? props.justify : 'space-between'};
   border-bottom: ${props => (props.borderBottom ? props.borderBottom : '1px')};
-  padding: 1em;
+  padding: 0.35em;
 `;
