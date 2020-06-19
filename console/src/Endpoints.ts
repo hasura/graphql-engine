527af4cdfa6ae4ebbdef9dda2356a40a6b595911
Sameer Kolhar <sameer@hasura.io>
2020-06-19 11:17:05 +0530

    change endpoint to staging table

--- console/src/Endpoints.ts
+++ console/src/Endpoints.ts
@@ -20,8 +20,7 @@ const Endpoints = {
   hasuractlMetadata: `${hasuractlUrl}/apis/metadata`,
   hasuractlMigrateSettings: `${hasuractlUrl}/apis/migrate/settings`,
   telemetryServer: 'wss://telemetry.hasura.io/v1/ws',
-  checkNotifications:
-    'https://data.hasura.io/v1/template/console_notifications',
+  checkNotifications: 'https://data.hasura-stg.hasura-app.io/v1/query',
 };
 
 const globalCookiePolicy = 'same-origin';
