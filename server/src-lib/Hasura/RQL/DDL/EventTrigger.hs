53b71a38f90a11e0810aacc6cb4c677fd7cd4548
Sameer Kolhar <sameer@hasura.io>
2020-06-21 14:25:17 +0530

    fix error

--- server/src-lib/Hasura/RQL/DDL/EventTrigger.hs
+++ server/src-lib/Hasura/RQL/DDL/EventTrigger.hs
@@ -131,7 +131,7 @@ addEventTriggerToCatalog qt etc = do
            INSERT into hdb_catalog.event_triggers
                        (name, type, schema_name, table_name, configuration)
            VALUES ($1, 'table', $2, $3, $4, $5)
-         |] (name, sn, tn, Q.AltJ $ toJSON etc, comment) False
+         |] (name, sn, tn, Q.AltJ $ toJSON etc, ctComment) False
   where
     QualifiedObject sn tn = qt
     (EventTriggerConf name _ _ _ _ _) = etc
