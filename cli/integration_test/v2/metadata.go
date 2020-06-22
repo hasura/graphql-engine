b2074cf94fe4003a6cf18efc71dbf0b63d26fa64
scriptonist <scriptonist@gmail.com>
2020-06-22 17:28:08 +0530

    add tests

--- cli/integration_test/v2/metadata.go
+++ cli/integration_test/v2/metadata.go
@@ -2,7 +2,10 @@ package v2
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
@@ -135,6 +138,90 @@ func TestMetadataCmd(t *testing.T, ec *cli.ExecutionContext) {
 					assert.Equal(t, string(expectedByt), string(actualByt))
 				}
 			}
+		})
+	}
+}
+
+func TestIncompleteMetadataDir(t *testing.T, ec *cli.ExecutionContext) {
+	currDir, _ := os.Getwd()
+	tt := []struct {
+		name               string
+		opts               metadataInterface
+		err                error
+		copyMetadataFolder string
+		goldenFile         string
+	}{
+		{
+			"t1",
+			&commands.MetadataExportOptions{
+				EC:         ec,
+				ActionType: "apply",
+			},
+			nil,
+			filepath.Join(currDir, "testdata/incomplete_metadata/t1/metadata"),
+			filepath.Join(currDir, "testdata/incomplete_metadata/t1/from-server.golden"),
+		},
+		{
+			"t2",
+			&commands.MetadataExportOptions{
+				EC:         ec,
+				ActionType: "apply",
+			},
+			nil,
+			filepath.Join(currDir, "testdata/incomplete_metadata/t2/metadata"),
+			filepath.Join(currDir, "testdata/incomplete_metadata/t2/from-server.golden"),
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.copyMetadataFolder != "" {
+				err := os.RemoveAll(ec.MetadataDir)
+				if err != nil {
+					t.Fatalf("%s: unable to remove metadata directory, got %v", tc.name, err)
+				}
+				err = util.CopyDir(tc.copyMetadataFolder, ec.MetadataDir)
+				if err != nil {
+					t.Fatalf("%s: unable to copy metadata file, got %v", tc.name, err)
+				}
+			}
+			err := tc.opts.Run()
+			if err != tc.err {
+				t.Fatalf("%s: expected %v, got %v", tc.name, tc.err, err)
+			}
+			// get metadata from server
+			testServerUrl := os.Getenv("HASURA_GRAPHQL_TEST_ENDPOINT")
+			if testServerUrl == "" {
+				testServerUrl = "http://localhost:8080"
+			}
+			url := fmt.Sprintf("%s/%s", testServerUrl, "v1/query")
+			var body = []byte(`
+{
+    "type" : "export_metadata",
+    "args": {}
+}
+`)
+			resp, err := http.Post(url, "application/json", bytes.NewReader(body))
+			defer resp.Body.Close()
+			assert.Equal(t, resp.StatusCode, http.StatusOK)
+			got, err := ioutil.ReadAll(resp.Body)
+			assert.NoError(t, err)
+
+			want, err := ioutil.ReadFile(tc.goldenFile)
+			assert.NoError(t, err)
+			var wantM, gotM map[string]interface{}
+
+			err = json.Unmarshal(want, &wantM)
+			assert.NoError(t, err)
+			err = json.Unmarshal(got, &gotM)
+			assert.NoError(t, err)
+
+			wantB, err := json.Marshal(wantM)
+			assert.NoError(t, err)
+			gotB, err := json.Marshal(gotM)
+			assert.NoError(t, err)
+
+			assert.Equal(t, string(wantB), string(gotB))
 		})
 	}
 }
