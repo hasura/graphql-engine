7ae3e55a88801de600f0c72dfac29fa95def5eb8
Aravind <aravindkp@outlook.in>
2020-07-04 10:15:37 +0530

    tag release v1.3.0-beta.4 (#5281)

--- install-manifests/docker-run/docker-run.sh
+++ install-manifests/docker-run/docker-run.sh
@@ -3,4 +3,4 @@ docker run -d -p 8080:8080 \
        -e HASURA_GRAPHQL_DATABASE_URL=postgres://username:password@hostname:port/dbname \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
        -e HASURA_GRAPHQL_DEV_MODE=true \
-       hasura/graphql-engine:v1.3.0-beta.3
+       hasura/graphql-engine:v1.3.0-beta.4
