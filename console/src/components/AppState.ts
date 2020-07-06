a8f6cbfae2e9ee0c2d9b5fa05ec9d14ec01de425
Sameer Kolhar <sameer@hasura.io>
2020-07-06 15:35:14 +0530

    change AppState to TS

---
+++ console/src/components/AppState.ts
@@ -0,0 +1,37 @@
+import globals from '../Globals';
+import { getLSItem, setLSItem, removeLSItem } from '../utils/localstorage';
+
+const stateKey = `CONSOLE_LOCAL_INFO:${globals.dataApiUrl}`;
+
+const CONSOLE_ADMIN_SECRET = 'CONSOLE_ADMIN_SECRET';
+
+const loadAppState = () => JSON.parse(getLSItem(stateKey) as string);
+
+const saveAppState = (state: string) => {
+    setLSItem(stateKey, JSON.stringify(state));
+};
+
+const loadAdminSecretState = () =>
+    getLSItem(CONSOLE_ADMIN_SECRET);
+
+const saveAdminSecretState = (state: string) => {
+    setLSItem(CONSOLE_ADMIN_SECRET, state);
+};
+
+const clearAdminSecretState = () => {
+    removeLSItem(CONSOLE_ADMIN_SECRET);
+
+    globals.adminSecret = null;
+};
+
+const clearState = () => removeLSItem(stateKey);
+
+export {
+    saveAppState,
+    saveAdminSecretState,
+    loadAppState,
+    loadAdminSecretState,
+    clearState,
+    clearAdminSecretState,
+    CONSOLE_ADMIN_SECRET,
+};
