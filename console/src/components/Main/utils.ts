70efc1e8dd28f2d8d191a48118f00b3c2d5df6a6
Sameer Kolhar <sameer@hasura.io>
2020-07-06 17:13:59 +0530

    revert utils change

--- console/src/components/Main/utils.ts
+++ console/src/components/Main/utils.ts
@@ -1,8 +1,28 @@
 import { setLSItem, getLSItem } from '../../utils/localstorage';
 
+const loveConsentState = 'console:loveIcon';
 const proClickState = 'console:pro';
+const defaultState = {
+  isDismissed: false,
+};
 const defaultProClickState = {
   isProClicked: false,
+};
+
+const setLoveConsentState = (stateData: { isDismissed: boolean }) => {
+  setLSItem(loveConsentState, JSON.stringify(stateData));
+};
+
+const getLoveConsentState = () => {
+  const s = getLSItem(loveConsentState);
+
+  if (s) {
+    return JSON.parse(s);
+  }
+
+  setLSItem(loveConsentState, JSON.stringify(defaultState));
+
+  return defaultState;
 };
 
 const setProClickState = (proStateData: { isProClicked: boolean }) => {
@@ -20,10 +40,15 @@ const getProClickState = () => {
     setLSItem(proClickState, JSON.stringify(defaultProClickState));
 
     return defaultProClickState;
-  } catch (e) {
-    console.error(e);
+  } catch (err) {
+    console.error(err);
     return defaultProClickState;
   }
 };
 
-export { getProClickState, setProClickState };
+export {
+  getLoveConsentState,
+  setLoveConsentState,
+  getProClickState,
+  setProClickState,
+};
