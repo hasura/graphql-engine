86adfd57885130362316dc3b19da506784b40a60
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2020-07-13 13:51:33 +0530

    [skip ci] validate variable usage in a GraphQL query
    - check if a variable has been declared,if it's been,  then it should be used
      atleast once in the query

--- server/src-lib/Hasura/GraphQL/Execute/Resolve.hs
+++ server/src-lib/Hasura/GraphQL/Execute/Resolve.hs
@@ -9,6 +9,9 @@ import           Hasura.Prelude
 import qualified Data.Aeson                             as J
 import qualified Data.HashMap.Strict.Extended           as Map
 import qualified Language.GraphQL.Draft.Syntax          as G
+import qualified Data.HashSet                           as HS
+import qualified Data.List                              as L
+import qualified Data.Text                              as T
 
 import qualified Hasura.GraphQL.Transport.HTTP.Protocol as GH
 
@@ -31,20 +34,28 @@ resolveVariables definitions jsonValues selSet = do
     \variableName variableDefinitions ->
       case variableDefinitions of
         a :| [] -> return a
-        _      -> throw400 ParseFailed $ "multiple definitions for variable " <>> variableName
-  traverse (traverse (resolveVariable uniqueVariables)) selSet
+        _      -> throw400 ParseFailed
+                   $ "multiple definitions for variable " <>> variableName
+  (selSet', usedVariables) <- flip runStateT mempty $
+    traverse (traverse (resolveVariable uniqueVariables)) selSet
+  let variablesByNameSet = HS.fromList . Map.keys $ variablesByName
+  when (usedVariables /= variablesByNameSet) $
+    throw400 ValidationFailed $
+    ("following variable(s) have been defined, but have not been used in the query - "
+     <> (T.concat $ L.intersperse ","
+         $ map G.unName $ HS.toList $
+           HS.difference variablesByNameSet usedVariables))
+  return selSet'
   where
     buildVariable :: G.VariableDefinition -> m Variable
     buildVariable G.VariableDefinition{ G._vdName, G._vdType, G._vdDefaultValue } = do
       let defaultValue = fromMaybe G.VNull _vdDefaultValue
-
       value <- case Map.lookup _vdName jsonValues of
         Just jsonValue -> jsonToGqlValue jsonValue
         Nothing
           | G.isNullable _vdType -> pure defaultValue
           | otherwise -> throw400 ValidationFailed $
             "expecting a value for non-nullable variable: " <>> _vdName
-
       pure $! Variable
         { vInfo = if G.isNullable _vdType
             then VIOptional _vdName defaultValue
@@ -52,10 +63,9 @@ resolveVariables definitions jsonValues selSet = do
         , vType = _vdType
         , vValue = value
         }
-
-    resolveVariable :: HashMap G.Name Variable -> G.Name -> m Variable
+    resolveVariable :: HashMap G.Name Variable -> G.Name -> StateT (HS.HashSet G.Name) m Variable
     resolveVariable variables name = case Map.lookup name variables of
-      Just variable -> pure variable
+      Just variable -> modify (HS.insert name) >> pure variable
       Nothing       -> throw400 ValidationFailed $ "unbound variable " <>> name
 
 jsonToGqlValue :: MonadError QErr m => J.Value -> m (G.Value a)
