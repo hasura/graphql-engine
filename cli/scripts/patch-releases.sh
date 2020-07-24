c02bb054f52b1559ecf9c0fa879d879d53cb0d3d
scriptonist <scriptonist@gmail.com>
2020-07-24 23:16:09 +0530

    add function to build and push binaries

--- cli/scripts/patch-releases.sh
+++ cli/scripts/patch-releases.sh
@@ -2,24 +2,30 @@
 
 set -evo pipefail
 
-ROOT="$(readlink -f ${BASH_SOURCE[0]%/*}/../../)"
-LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+ROOT="$(readlink -f "${BASH_SOURCE[0]%/*}"/../../)"
+LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+
+PATCH_RELEASE_FILENAME_HEAD="${LATEST_TAG}+cli."
+PATCH_RELEASE_FILENAME_TAIL=".md"
+PATCH_RELEASES_DIRECTORY="${ROOT}/cli/patch_releases"
+
+PATCH_RELEASES_GCLOUD_BUCKET="gs://hasura-oss-cli-cdn/test-releases"
+OUTPUT_DIR="${ROOT}/cli/_output"
 
 create_patch_release_file() {
-  PATCH_RELEASE_FILENAME_HEAD="${LATEST_TAG}+cli."
   PATCH_NUMBER=1
 
   for PATCH_NUMBER in 1 2 3 4 5 .. 50
   do
-    if [ ! -f "${ROOT}/cli/patch_releases/${PATCH_RELEASE_FILENAME_HEAD}${PATCH_NUMBER}" ]; then
+    if [ ! -f "${PATCH_RELEASES_DIRECTORY}/${PATCH_RELEASE_FILENAME_HEAD}${PATCH_NUMBER}${PATCH_RELEASE_FILENAME_TAIL}" ]; then
         break
     fi
   done
 
-  PATCH_RELEASE_FILENAME=${LATEST_TAG}+cli.${PATCH_NUMBER}
+  PATCH_RELEASE_FILENAME=${PATCH_RELEASE_FILENAME_HEAD}${PATCH_NUMBER}${PATCH_RELEASE_FILENAME_TAIL}
 
   # create patch release file with template
-  cp ${ROOT}/cli/scripts/patch-release-template.md ${ROOT}/cli/patch_releases/${PATCH_RELEASE_FILENAME}.md
+  cp "${ROOT}/cli/scripts/patch-release-template.md" "${PATCH_RELEASES_DIRECTORY}/${PATCH_RELEASE_FILENAME}"
 
   # TODO: append asset urls to file
 }
@@ -54,22 +60,22 @@ is_patch_release() {
   SEMVER_REGEX='((([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)'
   FILE_REGEX="^cli\/patch_releases\/v${SEMVER_REGEX}\.md$"
 
-  NO_OF_FILES=$(curl -s ${GITHUB_API_BASE_URL}/${PR_NUMBER}/files | jq '. | length')
-  PR_FILES=$(curl -s ${GITHUB_API_BASE_URL}/${PR_NUMBER}/files | jq )
+  NO_OF_FILES=$(curl -s ${GITHUB_API_BASE_URL}/"${PR_NUMBER}"/files | jq '. | length')
+  PR_FILES=$(curl -s ${GITHUB_API_BASE_URL}/"${PR_NUMBER}"/files | jq )
 
   # validate number of changed files
   if [[ ! $NO_OF_FILES -eq 1 ]] 
   then
     echo "diff has more than one file change, this is not expected. exiting"
-    echo $PR_FILES | jq 
+    echo "$PR_FILES" | jq
     exit 1
   fi
 
   # validate a patch_release file was changed  
-  FILENAME=$(echo ${PR_FILES} | '.[].filename')
+  FILENAME=$(echo "${PR_FILES}" | '.[].filename')
   # if a file inside cli/patch_releases/<semver>.md changed exit with success
   # grep options: -o means output the match, -P means use Perl regex
-  if (echo $FILENAME | grep -oP ${FILE_REGEX}) then
+  if (echo "${FILENAME}" | grep -oP "${FILE_REGEX}") then
     # validate PR has `c/cli` and `k/patch_release` label
     LABEL_COUNT=$(curl -s ${GITHUB_API_BASE_URL/${PR_NUMBER}} | jq '.labels[] | select( .name | contains("c/cli", "k/patch_release")) | .name' | wc -l)
     if [[ ! ${LABEL_COUNT} -eq 2 ]]
@@ -81,6 +87,34 @@ is_patch_release() {
   exit 0
 }
 
+build_and_push_patch_release() {
+  # find the latest patch release file
+  PATCH_NUMBER=1
+
+  for (( PATCH_NUMBER=50; PATCH_NUMBER >= 0; --PATCH_NUMBER ))
+  do
+    FILENAME="${PATCH_RELEASES_DIRECTORY}/${PATCH_RELEASE_FILENAME_HEAD}${PATCH_NUMBER}${PATCH_RELEASE_FILENAME_TAIL}"
+    echo "$FILENAME"
+    if [ -f  "${FILENAME}" ]; then
+        PATCH_VERSION_MD=$(basename -- "$FILENAME")
+        PATCH_VERSION="${PATCH_VERSION_MD%.*}"
+        break
+    fi
+  done
+  echo $PATCH_NUMBER
+  if [ $PATCH_NUMBER -eq 0 ]
+  then
+    echo "no patch release found for ${LATEST_TAG}"
+    exit 1
+  fi
+
+  # build binaries
+  "${ROOT}"/cli/scripts/build-cli.sh "${PATCH_VERSION}"
+	# ls "${OUTPUT_DIR}"/"${PATCH_VERSION}"/cli-hasura-* | xargs upx
+
+  # push binaries to gcloud bucket
+  gsutil -m cp -r "${ROOT}"/cli/_output/"${PATCH_VERSION}" ${PATCH_RELEASES_GCLOUD_BUCKET}/"${PATCH_VERSION}"
+}
 
 if [ "$#" -lt 1 ]; then
     echo "requires arguments"
@@ -92,6 +126,9 @@ case $1 in
     create_patch_release_file
     ;;
   "is_patch_release")
-    is_patch_release $2
+    is_patch_release "$2"
+    ;;
+  "build_and_push_patch_release")
+    build_and_push_patch_release
     ;;
 esac
