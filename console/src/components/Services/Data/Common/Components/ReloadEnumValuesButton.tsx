5a27629a97634f6ed76471feb9c09c0f8631abbd
Sameer Kolhar <sameer@hasura.io>
2020-07-24 13:20:07 +0530

    change ReloadEnumValuesBtn

--- console/src/components/Services/Data/Common/Components/ReloadEnumValuesButton.tsx
+++ console/src/components/Services/Data/Common/Components/ReloadEnumValuesButton.tsx
@@ -4,7 +4,6 @@ import { AnyAction } from 'redux';
 import ReloadMetadata from '../../../Settings/MetadataOptions/ReloadMetadata';
 
 export interface ReloadEnumValuesButtonProps {
-  isEnum: boolean;
   dispatch: ThunkDispatch<{}, {}, AnyAction>;
   tooltipStyle?: string;
 }
@@ -13,20 +12,16 @@ const TOOLTIP_TEXT =
   'Reload enum values in your GraphQL schema after inserting, updating or deleting enum values';
 
 const ReloadEnumValuesButton: React.FC<ReloadEnumValuesButtonProps> = ({
-  isEnum,
   dispatch,
   tooltipStyle = '',
-}) => {
-  if (!isEnum) return null;
-  return (
-    <ReloadMetadata
-      buttonText="Reload enum values"
-      btnTooltipMessage={TOOLTIP_TEXT}
-      showReloadRemoteSchemas={false}
-      dispatch={dispatch}
-      tooltipStyle={tooltipStyle}
-    />
-  );
-};
+}) => (
+  <ReloadMetadata
+    buttonText="Reload enum values"
+    btnTooltipMessage={TOOLTIP_TEXT}
+    showReloadRemoteSchemas={false}
+    dispatch={dispatch}
+    tooltipStyle={tooltipStyle}
+  />
+);
 
 export default ReloadEnumValuesButton;
