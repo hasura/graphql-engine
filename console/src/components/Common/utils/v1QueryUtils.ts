95d4c0302fa98b61e5ee2f6b8afaed7d065565a9
Sameer Kolhar <sameer@hasura.io>
2020-07-24 17:59:03 +0530

    fix formatting and lint issues

--- console/src/components/Common/utils/v1QueryUtils.ts
+++ console/src/components/Common/utils/v1QueryUtils.ts
@@ -563,20 +563,20 @@ export const generateCreateEventTriggerQuery = (
         state.webhook.type === 'env' ? state.webhook.value.trim() : null,
       insert: state.operations.insert
         ? {
-          columns: '*',
-        }
+            columns: '*',
+          }
         : null,
       update: state.operations.update
         ? {
-          columns: state.operationColumns
-            .filter(c => !!c.enabled)
-            .map(c => c.name),
-        }
+            columns: state.operationColumns
+              .filter(c => !!c.enabled)
+              .map(c => c.name),
+          }
         : null,
       delete: state.operations.delete
         ? {
-          columns: '*',
-        }
+            columns: '*',
+          }
         : null,
       enable_manual: state.operations.enable_manual,
       retry_conf: state.retryConf,
