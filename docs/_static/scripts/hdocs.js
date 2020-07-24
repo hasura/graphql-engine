a8a9e56f38c5de7fbbb453236d56b5c14032508a
sezgi <sezgi.ulucam@gmail.com>
2020-07-24 14:21:50 -0700

    move intercom into deferred hdocs.js

--- docs/_static/scripts/hdocs.js
+++ docs/_static/scripts/hdocs.js
@@ -24,6 +24,7 @@ window.hdocs = (function () {
       hdocs.getGithubStarCount();
       hdocs.setReleaseTags();
       hdocs.setExternalLinks();
+      hdocs.setupIntercom();
       hdocs.setupGraphiQL();
     },
     toggleMenu: function () {
@@ -98,6 +99,17 @@ window.hdocs = (function () {
     },
     setExternalLinks: function () {
       Array.from(document.getElementsByClassName('.external')).forEach(el => el.setAttribute('target', '_blank'));
+    },
+    setupIntercom: function () {
+      window.intercomSettings = {
+        app_id: "rucirpb3"
+      };
+      var w = window; var ic = w.Intercom; if (typeof ic === "function") { ic('reattach_activator'); ic('update', intercomSettings); } else {
+        var d = document; var i = function () { i.c(arguments) }; i.q = []; i.c = function (args) { i.q.push(args) }; w.Intercom = i; function l() {
+          var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://widget.intercom.io/widget/rucirpb3'; var
+            x = d.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+        } if (w.attachEvent) { w.attachEvent('onload', l); } else { w.addEventListener('load', l, false); }
+      }
     },
     transformSearchData: function (suggestions) {
       if (window.location.origin !== 'https://hasura.io') {
