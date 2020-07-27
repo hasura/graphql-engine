66ce7946f0632a5c1e324b61623100400a4b6417
soorajshankar <soorajshankar@gmail.com>
2020-07-27 11:08:53 +0530

    format

--- console/src/utils/migration/Migration.ts
+++ console/src/utils/migration/Migration.ts
@@ -1,5 +1,6 @@
 import { getDownQueryComments } from './utils';
 import { RunSqlType } from '../../types';
+
 // TODO use well typed interface after generating
 export interface RunSQLQueryType {
   type: string;
