6cbefee34ea43b2010486cffcda3338233d82c6f
soorajshankar <soorajshankar@gmail.com>
2020-07-28 11:34:56 +0530

    trim chars

--- console/src/components/Services/Data/TableBrowseRows/ViewRows.js
+++ console/src/components/Services/Data/TableBrowseRows/ViewRows.js
@@ -533,6 +533,9 @@ const ViewRows = ({
             typeof rowColumnValue === 'object'
           ) {
             cellValue = JSON.stringify(rowColumnValue, null, 4);
+            if (cellValue.length > 5000)
+              // trim 5000 chars which prevents browser freezing if there is large JSONB values
+              cellValue = cellValue.substring(1, 5000) + '...';
             cellTitle = cellValue;
           } else {
             cellValue = rowColumnValue.toString();
