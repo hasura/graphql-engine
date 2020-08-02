88f1988702f8e3853d4f20a7ad9a5c1f7a9d0c35
scriptonist <scriptonist@gmail.com>
2020-08-02 20:22:48 +0530

    fix version

--- cli/scripts/ci-copy-binaries.sh
+++ cli/scripts/ci-copy-binaries.sh
@@ -4,13 +4,14 @@ set -evo pipefail
 
 ROOT="$(readlink -f ${BASH_SOURCE[0]%/*}/../../)"
 OUTPUT_DIR="${ROOT}/cli/_output"
-VERSION=$(../scripts/get-version.sh)
+VERSION=$("${ROOT}"/scripts/get-version.sh)
 
-if [  -z "${1}" ]
+if [ -z "${1}" ]
 then
   VERSION=${1}
 fi
 
+echo "version: ${VERSION}"
 mkdir -p /build/_cli_output/binaries
 cp ${OUTPUT_DIR}/${VERSION}/cli-hasura-* /build/_cli_output/binaries
 echo "${VERSION}" > /build/_cli_output/version.txt
