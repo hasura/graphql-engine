b6db482b6736c183ebc31014497ebcac5d5780ad
Sameer Kolhar <sameer@hasura.io>
2020-08-20 19:25:43 +0530

    remove custom fns from trackables if used in CF

--- console/src/components/Services/Data/Schema/Schema.js
+++ console/src/components/Services/Data/Schema/Schema.js
@@ -205,6 +205,7 @@ class Schema extends Component {
       functionsList,
       nonTrackableFunctions,
       trackedFunctions,
+      allSchemas,
     } = this.props;
 
     const handleSchemaChange = e => {
@@ -220,7 +221,30 @@ class Schema extends Component {
         return !trackedFuncNames.includes(getFunctionName(func));
       };
 
-      return functionsList.filter(filterCondition);
+      const allTrackableFunctions = functionsList.filter(filterCondition);
+
+      // This function is to help filter out custom functions that are being used for computed fields.
+      // Functions that are being used within computed fields should not be tracked. See issue #3757
+      const allComputedFieldFunctions = allSchemas
+        .filter(tableInfo => tableInfo.table_schema === currentSchema)
+        .filter(table => {
+          if (!table.computed_fields || !table.computed_fields.length) {
+            return;
+          }
+          return table;
+        })
+        .map(table =>
+          table.computed_fields.map(comp => comp.definition.function.name)
+        )
+        .flat();
+
+      if (!allComputedFieldFunctions.length) {
+        return allTrackableFunctions;
+      }
+
+      return allTrackableFunctions.filter(
+        func => !allComputedFieldFunctions.includes(func.function_name)
+      );
     };
 
     const getSectionHeading = (headingText, tooltip, actionElement = null) => {
@@ -745,6 +769,7 @@ const mapStateToProps = state => ({
   nonTrackableFunctions: [...state.tables.nonTrackablePostgresFunctions],
   trackedFunctions: [...state.tables.trackedFunctions],
   serverVersion: state.main.serverVersion ? state.main.serverVersion : '',
+  allSchemas: state.tables.allSchemas,
 });
 
 const schemaConnector = connect => connect(mapStateToProps)(Schema);
