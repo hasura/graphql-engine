729c48bcdec846708eb94bfa3c31356df1d5d7f6
Sameer Kolhar <kolhar730@gmail.com>
2020-09-01 20:57:10 +0530

    update docs with examples

--- cli/commands/migrate_create.go
+++ cli/commands/migrate_create.go
@@ -31,6 +31,12 @@ const migrateCreateCmdExamples = `  # Setup migration files for the first time b
 
   # Take pg_dump from server and save it as a migration and specify the schemas to include
   hasura migrate create init --sql-from-server --schema myschema1,myschema2
+
+  # Take pg_dump from server with all schemas and extensions
+  hasura migrate create init --from-server --full
+
+  # Take pg_dump from server by excluding certain schemas
+  hasura migrate create init --from-server --exclude-schema public,hdb_catalog
   
   # Create up and down SQL migrations, providing contents as flags
   hasura migrate create migration-name --up-sql "CREATE TABLE article(id serial NOT NULL, title text NOT NULL, content text NOT NULL);"  --down-sql "DROP TABLE article;"
