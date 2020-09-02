29fb4dacbc92a3ea3c1f5cafe6064cde7ce5c0d5
Auke Booij <auke@hasura.io>
2020-09-02 18:01:49 +0200

    [skip ci] Try to determine the list of primary keys

--- server/src-lib/Hasura/Sources/MySQL/Metadata.hs
+++ server/src-lib/Hasura/Sources/MySQL/Metadata.hs
@@ -3,6 +3,8 @@ module Hasura.Sources.MySQL.Metadata
   ) where
 
 import Hasura.Prelude
+import Data.Sequence.NonEmpty
+import Data.Sequence (fromList)
 
 import qualified Database.MySQL.Base as My
 import Control.Concurrent.MVar
@@ -36,10 +38,14 @@ fetchTables = do
   , _tciEnumValues        :: !(Maybe EnumValues)
   , _tciCustomConfig      :: !TableConfig
   -}
-  tables <- IO.toList =<< snd <$> My.query_ conn (My.Query
-    "SELECT TABLE_SCHEMA, TABLE_NAME, TABLE_TYPE \
-    \FROM INFORMATION_SCHEMA.TABLES \
-    \WHERE TABLE_TYPE='BASE TABLE'")
+  tables <- fmap (\case [My.MySQLText schema, My.MySQLText name, My.MySQLText typ] -> (schema, name, typ)) <$>
+    ( IO.toList =<< snd <$> My.query_ conn
+      ( My.Query
+        "SELECT TABLE_SCHEMA, TABLE_NAME, TABLE_TYPE \
+        \FROM INFORMATION_SCHEMA.TABLES \
+        \WHERE TABLE_TYPE='BASE TABLE'"
+      )
+    )
   {-
   = PGColumnInfo
   { pgiColumn      :: !PGCol
@@ -50,49 +56,77 @@ fetchTables = do
   , pgiIsNullable  :: !Bool
   , pgiDescription :: !(Maybe G.Description)
   -}
-  columns <- IO.toList =<< snd <$> My.query_ conn (My.Query
-    "SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, ORDINAL_POSITION, IS_NULLABLE, DATA_TYPE, COLUMN_KEY \
-    \FROM INFORMATION_SCHEMA.COLUMNS")
-  foreigns <- IO.toList =<< snd <$> My.query_ conn (My.Query
-    "SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, CONSTRAINT_NAME, REFERENCED_TABLE_SCHEMA, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME \
-    \FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE")
-
-  tableCache' <- for tables \table -> do
-    let [   My.MySQLText tableSchema
-          , My.MySQLText tableName
-          , My.MySQLText tableType
---          , My.MySQLText tableComment -- hard to parse; could be null
-          ] = table
-        tableIden = QualifiedObject (SchemaName tableSchema) (TableName tableName)
-    if tableType == "BASE TABLE"
-      then do
-      fieldInfoMap <- Map.fromList . catMaybes <$> for columns \column -> do
-        let [   My.MySQLText tableSchema'
-              , My.MySQLText tableName'
-              , My.MySQLText columnName
-              , My.MySQLInt32U ordinalPosition
-              , My.MySQLText isNullable
-              , My.MySQLText dataType
-              , My.MySQLText columnKey
-              ] = column
-        if tableSchema == tableSchema' && tableName == tableName'
-          then Just <$> do
-          let pgci =
-                PGColumnInfo
+  columns <- fmap (
+    \case [   My.MySQLText tableSchema
+            , My.MySQLText tableName
+            , My.MySQLText columnName
+            , My.MySQLInt32U ordinalPosition
+            , My.MySQLText isNullable
+            , My.MySQLText dataType
+            , My.MySQLText columnKey
+            ] -> (tableSchema, tableName, PGColumnInfo
                 (unsafePGCol columnName)
                 (G.unsafeMkName columnName)
                 (fromIntegral ordinalPosition)
                 (PGColumnScalar (textToPGScalarType dataType))
                 (isNullable == "YES")
-                Nothing
-          return (FieldName columnName, FIColumn pgci)
+                Nothing, columnKey)) <$>
+    ( IO.toList =<< snd <$> My.query_ conn
+      ( My.Query
+        "SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, ORDINAL_POSITION, IS_NULLABLE, DATA_TYPE, COLUMN_KEY \
+        \FROM INFORMATION_SCHEMA.COLUMNS"
+      )
+    )
+  foreigns <- fmap (
+    \case [   My.MySQLText tableSchema
+            , My.MySQLText tableName
+            , columnName -- TODO maybe text
+            , constraintName -- TODO maybe text
+            , referencedTableSchema -- TODO maybe text
+            , refenencedTableName -- TODO maybe text
+            , referencedColumnName -- TODO maybe text
+            ] -> (tableSchema, tableName, columnName, constraintName, referencedTableSchema, refenencedTableName, referencedColumnName)) <$>
+    ( IO.toList =<< snd <$> My.query_ conn
+      ( My.Query
+        "SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, CONSTRAINT_NAME, REFERENCED_TABLE_SCHEMA, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME \
+        \FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE"
+      )
+    )
+
+  tableCache' <- for tables \table -> do
+    let (tableSchema, tableName, tableType) = table
+        tableIden = QualifiedObject (SchemaName tableSchema) (TableName tableName)
+    if tableType == "BASE TABLE"
+      then do
+      fieldInfoMap <- Map.fromList . catMaybes <$> for columns \column -> do
+        let (   tableSchema'
+              , tableName'
+              , pgci
+              , columnKey
+              ) = column
+        if tableSchema == tableSchema' && tableName == tableName'
+          then Just <$> do
+          return (FieldName (G.unName (pgiName pgci)), FIColumn pgci)
           else return Nothing
+      let primaryKeyCols = flip mapMaybe foreigns
+            \case (tableSchema', tableName', columnName, constraintName, referencedTableSchema, refenencedTableName, referencedColumnName) ->
+                    if tableSchema == tableSchema' && tableName == tableName' && constraintName == My.MySQLText "PRIMARY"
+                    then
+                      case columnName of
+                        My.MySQLText t -> Just t
+                        _ -> Nothing
+                    else Nothing
+          primKey = case fmap (\case (_,_,pgci,_) -> pgci) $ mapMaybe (\col -> find (\(ts', tn', pgci, _) -> tableSchema == ts' && tableName == tn' && G.unName (pgiName pgci) == col) columns) primaryKeyCols of
+            [] -> Nothing
+            -- TODO we're inserting some fake data here - MySQL has no OID, I think?
+            (c:cs) -> Just $ PrimaryKey (Constraint (ConstraintName "primarykey") (OID 0)) (c :<|| fromList cs)
+
       let tci = TableCoreInfo
                 tableIden
                 Nothing
                 (SystemDefined False)
                 fieldInfoMap
-                Nothing -- PrimaryKey
+                primKey -- PrimaryKey
                 mempty -- _uniqueConstraints
                 mempty -- _foreignKeys
                 Nothing -- ViewInfo
