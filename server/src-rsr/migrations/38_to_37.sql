830d5a45875507b209a177b629d0c7d8e7063a09
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2020-09-07 15:06:09 +0530

    fix the down migration

--- server/src-rsr/migrations/38_to_37.sql
+++ server/src-rsr/migrations/38_to_37.sql
@@ -2,13 +2,13 @@ ALTER TABLE hdb_catalog.hdb_cron_triggers
 DROP COLUMN created_at;
 
 ALTER TABLE hdb_catalog.hdb_cron_events
-ALTER COLUMN created_at TIMESTAMP default now();
+ALTER COLUMN created_at TYPE TIMESTAMP;
 
 ALTER TABLE hdb_catalog.hdb_cron_event_invocation_logs
-ALTER COLUMN created_at TIMESTAMP default now();
+ALTER COLUMN created_at TYPE TIMESTAMP;
 
 ALTER TABLE hdb_catalog.hdb_scheduled_events
-ALTER COLUMN created_at TIMESTAMP default now();
+ALTER COLUMN created_at TYPE TIMESTAMP;
 
 ALTER TABLE hdb_catalog.hdb_scheduled_event_invocation_logs
-ALTER COLUMN created_at TIMESTAMP default now();
+ALTER COLUMN created_at TYPE TIMESTAMP;
