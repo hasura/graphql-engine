b72ee9fcd08bd7d15492bacb66d0e1ccf8eee1ba
Aleksandra Sikora <aleksandra@hasura.io>
2020-09-08 13:07:18 +0200

    fix relationships bug

--- console/src/components/Services/Data/TableRelationships/autoRelations.js
+++ console/src/components/Services/Data/TableRelationships/autoRelations.js
@@ -52,7 +52,7 @@ const isExistingArrRel = (currentArrRels, relCols, relTable) => {
       currRCol = Object.values(arrRelDef.manual_configuration.column_mapping);
     }
 
-    if (currTable.table_name === relTable && sameRelCols(currRCol, relCols)) {
+    if (currTable.name === relTable && sameRelCols(currRCol, relCols)) {
       _isExistingArrRel = true;
       break;
     }
