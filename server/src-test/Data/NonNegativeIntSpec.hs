4d9627b99379a262b49c63317379ba3d40c7d4d2
Sameer Kolhar <kolhar730@gmail.com>
2020-09-10 17:58:38 +0530

    update NonNegInt spec

--- server/src-test/Data/NonNegativeIntSpec.hs
+++ server/src-test/Data/NonNegativeIntSpec.hs
@@ -2,9 +2,14 @@ module Data.NonNegativeIntSpec (spec) where
 -- | basic tests on NonNegativeIntType
 
 import           Data.Aeson              (ToJSON (toJSON), Value (Number), decode)
-import           Hasura.RQL.Types.Common (NonNegativeInt (..))
+import           Hasura.RQL.Types.Common (NonNegativeInt, mkNonNegativeInt)
 import           Prelude
 import           Test.Hspec              (Spec, describe, it, shouldBe, shouldNotBe)
+
+evalMaybeNonNegativeInt :: Maybe NonNegativeInt -> Bool
+evalMaybeNonNegativeInt x = case x of
+    Just _  -> True
+    Nothing -> False
 
 spec :: Spec
 spec = do
@@ -14,8 +19,8 @@ nonNegIntSpec :: Spec
 nonNegIntSpec =
     describe "non negative integer type" $ do
         it "only validates non negative integers" $ do
-            NonNegativeInt 23 `shouldBe` 23
-            NonNegativeInt (-23) `shouldNotBe` (-23)
+            (evalMaybeNonNegativeInt $ mkNonNegativeInt 23) `shouldBe` True
+            (evalMaybeNonNegativeInt $ mkNonNegativeInt (-23)) `shouldBe` False
 
         it "JSON accepts only non negative integers" $ do
             toJSON (23 :: NonNegativeInt) `shouldBe` Number 23
