887b3c78154f3afb1c31f9811b5bc9e4ee7bd39a
Amarnath Karthi <97amarnathk@gmail.com>
2020-10-04 19:17:45 +0530

    fix lint errors for /ci/migrate/util.go, fix #5831

--- cli/migrate/util.go
+++ cli/migrate/util.go
@@ -1,13 +1,10 @@
 package migrate
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	nurl "net/url"
 	"runtime"
 	"strings"
-	"time"
 
 	crontriggers "github.com/hasura/graphql-engine/cli/metadata/cron_triggers"
 
@@ -60,35 +57,6 @@ func suint64(n int64) uint64 {
 		panic(fmt.Sprintf("suint(%v) expects input >= 0", n))
 	}
 	return uint64(n)
-}
-
-// newSlowReader turns an io.ReadCloser into a slow io.ReadCloser.
-// Use this to simulate a slow internet connection.
-func newSlowReader(r io.ReadCloser) io.ReadCloser {
-	return &slowReader{
-		rx:     r,
-		reader: bufio.NewReader(r),
-	}
-}
-
-type slowReader struct {
-	rx     io.ReadCloser
-	reader *bufio.Reader
-}
-
-func (b *slowReader) Read(p []byte) (n int, err error) {
-	time.Sleep(10 * time.Millisecond)
-	c, err := b.reader.ReadByte()
-	if err != nil {
-		return 0, err
-	} else {
-		copy(p, []byte{c})
-		return 1, nil
-	}
-}
-
-func (b *slowReader) Close() error {
-	return b.rx.Close()
 }
 
 var errNoScheme = fmt.Errorf("no scheme")
