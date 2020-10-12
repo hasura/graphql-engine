e984ca293c26366cdc4572bd410faa16e0354616
rakeshkky <12475069+rakeshkky@users.noreply.github.com>
2020-10-12 19:05:08 +0530

    expose other functions/types in Server/SchemaUpdate.hs

--- server/src-lib/Hasura/Server/SchemaUpdate.hs
+++ server/src-lib/Hasura/Server/SchemaUpdate.hs
@@ -2,8 +2,17 @@
 module Hasura.Server.SchemaUpdate
   ( startSchemaSyncListenerThread
   , startSchemaSyncProcessorThread
+  , logThreadStarted
+  , processor
+  , refreshSchemaCache
+  , pgChannel
+  , logInfo
+  , logError
+  , ThreadError(..)
   , SchemaSyncEventRef
   , SchemaSyncCtx(..)
+  , ThreadType(..)
+  , SchemaSyncThreadLog(..)
   )
 where
 
