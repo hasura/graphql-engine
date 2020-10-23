c63145378ccd6de88b394f208f906e3c5c50dc89
Sameer Kolhar <kolhar730@gmail.com>
2020-10-23 18:29:10 +0530

    update and check request headers on Main

--- console/src/components/Main/Main.js
+++ console/src/components/Main/Main.js
@@ -35,10 +35,45 @@ import {
   setProClickState,
   getLoveConsentState,
   setLoveConsentState,
+  getUserType,
 } from './utils';
 import { getSchemaBaseRoute } from '../Common/utils/routesUtils';
 import LoveSection from './LoveSection';
 import { Help, ProPopup } from './components/';
+import { UPDATE_DATA_HEADERS } from '../Services/Data/DataActions';
+import { HASURA_COLLABORATOR_TOKEN } from '../../constants';
+import { UPDATE_CONSOLE_NOTIFICATIONS } from '../../telemetry/Actions';
+
+const updateRequestHeaders = props => {
+  const { requestHeaders, dispatch } = props;
+  const validHeaders = requestHeaders.filter(header => header.key !== '');
+  const headers = validHeaders.reduce((hdrs, header) => {
+    return { ...hdrs, [header.key]: header.value };
+  }, {});
+
+  dispatch({ type: UPDATE_DATA_HEADERS, data: headers });
+
+  const collabTokenPresent = validHeaders.find(
+    hdr => hdr.key.toLowerCase() === HASURA_COLLABORATOR_TOKEN
+  );
+
+  if (collabTokenPresent) {
+    const userID = getUserType(collabTokenPresent.value);
+    if (!props.console_opts.console_notifications[userID]) {
+      dispatch({
+        type: UPDATE_CONSOLE_NOTIFICATIONS,
+        data: {
+          ...props.console_opts.console_notifications,
+          [userID]: {
+            read: [],
+            date: null,
+            showBadge: true,
+          },
+        },
+      });
+    }
+  }
+};
 
 class Main extends React.Component {
   constructor(props) {
@@ -57,6 +92,7 @@ class Main extends React.Component {
   componentDidMount() {
     const { dispatch } = this.props;
 
+    updateRequestHeaders(this.props);
     dispatch(loadServerVersion()).then(() => {
       dispatch(featureCompatibilityInit());
 
@@ -72,6 +108,18 @@ class Main extends React.Component {
 
     dispatch(fetchPostgresVersion);
     dispatch(fetchServerConfig);
+  }
+
+  componentDidUpdate(prevProps) {
+    const prevHeaders = Object.keys(prevProps.requestHeaders);
+    const currHeaders = Object.keys(this.props.requestHeaders);
+
+    if (
+      prevHeaders.length !== currHeaders.length ||
+      prevHeaders.filter(hdr => !currHeaders.includes(hdr)).length
+    ) {
+      updateRequestHeaders(this.props);
+    }
   }
 
   toggleProPopup = () => {
@@ -368,6 +416,7 @@ const mapStateToProps = (state, ownProps) => {
     currentSchema: state.tables.currentSchema,
     metadata: state.metadata,
     console_opts: state.telemetry.console_opts,
+    requestHeaders: state.apiexplorer.displayedApi.request.headers,
   };
 };
 
