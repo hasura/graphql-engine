b380f2a888ea572cf145392e4bf81f631fdb58fd
Sameer Kolhar <kolhar730@gmail.com>
2020-10-24 11:27:33 +0530

    make minor changes for telemetry actions

--- console/src/telemetry/Actions.ts
+++ console/src/telemetry/Actions.ts
@@ -284,6 +284,20 @@ const loadConsoleOpts = () => {
                 },
               },
             });
+          } else if (
+            console_state.console_notifications &&
+            !console_state.console_notifications[userType]
+          ) {
+            dispatch({
+              type: UPDATE_CONSOLE_NOTIFICATIONS,
+              data: {
+                [userType]: {
+                  read: [],
+                  date: null,
+                  showBadge: true,
+                },
+              },
+            });
           }
 
           return Promise.resolve();
@@ -363,7 +377,10 @@ const telemetryReducer = (
         ...state,
         console_opts: {
           ...state.console_opts,
-          console_notifications: action.data,
+          console_notifications: {
+            ...state.console_opts?.console_notifications,
+            ...action.data,
+          },
         },
       };
     default:
