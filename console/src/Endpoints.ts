34b91a4370d3a7e39baa61d522162ba13d3b6b49
Sameer Kolhar <kolhar730@gmail.com>
2020-11-04 00:52:52 +0530

    update staging endpoint

--- console/src/Endpoints.ts
+++ console/src/Endpoints.ts
@@ -20,7 +20,8 @@ const Endpoints = {
   hasuractlMetadata: `${hasuractlUrl}/apis/metadata`,
   hasuractlMigrateSettings: `${hasuractlUrl}/apis/migrate/settings`,
   telemetryServer: 'wss://telemetry.hasura.io/v1/ws',
-  consoleNotificationsStg: 'https://data.hasura-stg.hasura-app.io/v1/query',
+  consoleNotificationsStg:
+    'https://notifications.hasura-stg.hasura-app.io/v1/graphql',
   consoleNotificationsProd: 'https://data.hasura.io/v1/query',
 };
 
