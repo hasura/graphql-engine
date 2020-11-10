64ab16f31c51e3a98dfc78e73875f5428bd0c2ed
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2020-11-10 19:13:02 +0530

    reorder the migration files instead of squashing

--- server/src-rsr/migrations/39_to_38.sql
+++ server/src-rsr/migrations/39_to_38.sql
@@ -1,3 +1,37 @@
-ALTER TABLE hdb_catalog.event_log ALTER COLUMN locked TYPE BOOLEAN USING locked IS NOT NULL;
-ALTER TABLE hdb_catalog.event_log ALTER COLUMN locked SET NOT NULL;
-ALTER TABLE hdb_catalog.event_log ALTER COLUMN locked SET DEFAULT false;
+CREATE
+OR REPLACE VIEW hdb_catalog.hdb_function_info_agg AS (
+    SELECT
+        function_name,
+        function_schema,
+        row_to_json (
+            (
+                SELECT
+                    e
+                FROM
+                    (
+                        SELECT
+                            description,
+                            has_variadic,
+                            function_type,
+                            return_type_schema,
+                            return_type_name,
+                            return_type_type,
+                            returns_set,
+                            input_arg_types,
+                            input_arg_names,
+                            default_args,
+                            exists(
+                                SELECT
+                                    1
+                                FROM
+                                    information_schema.tables
+                                WHERE
+                                    table_schema = return_type_schema
+                                    AND table_name = return_type_name
+                            ) AS returns_table
+                    ) AS e
+            )
+        ) AS "function_info"
+    FROM
+        hdb_catalog.hdb_function_agg
+);
