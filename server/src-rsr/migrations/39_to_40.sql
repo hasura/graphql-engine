1333a96fc03590bbb9bc91580c256dc974100ab6
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2020-11-10 18:16:49 +0530

    server: squash v1.3.3 migrations into a single file

--- server/src-rsr/migrations/39_to_40.sql
+++ server/src-rsr/migrations/39_to_40.sql
@@ -1,46 +1,34 @@
-CREATE
-OR REPLACE VIEW hdb_catalog.hdb_function_info_agg AS (
-    SELECT
-        function_name,
-        function_schema,
-        row_to_json (
-            (
-                SELECT
-                    e
-                FROM
-                    (
-                        SELECT
-                            description,
-                            has_variadic,
-                            function_type,
-                            return_type_schema,
-                            return_type_name,
-                            return_type_type,
-                            returns_set,
-                            input_arg_types,
-                            input_arg_names,
-                            default_args,
-                            exists(
-                                SELECT
-                                    1
-                                FROM
-                                    information_schema.tables
-                                WHERE
-                                    table_schema = return_type_schema
-                                    AND table_name = return_type_name
-                            )
-                            OR exists(
-                                SELECT
-                                    1
-                                FROM
-                                    pg_matviews
-                                WHERE
-                                    schemaname = return_type_schema
-                                    AND matviewname = return_type_name
-                            ) AS returns_table
-                    ) AS e
-            )
-        ) AS "function_info"
-    FROM
-        hdb_catalog.hdb_function_agg
-);
+CREATE FUNCTION hdb_catalog.gen_hasura_uuid() RETURNS uuid AS
+'select gen_random_uuid()' LANGUAGE SQL;
+
+ALTER TABLE hdb_catalog.hdb_version
+ALTER COLUMN hasura_uuid
+SET DEFAULT hdb_catalog.gen_hasura_uuid();
+
+ALTER TABLE hdb_catalog.event_log
+ALTER COLUMN id
+SET DEFAULT hdb_catalog.gen_hasura_uuid();
+
+ALTER TABLE hdb_catalog.event_invocation_logs
+ALTER COLUMN id
+SET DEFAULT hdb_catalog.gen_hasura_uuid();
+
+ALTER TABLE hdb_catalog.hdb_action_log
+ALTER COLUMN id
+SET DEFAULT hdb_catalog.gen_hasura_uuid();
+
+ALTER TABLE hdb_catalog.hdb_cron_events
+ALTER COLUMN id
+SET DEFAULT hdb_catalog.gen_hasura_uuid();
+
+ALTER TABLE hdb_catalog.hdb_cron_event_invocation_logs
+ALTER COLUMN id
+SET DEFAULT hdb_catalog.gen_hasura_uuid();
+
+ALTER TABLE hdb_catalog.hdb_scheduled_events
+ALTER COLUMN id
+SET DEFAULT hdb_catalog.gen_hasura_uuid();
+
+ALTER TABLE hdb_catalog.hdb_scheduled_event_invocation_logs
+ALTER COLUMN id
+SET DEFAULT hdb_catalog.gen_hasura_uuid();
