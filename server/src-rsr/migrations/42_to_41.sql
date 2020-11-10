0c62360aef8488ecd1d9fd8ef14a6cb8025395e6
Tirumarai Selvan A <tiru@hasura.io>
2020-11-10 11:47:32 +0530

    add trigger function to perform "ON UPDATE CASCADE"
    Previously, we were relying on "ON UPDATE CASCADE" on the foreign key constraint to do this automatically.

--- server/src-rsr/migrations/42_to_41.sql
+++ server/src-rsr/migrations/42_to_41.sql
@@ -1,3 +1,7 @@
+DROP TRIGGER event_trigger_table_name_update_trigger ON hdb_catalog.hdb_table;
+
+DROP FUNCTION hdb_catalog.event_trigger_table_name_update();
+
 ALTER TABLE hdb_catalog.event_triggers
   ADD CONSTRAINT event_triggers_schema_name_table_name_fkey
     FOREIGN KEY (schema_name, table_name)
