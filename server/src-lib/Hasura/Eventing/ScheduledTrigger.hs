ff62b01688fcf4280a887aac1acb8b932bdab8a7
rakeshkky <12475069+rakeshkky@users.noreply.github.com>
2020-11-12 12:53:54 +0530

    fix getDeprivedCronTriggerStats SQL query

--- server/src-lib/Hasura/Eventing/ScheduledTrigger.hs
+++ server/src-lib/Hasura/Eventing/ScheduledTrigger.hs
@@ -109,8 +109,8 @@ import           Hasura.HTTP
 import           Hasura.RQL.DDL.EventTrigger        (getHeaderInfosFromConf)
 import           Hasura.RQL.DDL.Headers
 import           Hasura.RQL.Types
-import           Hasura.SQL.Types
 import           Hasura.Server.Version              (HasVersion)
+import           Hasura.SQL.Types
 
 
 newtype ScheduledTriggerInternalErr
@@ -286,10 +286,17 @@ runCronEventsGenerator logger pgpool getSC = do
         map uncurryStats <$>
           Q.listQE defaultTxErrorHandler
           [Q.sql|
-           SELECT name, upcoming_events_count, max_scheduled_time
-            FROM hdb_catalog.hdb_cron_events_stats
-            WHERE upcoming_events_count < 100
-           |] () True
+            SELECT * FROM
+             ( SELECT
+                trigger_name,
+                 count(*) as upcoming_events_count,
+                 max(scheduled_time) as max_scheduled_time
+                FROM hdb_catalog.hdb_cron_events
+                WHERE tries = 0 and status = 'scheduled'
+                GROUP BY trigger_name
+             ) AS q
+            WHERE q.upcoming_events_count < 100
+          |] () True
 
       uncurryStats (n, count, maxTs) = CronTriggerStats n count maxTs
 
