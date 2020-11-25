e1f533bb44fbf1cf7d7eab5953aed78e37e02b3f
christophediprima <dipdipdip84@gmail.com>
2020-11-25 15:01:33 +0100

    fix test paths

--- server/tests-py/test_v1_queries.py
+++ server/tests-py/test_v1_queries.py
@@ -172,13 +172,13 @@ class TestV1SelectBoolExpSearch:
         check_query_f(hge_ctx, self.dir() + '/select_city_where_regex.yaml')
 
     def test_city_where_not_regex(self, hge_ctx):
-        check_query_f(hge_ctx, self.dir() + '/select_city_where_not_regex.yaml')
+        check_query_f(hge_ctx, self.dir() + '/select_city_where_nregex.yaml')
 
     def test_city_where_iregex(self, hge_ctx):
         check_query_f(hge_ctx, self.dir() + '/select_city_where_iregex.yaml')
 
     def test_city_where_not_iregex(self, hge_ctx):
-        check_query_f(hge_ctx, self.dir() + '/select_city_where_not_iregex.yaml')
+        check_query_f(hge_ctx, self.dir() + '/select_city_where_niregex.yaml')
 
     @classmethod
     def dir(cls):
