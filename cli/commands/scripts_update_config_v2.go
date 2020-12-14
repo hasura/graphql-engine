7d1027c559f2dee05bfb1adeee59e073b4a16d53
Kali Vara Purushotham Santhati <72007599+purush7@users.noreply.github.com>
2020-12-14 21:33:15 +0530

    update with logger.Warnln

--- cli/commands/scripts_update_config_v2.go
+++ cli/commands/scripts_update_config_v2.go
@@ -347,7 +347,7 @@ func newScriptsUpdateConfigV2Cmd(ec *cli.ExecutionContext) *cobra.Command {
 			if _, err = os.Stat(filepath.Join(ec.MigrationDir, "metadata.yaml")); err == nil || os.IsExist(err) {
 				err = os.Remove(filepath.Join(ec.MigrationDir, "metadata.yaml"))
 				if err != nil {
-					ec.Logger.Infoln("Warning: cannot remove metadata.yaml file")
+					ec.Logger.Warnln("Warning: cannot remove metadata.yaml file")
 				}
 			}
 
