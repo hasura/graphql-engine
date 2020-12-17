608959c4ee5835a7ad4f4170cd331c99444564aa
soorajshankar <soorajshankar@gmail.com>
2020-12-17 11:58:21 +0530

    dev changes

---
+++ console/src/components/Services/RemoteSchema/Permissions/ArgSelect.tsx
@@ -0,0 +1,69 @@
+import React ,{useRef,useEffect,useState}from 'react'
+import {  getChildArgument} from './utils';
+import RSPInput from './RSPInput';
+
+export const ArgSelect = ({ k, v, value, level, setArg = e => console.log(e) }) => {
+    const [expanded, setExpanded] = useState(false);
+    const [editMode, setEditMode] = useState(
+      value && (typeof value === 'string' && value.length > 0 || typeof value === 'number')
+    );
+    const prevState = useRef();
+    useEffect(() => {
+      if (value && typeof value === 'string' && value.length > 0 && !editMode) {
+        // show value instead of pen icon, if the value is defined in the prop
+        setEditMode(true);
+      }
+    }, [value]);
+  
+    const { children, path } = getChildArgument(v);
+  
+    const setArgVal = val => {
+      const prevVal = prevState.current;
+      if (prevVal) {
+        const newState = _.merge(prevVal, val);
+        setArg(newState);
+        prevState.current = newState;
+      } else {
+        setArg(val);
+        prevState.current = val;
+      }
+    };
+  
+    const toggleExpandMode = () => setExpanded(b => !b);
+  
+    if (children) {
+      return (
+        <ul>
+          <button onClick={toggleExpandMode} style={{ marginLeft: '-1em' }}>
+            {expanded ? '-' : '+'}
+          </button>
+          <label style={{ cursor: 'pointer' }} htmlFor={k}>
+            {k}:
+          </label>
+          {expanded &&
+            Object.values(children).map(i => {
+              const childVal = value ? value[i?.name] : undefined;
+              return (
+                <li>
+                  <ArgSelect
+                    {...{
+                      k: i.name,
+                      setArg: v => setArgVal({ [k]: v }),
+                      v: i,
+                      value: childVal,
+                      level: level + 1,
+                    }}
+                  />
+                </li>
+              );
+            })}
+        </ul>
+      );
+    }
+    return (
+      <li>
+        <RSPInput {...{ k, editMode, value, setArgVal, v, setEditMode }} />
+      </li>
+    );
+  };
+  
--- console/src/components/Services/RemoteSchema/Permissions/PermissionEditor.tsx
+++ console/src/components/Services/RemoteSchema/Permissions/PermissionEditor.tsx
@@ -1,21 +1,17 @@
 import React, {
-  createContext,
   useCallback,
-  useContext,
   useEffect,
-  useRef,
   useState,
 } from 'react';
 import * as GQL from 'graphql';
 import _ from 'lodash';
-import { generateSDL, generateConstantTypes, getChildArgument, getArgTreeFromPermissionSDL } from './utils';
+import { generateSDL, generateConstantTypes,  getArgTreeFromPermissionSDL } from './utils';
 import Button from '../../../Common/Button/Button';
 import styles from '../../../Common/Permissions/PermissionStyles.scss';
 import { DatasourceObject, FieldType } from './types';
-import RSPInput from './RSPInput';
-import { isEmpty } from '../../../Common/utils/jsUtils';
+import { Field } from './Field';
+import { PermissionEditorContext } from './context';
 
-const PermissionEditorContext = createContext({});
 interface RSPTreeComponentProps {
   list: FieldType[];
   setState: (d: FieldType[]) => void;
@@ -91,171 +87,10 @@ const Tree: React.FC<RSPTreeComponentProps> = ({ list, setState }) => {
 
 const MemoizedTree = React.memo(Tree);
 // TODO seperate components
-interface CollapsedFieldProps {
-  field: any;
-  i: FieldType;
-  onClick: (e: React.MouseEvent<HTMLAnchorElement>) => void;
-}
-const CollapsedField: React.FC<CollapsedFieldProps> = ({
-  field: i,
-  onClick,
-}) => (
-    <>
-      <b id={i.name}>{i.name}</b>
-      {i.return && (
-        <b>
-          :
-          <a
-            onClick={onClick}
-            id={`${i.return.replace(/[^\w\s]/gi, '')}`}
-            href={`${i.return.replace(/[^\w\s]/gi, '')}`}
-          >
-            {i.return}
-          </a>
-        </b>
-      )}
-    </>
-  );
+
 
 // TODO seperate components
-interface FieldProps {
-  i: FieldType;
-  setItem: (e?: FieldType) => void;
-}
 
-const Field: React.FC<FieldProps> = ({ i, setItem = e => console.log(e) }) => {
-  const context: any = useContext(PermissionEditorContext);
-  const initState = context.argTree && context.argTree[i.name] ? { ...context.argTree[i.name] } : {}
-  const [fieldVal, setfieldVal] = useState(initState);
-  const setArg = useCallback(
-    (k, v) => (vStr: Record<string, unknown>) => {
-      setfieldVal(oldVal => {
-        const newState = {
-          ...oldVal,
-          ...vStr,
-        };
-        return newState;
-      });
-    },
-    [setItem, i]
-  );
-  useEffect(() => {
-    if (fieldVal && fieldVal !== {} && Object.keys(fieldVal).length > 0 && !isEmpty(fieldVal)) {
-      context.setArgTree(argTree => {
-        return { ...argTree, [i.name]: fieldVal };
-      });
-    }
-  }, [fieldVal]);
-
-  const handleClick = e => {
-    e.preventDefault();
-    const selectedTypeName = e.target.id;
-    const selectedUrl = e.target.href;
-  };
-
-  if (!i.checked) return <CollapsedField field={i} onClick={handleClick} />;
-  return (
-    <b>
-      <b id={i.name}>{i.name}</b>
-      {i.args && ' ('}
-      <ul>
-        {i.args &&
-          Object.entries(i.args).map(([k, v]) => (
-            <ArgSelect
-              {...{
-                key: k,
-                k,
-                v,
-                value: fieldVal[k],
-                setArg: setArg(k, v),
-                level: 0,
-              }}
-            />
-          ))}
-      </ul>
-      <ul>
-        {i.args && ' )'}
-        {i.return && (
-          <>
-            :
-            <a
-              onClick={handleClick}
-              id={`${i.return.replace(/[^\w\s]/gi, '')}`}
-              href={`${i.return.replace(/[^\w\s]/gi, '')}`}
-            >
-              {i.return}
-            </a>
-          </>
-        )}
-      </ul>
-    </b>
-  );
-};
-
-const ArgSelect = ({ k, v, value, level, setArg = e => console.log(e) }) => {
-  const [expanded, setExpanded] = useState(false);
-  const [editMode, setEditMode] = useState(
-    value && (typeof value === 'string' && value.length > 0 || typeof value === 'number')
-  );
-  const prevState = useRef();
-  useEffect(() => {
-    if (value && typeof value === 'string' && value.length > 0 && !editMode) {
-      // show value instead of pen icon, if the value is defined in the prop
-      setEditMode(true);
-    }
-  }, [value]);
-
-  const { children, path } = getChildArgument(v);
-
-  const setArgVal = val => {
-    const prevVal = prevState.current;
-    if (prevVal) {
-      const newState = _.merge(prevVal, val);
-      setArg(newState);
-      prevState.current = newState;
-    } else {
-      setArg(val);
-      prevState.current = val;
-    }
-  };
-
-  const toggleExpandMode = () => setExpanded(b => !b);
-
-  if (children) {
-    return (
-      <ul>
-        <button onClick={toggleExpandMode} style={{ marginLeft: '-1em' }}>
-          {expanded ? '-' : '+'}
-        </button>
-        <label style={{ cursor: 'pointer' }} htmlFor={k}>
-          {k}:
-        </label>
-        {expanded &&
-          Object.values(children).map(i => {
-            const childVal = value ? value[i?.name] : undefined;
-            return (
-              <li>
-                <ArgSelect
-                  {...{
-                    k: i.name,
-                    setArg: v => setArgVal({ [k]: v }),
-                    v: i,
-                    value: childVal,
-                    level: level + 1,
-                  }}
-                />
-              </li>
-            );
-          })}
-      </ul>
-    );
-  }
-  return (
-    <li>
-      <RSPInput {...{ k, editMode, value, setArgVal, v, setEditMode }} />
-    </li>
-  );
-};
 
 declare const window: any;
 
@@ -351,8 +186,8 @@ const PermissionEditor = ({ ...props }: any) => {
     <div className={styles.activeEdit}>
       <div className={styles.tree}>
         <PermissionEditorContext.Provider value={{ argTree, setArgTree }}>
-          <MemoizedTree list={state} setState={setState} />
-          <code style={{ whiteSpace: 'pre-wrap' }}>{resultString}</code>
+          <MemoizedTree list={state as FieldType[]} setState={setState} />
+          {/* <code s tyle={{ whiteSpace: 'pre-wrap' }}>{resultString}</code> */}
         </PermissionEditorContext.Provider>
       </div>
       <Button
