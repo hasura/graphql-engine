15ed0cf53606be67abf69199efed70e5583b3900
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2021-03-03 17:16:08 +0530

    server: document the remote joins architecture
    GitOrigin-RevId: f142908fa3dd3b0cb8887521c639be4a017e606a

--- server/src-lib/Hasura/GraphQL/Execute/RemoteJoin.hs
+++ server/src-lib/Hasura/GraphQL/Execute/RemoteJoin.hs
@@ -24,6 +24,33 @@ import           Hasura.RQL.IR.Returning
 import           Hasura.RQL.IR.Select
 import           Hasura.RQL.Types
 
+{- Note: [Remote Joins Architecture]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ Unparsed Incoming GraphQL  +------------------------------+
+--------------------------> | Parsing of the GraphQL query |-----+
+                            +------------------------------+     |
+                                                                 | DB Query and remote joins (if any)
+                                                                 |
+                                                                 V
++----------------------------------+  SQL query response  +----------------------------+
+|  Traverse the DB response to     | <------------------- |  Execution of the DB query |
+|  get the values of the arguments |                      +----------------------------+
+|   of the remote field            |
++----------------------------------+
+             |
+             | Remote field arguments
+             V
++--------------------------+  Remote schema response   +----------------------------------------+
+| Query the remote schema  | ------------------------> | Replace the remote join fields in      |
+| with the remote field    |                           | the SQL query response (JSON) with     |
+| arguments to the remote  |                           | the response obtained from the remote  |
+| field configured in the  |                           | schema at appropriate places.          |
+| remote join.             |                           +----------------------------------------+
++--------------------------+
+-}
+
+
 
 -- remote joins info
 
