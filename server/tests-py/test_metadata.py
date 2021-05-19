03c70c7b7d4c8ecd271a74cb3a8200ecf228d21d
Antoine Leblanc <antoine@hasura.io>
2021-05-19 22:06:06 +0100

    tests: skip test_pg_multisource_table_name_conflict if the setup isn't correct
    GitOrigin-RevId: 4e658812eaac6534e6b6606f03e893449ad4b975

--- server/tests-py/test_metadata.py
+++ server/tests-py/test_metadata.py
@@ -1,6 +1,7 @@
 import ruamel.yaml as yaml
 from validate import check_query_f
 import pytest
+import os
 
 usefixtures = pytest.mark.usefixtures
 
@@ -51,6 +52,11 @@ class TestMetadata:
     def test_pg_multisource_query(self, hge_ctx):
         check_query_f(hge_ctx, self.dir() + '/pg_multisource_query.yaml')
 
+    @pytest.mark.skipif(
+        os.getenv('HASURA_GRAPHQL_PG_SOURCE_URL_1') == os.getenv('HASURA_GRAPHQL_PG_SOURCE_URL_2') or
+        os.getenv('HASURA_GRAPHQL_PG_SOURCE_URL_1') is None or
+        os.getenv('HASURA_GRAPHQL_PG_SOURCE_URL_2') is None,
+        reason="We need two different and valid instances of postgres for this test.")
     def test_pg_multisource_table_name_conflict(self, hge_ctx):
         check_query_f(hge_ctx, self.dir() + '/pg_multisource_table_name_conflict.yaml')
 
