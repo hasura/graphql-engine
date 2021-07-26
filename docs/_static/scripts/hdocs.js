8d5ae2a1ecc7a0494b08b46324c1329631ad5ffb
Rikin Kachhia <54616969+rikinsk@users.noreply.github.com>
2021-07-26 15:04:23 +0530

    docs: remove feedback section
    https://github.com/hasura/graphql-engine-mono/pull/1896
    
    GitOrigin-RevId: 31a60cc3b34f94fcb57033d3f789ecc0f9187dbb

--- docs/_static/scripts/hdocs.js
+++ docs/_static/scripts/hdocs.js
@@ -10,10 +10,11 @@ window.hdocs = (function () {
       document.getElementById('nav_tree_icon').addEventListener('click', hdocs.handleNavClick);
       document.getElementById('sidebar-close').addEventListener('click', hdocs.handleNavClick);
 
-      document.getElementById('thumb_up_button').addEventListener('click', function () { hdocs.sendFeedback('positive', 'Great to hear that! If you have any other feedback, please share here:') });
-      document.getElementById('thumb_down_button').addEventListener('click', function () { hdocs.sendFeedback('negative', 'Sorry to hear that. Please tell us what you were looking for:') });
-      document.getElementById('feedback_btn').addEventListener('click', hdocs.handleSubmitFeedback);
-      document.getElementById('close-banner').addEventListener('click', hdocs.closeBanner)
+      document.getElementById('close-banner').addEventListener('click', hdocs.closeBanner);
+
+      // document.getElementById('thumb_up_button').addEventListener('click', function () { hdocs.sendFeedback('positive', 'Great to hear that! If you have any other feedback, please share here:') });
+      // document.getElementById('thumb_down_button').addEventListener('click', function () { hdocs.sendFeedback('negative', 'Sorry to hear that. Please tell us what you were looking for:') });
+      // document.getElementById('feedback_btn').addEventListener('click', hdocs.handleSubmitFeedback);
 
       docsearch({
         appId: 'WCBB1VVLRC',
@@ -170,63 +171,63 @@ window.hdocs = (function () {
         body.classList.remove('no_scroll');
       }
     },
-    sendFeedback: function (feedback, feedbackDetailMsg) {
-      const insertQuery = {
-        'type': 'insert',
-        'args': {
-          'table': 'docs_feedback',
-          'objects': [
-            {
-              'page': window.location.pathname,
-              'feedback': feedback
-            }
-          ],
-          'returning': ['id']
-        }
-      };
-
-      hdocs.request(DB_URL, insertQuery, 'POST')
-        .then(function (response) {
-          const data = JSON.parse(response);
-
-          if (feedback == 'positive' || feedback == 'negative') {
-            document.getElementById('feedback_box').setAttribute('data-id', data['returning'][0]['id']);
-            document.getElementById('feedback_detail_msg').innerHTML = feedbackDetailMsg;
-            document.getElementById('feedback').classList.add('hide');
-            document.getElementById('detailed_feedback').classList.remove('hide');
-          }
-        })
-        .catch(function (err) {
-          alert('Error capturing feedback');
-          console.log(err);
-        });
-    },
-    handleSubmitFeedback: function () {
-      const feedbackBox = document.getElementById('feedback_box');
-      const feedbackId = feedbackBox.getAttribute('data-id');
-      const feedbackContent = feedbackBox.value;
-
-      const updateQuery = {
-        'type': 'update',
-        'args': {
-          'table': 'docs_feedback',
-          '$set': {
-            'feedback_content': feedbackContent
-          },
-          'where': { 'id': feedbackId }
-        }
-      };
-
-      hdocs.request(DB_URL, updateQuery, 'POST')
-        .then(function (response) {
-          document.getElementById('detailed_feedback').classList.add('hide');
-          document.getElementById('thank_you').classList.remove('hide');
-        })
-        .catch(function (err) {
-          alert('Error capturing feedback');
-          console.log(err);
-        });
-    },
+    // sendFeedback: function (feedback, feedbackDetailMsg) {
+    //   const insertQuery = {
+    //     'type': 'insert',
+    //     'args': {
+    //       'table': 'docs_feedback',
+    //       'objects': [
+    //         {
+    //           'page': window.location.pathname,
+    //           'feedback': feedback
+    //         }
+    //       ],
+    //       'returning': ['id']
+    //     }
+    //   };
+    //
+    //   hdocs.request(DB_URL, insertQuery, 'POST')
+    //     .then(function (response) {
+    //       const data = JSON.parse(response);
+    //
+    //       if (feedback == 'positive' || feedback == 'negative') {
+    //         document.getElementById('feedback_box').setAttribute('data-id', data['returning'][0]['id']);
+    //         document.getElementById('feedback_detail_msg').innerHTML = feedbackDetailMsg;
+    //         document.getElementById('feedback').classList.add('hide');
+    //         document.getElementById('detailed_feedback').classList.remove('hide');
+    //       }
+    //     })
+    //     .catch(function (err) {
+    //       alert('Error capturing feedback');
+    //       console.log(err);
+    //     });
+    // },
+    // handleSubmitFeedback: function () {
+    //   const feedbackBox = document.getElementById('feedback_box');
+    //   const feedbackId = feedbackBox.getAttribute('data-id');
+    //   const feedbackContent = feedbackBox.value;
+    //
+    //   const updateQuery = {
+    //     'type': 'update',
+    //     'args': {
+    //       'table': 'docs_feedback',
+    //       '$set': {
+    //         'feedback_content': feedbackContent
+    //       },
+    //       'where': { 'id': feedbackId }
+    //     }
+    //   };
+    //
+    //   hdocs.request(DB_URL, updateQuery, 'POST')
+    //     .then(function (response) {
+    //       document.getElementById('detailed_feedback').classList.add('hide');
+    //       document.getElementById('thank_you').classList.remove('hide');
+    //     })
+    //     .catch(function (err) {
+    //       alert('Error capturing feedback');
+    //       console.log(err);
+    //     });
+    // },
     graphQLFetcher: function (endpoint) {
       endpoint = endpoint || HDOCS_GRAPHIQL_DEFAULT_ENDPOINT;
 
