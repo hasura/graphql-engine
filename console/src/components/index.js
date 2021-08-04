121afe39fec37f9368862789bca6bc9b9a50987a
Varun Choudhary <68095256+Varun-Choudhary@users.noreply.github.com>
2021-08-04 19:06:11 +0530

    console: remove "UIKit" components from the console codebase
    https://github.com/hasura/graphql-engine-mono/pull/1834
    
    GitOrigin-RevId: 84d601072552f37740321686905e58677b5aa31c

--- console/src/components/index.js
+++ console/src/components/index.js
@@ -2,6 +2,5 @@ import App from './App/App';
 import Main from './Main/Main';
 import PageNotFound from './Error/PageNotFound';
 import Login from './Login/Login';
-import UIKit from './UIKit';
 
-export { App, Main, PageNotFound, Login, UIKit };
+export { App, Main, PageNotFound, Login };
