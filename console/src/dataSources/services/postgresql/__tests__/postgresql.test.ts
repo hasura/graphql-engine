4b7c0395f328883cec99fd42fb2c5bf3cfde71cf
Vijay Prasanna <vijayprasanna2896@gmail.com>
2021-08-18 15:02:14 +0530

    console: add quotes to column names
    https://github.com/hasura/graphql-engine-mono/pull/2127
    
    GitOrigin-RevId: 6c1596ad132a400f2c2864712d7ffa3a4a7ae2f2

--- console/src/dataSources/services/postgresql/__tests__/postgresql.test.ts
+++ console/src/dataSources/services/postgresql/__tests__/postgresql.test.ts
@@ -30,4 +30,79 @@ describe('postgresql datasource tests', () => {
       expect(query).toMatchSnapshot();
     });
   });
+
+  describe('getCreateIndexSQL', () => {
+    const { createIndexSql } = postgres;
+    it('generates proper SQL for column names containing no spaces', () => {
+      if (createIndexSql) {
+        const query = createIndexSql({
+          table: { schema: 'test_schema', name: 'test_table' },
+          columns: ['firstname', 'lastname'],
+          indexName: 'test_index_name',
+          indexType: 'btree',
+          unique: false,
+        });
+        expect(query).toMatchInlineSnapshot(`
+          "
+            CREATE  INDEX \\"test_index_name\\" on
+            \\"test_schema\\".\\"test_table\\" using btree (\\"firstname\\", \\"lastname\\");
+          "
+        `);
+      }
+    });
+
+    it('generates proper SQL for column names in camel case', () => {
+      if (createIndexSql) {
+        const query = createIndexSql({
+          table: { schema: 'test_schema', name: 'test_table' },
+          columns: ['firstName', 'lastName'],
+          indexName: 'test_index_name',
+          indexType: 'btree',
+          unique: false,
+        });
+        expect(query).toMatchInlineSnapshot(`
+          "
+            CREATE  INDEX \\"test_index_name\\" on
+            \\"test_schema\\".\\"test_table\\" using btree (\\"firstName\\", \\"lastName\\");
+          "
+        `);
+      }
+    });
+
+    it('generates proper SQL for column names containing spaces', () => {
+      if (createIndexSql) {
+        const query = createIndexSql({
+          table: { schema: 'test_schema', name: 'test_table' },
+          columns: ['first name', 'last name'],
+          indexName: 'test_index_name',
+          indexType: 'btree',
+          unique: false,
+        });
+        expect(query).toMatchInlineSnapshot(`
+          "
+            CREATE  INDEX \\"test_index_name\\" on
+            \\"test_schema\\".\\"test_table\\" using btree (\\"first name\\", \\"last name\\");
+          "
+        `);
+      }
+    });
+
+    it('generates proper SQL for with unique param set to true', () => {
+      if (createIndexSql) {
+        const query = createIndexSql({
+          table: { schema: 'test_schema', name: 'test_table' },
+          columns: ['first name', 'last_name', 'fullName'],
+          indexName: 'test_index_name',
+          indexType: 'btree',
+          unique: true,
+        });
+        expect(query).toMatchInlineSnapshot(`
+          "
+            CREATE UNIQUE INDEX \\"test_index_name\\" on
+            \\"test_schema\\".\\"test_table\\" using btree (\\"first name\\", \\"last_name\\", \\"fullName\\");
+          "
+        `);
+      }
+    });
+  });
 });
