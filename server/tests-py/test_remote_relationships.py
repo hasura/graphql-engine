88aa42a98686a182d239a527d3675ddff43ea347
Auke Booij <auke@hasura.io>
2021-08-25 23:05:00 +0200

    server: Add regression tests for hasura/graphql-engine#7172
    In hasura/graphql-engine#7172, an issue was found where under certain conditions a JSON field from Postgres would be parsed as a GraphQL input object, which is not possible in general, and also unnecessary. Luckily, this was already fixed by the time `v2.0.6` got around, presumably thanks to 4a83bb183422eabc9fff2109d8b832d8cd198bf3. This adds a regression test.
    
    https://github.com/hasura/graphql-engine-mono/pull/2158
    
    GitOrigin-RevId: 1ded1456f6b89726e08f77cf3383ad88c04de451

--- server/tests-py/test_remote_relationships.py
+++ server/tests-py/test_remote_relationships.py
@@ -186,6 +186,11 @@ class TestExecution:
         assert st_code == 200, resp
         check_query_f(hge_ctx, self.dir() + 'query_with_arr_rel.yaml')
 
+    def test_regression_7172(self, hge_ctx):
+        st_code, resp = hge_ctx.v1q_f(self.dir() + 'setup_regression_7172.yaml')
+        assert st_code == 200, resp
+        check_query_f(hge_ctx, self.dir() + 'regression_7172.yaml')
+
     def test_basic_relationship_joining_singleton_to_array(self, hge_ctx):
         st_code, resp = hge_ctx.v1q_f(self.dir() + 'setup_remote_rel_joining_singleton_with_array.yaml')
         assert st_code == 200, resp
