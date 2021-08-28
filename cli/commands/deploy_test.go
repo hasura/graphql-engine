2fc3972e1e29321c4506f4ea030726b562683f57
Ajay Tripathi <ajay39in@gmail.com>
2021-08-29 01:15:48 +0530

    cli: improved deploy e2e tests

--- cli/commands/deploy_test.go
+++ cli/commands/deploy_test.go
@@ -12,29 +12,49 @@ import (
 	. "github.com/onsi/gomega/gexec"
 )
 
-var commonDeployCommandsTest = func(projectDirectory string) {
+var commonDeployCommandsTest = func(projectDirectory string, dbName string) {
 	Context("should apply metadata, run migrations and reload metadata", func() {
+		// Run Deploy
 		session := testutil.Hasura(testutil.CmdOpts{
 			Args:             []string{"deploy"},
 			WorkingDirectory: projectDirectory,
 		})
 		Eventually(session, timeout).Should(Exit(0))
-		output := session.Err.Contents()
-		Expect(output).Should(ContainSubstring("Metadata applied"))
-		Expect(output).Should(ContainSubstring("migrations applied on database"))
-		Expect(output).Should(ContainSubstring("Metadata reloaded"))
+		deploy_output := session.Err.Contents()
+		Expect(deploy_output).Should(ContainSubstring("Metadata applied"))
+		Expect(deploy_output).Should(ContainSubstring("migrations applied on database"))
+		Expect(deploy_output).Should(ContainSubstring("Metadata reloaded"))
+
+		// Ensure metadata is applied
+		metadata_session := testutil.Hasura(testutil.CmdOpts{
+			Args:             []string{"metadata", "inconsistency", "status"},
+			WorkingDirectory: projectDirectory,
+		})
+		Eventually(metadata_session, timeout).Should(Exit(0))
+		metadata_output := string(metadata_session.Err.Contents())
+		Expect(metadata_output).Should(ContainSubstring("metadata is consistent"))
+
+		// Ensure migrations are applied
+		migrate_session := testutil.Hasura(testutil.CmdOpts{
+			Args:             []string{"migrate", "status", "--database-name", dbName},
+			WorkingDirectory: projectDirectory,
+		})
+		Eventually(migrate_session, timeout).Should(Exit(0))
+		migrate_output := string(migrate_session.Out.Contents())
+		Expect(migrate_output).Should(ContainSubstring("Present        Present"))
 	})
 }
 
 var _ = Describe("hasura deploy", func() {
 	var projectDirectory string
 	var teardown func()
+	sourceName := randomdata.SillyName()
+
 	BeforeEach(func() {
 		projectDirectory = testutil.RandDirName()
 		hgeEndPort, teardownHGE := testutil.StartHasura(GinkgoT(), testutil.HasuraDockerImage)
 		hgeEndpoint := fmt.Sprintf("http://0.0.0.0:%s", hgeEndPort)
 
-		sourceName := randomdata.SillyName()
 		connectionString, teardownPG := testutil.StartPGContainer(GinkgoT())
 		testutil.AddPGSourceToHasura(GinkgoT(), hgeEndpoint, connectionString, sourceName)
 		copyTestConfigV3Project(projectDirectory)
@@ -53,6 +73,6 @@ var _ = Describe("hasura deploy", func() {
 	})
 
 	It("deploy", func() {
-		commonDeployCommandsTest(projectDirectory)
+		commonDeployCommandsTest(projectDirectory, sourceName)
 	})
 })
