3bd7b9049d428ef3db003951985cb400fa18b317
Sameer Kolhar <sameer@hasura.io>
2021-09-06 19:59:44 +0530

    server: don't propogate `tracecontext` and `userinfo`  for GraphQL queries on PG backends
    https://github.com/hasura/graphql-engine-mono/pull/2294
    
    GitOrigin-RevId: c4be1c04a676154a233e75b31ff00b689443b933

--- server/src-lib/Hasura/Backends/Postgres/Instances/Transport.hs
+++ server/src-lib/Hasura/Backends/Postgres/Instances/Transport.hs
@@ -62,13 +62,11 @@ runPGQuery
   -> Maybe EQ.PreparedSql
   -> m (DiffTime, EncJSON)
   -- ^ Also return the time spent in the PG query; for telemetry.
-runPGQuery reqId query fieldName userInfo logger sourceConfig tx genSql = do
+runPGQuery reqId query fieldName _userInfo logger sourceConfig tx genSql = do
   -- log the generated SQL and the graphql query
-  traceCtx <- currentContext
   logQueryLog logger $ mkQueryLog query fieldName genSql reqId
   withElapsedTime $ trace ("Postgres Query for root field " <>> fieldName) $
-    Tracing.interpTraceT id $
-    hoist (runQueryTxWithCtx userInfo traceCtx $ _pscExecCtx sourceConfig) tx
+    Tracing.interpTraceT id $ hoist (runQueryTx $ _pscExecCtx sourceConfig) tx
 
 runPGMutation
   :: ( MonadIO m
