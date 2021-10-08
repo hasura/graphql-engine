e3fa6bee0a52c7a291c9ed1e4a0efd1d402ec74d
Kali Vara Purushotham Santhati <72007599+purush7@users.noreply.github.com>
2021-10-08 21:39:30 +0530

    cli: improve error/log messages
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/2513
    Co-authored-by: Aravind K P <8335904+scriptonist@users.noreply.github.com>
    GitOrigin-RevId: bff6e5f6d3553904914eabda02d3406c4b779f3c

--- cli/commands/metadata_reload_test.go
+++ cli/commands/metadata_reload_test.go
@@ -71,7 +71,7 @@ var _ = Describe("hasura metadata reload", func() {
 				Args:             []string{"metadata", "inconsistency", "status"},
 				WorkingDirectory: projectDirectory,
 			})
-			want = `metadata is inconsistent, use list command to see the objects`
+			want = `metadata is inconsistent, use 'hasura metadata ic list' command to see the inconsistent objects`
 			Eventually(session.Wait(timeout)).Should(Exit(1))
 			Expect(session.Err.Contents()).Should(ContainSubstring(want))
 		})
