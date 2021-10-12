3c3c048bb98fa7420b5f5c59db41790bbcac77b0
Robert <rob@vllmrt.net>
2021-10-12 11:33:13 +0200

    server: drop dependency on ghc-heap-view again for profiling builds (close #2554)
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/2557
    GitOrigin-RevId: e392651861140bfefc65ce387d5cfbd6d3211f66

--- server/src-lib/GHC/AssertNF/CPP.hs
+++ server/src-lib/GHC/AssertNF/CPP.hs
@@ -3,6 +3,29 @@
 -- | GHC.AssertNF.CPP localizes our use of CPP around calls
 -- to 'assertNFHere', primarily to give tooling (e.g. ormolu)
 -- an easier time.
+--
+-- We disable the 'assertNF'-related code because it is provided
+-- by the package ghc-heap-view, which can't be built using profiling.
+
+#ifdef PROFILING
+
+module GHC.AssertNF.CPP
+  ( assertNFHere,
+    disableAssertNF,
+  )
+where
+
+import Hasura.Prelude
+import Language.Haskell.TH
+
+assertNFHere :: Q Exp
+assertNFHere = [| const (return ()) |]
+
+disableAssertNF :: IO ()
+disableAssertNF = return ()
+
+#else
+
 module GHC.AssertNF.CPP
   ( assertNFHere,
     disableAssertNF,
@@ -13,14 +36,9 @@ where
 import GHC.AssertNF qualified
 import Hasura.Prelude
 import Language.Haskell.TH
-#ifndef PROFILING
-import           Text.Printf         (printf)
-#endif
+import Text.Printf (printf)
 
 assertNFHere :: Q Exp
-#ifdef PROFILING
-assertNFHere = [| const (return ()) |]
-#else
 -- This is a copy of 'GHC.AssertNF.assertNFHere'. We can't easily
 -- use the original because that relies on an import of "GHC.AssertNF".
 -- Instead, we rewrite it to use the re-exported 'assertNFNamed'.
@@ -32,11 +50,8 @@ assertNFHere = do
         formatLoc loc = let file = loc_filename loc
                             (line, col) = loc_start loc
                         in  printf "parameter at %s:%d:%d" file line col
-#endif
 
 disableAssertNF :: IO ()
-#ifdef PROFILING
-disableAssertNF = return ()
-#else
 disableAssertNF = GHC.AssertNF.disableAssertNF
+
 #endif
