23abe1a27a1533eb34795bf037889206a7058955
Abhijeet Khangarot <abhijeet40308@gmail.com>
2021-10-13 03:23:01 -0700

    console: fix console crash on giving action definition
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/2524
    GitOrigin-RevId: 0df67dec2a9f0975cb325a729a87d294f9f7b5a4

--- console/src/shared/utils/sdlUtils.js
+++ console/src/shared/utils/sdlUtils.js
@@ -207,11 +207,15 @@ export const getActionDefinitionFromSdl = sdl => {
     return definition;
   }
 
-  return {
-    ...definition,
-    type: actionType,
-    ...getActionFromOperationAstDef(sdlDef.fields[0]),
-  };
+  const defObj = sdlDef.fields.length
+    ? {
+        ...definition,
+        type: actionType,
+        ...getActionFromOperationAstDef(sdlDef.fields[0]),
+      }
+    : { ...definition, type: actionType };
+
+  return defObj;
 };
 
 const getArgumentsSdl = args => {
