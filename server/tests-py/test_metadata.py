5f7e4094cf3536e8d200e3de350efdebf7bb171d
Solomon Bothwell <ssbothwell@gmail.com>
2021-10-21 06:31:45 -0700

    Wrap string interpolations with double quotes on the backend
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/2601
    GitOrigin-RevId: 53c27dddd30968dd28325e3336721aa1a238815f

--- server/tests-py/test_metadata.py
+++ server/tests-py/test_metadata.py
@@ -197,13 +197,13 @@ class TestMetadata:
 
         check_query_f(hge_ctx, self.dir() + '/pg_track_function_with_comment_teardown.yaml')
 
-    def test_test_webhook_transform_success(self, hge_ctx):
+    def test_webhook_transform_success(self, hge_ctx):
         check_query_f(hge_ctx, self.dir() + '/test_webhook_transform_success.yaml')
 
-    def test_test_webhook_transform_bad_parse(self, hge_ctx):
+    def test_webhook_transform_bad_parse(self, hge_ctx):
         check_query_f(hge_ctx, self.dir() + '/test_webhook_transform_bad_parse.yaml')
 
-    def test_test_webhook_transform_bad_eval(self, hge_ctx):
+    def test_webhook_transform_bad_eval(self, hge_ctx):
         check_query_f(hge_ctx, self.dir() + '/test_webhook_transform_bad_eval.yaml')
 
     @pytest.mark.skipif(
