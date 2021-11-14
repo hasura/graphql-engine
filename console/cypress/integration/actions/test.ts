d867bb7303b507bec0628717abcee4034a49a2cd
Sooraj <soorajshankar@users.noreply.github.com>
2021-11-15 00:55:54 +0530

    console/actions: fix cypress tests
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/2780
    Co-authored-by: Vijay Prasanna <11921040+vijayprasanna13@users.noreply.github.com>
    GitOrigin-RevId: 4ce4e24f122705fb808de0a32a2c09a30a0d78d4

--- console/cypress/integration/actions/test.ts
+++ console/cypress/integration/actions/test.ts
@@ -1,13 +1,13 @@
 import {
   createMutationAction,
   modifyMutationAction,
-  // createQueryAction,
-  // modifyQueryAction,
+  createQueryAction,
+  modifyQueryAction,
   deleteMutationAction,
-  // deleteQueryAction,
-  // createActionTransform,
-  // modifyActionTransform,
-  // deleteActionTransform,
+  deleteQueryAction,
+  createActionTransform,
+  modifyActionTransform,
+  deleteActionTransform,
 } from './spec';
 import { testMode } from '../../helpers/common';
 import { setMetaData } from '../validators/validators';
@@ -28,13 +28,13 @@ export const runActionsTests = () => {
     // it('Verify Mutation Actions on GraphiQL', verifyMutation);
     it('Modify Mutation Action', modifyMutationAction);
     it('Delete Mutation Action', deleteMutationAction);
-    // it('Create Query Action', createQueryAction);
-    // // it('Verify Query Actions on GraphiQL', verifyQuery);
-    // it('Modify Query Action', modifyQueryAction);
-    // it('Delete Query Action', deleteQueryAction);
-    // it('Create Action With Transform', createActionTransform);
-    // it('Update Action With Transform', modifyActionTransform);
-    // it('Delete Action With Transform', deleteActionTransform);
+    it('Create Query Action', createQueryAction);
+    // it('Verify Query Actions on GraphiQL', verifyQuery);
+    it('Modify Query Action', modifyQueryAction);
+    it('Delete Query Action', deleteQueryAction);
+    it('Create Action With Transform', createActionTransform);
+    it('Update Action With Transform', modifyActionTransform);
+    it('Delete Action With Transform', deleteActionTransform);
   });
 };
 
