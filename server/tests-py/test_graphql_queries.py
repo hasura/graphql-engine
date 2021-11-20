39002bc572f3b5bec254d2c6cc09aca7b9e01562
Gil Mizrahi <gilmi@posteo.net>
2021-11-20 11:02:49 +0200

    Parse session variables to their correct type in mssql
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/2908
    GitOrigin-RevId: d9d8811e0d0658f6396a7c94bce9b7d2deb5245e

--- server/tests-py/test_graphql_queries.py
+++ server/tests-py/test_graphql_queries.py
@@ -342,6 +342,10 @@ class TestGraphQLQueryBasicMSSQL:
     def test_select_query_author_pk_null(self, hge_ctx, transport):
         check_query_f(hge_ctx, self.dir() + '/select_query_author_by_pkey_null.yaml', transport)
 
+    def test_author_with_permission(self, hge_ctx, transport):
+        check_query_f(hge_ctx, self.dir() + '/author_with_permission_mssql.yaml', transport)
+
+
     @classmethod
     def dir(cls):
         return 'queries/graphql_query/basic'
