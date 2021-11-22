55c1678e0816f587c23bb8b53257953561e0a7e7
Sai Krishna Prasad Kandula <kskprasad96@gmail.com>
2021-11-22 16:46:42 +0530

    fetcher middleware is whats using wondow, hence made that part alone conditional for browseronly
    GitOrigin-RevId: cdd5bf553daaeaaba75bd9e8ade1e5a765b55225

--- docs-new/src/components/GraphiQLIDE/GraphiQLIDE.tsx
+++ docs-new/src/components/GraphiQLIDE/GraphiQLIDE.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
 import GraphiQL from 'graphiql';
-import BrowserOnly from '@docusaurus/BrowserOnly';
+import useIsBrowser from '@docusaurus/useIsBrowser';
 import cslx from 'clsx';
 import { createGraphiQLFetcher } from '@graphiql/toolkit';
 import "graphiql/graphiql.min.css";
 import "./styles.css";
 
 
-const GraphiQLIDE = ({ query, variables, response, viewOnly=true }) => (
-  <BrowserOnly>
-    {() => (
-      <div className={`graphiql ${cslx({'with-vars': !!variables, 'view-only': viewOnly})}`}>
-        <GraphiQL
-          readOnly={viewOnly}
-          editorTheme={'dracula'}
-          schema={null}
-          fetcher={createGraphiQLFetcher({
-            url: 'https://hasura.io/graphql', // TODO: update later
-          })}
-          query={query}
-          variables={variables}
-          response={response}
-        />
-      </div>
-    )}
-  </BrowserOnly>
-);
+const GraphiQLIDE = ({ query, variables, response, viewOnly=true }) => {
+  const isBrowser = useIsBrowser();
+  return (
+    <div className={`graphiql ${cslx({'with-vars': !!variables, 'view-only': viewOnly})}`}>
+      <GraphiQL
+        readOnly={viewOnly}
+        editorTheme={'dracula'}
+        schema={null}
+        fetcher={isBrowser ? createGraphiQLFetcher({
+          url: 'https://hasura.io/graphql', // TODO: update later
+        }) : () => null}
+        query={query}
+        variables={variables}
+        response={response}
+      />
+    </div>
+  );
+}
 
 export default GraphiQLIDE;
