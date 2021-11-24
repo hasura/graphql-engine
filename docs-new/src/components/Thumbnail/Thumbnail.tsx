0d1ef1907c389f3816a9c98facd5757b2569175e
Sai Krishna Prasad Kandula <kskprasad96@gmail.com>
2021-11-24 17:45:26 +0530

    make the css file module
    GitOrigin-RevId: ee1e037cd7e28eb5fbea52a4a6ef213df23e5139

--- docs-new/src/components/Thumbnail/Thumbnail.tsx
+++ docs-new/src/components/Thumbnail/Thumbnail.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from "react-dom";
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import { CSSTransition } from "react-transition-group";
-import './Thumbnail.css';
+import styles from './Thumbnail.module.css';
 
 const Overlay = (props) => {
   const closeOnEscapeKeyDown = e => {
@@ -23,12 +23,16 @@ const Overlay = (props) => {
       in={props.show}
       unmountOnExit
       timeout={{ enter: 0, exit: 300 }}
+      classNames={{
+        enterDone: styles['enter-done'],
+        exit: styles['exit']
+      }}
     >
-      <div className="modal" onClick={props.onClose}>
-        <div className="modal-content" onClick={e => e.stopPropagation()}>
-          <div className="modal-body">{props.children}</div>
-          <div className="modal-footer">
-            <button onClick={props.onClose} className="button">
+      <div className={styles["modal"]} onClick={props.onClose}>
+        <div className={styles["modal-content"]} onClick={e => e.stopPropagation()}>
+          <div className={styles["modal-body"]}>{props.children}</div>
+          <div className={styles["modal-footer"]}>
+            <button onClick={props.onClose} className={styles["button"]}>
               Close
             </button>
           </div>
@@ -43,7 +47,7 @@ const Thumbnail = (props) => {
   const [showInModal, setShowInModal] = useState(false);
   return (
   <div
-    className={"thumbnail"}
+    className={styles["thumbnail"]}
     onClick={() => setShowInModal(true)}
   >
     <img {...props} />
