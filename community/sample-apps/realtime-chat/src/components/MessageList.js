6dcfcd7b17af3c6e6b4b04ba0c5babd7b5671b69
arjunyel <arjunyel@gmail.com>
2021-12-03 11:22:29 -0600

    community: update realtime-chat to React Hooks

--- community/sample-apps/realtime-chat/src/components/MessageList.js
+++ community/sample-apps/realtime-chat/src/components/MessageList.js
@@ -1,39 +1,26 @@
-import React from 'react';
 import '../App.js';
 import '../App.css';
 import moment from 'moment';
 
-export default class MessageList extends React.Component {
-  render() {
-    const { isNew } = this.props;
-
-    return (
-      <div className={isNew ? "messageWrapperNew" : "messageWrapper"}>
-        {
-          this.props.messages.map((m, i) => {
-            return (
-              <div key={m.id} className="message">
-                <div className="messageNameTime">
-                  <div className="messageName">
-                    <b>{m.username}</b>
-                  </div>
-                  <div className="messsageTime">
-                    <i>{moment(m.timestamp).fromNow()} </i>
-                  </div>
-                </div>
-                <div className="messageText">
-                  {m.text }
-                </div>
+export default function MessageList(props) {
+  return (
+    <div className={props.isNew ? 'messageWrapperNew' : 'messageWrapper'}>
+      {props.messages.map((m) => {
+        return (
+          <div key={m.id} className="message">
+            <div className="messageNameTime">
+              <div className="messageName">
+                <b>{m.username}</b>
               </div>
-            );
-          })
-        }
-        <div
-          style={{ "height": 0 }}
-          id="lastMessage"
-        >
-        </div>
-      </div>
-    );
-  }
-};
+              <div className="messsageTime">
+                <i>{moment(m.timestamp).fromNow()} </i>
+              </div>
+            </div>
+            <div className="messageText">{m.text}</div>
+          </div>
+        );
+      })}
+      <div style={{ height: 0 }} id="lastMessage"></div>
+    </div>
+  );
+}
---
+++ community/sample-apps/realtime-chat/src/hooks/useInterval.js
@@ -0,0 +1,22 @@
+// Credit to Dan Abramov https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+import { useEffect, useRef } from 'react';
+
+export function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
