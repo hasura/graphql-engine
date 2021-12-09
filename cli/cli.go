048471bdc00874c0677e4117ac57f65c7dc376ef
Aravind K P <aravindkp@hasura.io>
2021-12-09 18:21:23 +0530

    cli: stop parsing  unnecessary fields from config API
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/2899
    GitOrigin-RevId: 99dacb9dcbc6f1093b01aefbf7cbdc975d47569e

--- cli/cli.go
+++ cli/cli.go
@@ -233,11 +233,6 @@ func (c *ServerConfig) GetHasuraInternalServerConfig() error {
 // HasuraServerConfig is the type returned by the v1alpha1/config API
 // TODO: Move this type to a client implementation for hasura
 type HasuraServerInternalConfig struct {
-	Version          string `json:"version"`
-	IsAdminSecretSet bool   `json:"is_admin_secret_set"`
-	IsAuthHookSet    bool   `json:"is_auth_hook_set"`
-	IsJwtSet         bool   `json:"is_jwt_set"`
-	JWT              string `json:"jwt"`
 	ConsoleAssetsDir string `json:"console_assets_dir"`
 }
 
