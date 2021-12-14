975148fa54eb73d1d495c15906e696165f1635e3
Vishnu Bharathi <vishnubharathi04@gmail.com>
2021-12-14 20:20:25 +0530

    ci: add build and test job for arm64 oss cli migrations
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/3128
    GitOrigin-RevId: a8ab8fc8e022166e27fcc68199311b1626930759

--- scripts/cli-migrations/v3/test/test.sh
+++ scripts/cli-migrations/v3/test/test.sh
@@ -4,11 +4,21 @@ set -evo pipefail
 IFS=$'\n\t'
 ROOT="$(readlink -f ${BASH_SOURCE[0]%/*}/../../)"
 
+if [[ -z "$TEST_IMAGE_NAME" ]]; then
+  echo "please set TEST_IMAGE_NAME env to be a non-empty value and retry."
+  exit 1
+fi
+
+if [[ -z "$TEST_PLATFORM" ]]; then
+  echo "please set TEST_PLATFORM env to be a non-empty value and retry."
+  exit 1
+fi
+
 wait_for_server() {
     echo "waiting for server"
     for _ in $(seq 1 60);
     do
-      docker run --network container:graphql-engine appropriate/curl http://127.0.0.1:8080/v1/version && return
+      docker run --rm --network container:graphql-engine curlimages/curl http://127.0.0.1:8080/v1/version && return
       echo -n .
       sleep 1
     done
@@ -25,8 +35,8 @@ docker cp metadata/. graphql-engine:/hasura-metadata
 docker-compose up -d --no-recreate graphql-engine
 wait_for_server
 # export metadata and run diff with validation/metadata.json
-docker run --network container:graphql-engine appropriate/curl -s -f   -d'{"type" : "export_metadata", "args" : {} }' localhost:8080/v1/metadata | jq -j '.' | diff validation/metadata.json -
+docker run --network container:graphql-engine curlimages/curl -s -f -d'{"type" : "export_metadata", "args" : {} }' localhost:8080/v1/metadata | jq -j '.' | diff validation/metadata.json -
 # get list of migrations applied from graphql-engine server
-docker run --network container:graphql-engine appropriate/curl -s -f   -d'{"type" : "get_catalog_state", "args" : {} }' localhost:8080/v1/metadata | jq .cli_state | diff validation/catalog_cli_state.json -
+docker run --network container:graphql-engine curlimages/curl -s -f -d'{"type" : "get_catalog_state", "args" : {} }' localhost:8080/v1/metadata | jq .cli_state | diff validation/catalog_cli_state.json -
 # delete postgres and graphql-engine
 docker-compose down -v
