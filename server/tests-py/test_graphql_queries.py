f58c0e7c4c19a5e13da28eece1bbfa3d44f3b991
Kirill Zaborsky <qrilka@gmail.com>
2021-12-15 18:18:56 +0300

    Fix support for multiple array relationships in BigQuery
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/3003
    GitOrigin-RevId: 7a9bf10be6436da2d87d5555f589e23c40c76dd6

--- server/tests-py/test_graphql_queries.py
+++ server/tests-py/test_graphql_queries.py
@@ -210,6 +210,9 @@ class TestGraphQLQueryBasicBigquery:
     def test_basic_remote_join(self, hge_ctx, transport):
         check_query_f(hge_ctx, self.dir() + "/basic_remote_joins.yaml", transport)
 
+    def test_nested_array_relationships(self, hge_ctx, transport):
+        check_query_f(hge_ctx, self.dir() + "/nested_array_relationships.yaml", transport)
+
     @classmethod
     def dir(cls):
         return 'queries/graphql_query/bigquery'
