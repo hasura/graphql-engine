517c7dfdde24d5f0e7c4fd98294eefdbc5c5275d
Sai Krishna Prasad Kandula <kskprasad96@gmail.com>
2021-12-15 21:22:05 +0530

    WIP: add script for auto transform using pandoc
    GitOrigin-RevId: 45907238f381ba4ebc7e93781f1d2d05cbf3afdd

---
+++ docs-new/scripts/pandoc-auto-transform.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Run this from root of repo - graphql-engine-mono
+# ./docs-new/scripts/pandoc-auto-transform.sh "docs-new/docs"
+
+# Make sure globstar is enabled
+shopt -s globstar
+cd docs
+for f in **/*.rst; do # Whitespace-safe and recursive
+    # echo "../$1/${f%.*}"
+    mkdir -pv -- "../$1/${f%/*}" && touch -- "../$1/${f%}.txt";
+    pandoc "$f" -f rst -t gfm -s -o "../$1/${f%}.txt";
+done
+cd ..
+
+  # mkdir -pv -- "$1/${f%/*}" && touch -- "$1/${f%}.txt";
+  # pandoc "$f" -f rst -t gfm -s -o "$1/${f%}.txt";
