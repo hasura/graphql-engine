f5a1beb89868b6aa50cd5d888d59d1518cf4d217
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-01-01 21:37:08 +0900

    refactor types

--- console/src/components/Services/Data/Common/Components/FrequentlyUsedColumnSelector.tsx
+++ console/src/components/Services/Data/Common/Components/FrequentlyUsedColumnSelector.tsx
@@ -27,18 +27,18 @@ const getFreqUsedColDisplayInfo = (c: FrequentlyUsedColumn) => {
 };
 
 interface FrequentlyUsedColumnSelectorProps {
-  onSelect: any;
-  action: ColumnAction | null;
-  dispatch: Dispatch | null;
+  onSelect: (column: FrequentlyUsedColumn) => any;
   postgresVersion: string | null;
+  action?: ColumnAction | null;
+  dispatch?: Dispatch | null;
 }
 
-const FrequentlyUsedColumnSelector = ({
+const FrequentlyUsedColumnSelector: React.VFC<FrequentlyUsedColumnSelectorProps> = ({
   onSelect,
+  postgresVersion,
   action = null,
   dispatch = null,
-  postgresVersion,
-}: FrequentlyUsedColumnSelectorProps) => {
+}) => {
   const frequentlyUsedColumnsOptions = frequentlyUsedColumns
     .filter(fuc => !action || fuc.validFor.includes(action))
     .filter(col =>
