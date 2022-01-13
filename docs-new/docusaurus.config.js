4648390ab6f4cf9b57438e0fd9ab50fee89604f1
Sai Krishna Prasad Kandula <kskprasad96@gmail.com>
2022-01-13 23:31:33 +0530

    initial base config for algolia docsearch
    GitOrigin-RevId: 6d26ce4f7385b17b72507aa325673568fa1d1c03

--- docs-new/docusaurus.config.js
+++ docs-new/docusaurus.config.js
@@ -121,6 +121,19 @@ const config = {
         darkTheme: darkCodeTheme,
         additionalLanguages: ['rest', 'http', 'haskell', 'plsql', 'docker']
       },
+      algolia: {
+        // If Algolia did not provide you any appId, use 'BH4D9OD16A'
+        appId: 'YOUR_APP_ID',
+        // Public API key: it is safe to commit it
+        apiKey: 'YOUR_SEARCH_API_KEY',
+        indexName: 'YOUR_INDEX_NAME',
+        // Optional: see doc section below
+        // contextualSearch: true,
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+        // Optional: Algolia search parameters
+        // searchParameters: {},
+      },
     }),
 };
 
