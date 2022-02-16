cdc080060fcfc38122ccadb41b00a9fb55a14b90
Divi <divyansh@hasura.io>
2022-02-16 13:50:42 +0530

    ci: fix bugs faced during v2.2.0 release
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/3659
    Co-authored-by: Vishnu Bharathi <4211715+scriptnull@users.noreply.github.com>
    GitOrigin-RevId: 8ddc98a2c06f64ae86f3a011f79f93e7c15161a0

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -2,63 +2,50 @@
 #
 # tag-release.sh
 #
-# Update installation manifests with the release tag, and execute other release
-# hygiene tasks, makes a commit and tags that commit with the given tag.
+# This script is executed before every OSS and Pro releases.
 #
-# Usage: ./tag-release.sh <tag> [<optional-tag-message>]
+# Usage: ./tag-release.sh <oss_tag> <pro_tag>
 #
-# Example: ./tag-release.sh v1.1.0
+# Example: ./tag-release.sh v1.1.0 v1.1.0-pro.1
 #
 
 # exit on error
-set -e
+set -eo pipefail
 
 # get the repo root
-ROOT="$(readlink -f ${BASH_SOURCE[0]%/*}/../)"
+ROOT="$(readlink -f "${BASH_SOURCE[0]%/*}/../")"
+
+# assign arguments to variables
+OSS_TAG="$1"
+PRO_TAG="$2"
 
 # check if required argument is set
-if [ -z "$1" ]; then
-    echo "Usage: ./tag-release.sh <tag> [<optional-tag-message>]"
+if [[ -z "$OSS_TAG" || -z "$PRO_TAG" ]]; then
+    echo "Please mention both OSS_TAG and PRO_TAG"
+    echo ""
+    echo "Usage: ./tag-release.sh <oss_tag> <pro_tag>"
     exit 1
 fi
 
-# assign arguments to variables
-TAG=$1
-MESSAGE=$2
-
-# default message to tag
-if [ -z "$MESSAGE" ]; then
-    MESSAGE="$TAG"
-fi
-
-# replace the image version with latest tag for all references in install-manifests
-find "$ROOT/install-manifests" \
-     -type f -exec sed -i -E \
-     's#(hasura/graphql-engine:)v(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(\-[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?(.*)*$#\1'"${TAG}"'\9#' {} \;
-
-# update version in CLI installation instructions
-sed -i -E 's#(.*)v(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(\-[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?(\+[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?(.*)*$#\1'"${TAG}"'\9#' \
-    "${ROOT}/cli/README.md" \
-    "${ROOT}/docs/graphql/core/hasura-cli/install-hasura-cli.rst" \
-
-# update version in CI image scanning tags
-sed -i -E 's#v(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)#'"${TAG}"'#' \
-    "${ROOT}/.buildkite/pipeline-gen/pipeline/scan_graphql_engine_images.go" \
-    "${ROOT}/.buildkite/pipeline-gen/pipeline/scan_graphql_engine_pro_images.go"
-
 # add the latest tag to the catalog_versions file
 [ -n "$(tail -c1 "$ROOT/server/src-rsr/catalog_versions.txt")" ] && echo >> "$ROOT/server/src-rsr/catalog_versions.txt"
-echo $TAG $(cat "$ROOT/server/src-rsr/catalog_version.txt") >> "$ROOT/server/src-rsr/catalog_versions.txt"
+echo "$OSS_TAG $(cat "$ROOT/server/src-rsr/catalog_version.txt")" >> "$ROOT/server/src-rsr/catalog_versions.txt"
 
-git add "$ROOT/install-manifests" \
-        "$ROOT/server/src-rsr" \
-        "${ROOT}/cli/README.md" \
-        "${ROOT}/docs/graphql/core/hasura-cli/install-hasura-cli.rst" \
-        "${ROOT}/.buildkite/pipeline-gen/pipeline/scan_graphql_engine_images.go" \
-        "${ROOT}/.buildkite/pipeline-gen/pipeline/scan_graphql_engine_pro_images.go"
+# update OSS changelog
+OSS_VERSION_ENTRY="## Next release\n\n### Bug fixes and improvements\n\n## $OSS_TAG"
+sed -i "s/## Next release/${OSS_VERSION_ENTRY}/" "$ROOT/CHANGELOG.md"
 
-git commit -m "tag release $TAG"
+# update Pro changelog
+PRO_NEXT_RELEASE_TEXT="## Next release\n(Add entries below in the order of multitenant, server, console, cli, docs, others)"
+sed -i "N;s/${PRO_NEXT_RELEASE_TEXT}/${PRO_NEXT_RELEASE_TEXT}\n\n## ${PRO_TAG}/" "$ROOT/pro/CHANGELOG.md"
 
-git tag -a "$TAG" -m "$MESSAGE"
+git add "$ROOT/server/src-rsr" \
+    "$ROOT/CHANGELOG.md" \
+    "$ROOT/pro/CHANGELOG.md"
 
-echo "tagged $TAG"
+git commit -m "ci: tag release $OSS_TAG and $PRO_TAG"
+
+git tag -a "$OSS_TAG" -m "tag release $OSS_TAG"
+git tag -a "$PRO_TAG" -m "tag release $PRO_TAG"
+
+echo "tagged $OSS_TAG and $PRO_TAG"
