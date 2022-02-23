c595aca9b0b434900c9e623cfae670d234ce1a54
Gil Mizrahi <gilmi@posteo.net>
2022-02-23 17:26:20 +0200

    tests-hspec refactor leftovers and another small refactor: typos, setup/teardown catch, better error messages for env-vars
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/3748
    GitOrigin-RevId: 4e4959e41ff33539d82968bbdf56dc10b362e641

--- server/tests-hspec/Harness/Env.hs
+++ server/tests-hspec/Harness/Env.hs
@@ -1,32 +1,55 @@
 {-# OPTIONS -Wno-redundant-constraints #-}
 
+-- | Read environment variables
 module Harness.Env (getEnvRead, getEnvJSON, getEnvString) where
 
 import Data.Aeson qualified as Aeson
 import Data.String
+import Data.Typeable (Proxy (Proxy), Typeable, typeRep)
 import GHC.Stack
 import Hasura.Prelude
-import System.Environment (getEnv)
+import System.Environment (lookupEnv)
 
-getEnvRead :: (Read a, HasCallStack) => String -> IO a
-getEnvRead var = do
-  str <- getEnv var
-  onNothing
-    (readMaybe str)
-    ( error
-        ( unlines
-            ["Failure parsing " <> var, " containing value " <> show str]
-        )
-    )
+-- * API
+
+getEnvRead :: (Read a, Typeable a, HasCallStack) => String -> IO a
+getEnvRead var =
+  withFrozenCallStack $ do
+    readVarValue var =<< getEnv var
 
 getEnvString :: (IsString a, HasCallStack) => String -> IO a
-getEnvString var = fromString <$> getEnv var
+getEnvString var =
+  withFrozenCallStack $
+    fromString <$> getEnv var
 
-getEnvJSON :: (Aeson.FromJSON a, HasCallStack) => String -> IO a
-getEnvJSON var = do
-  accountString <- getEnv var
-  onLeft
-    (Aeson.eitherDecode' (fromString accountString))
-    ( \err ->
-        error (unlines ["Failure parsing " <> var <> ":", show err])
-    )
+getEnvJSON :: forall a. (Typeable a, Aeson.FromJSON a, HasCallStack) => String -> IO a
+getEnvJSON var =
+  withFrozenCallStack $ do
+    accountString <- getEnv var
+    onLeft
+      (Aeson.eitherDecode' (fromString accountString))
+      ( \err ->
+          let expectedType = show (typeRep (Proxy :: Proxy a))
+           in error (unlines ["Failure parsing '" <> var <> "' to type '" <> expectedType <> "':", show err])
+      )
+
+-- * Helpers
+
+getEnv :: HasCallStack => String -> IO String
+getEnv var = do
+  value <- lookupEnv var
+  onNothing value (error $ "getEnv: " <> var <> " does not exist (no environment variable)")
+
+-- | Read a variable to a specific type.
+readVarValue :: forall a. (Read a, Typeable a, HasCallStack) => String -> String -> IO a
+readVarValue var value =
+  onNothing
+    (readMaybe value)
+    let expectedType = show (typeRep (Proxy :: Proxy a))
+     in error
+          ( unwords
+              [ "Failure parsing '" <> var <> "'",
+                "to type '" <> expectedType <> "';",
+                "containing value '" <> show value <> "'."
+              ]
+          )
