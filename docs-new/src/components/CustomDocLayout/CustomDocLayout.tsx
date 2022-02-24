13d407a85cde15016a5b350acf5a87cde0fa033f
surendran <surendran@hasura.io>
2022-02-24 11:07:49 +0530

    graphql with hasura banner updated

--- docs-new/src/components/CustomDocLayout/CustomDocLayout.tsx
+++ docs-new/src/components/CustomDocLayout/CustomDocLayout.tsx
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 import ActualDocPage from '@theme/DocItem';
 
+import GraphQLWithHasuraBanner from "@site/src/components/GraphQLWithHasuraBanner/GraphQLWithHasuraBanner";
+
 const CustomDocLayout = (props) => (
   <div className='custom_doc_layout_wrapper'>
     <ActualDocPage {...props} />
     Custom Doc Item Layout FOOTER - ideally "do u like this page?" kind of stuff.
+    <GraphQLWithHasuraBanner />
   </div>
 )
 
