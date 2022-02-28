853e9b760d55dd5bbde6219070a54a1458188113
Aniket Deshpande <aniketd@users.noreply.github.com>
2022-03-01 04:17:51 +0530

    Draft: server: HSpec: Unify setup/teardown data and specialize for backends
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/3765
    GitOrigin-RevId: 22c36c83ff900b3423144353ccdf61980b66bc6b

--- server/tests-hspec/Harness/Backend/Sqlserver.hs
+++ server/tests-hspec/Harness/Backend/Sqlserver.hs
@@ -6,6 +6,13 @@ module Harness.Backend.Sqlserver
     run_,
     defaultSourceMetadata,
     defaultSourceConfiguration,
+    createTable,
+    insertTable,
+    trackTable,
+    dropTable,
+    untrackTable,
+    setup,
+    teardown,
   )
 where
 
@@ -13,11 +20,19 @@ import Control.Concurrent
 import Control.Exception
 import Control.Monad.Reader
 import Data.Aeson (Value)
+import Data.Bool (bool)
+import Data.Foldable (for_)
 import Data.String
+import Data.Text (Text)
+import Data.Text qualified as T (pack, unpack, unwords)
+import Data.Text.Extended (commaSeparated)
 import Database.ODBC.SQLServer qualified as Sqlserver
 import GHC.Stack
 import Harness.Constants as Constants
+import Harness.GraphqlEngine qualified as GraphqlEngine
 import Harness.Quoter.Yaml (yaml)
+import Harness.State (State)
+import Harness.Test.Schema qualified as Schema
 import System.Process.Typed
 import Prelude
 
@@ -77,3 +92,137 @@ connection_info:
   database_url: *sqlserverConnectInfo
   pool_settings: {}
 |]
+
+-- | Serialize Table into a T-SQL statement, as needed, and execute it on the Sqlserver backend
+createTable :: Schema.Table -> IO ()
+createTable Schema.Table {tableName, tableColumns, tablePrimaryKey = pk, tableReferences} = do
+  run_ $
+    T.unpack $
+      T.unwords
+        [ "CREATE TABLE",
+          T.pack Constants.sqlserverDb <> "." <> tableName,
+          "(",
+          commaSeparated $
+            (mkColumn <$> tableColumns)
+              <> (bool [mkPrimaryKey pk] [] (null pk))
+              <> (mkReference <$> tableReferences),
+          ");"
+        ]
+  where
+    scalarType :: Schema.ScalarType -> Text
+    scalarType = \case
+      Schema.TInt -> "INT"
+      Schema.TStr -> "TEXT"
+    mkColumn :: Schema.Column -> Text
+    mkColumn Schema.Column {columnName, columnType, columnNullable, columnDefault} =
+      T.unwords
+        [ columnName,
+          scalarType columnType,
+          bool "NOT NULL" "DEFAULT NULL" columnNullable,
+          maybe "" ("DEFAULT " <>) columnDefault
+        ]
+    mkPrimaryKey :: [Text] -> Text
+    mkPrimaryKey key =
+      T.unwords
+        [ "PRIMARY KEY",
+          "(",
+          commaSeparated key,
+          ")"
+        ]
+    mkReference :: Schema.Reference -> Text
+    mkReference Schema.Reference {referenceLocalColumn, referenceTargetTable, referenceTargetColumn} =
+      T.unwords
+        [ "CONSTRAINT FOREIGN KEY",
+          referenceLocalColumn,
+          "REFERENCES",
+          referenceTargetTable,
+          "(",
+          referenceTargetColumn,
+          ")",
+          "ON DELETE CASCADE",
+          "ON UPDATE CASCADE"
+        ]
+
+-- | Serialize tableData into a T-SQL insert statement and execute it.
+insertTable :: Schema.Table -> IO ()
+insertTable Schema.Table {tableName, tableColumns, tableData} =
+  run_ $
+    T.unpack $
+      T.unwords
+        [ "INSERT INTO",
+          T.pack Constants.sqlserverDb <> "." <> tableName,
+          "(",
+          commaSeparated (Schema.columnName <$> tableColumns),
+          ")",
+          "VALUES",
+          commaSeparated $ mkRow <$> tableData,
+          ";"
+        ]
+  where
+    mkRow :: [Schema.ScalarValue] -> Text
+    mkRow row =
+      T.unwords
+        [ "(",
+          commaSeparated $ Schema.serialize <$> row,
+          ")"
+        ]
+
+-- | Post an http request to start tracking the table
+trackTable :: State -> Schema.Table -> IO ()
+trackTable state Schema.Table {tableName} = do
+  let schemaName = T.pack Constants.sqlserverDb
+  GraphqlEngine.postMetadata_ state $
+    [yaml|
+type: mssql_track_table
+args:
+  source: mssql
+  table:
+    schema: *schemaName
+    name: *tableName
+|]
+
+-- | Serialize Table into a T-SQL DROP statement and execute it
+dropTable :: Schema.Table -> IO ()
+dropTable Schema.Table {tableName} = do
+  run_ $
+    T.unpack $
+      T.unwords
+        [ "DROP TABLE",
+          T.pack Constants.sqlserverDb <> "." <> tableName,
+          ";"
+        ]
+
+-- | Post an http request to stop tracking the table
+untrackTable :: State -> Schema.Table -> IO ()
+untrackTable state Schema.Table {tableName} = do
+  let schemaName = T.pack Constants.sqlserverDb
+  GraphqlEngine.postMetadata_ state $
+    [yaml|
+type: mssql_untrack_table
+args:
+  source: mssql
+  table:
+    schema: *schemaName
+    name: *tableName
+|]
+
+-- | Setup the schema in the most expected way.
+-- NOTE: Certain test modules may warrant having their own local version.
+setup :: [Schema.Table] -> (State, ()) -> IO ()
+setup tables (state, _) = do
+  -- Clear and reconfigure the metadata
+  GraphqlEngine.setSource state defaultSourceMetadata
+
+  -- Setup and track tables
+  for_ tables $ \table -> do
+    createTable table
+    insertTable table
+    trackTable state table
+
+-- | Teardown the schema and tracking in the most expected way.
+-- NOTE: Certain test modules may warrant having their own version.
+teardown :: [Schema.Table] -> (State, ()) -> IO ()
+teardown tables (state, _) =
+  for_ tables $ \table -> do
+    untrackTable state table
+    dropTable table
--- server/tests-hspec/Test/BasicFieldsSpec.hs
+++ server/tests-hspec/Test/BasicFieldsSpec.hs
@@ -1,18 +1,18 @@
 -- | Test querying an entity for a couple fields.
 module Test.BasicFieldsSpec (spec) where
 
-import Harness.Backend.BigQuery qualified as BigQuery
+import Harness.Backend.BigQuery qualified as Bigquery
 import Harness.Backend.Citus qualified as Citus
 import Harness.Backend.Mysql qualified as Mysql
 import Harness.Backend.Postgres qualified as Postgres
 import Harness.Backend.Sqlserver qualified as Sqlserver
 import Harness.GraphqlEngine qualified as GraphqlEngine
 import Harness.Quoter.Graphql (graphql)
-import Harness.Quoter.Sql (sql)
 import Harness.Quoter.Yaml (shouldReturnYaml, yaml)
 import Harness.State (State)
 import Harness.Test.Context qualified as Context
-import Test.Hspec (SpecWith, it)
+import Harness.Test.Schema qualified as Schema
+import Test.Hspec (SpecWith, describe, it)
 import Prelude
 
 --------------------------------------------------------------------------------
@@ -24,36 +24,36 @@ spec =
     [ Context.Context
         { name = Context.MySQL,
           mkLocalState = Context.noLocalState,
-          setup = mysqlSetup,
-          teardown = mysqlTeardown,
+          setup = Mysql.setup schema,
+          teardown = Mysql.teardown schema,
           customOptions = Nothing
         },
       Context.Context
         { name = Context.Postgres,
           mkLocalState = Context.noLocalState,
-          setup = postgresSetup,
-          teardown = postgresTeardown,
+          setup = Postgres.setup schema,
+          teardown = Postgres.teardown schema,
           customOptions = Nothing
         },
       Context.Context
         { name = Context.Citus,
           mkLocalState = Context.noLocalState,
-          setup = citusSetup,
-          teardown = citusTeardown,
+          setup = Citus.setup schema,
+          teardown = Citus.teardown schema,
           customOptions = Nothing
         },
       Context.Context
         { name = Context.SQLServer,
           mkLocalState = Context.noLocalState,
-          setup = sqlserverSetup,
-          teardown = sqlserverTeardown,
+          setup = Sqlserver.setup schema,
+          teardown = Sqlserver.teardown schema,
           customOptions = Nothing
         },
       Context.Context
         { name = Context.BigQuery,
           mkLocalState = Context.noLocalState,
-          setup = bigquerySetup,
-          teardown = bigqueryTeardown,
+          setup = Bigquery.setup schema,
+          teardown = Bigquery.teardown schema,
           customOptions =
             Just $
               Context.Options
@@ -64,259 +64,44 @@ spec =
     tests
 
 --------------------------------------------------------------------------------
--- MySQL backend
+-- Schema
 
-mysqlSetup :: (State, ()) -> IO ()
-mysqlSetup (state, _) = do
-  -- Clear and reconfigure the metadata
-  GraphqlEngine.setSource state Mysql.defaultSourceMetadata
+schema :: [Schema.Table]
+schema = [author]
 
-  -- Setup tables
-  Mysql.run_
-    [sql|
-CREATE TABLE hasura.author
-(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(45) UNIQUE KEY
-);
-|]
-  Mysql.run_
-    [sql|
-INSERT INTO hasura.author
-    (name)
-VALUES
-    ( 'Author 1'),
-    ( 'Author 2');
-|]
+author :: Schema.Table
+author =
+  Schema.Table
+    "author"
+    [ Schema.column "id" Schema.TInt,
+      Schema.column "name" Schema.TStr
+    ]
+    ["id"]
+    []
+    [ [Schema.VInt 1, Schema.VStr "Author 1"],
+      [Schema.VInt 2, Schema.VStr "Author 2"]
+    ]
 
-  -- Track the tables
-  GraphqlEngine.postMetadata_
-    state
-    [yaml|
-type: mysql_track_table
-args:
-  source: mysql
-  table:
-    schema: hasura
-    name: author
-|]
-
-mysqlTeardown :: (State, ()) -> IO ()
-mysqlTeardown _ = do
-  Mysql.run_
-    [sql|
-DROP TABLE hasura.author;
-|]
-
---------------------------------------------------------------------------------
--- PostgreSQL backend
-
-postgresSetup :: (State, ()) -> IO ()
-postgresSetup (state, _) = do
-  -- Clear and reconfigure the metadata
-  GraphqlEngine.setSource state Postgres.defaultSourceMetadata
-
-  -- Setup tables
-  Postgres.run_
-    [sql|
-CREATE TABLE hasura.author
-(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(45) UNIQUE
-);
-|]
-  Postgres.run_
-    [sql|
-INSERT INTO hasura.author
-    (name)
-VALUES
-    ( 'Author 1'),
-    ( 'Author 2');
-|]
-
-  -- Track the tables
-  GraphqlEngine.postMetadata_
-    state
-    [yaml|
-type: postgres_track_table
-args:
-  source: postgres
-  table:
-    schema: hasura
-    name: author
-|]
-
-postgresTeardown :: (State, ()) -> IO ()
-postgresTeardown _ = do
-  Postgres.run_
-    [sql|
-DROP TABLE hasura.author;
-|]
-
---------------------------------------------------------------------------------
--- Citus backend
-
-citusSetup :: (State, ()) -> IO ()
-citusSetup (state, ()) = do
-  -- Clear and reconfigure the metadata
-  GraphqlEngine.setSource state Citus.defaultSourceMetadata
-
-  -- Setup tables
-  Citus.run_
-    [sql|
-CREATE TABLE hasura.author
-(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(45) UNIQUE
-);
-|]
-  Citus.run_
-    [sql|
-INSERT INTO hasura.author
-    (name)
-VALUES
-    ( 'Author 1'),
-    ( 'Author 2');
-|]
-
-  -- Track the tables
-  GraphqlEngine.postMetadata_
-    state
-    [yaml|
-type: citus_track_table
-args:
-  source: citus
-  table:
-    schema: hasura
-    name: author
-|]
-
-citusTeardown :: (State, ()) -> IO ()
-citusTeardown _ = do
-  Citus.run_
-    [sql|
-DROP TABLE IF EXISTS hasura.author;
-|]
-
---------------------------------------------------------------------------------
--- SQL Server backend
-
-sqlserverSetup :: (State, ()) -> IO ()
-sqlserverSetup (state, ()) = do
-  -- Clear and reconfigure the metadata
-  GraphqlEngine.setSource state Sqlserver.defaultSourceMetadata
-
-  -- Setup tables
-  Sqlserver.run_
-    [sql|
-CREATE TABLE hasura.author
-(
-    id INT NOT NULL IDENTITY(1,1) PRIMARY KEY CLUSTERED,
-    name NVARCHAR(45) NOT NULL UNIQUE NONCLUSTERED
-);
-|]
-  Sqlserver.run_
-    [sql|
-INSERT INTO hasura.author
-    (name)
-VALUES
-    ('Author 1'),
-    ('Author 2');
-|]
-
-  -- Track the tables
-  GraphqlEngine.postMetadata_
-    state
-    [yaml|
-type: mssql_track_table
-args:
-  source: mssql
-  table:
-    schema: hasura
-    name: author
-|]
-
-sqlserverTeardown :: (State, ()) -> IO ()
-sqlserverTeardown _ = do
-  Sqlserver.run_
-    [sql|
-DROP TABLE hasura.author;
-|]
-
---------------------------------------------------------------------------------
--- BigQuery backend
-
-bigquerySetup :: (State, ()) -> IO ()
-bigquerySetup (state, ()) = do
-  -- Clear and reconfigure the metadata
-  serviceAccount <- BigQuery.getServiceAccount
-  projectId <- BigQuery.getProjectId
-  GraphqlEngine.postMetadata_
-    state
-    [yaml|
-type: replace_metadata
-args:
-  version: 3
-  sources:
-  - name: bigquery
-    kind: bigquery
-    tables: []
-    configuration:
-      service_account: *serviceAccount
-      project_id: *projectId
-      datasets: [hasura]
-|]
-
-  -- Setup tables
-  BigQuery.run_
-    serviceAccount
-    projectId
-    [sql|
-CREATE TABLE hasura.author
-(
-    id INT64,
-    name STRING
-);
-|]
-  BigQuery.run_
-    serviceAccount
-    projectId
-    [sql|
-INSERT INTO hasura.author
-    (id, name)
-VALUES
-    (1, 'Author 1'),
-    (2, 'Author 2');
-|]
-
-  -- Track the tables
-  GraphqlEngine.postMetadata_
-    state
-    [yaml|
-type: bigquery_track_table
-args:
-  source: bigquery
-  table:
-    dataset: hasura
-    name: author
-|]
-
-bigqueryTeardown :: (State, ()) -> IO ()
-bigqueryTeardown _ = do
-  serviceAccount <- BigQuery.getServiceAccount
-  projectId <- BigQuery.getProjectId
-  BigQuery.run_
-    serviceAccount
-    projectId
-    [sql|
-DROP TABLE hasura.author;
-|]
+_article :: Schema.Table
+_article =
+  Schema.Table
+    "article"
+    [ Schema.column "id" Schema.TInt,
+      Schema.column "name" Schema.TStr,
+      Schema.column "author_id" Schema.TInt
+    ]
+    ["id"]
+    [Schema.Reference "author_id" "hasura.author" "id"]
+    [ [Schema.VInt 1, Schema.VStr "article1", Schema.VInt 1],
+      [Schema.VInt 2, Schema.VStr "article2", Schema.VInt 2],
+      [Schema.VInt 3, Schema.VStr "article3", Schema.VInt 3]
+    ]
 
 --------------------------------------------------------------------------------
 -- Tests
 
 tests :: Context.Options -> SpecWith State
-tests opts = do
+tests opts = describe "BasicFieldsSpec" $ do
   it "Author fields" $ \state ->
     shouldReturnYaml
       opts
