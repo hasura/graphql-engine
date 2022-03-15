4ec4928c34336a64a28329c96e566606a95bbfac
Sai Krishna Prasad Kandula <kskprasad96@gmail.com>
2022-03-15 16:00:25 +0530

    remove unused imports

--- docs-new/src/components/VersionedLink.tsx
+++ docs-new/src/components/VersionedLink.tsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Link from "@docusaurus/Link";
-import { usePluginData } from '@docusaurus/useGlobalData';
-import {DEFAULT_PLUGIN_ID} from '@docusaurus/constants';
 
 const VersionedLink = ({ to, children }) => (
   <Link to={`/latest${to}`}>
