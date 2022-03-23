adb648b429eb869d85056206fdde3c9df447af9f
jkachmar <joe@hasura.io>
2022-03-23 16:23:46 -0400

    server: Rework internal webhook request transform components
    ## Description
    
    Some of the documentation/organizational changes I was putting into the suggestions for #3624 were a bit too convoluted for GitHub's suggestion interface, so I'm putting them here instead.
    
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/3910
    Co-authored-by: Solomon <24038+solomon-b@users.noreply.github.com>
    GitOrigin-RevId: 06e0cb08bd18e7f8b21452df0697cfd80bc56fde

--- server/src-lib/Data/Aeson/Extended.hs
+++ server/src-lib/Data/Aeson/Extended.hs
@@ -1,28 +1,32 @@
 module Data.Aeson.Extended
-  ( module J,
-    encodeToStrictText,
+  ( FromJSONKeyValue (..),
     ToJSONKeyValue (..),
-    FromJSONKeyValue (..),
     mapWithJSONPath,
+    encodeToStrictText,
+    (.=?),
+
+    -- * Re-exports
+    module Data.Aeson,
   )
 where
 
-import Data.Aeson as J
+-------------------------------------------------------------------------------
+
+import Data.Aeson
 import Data.Aeson.Text (encodeToTextBuilder)
 import Data.Aeson.Types (JSONPathElement (..), Parser)
-import Data.Functor.Const
+import Data.Functor.Const (getConst)
 import Data.Text.Lazy (toStrict)
 import Data.Text.Lazy.Builder (toLazyText)
 import Hasura.Prelude
 
-encodeToStrictText :: (ToJSON a) => a -> Text
-encodeToStrictText = toStrict . toLazyText . encodeToTextBuilder
+-------------------------------------------------------------------------------
 
 class ToJSONKeyValue a where
-  toJSONKeyValue :: a -> (Text, J.Value)
+  toJSONKeyValue :: a -> (Text, Value)
 
 class FromJSONKeyValue a where
-  parseJSONKeyValue :: (Text, J.Value) -> Parser a
+  parseJSONKeyValue :: (Text, Value) -> Parser a
 
 instance ToJSONKeyValue Void where
   toJSONKeyValue = absurd
@@ -30,7 +34,23 @@ instance ToJSONKeyValue Void where
 instance ToJSONKeyValue a => ToJSONKeyValue (Const a b) where
   toJSONKeyValue = toJSONKeyValue . getConst
 
--- | map a 'Parser' over a list, keeping the JSONPath context
+-------------------------------------------------------------------------------
+
+-- | An optional key-value pair for encoding a JSON object.
+--
+-- @
+-- object $ ["foo" .= 0] <> catMaybes [ "bar" .=? Nothing, "baz" .=? 2 ]
+-- @
+(.=?) :: (ToJSON v, KeyValue kv) => Text -> Maybe v -> Maybe kv
+(.=?) k = fmap (k .=)
+{-# INLINE (.=?) #-}
+
+infixr 8 .=?
+
+-- | Map a 'Parser' over a list, keeping the JSONPath context
 mapWithJSONPath :: (a -> Parser b) -> [a] -> Parser [b]
 mapWithJSONPath parser xs =
   traverse (\(idx, item) -> parser item <?> Index idx) $ zip [0 ..] xs
+
+encodeToStrictText :: ToJSON a => a -> Text
+encodeToStrictText = toStrict . toLazyText . encodeToTextBuilder
--- server/src-lib/Hasura/RQL/DDL/Webhook/Transform/Headers.hs
+++ server/src-lib/Hasura/RQL/DDL/Webhook/Transform/Headers.hs
@@ -1,16 +1,13 @@
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE ViewPatterns #-}
 
 module Hasura.RQL.DDL.Webhook.Transform.Headers
   ( -- * Header Transformations
     Headers (..),
     TransformFn (..),
-
-    -- ** Header Transformation Action
-    HeadersTransformAction (..),
-    ReplaceHeaderFields (..),
+    HeadersTransformFn (..),
+    AddReplaceOrRemoveFields (..),
   )
 where
 
@@ -38,82 +35,116 @@ import Network.HTTP.Types qualified as HTTP.Types
 
 -------------------------------------------------------------------------------
 
--- | The actual header data we are transforming.
+-- | The actual header data we are transforming..
 --
 -- This newtype is necessary because otherwise we end up with an
 -- orphan instance.
 newtype Headers = Headers [HTTP.Types.Header]
 
 instance Transform Headers where
-  newtype TransformFn Headers
-    = HeadersTransform HeadersTransformAction
+  -- NOTE: GHC does not let us attach Haddock documentation to data family
+  -- instances, so 'HeadersTransformFn' is defined separately from this
+  -- wrapper.
+  newtype TransformFn Headers = HeadersTransformFn_ HeadersTransformFn
     deriving stock (Eq, Generic, Show)
-    deriving newtype (FromJSON, ToJSON)
-    deriving anyclass (Cacheable, NFData)
+    deriving newtype (Cacheable, NFData, FromJSON, ToJSON)
 
-  transform ::
-    MonadError TransformErrorBundle m =>
-    TransformFn Headers ->
-    RequestTransformCtx ->
-    Headers ->
-    m Headers
-  transform (HeadersTransform transformation) context (Headers originalHeaders) =
-    case transformation of
-      ReplaceHeaders ReplaceHeaderFields {rhf_addHeaders, rhf_removeHeaders} -> do
-        let removeHeadersBytes = fmap (CI.map TE.encodeUtf8) rhf_removeHeaders
-            filteredHeaders =
-              originalHeaders & filter \(key, _val) ->
-                key `notElem` removeHeadersBytes
+  -- NOTE: GHC does not let us attach Haddock documentation to typeclass
+  -- method implementations, so 'applyHeadersTransformFn' is defined
+  -- separately.
+  transform (HeadersTransformFn_ fn) = applyHeadersTransformFn fn
 
-        -- NOTE: We use `ApplicativeDo` here to take advantage of
-        -- Validation's applicative sequencing.
-        newHeaders <- liftEither . V.toEither $ for rhf_addHeaders \(rawKey, rawValue) -> do
-          let key = CI.map TE.encodeUtf8 rawKey
-          value <- runUnescapedRequestTemplateTransform' context rawValue
-          pure (key, value)
-
-        pure . Headers $ filteredHeaders <> newHeaders
-
-  validate ::
-    TemplatingEngine ->
-    TransformFn Headers ->
-    Validation TransformErrorBundle ()
-  validate engine (HeadersTransform (ReplaceHeaders (fmap snd . rhf_addHeaders -> templates))) =
-    traverse_ (validateRequestUnescapedTemplateTransform' engine) templates
+  -- NOTE: GHC does not let us attach Haddock documentation to typeclass
+  -- method implementations, so 'validateHeadersTransformFn' is defined
+  -- separately.
+  validate engine (HeadersTransformFn_ fn) =
+    validateHeadersTransformFn engine fn
 
 -- | The defunctionalized transformation on 'Headers'
-newtype HeadersTransformAction
-  = ReplaceHeaders ReplaceHeaderFields
-  deriving stock (Generic)
-  deriving newtype (Eq, Show, FromJSON, ToJSON)
-  deriving anyclass (Cacheable, NFData)
+newtype HeadersTransformFn
+  = -- | Add or replace matching 'HTTP.Types.Header's.
+    AddReplaceOrRemove AddReplaceOrRemoveFields
+  deriving stock (Eq, Generic, Show)
+  deriving newtype (Cacheable, NFData, FromJSON, ToJSON)
 
 -- | The user can supply a set of header keys to be filtered from the
 -- request and a set of headers to be added to the request.
-data ReplaceHeaderFields = ReplaceHeaderFields
-  { rhf_addHeaders :: [(CI.CI Text, UnescapedTemplate)],
-    rhf_removeHeaders :: [CI.CI Text]
+data AddReplaceOrRemoveFields = AddReplaceOrRemoveFields
+  { -- | A list of key-value pairs for 'HTTP.Types.Header's which
+    -- should be added (if they don't exist) or replaced (if they do) within
+    -- the HTTP message.
+    addOrReplaceHeaders :: [(CI.CI Text, UnescapedTemplate)],
+    -- | A list of 'HTTP.Type.Header' keys which should be removed from the
+    -- HTTP message.
+    removeHeaders :: [CI.CI Text]
   }
   deriving stock (Show, Eq, Ord, Generic)
   deriving anyclass (NFData, Cacheable)
 
-instance ToJSON ReplaceHeaderFields where
-  toJSON ReplaceHeaderFields {..} =
-    J.object
-      [ "add_headers" J..= M.fromList (fmap (first CI.original) rhf_addHeaders),
-        "remove_headers" J..= fmap CI.original rhf_removeHeaders
-      ]
+-- | Provide an implementation for the transformations defined by
+-- 'HeadersTransformFn'.
+--
+-- If one views 'HeadersTransformFn' as an interface describing HTTP message
+-- header transformations, this can be seen as an implementation of these
+-- transformations as normal Haskell functions.
+applyHeadersTransformFn ::
+  MonadError TransformErrorBundle m =>
+  HeadersTransformFn ->
+  RequestTransformCtx ->
+  Headers ->
+  m Headers
+applyHeadersTransformFn fn context (Headers originalHeaders) = case fn of
+  AddReplaceOrRemove fields -> do
+    -- NOTE: 'TE.decodeUtf8' can fail with an impure exception; conversion
+    -- to bytes is infallible.
+    let AddReplaceOrRemoveFields {addOrReplaceHeaders, removeHeaders} = fields
+        removeHeadersBytes = fmap (CI.map TE.encodeUtf8) removeHeaders
+        filteredHeaders =
+          originalHeaders & filter \(key, _val) ->
+            key `notElem` removeHeadersBytes
 
-instance FromJSON ReplaceHeaderFields where
-  parseJSON = J.withObject "ReplaceHeaderFields" $ \o -> do
-    addHeaders <- o J..:? "add_headers" J..!= mempty
-    let rhf_addHeaders = M.toList $ mapKeys CI.mk addHeaders
+    -- NOTE: We use `ApplicativeDo` here to take advantage of Validation's
+    -- applicative sequencing
+    newHeaders <- liftEither . V.toEither $
+      for addOrReplaceHeaders \(rawKey, rawValue) -> do
+        let key = CI.map TE.encodeUtf8 rawKey
+        value <- runUnescapedRequestTemplateTransform' context rawValue
+        pure (key, value)
 
-    removeHeaders <- o J..:? "remove_headers" J..!= mempty
+    pure . Headers $ filteredHeaders <> newHeaders
+
+-- | Validate that the provided 'HeadersTransformFn' is correct in the context
+-- of a particular 'TemplatingEngine'.
+--
+-- This is a product of the fact that the correctness of a given transformation
+-- may be dependent on zero, one, or more of the templated transformations
+-- encoded within the given 'HeadersTransformFn'.
+validateHeadersTransformFn ::
+  TemplatingEngine ->
+  HeadersTransformFn ->
+  Validation TransformErrorBundle ()
+validateHeadersTransformFn engine = \case
+  AddReplaceOrRemove fields -> do
+    let templates = fields & addOrReplaceHeaders & map snd
+    traverse_ (validateRequestUnescapedTemplateTransform' engine) templates
+
+instance FromJSON AddReplaceOrRemoveFields where
+  parseJSON = J.withObject "AddReplaceRemoveFields" $ \o -> do
+    addOrReplaceHeadersTxt <- o J..:? "add_headers" J..!= mempty
+    let addOrReplaceHeaders = M.toList $ mapKeys CI.mk addOrReplaceHeadersTxt
+
+    removeHeadersTxt <- o J..:? "remove_headers" J..!= mempty
     -- NOTE: Ensure that the FromJSON instance is used for deserialization.
-    let rhf_removeHeaders = coerce @[HeaderKey] removeHeaders
+    let removeHeaders = coerce @[HeaderKey] removeHeadersTxt
 
-    pure ReplaceHeaderFields {rhf_addHeaders, rhf_removeHeaders}
+    pure AddReplaceOrRemoveFields {addOrReplaceHeaders, removeHeaders}
+
+instance ToJSON AddReplaceOrRemoveFields where
+  toJSON AddReplaceOrRemoveFields {..} =
+    J.object
+      [ "add_headers" J..= M.fromList (fmap (first CI.original) addOrReplaceHeaders),
+        "remove_headers" J..= fmap CI.original removeHeaders
+      ]
 
 -- | This newtype exists solely to anchor a `FromJSON` instance and is
 -- eliminated in the `TransformHeaders` `FromJSON` instance.
