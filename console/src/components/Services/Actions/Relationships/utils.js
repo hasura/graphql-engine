6fc3f8538e33075596276454229c4aa60d1f66bc
Abhijeet Khangarot <abhijeet40308@gmail.com>
2022-03-25 11:54:29 +0530

    console: handle console crash on action relationships with scalar output type
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/4028
    GitOrigin-RevId: 33eb02569cd1258495ae7703c67fa2183a082af1

--- console/src/components/Services/Actions/Relationships/utils.js
+++ console/src/components/Services/Actions/Relationships/utils.js
@@ -108,3 +108,9 @@ export const validateRelTypename = (types, typename, relname) => {
   }
   return null;
 };
+
+export const getScalarOutputType = typeName => ({
+  kind: 'scalar',
+  name: typeName,
+  fields: [],
+});
