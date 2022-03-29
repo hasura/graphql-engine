e2ddb09b3384867a31c3181ae7ec4374cfb46a62
Catalin Pit <catalinpit@gmail.com>
2022-03-29 17:04:11 +0300

    updated nuxtjs-postgres-graphql sample app

--- community/sample-apps/nuxtjs-postgres-graphql/apollo/clientConfig.js
+++ community/sample-apps/nuxtjs-postgres-graphql/apollo/clientConfig.js
@@ -1,11 +1,12 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
-export default function(context){
+
+export default function (context) {
   return {
-  		httpLinkOptions: {
-    		uri: 'http://localhost:8080/v1/graphql',
-    		credentials: 'same-origin'
-  		},
-  		cache: new InMemoryCache(),
-	    wsEndpoint: 'ws://localhost:8080/v1/graphql',
-  	}
+    httpLinkOptions: {
+      uri: "http://localhost:8080/v1/graphql",
+      credentials: "same-origin",
+    },
+    cache: new InMemoryCache(),
+    wsEndpoint: "ws://localhost:8080/v1/graphql",
+  };
 }
