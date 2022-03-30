7bdb114ddef3ffde2cc6948f24629565884f04d5
Catalin Pit <catalinpit@gmail.com>
2022-03-30 14:02:52 +0300

    updated gridsome-postgres-graphql sample app

--- community/sample-apps/gridsome-postgres-graphql/gridsome.server.js
+++ community/sample-apps/gridsome-postgres-graphql/gridsome.server.js
@@ -5,12 +5,12 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-module.exports = function (api) {
+module.exports = function(api) {
   api.loadSource(({ addContentType }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api
-  })
+  });
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api
-  })
-}
+  });
+};
--- community/sample-apps/gridsome-postgres-graphql/src/pages/Articles.vue
+++ community/sample-apps/gridsome-postgres-graphql/src/pages/Articles.vue
@@ -2,12 +2,16 @@
   <Layout>
     <h1>Articles</h1>
     <div v-if="$page.hasura.article.length">
-	    <div class="articles" v-for="article in $page.hasura.article" :key="article.id">
-	      <p>{{ article.title }} by {{ article.author.name }}</p>
-	    </div>
+      <div
+        class="articles"
+        v-for="article in $page.hasura.article"
+        :key="article.id"
+      >
+        <p>{{ article.title }} by {{ article.author.name }}</p>
+      </div>
     </div>
     <div v-else>
-    	<p>No articles found</p>
+      <p>No articles found</p>
     </div>
   </Layout>
 </template>
@@ -29,7 +33,7 @@ query {
 <script>
 export default {
   metaInfo: {
-    title: 'Articles'
-  }
-}
+    title: "Articles",
+  },
+};
 </script>
