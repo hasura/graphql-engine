dff123b6f23c7e71403e766e8ffd06680f6b98f7
Shraddha Agrawal <shraddha.agrawal000@gmail.com>
2022-04-18 20:25:14 +0530

    docs: add cloud docs for billing owner
    fixes https://github.com/hasura/lux/issues/3686
    
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/3732
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Divya Bhushan <11659160+divyabhushan@users.noreply.github.com>
    Co-authored-by: Rikin Kachhia <54616969+rikinsk@users.noreply.github.com>
    GitOrigin-RevId: b1bbd2d6bff206d7fae511629a9ef7a86a402891

--- docs/scripts/pandoc-auto-transform.sh
+++ docs/scripts/pandoc-auto-transform.sh
@@ -19,7 +19,7 @@
 
 # run on only one file
 # ./docs/scripts/pandoc-auto-transform.sh "graphql/core/actions/create.rst"
-# pandoc "docs/$1" -f rst -t gfm -s -o "docs/docs/${1%}.txt";
+# pandoc "docs-old/$1" -f rst -t gfm -s -o "docs/docs/${1%}.txt";
 
 # run on list of files
 # Ref: https://stackoverflow.com/a/424142/10208226 to get list of changed files in a commit
@@ -28,7 +28,7 @@
 # files=( "graphql/core/databases/ms-sql-server/index.rst" "graphql/core/databases/ms-sql-server/mutations/index.rst" "graphql/core/databases/ms-sql-server/mutations/insert.rst" "graphql/core/databases/ms-sql-server/mutations/upsert.rst" "graphql/core/databases/postgres/mutations/upsert.rst" )
 # for f in "${files[@]}"; do
 #     mkdir -pv -- "docs/${f%/*}" && touch -- "docs/docs/${f%}.txt";
-#     pandoc "docs/$f" -f rst -t gfm -s -o "docs/docs/${f%}.txt";
+#     pandoc "docs-old/$f" -f rst -t gfm -s -o "docs/docs/${f%}.txt";
 # done
 
   # mkdir -pv -- "$1/${f%/*}" && touch -- "$1/${f%}.txt";
