20433d94b59bf899056a533e9b469808c61f85bc
Vijay Prasanna <vijayprasanna2896@gmail.com>
2022-04-21 16:10:56 +0530

    console: create a reusable database selector widget
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/4295
    GitOrigin-RevId: 962e0f1e25d0100eb452cd609401ca593963e188

---
+++ console/src/features/Data/components/DatabaseSelector/Select.tsx
@@ -0,0 +1,50 @@
+import { FieldWrapper } from '@/new-components/Form';
+import clsx from 'clsx';
+import React from 'react';
+import { FieldError } from 'react-hook-form';
+
+interface TSelect extends React.ComponentProps<'select'> {
+  options: string[];
+  error?: FieldError;
+  icon?: React.ReactElement;
+  label: string;
+}
+
+export const Select = ({
+  options,
+  placeholder,
+  disabled,
+  error,
+  ...props
+}: TSelect) => (
+  <FieldWrapper
+    id={props.name}
+    error={error}
+    labelIcon={props.icon}
+    label={props.label}
+  >
+    <select
+      {...props}
+      className={clsx(
+        'block w-full h-input shadow-sm rounded border border-gray-300 hover:border-gray-400 focus:outline-0 focus:ring-2 focus:ring-yellow-200 focus:border-yellow-400',
+        disabled
+          ? 'cursor-not-allowed bg-gray-100 border-gray-100'
+          : 'hover:border-gray-400'
+      )}
+      disabled={disabled}
+      value={props.value}
+    >
+      {placeholder ? (
+        <option disabled value="">
+          {placeholder}
+        </option>
+      ) : null}
+
+      {options.map((op, i) => (
+        <option value={op} key={i}>
+          {op}
+        </option>
+      ))}
+    </select>
+  </FieldWrapper>
+);
