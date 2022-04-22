bc8acb63ba6fba4d7583ce50ba532b7211e744d5
Antoine Leblanc <antoine@hasura.io>
2022-04-22 17:01:07 +0100

    Remove `HasSystemDefined`.
    ### Description
    
    `HasSystemDefined` is defined in `RQL.Types`, but only used in one place, `LegacyCatalog`, to avoid passing a boolean around. It is easily replaced by an ad-hoc `ReaderT`.
    
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/4337
    GitOrigin-RevId: 649d758bb2b18b39533429dda5ab71afde62fb53

--- server/src-lib/Hasura/RQL/Types.hs
+++ server/src-lib/Hasura/RQL/Types.hs
@@ -2,9 +2,6 @@ module Hasura.RQL.Types
   ( MonadTx (..),
     SQLGenCtx (..),
     RemoteSchemaPermsCtx (..),
-    HasSystemDefined (..),
-    HasSystemDefinedT,
-    runHasSystemDefinedT,
     askSourceInfo,
     askSourceConfig,
     askSourceTables,
@@ -70,9 +67,6 @@ import Hasura.RQL.Types.SourceCustomization as R
 import Hasura.RQL.Types.Subscription as R
 import Hasura.RQL.Types.Table as R
 import Hasura.SQL.Backend as R
-import Hasura.Session
-import Hasura.Tracing
-import Network.HTTP.Client.Manager (HasHttpManagerM (..))
 
 askSourceInfo ::
   forall b m.
@@ -163,43 +157,6 @@ askTableMetadata sourceName tableName = do
         . toSourceMetadata @b
         . smTables
         . ix tableName
-
-class (Monad m) => HasSystemDefined m where
-  askSystemDefined :: m SystemDefined
-
-instance (HasSystemDefined m) => HasSystemDefined (ReaderT r m) where
-  askSystemDefined = lift askSystemDefined
-
-instance (HasSystemDefined m) => HasSystemDefined (StateT s m) where
-  askSystemDefined = lift askSystemDefined
-
-instance (Monoid w, HasSystemDefined m) => HasSystemDefined (WriterT w m) where
-  askSystemDefined = lift askSystemDefined
-
-instance (HasSystemDefined m) => HasSystemDefined (TraceT m) where
-  askSystemDefined = lift askSystemDefined
-
-newtype HasSystemDefinedT m a = HasSystemDefinedT {unHasSystemDefinedT :: ReaderT SystemDefined m a}
-  deriving
-    ( Functor,
-      Applicative,
-      Monad,
-      MonadTrans,
-      MonadIO,
-      MonadError e,
-      MonadTx,
-      HasHttpManagerM,
-      SourceM,
-      TableCoreInfoRM b,
-      CacheRM,
-      UserInfoM
-    )
-
-runHasSystemDefinedT :: SystemDefined -> HasSystemDefinedT m a -> m a
-runHasSystemDefinedT systemDefined = flip runReaderT systemDefined . unHasSystemDefinedT
-
-instance (Monad m) => HasSystemDefined (HasSystemDefinedT m) where
-  askSystemDefined = HasSystemDefinedT ask
 
 throwTableDoesNotExist :: forall b m a. (QErrM m, Backend b) => TableName b -> m a
 throwTableDoesNotExist tableName = throw400 NotExists ("table " <> tableName <<> " does not exist")
