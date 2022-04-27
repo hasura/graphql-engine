6a12893109e6b9fbbd7602c7d8f181193de69678
Vijay Prasanna <vijayprasanna2896@gmail.com>
2022-04-27 12:47:35 +0530

    console: improvements to ListMap component
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/4331
    GitOrigin-RevId: 4d2053ba1793f5419aff1f58357a40b2e777924b

--- console/src/new-components/ListMap/ListMap.stories.tsx
+++ console/src/new-components/ListMap/ListMap.stories.tsx
@@ -1,9 +1,9 @@
 import { ComponentMeta, Story } from '@storybook/react';
 import { userEvent, within } from '@storybook/testing-library';
 import React from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
 import { expect } from '@storybook/jest';
 import { GiCarrot, GiShinyApple } from 'react-icons/gi';
+import { FaCircle } from 'react-icons/fa';
 import { z } from 'zod';
 import { ListMap } from '.';
 import { Button } from '../Button';
@@ -14,16 +14,41 @@ export default {
   component: ListMap,
 } as ComponentMeta<typeof ListMap>;
 
-export const Playground: Story = () => {
+export const ArrayToArray: Story = () => {
   return (
     <ListMap
-      fromLabel="Source Column"
-      toLabel="Reference Column"
-      maps={{ apple: 'tomato', cherry: 'chilli' }}
-      fromOptions={['apple', 'cherry', 'banana', 'pineapple', 'strawberry']}
-      toOptions={['tomato', 'chilli', 'pumpkin', 'onion', 'carrot']}
+      value={{ apple: 'tomato', cherry: 'chilli' }}
       name="type_selector"
       onChange={e => console.log(e)}
+      from={{
+        options: ['apple', 'cherry', 'banana', 'pineapple', 'strawberry'],
+        label: 'Source Column',
+        placeholder: 'Select a fruit',
+      }}
+      to={{
+        type: 'array',
+        options: ['tomato', 'chilli', 'pumpkin', 'onion', 'carrot'],
+        label: 'Reference Column',
+        placeholder: 'Select a vegatable',
+      }}
+    />
+  );
+};
+
+export const ArrayToString: Story = () => {
+  return (
+    <ListMap
+      value={{ apple: 'tomato', cherry: 'chilli' }}
+      name="type_selector"
+      onChange={e => console.log(e)}
+      from={{
+        options: ['apple', 'cherry', 'banana', 'pineapple', 'strawberry'],
+        label: 'Source Column',
+      }}
+      to={{
+        type: 'string',
+        label: 'Reference Column',
+      }}
     />
   );
 };
@@ -31,17 +56,65 @@ export const Playground: Story = () => {
 export const WithIcons: Story = () => {
   return (
     <ListMap
-      fromLabel="Source Column"
-      toLabel="Reference Column"
-      maps={{ apple: 'tomato', cherry: 'chilli' }}
-      fromOptions={['apple', 'cherry', 'banana', 'pineapple', 'strawberry']}
-      toOptions={['tomato', 'chilli', 'pumpkin', 'onion', 'carrot']}
+      value={{ apple: 'tomato', cherry: 'chilli' }}
       name="type_selector"
       onChange={e => console.log(e)}
-      fromIcon={<GiShinyApple />}
-      toIcon={<GiCarrot />}
+      from={{
+        options: ['apple', 'cherry', 'banana', 'pineapple', 'strawberry'],
+        label: 'Source Column',
+        icon: <GiShinyApple />,
+      }}
+      to={{
+        type: 'array',
+        options: ['tomato', 'chilli', 'pumpkin', 'onion', 'carrot'],
+        label: 'Reference Column',
+        icon: <GiCarrot />,
+      }}
     />
   );
+};
+
+export const WithMultipleIcons: Story = () => {
+  return (
+    <ListMap
+      value={{ apple: 'tomato', cherry: 'chilli' }}
+      name="type_selector"
+      onChange={e => console.log(e)}
+      from={{
+        options: ['apple', 'cherry', 'banana', 'pineapple', 'strawberry'],
+        label: 'Source Column',
+        icon: [<FaCircle />, <GiShinyApple />],
+      }}
+      to={{
+        type: 'array',
+        options: ['tomato', 'chilli', 'pumpkin', 'onion', 'carrot'],
+        label: 'Reference Column',
+        icon: [<FaCircle />, <GiShinyApple />],
+      }}
+    />
+  );
+};
+
+export const WithNoLabelsAndNoBackground: Story = ({ className }) => {
+  return (
+    <ListMap
+      value={{ apple: 'tomato', cherry: 'chilli' }}
+      name="type_selector"
+      onChange={e => console.log(e)}
+      from={{
+        options: ['apple', 'cherry', 'banana', 'pineapple', 'strawberry'],
+      }}
+      to={{
+        type: 'array',
+        options: ['tomato', 'chilli', 'pumpkin', 'onion', 'carrot'],
+      }}
+      className={className}
+    />
+  );
+};
+
+WithNoLabelsAndNoBackground.args = {
+  className: 'bg-transparent border-none',
 };
 
 type Schema = z.infer<typeof schema>;
@@ -51,30 +124,21 @@ const schema = z.object({
 });
 
 const FormElements = () => {
-  const { control } = useFormContext<Schema>();
-
   return (
     <>
-      <Controller
-        control={control}
+      <ListMap
+        from={{
+          options: ['apple', 'cherry', 'banana', 'pineapple', 'strawberry'],
+          label: 'Source Column',
+          placeholder: 'Select a fruit',
+        }}
+        to={{
+          type: 'array',
+          options: ['tomato', 'chilli', 'pumpkin', 'onion', 'carrot'],
+          label: 'Reference Column',
+          placeholder: 'Select a vegatable',
+        }}
         name="mapping"
-        render={({ field: { onChange, value } }) => (
-          <ListMap
-            onChange={onChange}
-            fromLabel="Source Column"
-            toLabel="Reference Column"
-            maps={value}
-            fromOptions={[
-              'apple',
-              'cherry',
-              'banana',
-              'pineapple',
-              'strawberry',
-            ]}
-            toOptions={['tomato', 'chilli', 'pumpkin', 'onion', 'carrot']}
-            name="mapping"
-          />
-        )}
       />
     </>
   );
@@ -119,11 +183,14 @@ WithReactFormHookNested.play = async ({ canvasElement }) => {
   expect(fromLabel).toBeInTheDocument();
   expect(toLabel).toBeInTheDocument();
 
-  // update fields
-  const fromInput = await canvas.getByTestId('mapping_from_user_input');
+  const addNewRowBtn = await canvas.getByTestId('mapping_add_new_row');
+  userEvent.click(addNewRowBtn);
+
+  // // update fields
+  const fromInput = await canvas.getByTestId('mapping_source_input_2');
   userEvent.selectOptions(fromInput, 'banana');
 
-  const toInput = await canvas.getByTestId('mapping_to_user_input');
+  const toInput = await canvas.getByTestId('mapping_target_input_2');
   userEvent.selectOptions(toInput, 'carrot');
 
   const submitButton = await canvas.getByTestId('submit');
