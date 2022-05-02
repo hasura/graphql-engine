77389352cc8590e91240592161e90757ec428f75
Varun Choudhary <68095256+Varun-Choudhary@users.noreply.github.com>
2022-05-02 17:49:25 +0530

    CON-128-filter out scalar type from source type for RS-to-RS relationships
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/4406
    GitOrigin-RevId: 6421e1b2695870ba48ab0f095c3c82568e6ffd55

--- console/src/features/RemoteRelationships/RemoteSchemaRelationships/utils.ts
+++ console/src/features/RemoteRelationships/RemoteSchemaRelationships/utils.ts
@@ -1,17 +1,13 @@
-import { checkDefaultGQLScalarType } from '@/components/Services/RemoteSchema/Permissions/utils';
-import { GraphQLSchema } from 'graphql';
+import { GraphQLSchema, isObjectType } from 'graphql';
 
 export const getTypesFromIntrospection = (data: GraphQLSchema) => {
   return Object.entries(data.getTypeMap())
+    .filter(([typeName, i]) => isObjectType(i) && !typeName.startsWith('__'))
     .map(([typeName, x]) => ({
       typeName,
       // eslint-disable-next-line no-underscore-dangle
       fields: Object.keys((x as any)._fields || {}),
-    }))
-    .filter(
-      ({ typeName }) =>
-        !checkDefaultGQLScalarType(typeName) && !typeName.startsWith('__')
-    );
+    }));
 };
 
 type rsType = {
