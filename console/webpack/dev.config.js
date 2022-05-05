db1e6cc79fda313433e53e104ab4fd36c43e6283
Nicolas Beaussart <nic.beaussart@gmail.com>
2022-05-05 19:46:23 +0200

    console: removing bootstrap webpack loader [CON-153]
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/4328
    GitOrigin-RevId: d8189f4bdc7fdeecf575a40843f48c7cc727ea4d

--- console/webpack/dev.config.js
+++ console/webpack/dev.config.js
@@ -30,7 +30,6 @@ module.exports = {
   entry: {
     main: [
       `webpack-hot-middleware/client?path=http://${host}:${port}/__webpack_hmr`,
-      'bootstrap-loader?extractStyles',
       'font-awesome-webpack!./src/theme/font-awesome.config.js',
       './src/client.js',
     ],
