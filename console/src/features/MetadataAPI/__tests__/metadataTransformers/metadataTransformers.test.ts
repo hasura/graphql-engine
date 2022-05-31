f611ee232a8f1d1193adafbee0deecc9ad2d8a4d
Matt Hardman <mattshardman@gmail.com>
2022-05-31 18:43:31 +0700

    CON-151-create-db-to-local-relationship-hook-for-object-and-array-relationships
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/4472
    GitOrigin-RevId: e10abcfa861067ba101348d0cd4e670b8dcbadf8

--- console/src/features/MetadataAPI/__tests__/metadataTransformers/metadataTransformers.test.ts
+++ console/src/features/MetadataAPI/__tests__/metadataTransformers/metadataTransformers.test.ts
@@ -1,5 +1,9 @@
 import { MetadataTransformer } from '../../hooks/metadataTransformers';
-import { dbToRemoteSchemaRelationships } from './__fixtures__';
+import {
+  dbToLocalDbRelationship,
+  dbToRemoteSchemaRelationships,
+  tableRelationships,
+} from './__fixtures__';
 
 test('transformDbToRemoteSchema returns new and legacy formats consistently', () => {
   const result = MetadataTransformer.transformDbToRemoteSchema(
@@ -53,6 +57,39 @@ test('transformDbToRemoteSchema returns new and legacy formats consistently', ()
           "schema": "public",
           "table": "user",
         },
+      },
+    ]
+  `);
+});
+
+test('transformDbToLocal returns object relationships correctly', () => {
+  const result = MetadataTransformer.transformTableRelationships({
+    target: { table: 'product', schema: 'public', database: 'default' },
+    relationships: {
+      objectRelationships: dbToLocalDbRelationship.objectRelationships,
+      arrayRelationships: [],
+    },
+    tableRelationships,
+  });
+
+  expect(result).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "comment": undefined,
+        "from": Object {
+          "column": Array [
+            "fk_user_id",
+          ],
+          "table": "product",
+        },
+        "name": "product_user",
+        "to": Object {
+          "column": Array [
+            "id",
+          ],
+          "table": "\\"user\\"",
+        },
+        "type": "object",
       },
     ]
   `);
--- console/src/features/MetadataAPI/hooks/useMetadataTables.ts
+++ console/src/features/MetadataAPI/hooks/useMetadataTables.ts
@@ -1,6 +1,8 @@
-import { DataTarget } from '@/features/Datasources';
+import { useTableRelationships, DataTarget } from '@/features/Datasources';
+
 import { QualifiedTable } from '@/metadata/types';
 import { MetadataSelector } from './metadataSelectors';
+import { MetadataTransformer } from './metadataTransformers';
 import { useMetadata } from './useMetadata';
 
 export const useMetadataTables = (dataSource: string) => {
@@ -9,6 +11,42 @@ export const useMetadataTables = (dataSource: string) => {
 
 export const useTables = (database: string) => {
   return useMetadata(MetadataSelector.getTables(database));
+};
+
+export const useExistingRelationships = (
+  database: string,
+  table: QualifiedTable
+) => {
+  // metadata doesn't contain sufficient info
+  // therefore have to get full table relationship info
+  const {
+    data: tableRelationships,
+    isLoading: loadingTableRelationships,
+    error: tableRelationshipsError,
+  } = useTableRelationships({
+    target: { database, schema: table.schema, table: table.name },
+  });
+
+  const { error, isLoading: loadingMetadata, ...rest } = useMetadata(
+    MetadataSelector.createGetLocalDBRelationships(database, table),
+    relationships =>
+      MetadataTransformer.transformTableRelationships({
+        target: {
+          database,
+          table: table.name,
+          schema: table.schema,
+        },
+        relationships,
+        tableRelationships,
+      }),
+    {
+      enabled: !!tableRelationships?.length,
+    }
+  );
+
+  const isLoading = loadingMetadata || loadingTableRelationships;
+
+  return { ...rest, isLoading, error: error || tableRelationshipsError };
 };
 
 export const useRemoteDatabaseRelationships = (target: DataTarget) => {
