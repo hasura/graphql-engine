d76980008640269af1736ec52f990eb35a7f2fa3
Stefano Magni <nori.ste.magni@gmail.com>
2022-06-16 18:17:17 +0200

    console: Halt collecting the E2E tests code coverage CON-203
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/4510
    GitOrigin-RevId: 2fff40627278e6e8eb48bc4d466ce499e7669ca7

--- console/cypress/support/index.js
+++ console/cypress/support/index.js
@@ -15,28 +15,3 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-
-const istanbul = require('istanbul-lib-coverage');
-
-const map = istanbul.createCoverageMap({});
-
-Cypress.on('window:before:unload', e => {
-  const coverage = e.currentTarget.__coverage__;
-
-  if (coverage) {
-    map.merge(coverage);
-  }
-});
-
-after(() => {
-  cy.window().then(win => {
-    const coverage = win.__coverage__;
-
-    if (coverage) {
-      map.merge(coverage);
-    }
-
-    cy.writeFile('.nyc_output/out.json', JSON.stringify(map));
-    cy.exec('nyc report --reporter=html');
-  });
-});
