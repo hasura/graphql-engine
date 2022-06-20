1aa71145ba180894ed839c1c7a91080995d233ec
Stefano Magni <nori.ste.magni@gmail.com>
2022-06-20 23:39:04 +0200

    console: Unify the Mutation Action E2E tests
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/4509
    GitOrigin-RevId: e5b579988b426cb44575c9674056f4b2e539c165

--- console/cypress/integration/actions/mutation/mutationAction.e2e.test.ts
+++ console/cypress/integration/actions/mutation/mutationAction.e2e.test.ts
@@ -1,15 +1,19 @@
 import { testMode } from '../../../helpers/common';
 
 import { logMetadataRequests } from './utils/requests/logMetadataRequests';
-import { loginActionMustExist } from './utils/testState/loginActionMustExist';
-import { mustBeOnTheActionsPage } from './utils/testState/mustBeOnTheActionsPage';
 import { loginActionMustNotExist } from './utils/testState/loginActionMustNotExist';
-import { hakunaMatataPermissionMustNotExist } from './utils/testState/hakunaMatataPermissionMustNotExist';
 
 // NOTE: This test suite does not include cases for relationships, headers and the codegen part
 
-function runTests() {
+if (testMode !== 'cli') {
   describe('Mutation Actions', () => {
+    before(() => {
+      loginActionMustNotExist();
+      logMetadataRequests();
+
+      cy.visit('/actions/manage/actions');
+    });
+
     after(() => {
       // Cleanup after the whole test file run
 
@@ -21,167 +25,153 @@ function runTests() {
       loginActionMustNotExist({ reloadPage: false });
     });
 
-    // FYI: This is the same flow described in the Hasura's official documentation, under the
-    // "Creating Actions" section https://hasura.io/docs/latest/graphql/core/actions/create/
-    describe('When the user creates a new Action, everything should work', () => {
-      before(() => {
-        loginActionMustNotExist();
-        mustBeOnTheActionsPage();
-      });
+    it('When the users create, edit, and delete a Mutation Action, everything should work', () => {
+      cy.log('**------------------------------**');
+      cy.log('**------------------------------**');
+      cy.log('**------------------------------**');
+      cy.log('**--- Step 1: Mutation Action creation**');
+      cy.log('**------------------------------**');
+      cy.log('**------------------------------**');
+      cy.log('**------------------------------**');
 
-      it('Test the feature', () => {
-        logMetadataRequests();
+      cy.log('**--- Click on the Create button of the Actions panel**');
+      cy.getBySel('data-create-actions').click();
 
-        cy.log('**--- Click on the Create button of the Actions panel**');
-        cy.getBySel('data-create-actions').click();
+      // Assign an alias to the most unclear selectors for future references
+      cy.get('textarea').eq(0).as('actionDefinitionTextarea');
+      cy.get('textarea').eq(1).as('typeConfigurationTextarea');
 
-        // Assign an alias to the most unclear selectors for future references
-        cy.get('textarea').eq(0).as('actionDefinitionTextarea');
-        cy.get('textarea').eq(1).as('typeConfigurationTextarea');
-
-        // --------------------
-        cy.log('**--- Type in the Action Definition textarea**');
-        cy.get('@actionDefinitionTextarea').clearConsoleTextarea().type(
-          `type Mutation {
+      // --------------------
+      cy.log('**--- Type in the Action Definition textarea**');
+      cy.get('@actionDefinitionTextarea').clearConsoleTextarea().type(
+        `type Mutation {
               login (username: String!, password: String!): LoginResponse
             }`,
-          { force: true, delay: 0 }
-        );
+        { force: true, delay: 0 }
+      );
 
-        // --------------------
-        cy.log('**--- Type in the Type Configuration textarea**');
-        cy.get('@typeConfigurationTextarea').clearConsoleTextarea().type(
-          `type LoginResponse {
+      // --------------------
+      cy.log('**--- Type in the Type Configuration textarea**');
+      cy.get('@typeConfigurationTextarea').clearConsoleTextarea().type(
+        `type LoginResponse {
             accessToken: String!
           }
           `,
-          { force: true, delay: 0 }
-        );
+        { force: true, delay: 0 }
+      );
 
-        cy.log('**--- Type in the Webhook Handler field**');
-        cy.getBySel('action-create-handler-input')
-          .clearConsoleTextarea()
-          .type('https://hasura-actions-demo.glitch.me/login', {
-            delay: 0,
-            parseSpecialCharSequences: false,
-          });
-
-        // Please note: we should wait for the outgoing request but at the moment of writing, I'm not
-        // sure the Console locally works as the one in CI. The request was `test_webhook_transform`
-
-        // --------------------
-        // Pleas note that the custom timeout is not checked explicitly checked, but asserting on
-        // the payload (see the next cy.intercept) means asserting on it too
-        cy.log('**--- Type in the Custom Timeout field**');
-        cy.getBySel('action-timeout-seconds').clear().type('25');
-
-        cy.log('**--- Click the Create button**');
-        cy.getBySel('create-action-btn').click();
-
-        // Please note: we should wait for the outgoing request but at the moment of writing, I'm not
-        // sure the Console locally works as the one in CI. The request was `create_action`
-
-        // --------------------
-        cy.log('**--- Check if the success notification is visible**');
-        cy.get(
-          '.notification',
-          // The custom timeout aims to replace the lack of waiting for the outgoing request
-          { timeout: 10000 }
-        )
-          .should('be.visible')
-          .and('contain', 'Created action successfully');
-
-        // TODO: check if it exists in the database? Other tests do that
-      });
-    });
-
-    describe('When the user add a permission, everything should work', () => {
-      before(() => {
-        loginActionMustExist();
-        hakunaMatataPermissionMustNotExist();
-        mustBeOnTheActionsPage();
-      });
-
-      it('Test the feature', () => {
-        logMetadataRequests();
-
-        cy.log('**--- Go the the action page**');
-        cy.getBySel('actions-table-links').within(() => {
-          cy.getBySel('login').click();
+      cy.log('**--- Type in the Webhook Handler field**');
+      cy.getBySel('action-create-handler-input')
+        .clearConsoleTextarea()
+        .type('https://hasura-actions-demo.glitch.me/login', {
+          delay: 0,
+          parseSpecialCharSequences: false,
         });
 
-        // --------------------
-        cy.log('**--- Click the Permissions tab**');
-        cy.getBySel('actions-permissions').click();
+      // Please note: we should wait for the outgoing request but at the moment of writing, I'm not
+      // sure the Console locally works as the one in CI. The request was `test_webhook_transform`
 
-        // --------------------
-        cy.log('**--- Enter a new role**');
-        cy.getBySel('role-textbox').type('hakuna_matata');
-        cy.getBySel('hakuna_matata-Permission').click();
+      // --------------------
+      // Pleas note that the custom timeout is not checked explicitly checked, but asserting on
+      // the payload (see the next cy.intercept) means asserting on it too
+      cy.log('**--- Type in the Custom Timeout field**');
+      cy.getBySel('action-timeout-seconds').clear().type('25');
 
-        cy.getBySel('save-permissions-for-action').click();
+      cy.log('**--- Click the Create button**');
+      cy.getBySel('create-action-btn').click();
 
-        // Please note: we should wait for the outgoing request but at the moment of writing, I'm not
-        // sure the Console locally works as the one in CI. The request was `create_action_permission`
+      // Please note: we should wait for the outgoing request but at the moment of writing, I'm not
+      // sure the Console locally works as the one in CI. The request was `create_action`
 
-        // --------------------
-        cy.log('**--- Check if the success notification is visible**');
-        cy.get(
-          '.notification',
-          // The custom timeout aims to replace the lack of waiting for the outgoing request
-          { timeout: 10000 }
-        )
-          .should('be.visible')
-          .and('contain', 'Permission saved successfully');
+      // --------------------
+      cy.log('**--- Check if the success notification is visible**');
+      cy.get(
+        '.notification',
+        // The custom timeout aims to replace the lack of waiting for the outgoing request
+        { timeout: 10000 }
+      )
+        .should('be.visible')
+        .and('contain', 'Created action successfully');
 
-        // TODO: check if it exists in the database? Other tests do that
+      // TODO: check if it exists in the database? Other tests do that
+
+      cy.log('**------------------------------**');
+      cy.log('**------------------------------**');
+      cy.log('**------------------------------**');
+      cy.log('**--- Step 2: Permission add**');
+      cy.log('**------------------------------**');
+      cy.log('**------------------------------**');
+      cy.log('**------------------------------**');
+
+      cy.log('**--- Go the the action page**');
+      cy.getBySel('actions-table-links').within(() => {
+        cy.getBySel('login').click();
       });
-    });
 
-    describe('When the user deletes a Action, everything should work', () => {
-      before(() => {
-        loginActionMustExist();
-        mustBeOnTheActionsPage();
+      // --------------------
+      cy.log('**--- Click the Permissions tab**');
+      cy.getBySel('actions-permissions').click();
+
+      // --------------------
+      cy.log('**--- Enter a new role**');
+      cy.getBySel('role-textbox').type('hakuna_matata');
+      cy.getBySel('hakuna_matata-Permission').click();
+
+      cy.getBySel('save-permissions-for-action').click();
+
+      // Please note: we should wait for the outgoing request but at the moment of writing, I'm not
+      // sure the Console locally works as the one in CI. The request was `create_action_permission`
+
+      // --------------------
+      cy.log('**--- Check if the success notification is visible**');
+      cy.get(
+        '.notification',
+        // The custom timeout aims to replace the lack of waiting for the outgoing request
+        { timeout: 10000 }
+      )
+        .should('be.visible')
+        .and('contain', 'Permission saved successfully');
+
+      // TODO: check if it exists in the database? Other tests do that
+
+      cy.log('**------------------------------**');
+      cy.log('**------------------------------**');
+      cy.log('**------------------------------**');
+      cy.log('**--- Step 3: Action delete**');
+      cy.log('**------------------------------**');
+      cy.log('**------------------------------**');
+      cy.log('**------------------------------**');
+
+      cy.log('**--- Go the the action page**');
+      cy.getBySel('actions-table-links').within(() => {
+        cy.getBySel('login').click();
       });
 
-      it('Test the feature', () => {
-        logMetadataRequests();
+      // --------------------
+      cy.log('**--- Set the prompt value**');
+      cy.window().then(win => cy.stub(win, 'prompt').returns('login'));
 
-        cy.log('**--- Go the the action page**');
-        cy.getBySel('actions-table-links').within(() => {
-          cy.getBySel('login').click();
-        });
+      cy.log('**--- Click the Delete button**');
+      cy.getBySel('delete-action').click();
 
-        // --------------------
-        cy.log('**--- Set the prompt value**');
-        cy.window().then(win => cy.stub(win, 'prompt').returns('login'));
+      // --------------------
+      cy.log('**--- Check the prompt has been called**');
+      cy.window().its('prompt').should('be.called');
 
-        cy.log('**--- Click the Delete button**');
-        cy.getBySel('delete-action').click();
+      // Please note: we should wait for the outgoing request but at the moment of writing, I'm not
+      // sure the Console locally works as the one in CI. The request was `drop_action`
 
-        // --------------------
-        cy.log('**--- Check the prompt has been called**');
-        cy.window().its('prompt').should('be.called');
+      // --------------------
+      cy.log('**--- Check if the success notification is visible**');
+      cy.get(
+        '.notification',
+        // The custom timeout aims to replace the lack of waiting for the outgoing request
+        { timeout: 10000 }
+      )
+        .should('be.visible')
+        .and('contain', 'Action deleted successfully');
 
-        // Please note: we should wait for the outgoing request but at the moment of writing, I'm not
-        // sure the Console locally works as the one in CI. The request was `drop_action`
-
-        // --------------------
-        cy.log('**--- Check if the success notification is visible**');
-        cy.get(
-          '.notification',
-          // The custom timeout aims to replace the lack of waiting for the outgoing request
-          { timeout: 10000 }
-        )
-          .should('be.visible')
-          .and('contain', 'Action deleted successfully');
-
-        // TODO: check if it does not exist in the database? Other tests do that
-      });
+      // TODO: check if it does not exist in the database? Other tests do that
     });
   });
-}
-// TODO: move to https://github.com/cypress-io/cypress-skip-test
-if (testMode !== 'cli') {
-  runTests();
 }
--- console/cypress/integration/actions/mutation/utils/testState/loginActionMustNotExist.ts
+++ console/cypress/integration/actions/mutation/utils/testState/loginActionMustNotExist.ts
@@ -1,54 +1,21 @@
 import { readMetadata } from '../services/readMetadata';
 import { deleteLoginAction } from '../services/deleteLoginAction';
 
-interface Options {
-  reloadPage: boolean;
-}
-
-const defaultOptions: Options = {
-  reloadPage: false,
-};
-
 /**
  * Ensure the Action does not exist.
- *
- * ATTENTION: if you get the "setup function changed for session..." error, simply close the
- * Cypress-controlled browser and re-launch the test file.
  */
-export function loginActionMustNotExist(options = defaultOptions) {
-  cy.dataSession({
-    name: 'loginActionMustNotExist',
+export function loginActionMustNotExist() {
+  Cypress.log({ message: '**--- Action check: start**' });
 
-    // Without it, cy.dataSession run the setup function also the very first time, trying to
-    // delete an Action that does not exist.
-    init: () => true,
+  readMetadata().then(response => {
+    const actionExists = !!response.body.actions?.find(
+      // TODO: properly type it
+      action => action.name === 'login'
+    );
 
-    // Check if the Action exists
-    validate: () => {
-      Cypress.log({ message: '**--- Action check: start**' });
-
-      return readMetadata().then(response => {
-        const action = response.body.actions?.find(
-          // TODO: properly type it
-          action => action.name === 'login'
-        );
-
-        // Return true if the action does no exist
-        return !action;
-      });
-    },
-
-    preSetup: () =>
+    if (actionExists) {
       Cypress.log({ message: '**--- The Action must be deleted**' }),
-
-    // Delete the existing action
-    setup: () => {
-      deleteLoginAction();
-
-      if (options.reloadPage) {
-        // Ensure the UI read the latest data if it were previously loaded
-        cy.reload();
-      }
-    },
+        deleteLoginAction();
+    }
   });
 }
