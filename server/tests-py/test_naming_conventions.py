cb8ac0c2f760c80db1e95bff48015e7a643bd26f
Samir Talwar <samir.talwar@hasura.io>
2022-07-05 14:36:17 +0200

    server/pytest: Attempting to improve TestNamingConventions.
    I found a couple of flaws in `TestNamingConventions` and friends:
    
    1. We had two test cases with the same name, which means one of them
       would be overwritten. Renamed to avoid conflict.
    2. The `skipif` check for
       `TestNamingConventionWithoutExperimentalFeature` seemed broken. I
       have fixed it by making it line up with its `reason`, and extracted
       some logic out into a function to avoid duplication.
    
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/4958
    GitOrigin-RevId: f572d86c893135839dbaba70bf89984bc9d79331

--- server/tests-py/test_naming_conventions.py
+++ server/tests-py/test_naming_conventions.py
@@ -3,9 +3,13 @@ from validate import check_query_f
 import pytest
 import os
 
+
+def env_var_contains(name, contents):
+    value = os.getenv(name)
+    return value != None and contents in value
+
 @pytest.mark.skipif(
-    os.getenv('HASURA_GRAPHQL_EXPERIMENTAL_FEATURES') is None or
-    not 'naming_convention' in os.getenv('HASURA_GRAPHQL_EXPERIMENTAL_FEATURES'),
+    not env_var_contains('HASURA_GRAPHQL_EXPERIMENTAL_FEATURES', 'naming_convention'),
     reason="This test expects the (naming_convention) experimental feature turned on")
 class TestNamingConventions:
 
@@ -18,11 +22,11 @@ class TestNamingConventions:
 
     def test_field_name_precedence(self, hge_ctx):
         check_query_f(hge_ctx, self.dir() + '/field_name_precedence.yaml')
-    
+
     def test_enum_value_convention(self, hge_ctx):
         check_query_f(hge_ctx, self.dir() + '/enum_value_convention.yaml')
-    
-    def test_type_and_field_names(self, hge_ctx):
+
+    def test_type_and_field_names_with_prefix_and_suffix(self, hge_ctx):
         check_query_f(hge_ctx, self.dir() + '/type_and_field_names_with_prefix_and_suffix.yaml')
 
 @pytest.mark.parametrize("backend", ['mssql'])
@@ -30,31 +34,30 @@ class TestNamingConventionsFailure:
     @classmethod
     def dir(cls):
         return "queries/naming_conventions"
-    
+
     def test_other_than_pg_db_failure(self, hge_ctx):
         check_query_f(hge_ctx, self.dir() + '/mssql_naming_convention.yaml')
 
 @pytest.mark.skipif(
-    os.getenv('HASURA_GRAPHQL_DEFAULT_NAMING_CONVENTION') is None or
-    not 'graphql-default' in os.getenv('HASURA_GRAPHQL_DEFAULT_NAMING_CONVENTION'),
+    not env_var_contains('HASURA_GRAPHQL_DEFAULT_NAMING_CONVENTION', 'graphql-default'),
     reason="This test expects the HASURA_GRAPHQL_DEFAULT_NAMING_CONVENTION environment variable set to graphql-default")
 class TestDefaultNamingConvention:
 
     @classmethod
     def dir(cls):
         return "queries/naming_conventions"
-    
+
     def test_default_global_naming_convention(self, hge_ctx):
         check_query_f(hge_ctx, self.dir() + '/default_global_naming_convention.yaml')
 
-@pytest.mark.skipif( not (os.getenv('HASURA_GRAPHQL_EXPERIMENTAL_FEATURES') is None or
-    not 'graphql-default' in os.getenv('HASURA_GRAPHQL_EXPERIMENTAL_FEATURES')),
+@pytest.mark.skipif(
+    env_var_contains('HASURA_GRAPHQL_EXPERIMENTAL_FEATURES', 'naming_convention'),
     reason="This test expects the (naming_convention) experimental feature turned OFF")
 class TestNamingConventionWithoutExperimentalFeature:
 
     @classmethod
     def dir(cls):
         return "queries/naming_conventions"
-    
+
     def test_naming_convention_without_feature_turned_on(self, hge_ctx):
         check_query_f(hge_ctx, self.dir() + '/naming_convention_without_feature_turned_on.yaml')
