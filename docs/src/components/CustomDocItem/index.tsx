dbb9e7bab06e18cf8d373edb317250f11da1671c
Marion Schleifer <marion@hasura.io>
2022-07-20 18:34:24 +0200

    docs: epic restructure
    [Preview link](https://marion-docs-restructure.hasura-docs-mono.pages.dev/docs/latest/index/)
    
    This PR is a complete restructure of documentation including the following:
    - Merge [core](https://hasura.io/docs/latest/graphql/core/index/) and [cloud](https://hasura.io/docs/latest/graphql/cloud/index/) docs
    - Integrate [enterprise](https://docs.ee.hasura.io/) docs
    - Flip [database section](https://hasura.io/docs/latest/graphql/core/databases/index/)
    - Much more
    
    @robertjdominguez @seanparkross please add improvements and link to the sections that you've updated, so that it's easy to find for reviewers.
    
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/4882
    Co-authored-by: surendran82 <26085612+surendran82@users.noreply.github.com>
    Co-authored-by: Abby Sassel <3883855+sassela@users.noreply.github.com>
    Co-authored-by: Sean Park-Ross <94021366+seanparkross@users.noreply.github.com>
    Co-authored-by: Rob Dominguez <24390149+robertjdominguez@users.noreply.github.com>
    Co-authored-by: Rikin Kachhia <54616969+rikinsk@users.noreply.github.com>
    GitOrigin-RevId: 897191003a5e20dcc525b15e571725a34dc7d76e

--- docs/src/components/CustomDocItem/index.tsx
+++ docs/src/components/CustomDocItem/index.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import ActualDocItem from '@theme/DocItem';
-import HasuraConBanner from "@site/src/components/HasuraConBanner";
-import GraphQLWithHasuraBanner from "@site/src/components/GraphQLWithHasuraBanner";
-import PageHelpful from "@site/src/components/PageHelpful";
+import HasuraConBanner from '@site/src/components/HasuraConBanner';
+import GraphQLWithHasuraBanner from '@site/src/components/GraphQLWithHasuraBanner';
+import PageHelpful from '@site/src/components/PageHelpful';
 import CustomFooter from '@site/src/components/CustomFooter';
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss';
 
-const CustomDocItem = (props) => (
-  <div className={`custom_doc_item_wrapper ${styles["custom_doc_item_wrapper"]}`}>
-    <ActualDocItem {...props} />
-    <div className={styles['custom_doc_item_footer']}>
-      <PageHelpful />
-      <HasuraConBanner {...props} />
-      <GraphQLWithHasuraBanner />
-      <CustomFooter />
+const CustomDocItem = (props) => {
+  return (
+    <div
+      className={
+        props.location.pathname === `/docs/latest/index/`
+          ? `custom_doc_item_wrapper custom_doc_item_wrapper-x-wide`
+          : `custom_doc_item_wrapper ${styles['custom_doc_item_wrapper']}`
+      }
+    >
+      <ActualDocItem {...props} />
+      <div className={styles['custom_doc_item_footer']}>
+        <PageHelpful />
+        <HasuraConBanner {...props} />
+        <GraphQLWithHasuraBanner />
+        <CustomFooter />
+      </div>
     </div>
-  </div>
-)
+  );
+};
 
 export default CustomDocItem;
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -8,46 +8,45 @@ import VersionedLink from '@site/src/components/VersionedLink';
 import styles from './index.module.scss';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      <div className='container'>
+        <h1 className='hero__title'>{siteConfig.title}</h1>
+        <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.links}>
           <VersionedLink
-            className="button button--primary button--lg"
-            to="/graphql/core/index">
-            Hasura Core Docs
+            className='button button--primary button--lg'
+            to='/index'
+          >
+            Hasura Docs
           </VersionedLink>
-          <VersionedLink
-            className="button button--success button--lg"
-            to="/graphql/cloud/index">
-            Hasura Cloud Docs
-          </VersionedLink>
+          <div className={styles.links}>
+            <Link className='button button--secondary button--lg' to='/wiki/'>
+              Docs Wiki
+            </Link>
+          </div>
+          <div className={styles.links}>
+            <Link className='button button--success button--lg' to='/wiki/style/index/'>
+              Docs Style Guide
+            </Link>
+          </div>
         </div>
-        <div className={styles.links}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/wiki/">
-            Docs Wiki
-          </Link>
-        </div>
-        <img src={hasuras} alt="Hasuras Image" />
+        <img src={hasuras} alt='Hasuras Image' />
       </div>
     </header>
   );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={siteConfig.title}
-      description="Hasura gives you instant GraphQL APIs on your data sources. Point Hasura to your preferred internal and external data sources, setup relationships and security rules on your data models across sources and get a managed unified GraphQL API to build modern applications, instantly.">
+      description='Hasura gives you instant GraphQL APIs on your data sources. Point Hasura to your preferred internal and external data sources, setup relationships and security rules on your data models across sources and get a managed unified GraphQL API to build modern applications, instantly.'
+    >
       <HomepageHeader />
-      <main>
-      </main>
+      <main></main>
     </Layout>
   );
 }
