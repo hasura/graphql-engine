d2fdfbf48b9d806f9f04fdca63a8fc4a610f06dc
Tom Harding <tomjharding@live.co.uk>
2022-07-25 15:51:32 +0100

    Add the `Schema.TableRelationshipsSpec` tests
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/5133
    GitOrigin-RevId: 29b749ffc1e47f0d64cc6334bedb265b75fa7aa2

---
+++ server/tests-hspec/Test/Schema/TableRelationships/ArrayRelationshipsSpec.hs
@@ -0,0 +1,152 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Test.Schema.TableRelationships.ArrayRelationshipsSpec (spec) where
+
+import Data.Aeson (Value)
+import Harness.Backend.BigQuery qualified as BigQuery
+import Harness.Backend.Citus qualified as Citus
+import Harness.Backend.Mysql qualified as Mysql
+import Harness.Backend.Postgres qualified as Postgres
+import Harness.Backend.Sqlserver qualified as Sqlserver
+import Harness.GraphqlEngine (postGraphql)
+import Harness.Quoter.Graphql (graphql)
+import Harness.Quoter.Yaml (shouldReturnYaml, yaml)
+import Harness.Test.Context qualified as Context
+import Harness.Test.Schema (Table (..), table)
+import Harness.Test.Schema qualified as Schema
+import Harness.TestEnvironment (TestEnvironment)
+import Test.Hspec (SpecWith, describe, it)
+import Prelude
+
+spec :: SpecWith TestEnvironment
+spec = do
+  Context.run
+    [ Context.Context
+        { name = Context.Backend Context.MySQL,
+          mkLocalTestEnvironment = Context.noLocalTestEnvironment,
+          setup = Mysql.setup schema,
+          teardown = Mysql.teardown schema,
+          customOptions = Nothing
+        },
+      Context.Context
+        { name = Context.Backend Context.Postgres,
+          mkLocalTestEnvironment = Context.noLocalTestEnvironment,
+          setup = Postgres.setup schema,
+          teardown = Postgres.teardown schema,
+          customOptions = Nothing
+        },
+      Context.Context
+        { name = Context.Backend Context.Citus,
+          mkLocalTestEnvironment = Context.noLocalTestEnvironment,
+          setup = Citus.setup schema,
+          teardown = Citus.teardown schema,
+          customOptions = Nothing
+        },
+      Context.Context
+        { name = Context.Backend Context.SQLServer,
+          mkLocalTestEnvironment = Context.noLocalTestEnvironment,
+          setup = Sqlserver.setup schema,
+          teardown = Sqlserver.teardown schema,
+          customOptions = Nothing
+        },
+      Context.Context
+        { name = Context.Backend Context.BigQuery,
+          mkLocalTestEnvironment = Context.noLocalTestEnvironment,
+          setup = BigQuery.setup schema,
+          teardown = BigQuery.teardown schema,
+          customOptions =
+            Just $
+              Context.Options
+                { stringifyNumbers = True
+                }
+        }
+    ]
+    tests
+
+--------------------------------------------------------------------------------
+-- Schema
+
+schema :: [Schema.Table]
+schema =
+  [ (table "author")
+      { tableColumns =
+          [ Schema.column "id" Schema.TInt,
+            Schema.column "name" Schema.TStr
+          ],
+        tablePrimaryKey = ["id"],
+        tableData =
+          [ [Schema.VInt 1, Schema.VStr "Author 1"],
+            [Schema.VInt 2, Schema.VStr "Author 2"]
+          ]
+      },
+    (table "article")
+      { tableColumns =
+          [ Schema.column "id" Schema.TInt,
+            Schema.column "title" Schema.TStr,
+            Schema.column "author_id" Schema.TInt
+          ],
+        tablePrimaryKey = ["id"],
+        tableReferences =
+          [ Schema.Reference "author_id" "author" "id"
+          ],
+        tableData =
+          [ [ Schema.VInt 1,
+              Schema.VStr "Article 1",
+              Schema.VInt 1
+            ],
+            [ Schema.VInt 2,
+              Schema.VStr "Article 2",
+              Schema.VInt 1
+            ],
+            [ Schema.VInt 3,
+              Schema.VStr "Article 3",
+              Schema.VInt 2
+            ]
+          ]
+      }
+  ]
+
+--------------------------------------------------------------------------------
+-- Tests
+
+tests :: Context.Options -> SpecWith TestEnvironment
+tests opts = do
+  let shouldBe :: IO Value -> Value -> IO ()
+      shouldBe = shouldReturnYaml opts
+
+  describe "Array relationships" do
+    it "Select authors and their articles" \testEnvironment -> do
+      let expected :: Value
+          expected =
+            [yaml|
+              data:
+                hasura_author:
+                - id: 1
+                  articles_by_id_to_author_id:
+                  - id: 1
+                  - id: 2
+
+                - id: 2
+                  articles_by_id_to_author_id:
+                  - id: 3
+            |]
+
+          -- We have to provide explicit orderings because BigQuery doesn't
+          -- seem to return results in a deterministic order.
+          actual :: IO Value
+          actual =
+            postGraphql
+              testEnvironment
+              [graphql|
+                query {
+                  hasura_author(order_by: [{ id: asc }]) {
+                    id
+
+                    articles_by_id_to_author_id(order_by: [{ id: asc }]) {
+                      id
+                    }
+                  }
+                }
+              |]
+
+      actual `shouldBe` expected
