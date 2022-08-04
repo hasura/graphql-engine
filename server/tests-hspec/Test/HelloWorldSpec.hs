d8a4f2cf9b0135025bd70d6bf826085f250bbefb
Tom Harding <tomjharding@live.co.uk>
2022-08-04 15:33:05 +0100

    Update `HelloWorldSpec` to reflect the usual test structure
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/5325
    GitOrigin-RevId: 85b5aabcdf9fbca51d31eaa36e8a81dfac0a40d1

--- server/tests-hspec/Test/HelloWorldSpec.hs
+++ server/tests-hspec/Test/HelloWorldSpec.hs
@@ -1,29 +1,58 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 -- | A starting point feature test.
 module Test.HelloWorldSpec (spec) where
 
-import Data.List.NonEmpty qualified as NE
-import Harness.Test.Context qualified as Context
+import Data.Aeson (Value (Null))
+import Harness.Backend.Postgres qualified as Postgres
+import Harness.Quoter.Yaml (shouldReturnYaml, yaml)
+import Harness.Test.Fixture qualified as Fixture
+import Harness.Test.Schema qualified as Schema
 import Harness.TestEnvironment (TestEnvironment (..))
-import Test.Hspec (SpecWith, describe, it, shouldBe)
-
---------------------------------------------------------------------------------
--- Preamble
+import Hasura.Prelude
+import Test.Hspec (SpecWith, describe, it)
 
 spec :: SpecWith TestEnvironment
 spec =
-  Context.run
-    ( NE.fromList
-        [ Context.context (Context.Backend Context.Postgres)
-        ]
-    )
+  Fixture.run
+    [ (Fixture.fixture $ Fixture.Backend Fixture.Postgres)
+        { Fixture.setupTeardown = \(testEnvironment, _) ->
+            [ Postgres.setupTablesAction schema testEnvironment
+            ]
+        }
+    ]
     tests
+
+--------------------------------------------------------------------------------
+-- Schema
+
+schema :: [Schema.Table]
+schema =
+  [ (Schema.table "example")
+      { Schema.tableColumns =
+          [ Schema.column "id" Schema.TInt,
+            Schema.column "name" Schema.TStr
+          ],
+        Schema.tablePrimaryKey = ["id"],
+        Schema.tableData = []
+      }
+  ]
 
 --------------------------------------------------------------------------------
 -- Tests
 
-tests :: Context.Options -> SpecWith TestEnvironment
-tests _opts =
-  describe "HelloWorld" do
-    it "No-op" \te -> do
-      logger te "woop\n"
-      () `shouldBe` ()
+tests :: Fixture.Options -> SpecWith TestEnvironment
+tests opts = do
+  let shouldBe :: IO Value -> Value -> IO ()
+      shouldBe = shouldReturnYaml opts
+
+  describe "Example test" do
+    it "Works as expected" \testEnvironment -> do
+      let expected :: Value
+          expected = [yaml| null |]
+
+          actual :: IO Value
+          actual = pure Null
+
+      logger testEnvironment "A log message\n"
+      actual `shouldBe` expected
