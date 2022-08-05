be5b19128ebb1a27b6462e5f15efc8d15fe2271f
Tom Harding <tomjharding@live.co.uk>
2022-08-05 13:21:52 +0100

    Migrate the `GatheringUniqueConstraintsSpec` test to the new structure
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/5350
    GitOrigin-RevId: ae622a859af4c308daf1aacc2825657ec7f0f8ab

---
+++ server/tests-hspec/Test/Postgres/UniqueConstraintsSpec.hs
@@ -0,0 +1,53 @@
+-- |
+-- Tests to determine whether we can create tables with unique constraints.
+--
+-- https://hasura.io/docs/latest/guides/postgres/constraints/#unique-constraints
+module Test.Postgres.UniqueConstraintsSpec (spec) where
+
+import Harness.Backend.Citus qualified as Citus
+import Harness.Backend.Postgres qualified as Postgres
+import Harness.Test.Fixture qualified as Fixture
+import Harness.Test.Schema (Table (..), table)
+import Harness.Test.Schema qualified as Schema
+import Harness.TestEnvironment (TestEnvironment)
+import Hasura.Prelude
+import Test.Hspec (SpecWith, describe, it)
+
+spec :: SpecWith TestEnvironment
+spec = do
+  Fixture.run
+    [ (Fixture.fixture $ Fixture.Backend Fixture.Postgres)
+        { Fixture.setupTeardown = \(testEnv, _) ->
+            [ Postgres.setupTablesAction schema testEnv
+            ]
+        },
+      (Fixture.fixture $ Fixture.Backend Fixture.Citus)
+        { Fixture.setupTeardown = \(testEnv, _) ->
+            [ Citus.setupTablesAction schema testEnv
+            ]
+        }
+    ]
+    tests
+
+--------------------------------------------------------------------------------
+-- Schema
+
+schema :: [Table]
+schema =
+  [ (table "test")
+      { tableColumns =
+          [ Schema.column "id" Schema.TInt,
+            Schema.column "username" Schema.TStr
+          ],
+        tableUniqueConstraints =
+          [ Schema.UniqueConstraintExpression "lower(username)"
+          ]
+      }
+  ]
+
+--------------------------------------------------------------------------------
+-- Tests
+
+tests :: Fixture.Options -> SpecWith TestEnvironment
+tests _ = describe "Unique constraints" do
+  it "Creates a schema with unique constraints" \_ -> pure @IO ()
