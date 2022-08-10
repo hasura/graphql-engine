4a792a20543174db36a19a07a622d0e33f7e5d49
Matt Hardman <mattshardman@gmail.com>
2022-08-10 09:23:24 +0100

    refactor (console/gdc):  clean up the connect DB components + hooks
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/5390
    GitOrigin-RevId: 7f1f13ca4453c7c436880494576d916403ed974f

--- console/src/features/ConnectDB/hooks/useLoadSchemas.tsx
+++ console/src/features/ConnectDB/hooks/useLoadSchemas.tsx
@@ -1,46 +1,55 @@
 import axios from 'axios';
-import { useQuery } from 'react-query';
+import { useQueries } from 'react-query';
 
 import { DataSource } from '@/features/DataSource';
+import { useDefaultValues } from './useDefaultValues';
 
 const fetch = axios.create();
 const dataSourceFetch = DataSource(fetch);
 
-const useAvailableDrivers = () => {
-  return useQuery({
-    queryKey: ['getDrivers'],
-    queryFn: async () => {
-      return dataSourceFetch.driver.getSupportedDrivers();
-    },
-  });
+const possibleFormSchemasQuery = {
+  queryKey: ['validation-schemas'],
+  queryFn: async () => dataSourceFetch.connectDB.getFormSchema(),
 };
 
-const usePossibleFormSchemas = () => {
-  return useQuery({
-    queryKey: ['validation-schemas'],
-    queryFn: async () => {
-      return dataSourceFetch.connectDB.getFormSchema();
-    },
-  });
+const availableDriversQuery = {
+  queryKey: ['getDrivers'],
+  queryFn: async () => dataSourceFetch.driver.getSupportedDrivers(),
 };
 
-export const useLoadSchema = () => {
-  const {
-    data: schemas,
-    isLoading: schemasLoading,
-    isError: isSchemaError,
-    error: schemaError,
-  } = usePossibleFormSchemas();
-  const {
-    data: drivers,
-    isLoading: driversLoading,
-    isError: isDriversError,
-    error: driversError,
-  } = useAvailableDrivers();
+interface Args {
+  name: string;
+  driver: string;
+}
 
-  const isLoading = schemasLoading || driversLoading;
-  const isError = isSchemaError || isDriversError;
-  const error = schemaError || driversError;
+export const useLoadSchema = ({ name, driver }: Args) => {
+  const results = useQueries([possibleFormSchemasQuery, availableDriversQuery]);
 
-  return { data: { schemas, drivers }, isLoading, isError, error };
+  // get default values if existing connection info is passed in
+  // it would be nice to do this as part of the useQueries array above
+  // but currently not possible because of the way we fetch metadata
+  const {
+    data: defaultValues,
+    isLoading: defaultValuesIsLoading,
+    isError: defaultValuesIsError,
+    error: defaultValuesError,
+  } = useDefaultValues({ name, driver });
+
+  const isLoading =
+    results.some(result => result.isLoading) || defaultValuesIsLoading;
+  const isError =
+    results.some(result => result.isError) || defaultValuesIsError;
+
+  const [schemasResult, driversResult] = results;
+
+  const schemas = schemasResult.data;
+  const drivers = driversResult.data;
+
+  const error = results.some(result => result.error) || defaultValuesError;
+  return {
+    data: { schemas, drivers, defaultValues },
+    isLoading,
+    isError,
+    error,
+  };
 };
