8888e06bbbbaf30ee834da1ab0198658e2acf2f6
Auke Booij <aukebb@hasura.io>
2022-08-11 11:09:53 +0200

    Spell out the TH in `Hasura.SQL.{Tag,AnyBackend}`
    The module `Hasura.SQL.AnyBackend` was introduced (in #751) to centralize the logic for case-switching behavior that depends on the particular flavor of relational DB backend (Postgres vs MSSQL vs BigQuery vs MySQL vs DataConnectors). This allows us to write a bunch of code in a backend-agnostic way, even if runtime behavior does depend on the chosen backend. At the same time, it allows us to write backend-specific code without having to care (too much) about the existence of other backends.
    
    In #851 this module was rewritten to use Template Haskell.
    
    I've heard that one of the reasons for the use of TH was that this would make it easier to keep backends out of the compilation product entirely. This would allow customers, especially on OSS, to benefit from simpler software licensing.
    
    However:
    1. This conditional compilation never materialized.
    2. It's not clear whether writing this particular module based on TH would be sufficient for conditional compilation. And in any case, it can be done using CPP pragmas as well.
    3. The TH code is extraordinarily complex. Since its introduction, it has been documented extraordinarily well, but it's still very difficult to maintain and/or refactor, due to its non-idiomatic nature.
    4. Hasura's company objectives are now Cloud-oriented, so that software licensing issues work differently, and in particular, do not depend on what's part of the compilation product.
    
    So this PR reverts on #851 by spelling out the code generated by TH. This is a net-negative diff size. IOW we used to generate less code than the size of the code doing the generating. This makes the code readable and maintainable.
    
    The generated code has been modified in one way, which I'll now describe.
    
    In the scenario that support for a new backend is introduced, a constructor is added to the `BackendType` type. This would then cause `liftTag` to be partial, thus raising a compiler warning. Resolving this requires adding corresponding constructors to the `BackendTag` and `AnyBackend` types. This would then require amending **almost** all other methods.
    
    The exceptions are `composeAnyBackend` and `unpackAnyBackend`. These methods test whether two values are compatible, i.e. belong to the same backend. Both have a default case that in one way or another ignores the input values. Using TH here ensures that all values that belong together are caught. But after spelling out the TH, the presence of the default case means that no compiler warning is thrown for a missing match of matching values. So in the default case, we now do an explicit check for equality. If there _is_ an equality, that means that there is a missing `case`. So this is reported as an `error` (which is very crude, but it should be).
    
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/5333
    GitOrigin-RevId: 5aaf0a93394bd740aa7371526d3175c8142b3541

--- server/src-lib/Hasura/SQL/Tag.hs
+++ server/src-lib/Hasura/SQL/Tag.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 module Hasura.SQL.Tag
   ( BackendTag (..),
     HasTag (..),
@@ -7,74 +5,45 @@ module Hasura.SQL.Tag
   )
 where
 
-import Hasura.Prelude
 import Hasura.SQL.Backend
-import Hasura.SQL.TH
-import Language.Haskell.TH hiding (reify)
 
 -- | A singleton-like GADT that associates a tag to each backend.
--- It is generated with Template Haskell for each 'Backend'. Its
--- declaration results in the following type:
---
---   data BackendTag (b :: BackendType) where
---     PostgresVanillaTag :: BackendTag ('Postgres 'Vanilla)
---     PostgresCitusTag   :: BackendTag ('Postgres 'Citus)
---     MSSQLTag           :: BackendTag 'MSSQL
---     ...
-$( let name = mkName "BackendTag"
-    in backendData
-         -- the name of the type
-         name
-         -- the type variable
-         [KindedTV (mkName "b") $ ConT ''BackendType]
-         -- the constructor for each backend
-         ( \b ->
-             pure $
-               GadtC
-                 -- the name of the constructor (FooTag)
-                 [getBackendTagName b]
-                 -- no type argument
-                 []
-                 -- the resulting type (BackendTag 'Foo)
-                 (AppT (ConT name) (getBackendTypeValue b))
-         )
-         -- deriving clauses
-         []
- )
+data BackendTag (b :: BackendType) where
+  PostgresVanillaTag :: BackendTag ('Postgres 'Vanilla)
+  PostgresCitusTag :: BackendTag ('Postgres 'Citus)
+  MSSQLTag :: BackendTag 'MSSQL
+  BigQueryTag :: BackendTag 'BigQuery
+  MySQLTag :: BackendTag 'MySQL
+  DataConnectorTag :: BackendTag 'DataConnector
 
 -- | This class describes how to get a tag for a given type.
 -- We use it in AnyBackend: `case backendTag @b of`...
 class HasTag (b :: BackendType) where
   backendTag :: BackendTag b
 
--- | This generates the instance of HasTag for every backend.
-$( concat <$> forEachBackend \b -> do
-     -- the name of the tag: FooTag
-     let tagName = pure $ ConE $ getBackendTagName b
-     -- the promoted version of b: 'Foo
-     let promotedName = pure $ getBackendTypeValue b
-     -- the instance:
-     --  instance HasTag 'Foo          where backendTag = FooTag
-     [d|instance HasTag $promotedName where backendTag = $tagName|]
- )
+instance HasTag ('Postgres 'Vanilla) where
+  backendTag = PostgresVanillaTag
 
--- | How to convert back from a tag to a runtime value. This function
--- is generated with Template Haskell for each 'Backend'. The case
--- switch looks like this:
---
---   PostgresVanillaTag -> Postgres Vanilla
---   PostgresCitusTag   -> Postgres Citus
---   MSSQLTag           -> MSSQL
---   ...
+instance HasTag ('Postgres 'Citus) where
+  backendTag = PostgresCitusTag
+
+instance HasTag 'MSSQL where
+  backendTag = MSSQLTag
+
+instance HasTag 'BigQuery where
+  backendTag = BigQueryTag
+
+instance HasTag 'MySQL where
+  backendTag = MySQLTag
+
+instance HasTag 'DataConnector where
+  backendTag = DataConnectorTag
+
+-- | How to convert back from a tag to a runtime value.
 reify :: BackendTag b -> BackendType
-reify t =
-  $( backendCase
-       -- the expression on which we do the case switch
-       [|t|]
-       -- the pattern for a given backend: just its tag, no argument
-       (\b -> pure $ ConP (getBackendTagName b) [])
-       -- the body for a given backend: the backend constructor itself
-       (\b -> pure $ getBackendValue b)
-       -- no default case: every constructor should be handled
-       Nothing
-   )
+reify PostgresVanillaTag = Postgres Vanilla
+reify PostgresCitusTag = Postgres Citus
+reify MSSQLTag = MSSQL
+reify BigQueryTag = BigQuery
+reify MySQLTag = MySQL
+reify DataConnectorTag = DataConnector
