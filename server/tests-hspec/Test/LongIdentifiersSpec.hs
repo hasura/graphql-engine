21d12853907bf8f9634f84d5dee411533df1b403
Brandon Martin <brandon@codedmart.com>
2022-08-15 09:41:28 -0600

    Merge pull request #5477 from hasura/gil/fix_ulls_first
    server/postgres: Fix ulls first parsers + long identifiers + test
    
    GitOrigin-RevId: 74e067b13ca0b4bc47147e715b87a187cd1aeb29

--- server/tests-hspec/Test/LongIdentifiersSpec.hs
+++ server/tests-hspec/Test/LongIdentifiersSpec.hs
@@ -66,16 +66,26 @@ spec = do
         ]
     )
     tests
+  Fixture.run
+    ( NE.fromList
+        [ (Fixture.fixture $ Fixture.Backend Fixture.Postgres)
+            { Fixture.setupTeardown = \(testEnv, _) ->
+                [ Postgres.setupTablesAction schema testEnv
+                ]
+            }
+        ]
+    )
+    testInsert
 
 --------------------------------------------------------------------------------
 -- Schema
 
 schema :: [Schema.Table]
-schema = [regular, longtable]
+schema = [regular, longtable, longtable2, multitable]
 
 regular :: Schema.Table
 regular =
-  (table "regular")
+  (table "regular_table_with_a_long_name_to_test_rename_identifiers")
     { tableColumns =
         [ Schema.column "id" Schema.TInt,
           Schema.column "name" Schema.TStr
@@ -100,13 +110,55 @@ longtable =
       tableReferences =
         [ Schema.Reference
             { referenceLocalColumn = "regular_id",
-              referenceTargetTable = "regular",
+              referenceTargetTable = "regular_table_with_a_long_name_to_test_rename_identifiers",
               referenceTargetColumn = "id"
             }
         ],
       tableData =
         [ [Schema.VInt 1, Schema.VInt 1, Schema.VInt 1, Schema.VInt 1],
           [Schema.VInt 2, Schema.VInt 2, Schema.VInt 2, Schema.VInt 2]
+        ]
+    }
+
+longtable2 :: Schema.Table
+longtable2 =
+  (table "i_need_a_table_with_a_long_name_to_test_rename_identifiers2") -- 59 characters
+    { tableColumns =
+        [ Schema.column "id" Schema.TInt,
+          Schema.column "i_need_a_column_with_a_long_name_to_test_rename_identifiers" Schema.TInt, -- 59 characters
+          Schema.column "i_need_a_column_with_a_long_name_but_is_different" Schema.TInt -- 49 characters
+        ],
+      tablePrimaryKey = ["id"],
+      tableData =
+        [ [Schema.VInt 1, Schema.VInt 1, Schema.VInt 1],
+          [Schema.VInt 2, Schema.VInt 2, Schema.VInt 2]
+        ]
+    }
+
+multitable :: Schema.Table
+multitable =
+  (table "table_with_multiple_relationships_and_long_name_2_test_rewrite") -- 62 characters
+    { tableColumns =
+        [ Schema.column "id" Schema.TInt,
+          Schema.column "reference_for_longtable_id" Schema.TInt,
+          Schema.column "reference_for_longtable2_id" Schema.TInt
+        ],
+      tablePrimaryKey = ["id"],
+      tableReferences =
+        [ Schema.Reference
+            { referenceLocalColumn = "reference_for_longtable_id",
+              referenceTargetTable = "i_need_a_table_with_a_long_name_to_test_rename_identifiers",
+              referenceTargetColumn = "id"
+            },
+          Schema.Reference
+            { referenceLocalColumn = "reference_for_longtable2_id",
+              referenceTargetTable = "i_need_a_table_with_a_long_name_to_test_rename_identifiers2",
+              referenceTargetColumn = "id"
+            }
+        ],
+      tableData =
+        [ [Schema.VInt 1, Schema.VInt 1, Schema.VInt 1],
+          [Schema.VInt 2, Schema.VInt 2, Schema.VInt 2]
         ]
     }
 
@@ -174,7 +226,7 @@ data:
           testEnvironment
           [interpolateYaml|
 query {
-  #{schemaName}_regular(order_by:[{id:asc}]) {
+  #{schemaName}_regular_table_with_a_long_name_to_test_rename_identifiers(order_by:[{id:asc}]) {
     id
     i_need_a_table_with_a_long_name_to_test_rename_identifierss_by_id_to_regular_id(order_by:[{i_need_a_column_with_a_long_name_to_test_rename_identifiers:asc, i_need_a_column_with_a_long_name_but_is_different:asc}]) {
       i_need_a_column_with_a_long_name_to_test_rename_identifiers
@@ -186,7 +238,7 @@ query {
       )
       [interpolateYaml|
 data:
-  #{schemaName}_regular:
+  #{schemaName}_regular_table_with_a_long_name_to_test_rename_identifiers:
   - id: 1
     i_need_a_table_with_a_long_name_to_test_rename_identifierss_by_id_to_regular_id:
       - i_need_a_column_with_a_long_name_to_test_rename_identifiers: 1
@@ -196,3 +248,138 @@ data:
       - i_need_a_column_with_a_long_name_to_test_rename_identifiers: 2
         i_need_a_column_with_a_long_name_but_is_different: 2
 |]
+
+testInsert :: Fixture.Options -> SpecWith TestEnvironment
+testInsert opts = do
+  it "insert to regular table" $ \testEnvironment -> do
+    let schemaName = Schema.getSchemaName testEnvironment
+
+    shouldReturnYaml
+      opts
+      ( GraphqlEngine.postGraphqlYaml
+          testEnvironment
+          [interpolateYaml|
+          query: |
+            mutation MyMutation(
+                $x: Int
+              ) {
+              insert_#{schemaName}_regular_table_with_a_long_name_to_test_rename_identifiers
+                ( objects:
+                  { name: "bbb",
+                    id: $x
+                  }
+                ) {
+                affected_rows
+                returning {
+                  id
+                  name
+                }
+              }
+            }
+          variables:
+            x: 3
+          |]
+      )
+      [interpolateYaml|
+      data:
+        insert_#{schemaName}_regular_table_with_a_long_name_to_test_rename_identifiers:
+          affected_rows: 1
+          returning:
+          - id: 3
+            name: bbb
+      |]
+
+  it "nested insert to long table name" $ \testEnvironment -> do
+    let schemaName = Schema.getSchemaName testEnvironment
+
+    shouldReturnYaml
+      opts
+      ( GraphqlEngine.postGraphqlYaml
+          testEnvironment
+          [interpolateYaml|
+          query: |
+            mutation MyMutation(
+                $x: Int
+              ) {
+              insert_#{schemaName}_regular_table_with_a_long_name_to_test_rename_identifiers
+                ( objects:
+                  { name: "bbb",
+                    id: 4,
+                    i_need_a_table_with_a_long_name_to_test_rename_identifierss_by_id_to_regular_id:
+                      { data:
+                        { i_need_a_column_with_a_long_name_but_is_different: 4,
+                          i_need_a_column_with_a_long_name_to_test_rename_identifiers: $x,
+                          id: 4
+                        }
+                      }
+                  }
+                ) {
+                affected_rows
+                returning {
+                  id
+                  name
+                }
+              }
+            }
+          variables:
+            x: 4
+          |]
+      )
+      [interpolateYaml|
+      data:
+        insert_#{schemaName}_regular_table_with_a_long_name_to_test_rename_identifiers:
+          affected_rows: 2
+          returning:
+          - id: 4
+            name: bbb
+      |]
+
+  it "nested insert to table with multiple relationships" $ \testEnvironment -> do
+    let schemaName = Schema.getSchemaName testEnvironment
+
+    shouldReturnYaml
+      opts
+      ( GraphqlEngine.postGraphqlYaml
+          testEnvironment
+          [interpolateYaml|
+          query: |
+            mutation MyMutation(
+                $x: Int
+              ) {
+              insert_#{schemaName}_table_with_multiple_relationships_and_long_name_2_test_rewrite
+                ( objects:
+                  { id: 5,
+                    i_need_a_table_with_a_long_name_to_test_rename_identifiers_by_reference_for_longtable_id_to_id:
+                      { data:
+                        { i_need_a_column_with_a_long_name_but_is_different: 5,
+                          i_need_a_column_with_a_long_name_to_test_rename_identifiers: $x,
+                          id: 5,
+                          regular_id: 1,
+                        }
+                      },
+                    i_need_a_table_with_a_long_name_to_test_rename_identifiers2_by_reference_for_longtable2_id_to_id:
+                      { data:
+                        { i_need_a_column_with_a_long_name_but_is_different: 5,
+                          i_need_a_column_with_a_long_name_to_test_rename_identifiers: $x,
+                          id: 5
+                        }
+                      }
+                  }
+                ) {
+                affected_rows
+                returning {
+                  id
+                }
+              }
+            }
+          variables:
+            x: 5
+          |]
+      )
+      [interpolateYaml|
+      data:
+        insert_#{schemaName}_table_with_multiple_relationships_and_long_name_2_test_rewrite:
+          affected_rows: 3
+          returning:
+          - id: 5
+      |]
