9b091ef464a82e5887b41f1bdf19226420d5f0d4
Matt Hardman <mattshardman@gmail.com>
2022-08-16 06:46:18 +0100

    console: create suggested relationship form
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/4920
    Co-authored-by: mattwhaleblue <97981123+mattwhaleblue@users.noreply.github.com>
    Co-authored-by: Vijay Prasanna <11921040+vijayprasanna13@users.noreply.github.com>
    GitOrigin-RevId: 5afb347bd5f3bdd2f14e47e279ee96b9bd928d00

--- console/src/features/DataRelationships/components/SuggestedRelationships/SuggestedRelationships.stories.tsx
+++ console/src/features/DataRelationships/components/SuggestedRelationships/SuggestedRelationships.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 import { ReactQueryDecorator } from '@/storybook/decorators/react-query';
+import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 import { DataTarget } from '@/features/Datasources';
 import { handlers } from '../../hooks/mocks/handlers.mock';
 
@@ -29,4 +31,24 @@ export const Primary: Story<SuggestedRelationshipProps> = args => (
 );
 Primary.args = {
   target,
+};
+
+export const WithInteraction: Story<SuggestedRelationshipProps> = args => (
+  <SuggestedRelationships {...args} />
+);
+WithInteraction.args = Primary.args;
+WithInteraction.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  const button = await canvas.findByRole('button', { name: 'Add' });
+  userEvent.click(button);
+
+  const label = await canvas.findByLabelText('Relationship Name');
+  userEvent.type(label, 's');
+  expect(label).toBeInTheDocument();
+
+  const submitButton = await canvas.findByRole('button', {
+    name: 'Add Relationship',
+  });
+  userEvent.click(submitButton);
 };
