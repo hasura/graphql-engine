150fbaea7cf5aff7f51a4da3086e8fcce4d4c50a
Abhijeet Khangarot <abhijeet40308@gmail.com>
2022-08-24 12:54:07 +0530

    pro-console: integrate onboarding wizard
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/5559
    Co-authored-by: Rishichandra Wawhal <27274869+wawhal@users.noreply.github.com>
    GitOrigin-RevId: 35da0aa3c22f030f78d90bdd0ef6a7526c566016

--- console/src/features/OnboardingWizard/Root.tsx
+++ console/src/features/OnboardingWizard/Root.tsx
@@ -1,19 +1,35 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 import { TopHeaderBar, ConnectDBScreen } from './components';
+import { useWizardState } from './hooks';
+import { GrowthExperimentsClient } from '../GrowthExperiments';
 
-export function Root() {
+type Props = {
+  growthExperimentsClient: GrowthExperimentsClient;
+};
+
+/**
+ * Parent container for the onboarding wizard. Takes care of assembling and rendering all steps.
+ */
+export function Root(props: Props) {
+  const { growthExperimentsClient } = props;
+
   // dialog cannot be reopened once closed
-  const [isWizardOpen, closeWizard] = useReducer(() => false, true);
+  const { isWizardOpen, skipOnboarding, completeOnboarding, setIsWizardOpen } =
+    useWizardState(growthExperimentsClient);
 
-  // this dialog is being used to create a layover component over the whole app using react portal, and other handy functionalities radix dialog provides, which otherwise we'll have to implement manually
-  // note that we have a common radix dialog, but that component has very specific styling, doesn't include react portal, and it did not make sense to extend that to fit this particular one-off use case
+  // Radix dialog is being used for creating a layover component over the whole app using react portal,
+  // and for other handy functionalities radix-dialog provides, which otherwise we'll have to implement manually.
+  // Not extending the common dialog, as it does not make sense to update common component to fit this one-off use case.
   return (
-    <Dialog.Root open={isWizardOpen} onOpenChange={closeWizard}>
+    <Dialog.Root open={isWizardOpen} onOpenChange={setIsWizardOpen}>
       <Dialog.Portal>
         <Dialog.Content className="fixed top-0 w-full h-full focus:outline-none bg-gray-50 overflow-hidden z-[101]">
           <TopHeaderBar />
-          <ConnectDBScreen closeWizard={closeWizard} />
+          <ConnectDBScreen
+            skipOnboarding={skipOnboarding}
+            completeOnboarding={completeOnboarding}
+          />
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
