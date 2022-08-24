7c0e3784176270e780b2f23236283eb3c9eb8539
Rishichandra Wawhal <rishi@hasura.io>
2022-08-24 22:19:27 +0530

    console: add experiment as user property on heap
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/5601
    Co-authored-by: Abhijeet Khangarot <26903230+abhi40308@users.noreply.github.com>
    GitOrigin-RevId: 6b7037750cab35e40c7b843520da4f49680043e6

--- console/src/metadata/actions.ts
+++ console/src/metadata/actions.ts
@@ -314,9 +314,10 @@ export const addDataSource =
               } successfully!`,
               level: 'success',
               autoDismiss: 0,
-              action: {
+              alternateActionButtonProps: {
                 label: 'View Database',
-                callback: onButtonClick,
+                onClick: onButtonClick,
+                trackId: 'data-tab-view-database-notification-button-add-db',
               },
             },
             'success'
@@ -414,9 +415,11 @@ export const renameDataSource =
                 title: `Data source updated successfully!`,
                 level: 'success',
                 autoDismiss: 0,
-                action: {
+                alternateActionButtonProps: {
                   label: 'View Database',
-                  callback: onButtonClick,
+                  onClick: onButtonClick,
+                  trackId:
+                    'data-tab-view-database-notification-button-rename-db',
                 },
               },
               'success'
