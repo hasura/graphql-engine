b53cc2e81c0786f8ba14d5c64d5d339b2fb12d33
Nicolas Inchauspe <nicoinch@users.noreply.github.com>
2022-08-30 10:03:14 +0200

    console: add storybook and tailwind to console-legacy-oss
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/5590
    GitOrigin-RevId: 911fa2b8f59383fc10a08afac3138aacfe0927d3

---
+++ frontend/libs/console/legacy-oss/postcss.config.js
@@ -0,0 +1,15 @@
+const { join } = require('path');
+
+// Note: If you use library-specific PostCSS/Tailwind configuration then you should remove the `postcssConfig` build
+// option from your application's configuration (i.e. project.json).
+//
+// See: https://nx.dev/guides/using-tailwind-css-in-react#step-4:-applying-configuration-to-libraries
+
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      config: join(__dirname, 'tailwind.config.js'),
+    },
+    autoprefixer: {},
+  },
+};
--- frontend/libs/console/legacy-oss/src/lib/features/PermissionsForm/mocks/handlers.mock.ts
+++ frontend/libs/console/legacy-oss/src/lib/features/PermissionsForm/mocks/handlers.mock.ts
@@ -1,12 +1,10 @@
 import { rest } from 'msw';
 
-const baseUrl = 'http://localhost:8080';
-
-export const handlers = (url = baseUrl) => [
-  rest.post(`${url}/v2/query`, (req, res, ctx) => {
+export const handlers = () => [
+  rest.post(`/v2/query`, (req, res, ctx) => {
     const body = req.body as Record<string, any>;
 
-    const isUseSchemaList = body?.args?.sql?.includes('SELECT schema_name');
+    const isUseSchemaList = body?.['args']?.sql?.includes('SELECT schema_name');
 
     if (isUseSchemaList) {
       return res(
@@ -30,10 +28,10 @@ export const handlers = (url = baseUrl) => [
     );
   }),
 
-  rest.post(`${url}/v1/metadata`, (req, res, ctx) => {
+  rest.post(`/v1/metadata`, (req, res, ctx) => {
     const body = req.body as Record<string, any>;
 
-    if (body.type === 'export_metadata') {
+    if (body['type'] === 'export_metadata') {
       return res(
         ctx.json({
           resource_version: 30,
