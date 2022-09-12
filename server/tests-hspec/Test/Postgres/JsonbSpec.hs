00b1acaf3837a228ea3c060b254d78a2f0c72f01
Daniel Harvey <danieljamesharvey@gmail.com>
2022-09-12 15:16:03 +0100

    tests: JSON and JSONB hspec tests
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/5721
    GitOrigin-RevId: dde515a364566aa8d517923281ea670a8decbdd9

---
+++ server/tests-hspec/Test/Postgres/JsonbSpec.hs
@@ -0,0 +1,136 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+-- |
+-- Tests that we can decode JSONB values correctly
+--
+-- https://hasura.io/docs/latest/schema/postgres/default-values/postgres-defaults/
+module Test.Postgres.JsonbSpec (spec) where
+
+import Data.Aeson (Value)
+import Data.List.NonEmpty qualified as NE
+import Harness.Backend.Citus qualified as Citus
+import Harness.Backend.Cockroach qualified as Cockroach
+import Harness.Backend.Postgres qualified as Postgres
+import Harness.GraphqlEngine (postGraphql)
+import Harness.Quoter.Graphql (graphql)
+import Harness.Quoter.Yaml (yaml)
+import Harness.Test.Fixture qualified as Fixture
+import Harness.Test.Schema qualified as Schema
+import Harness.TestEnvironment (TestEnvironment)
+import Harness.Yaml (shouldReturnYaml)
+import Hasura.Prelude
+import Test.Hspec (SpecWith, describe, it)
+
+spec :: SpecWith TestEnvironment
+spec =
+  Fixture.run
+    ( NE.fromList
+        [ (Fixture.fixture $ Fixture.Backend Fixture.Postgres)
+            { Fixture.setupTeardown = \(testEnv, _) ->
+                [ Postgres.setupTablesAction schema testEnv
+                ]
+            },
+          (Fixture.fixture $ Fixture.Backend Fixture.Citus)
+            { Fixture.setupTeardown = \(testEnv, _) ->
+                [ Citus.setupTablesAction schema testEnv
+                ]
+            },
+          (Fixture.fixture $ Fixture.Backend Fixture.Cockroach)
+            { Fixture.setupTeardown = \(testEnv, _) ->
+                [ Cockroach.setupTablesAction schema testEnv
+                ],
+              Fixture.customOptions =
+                Just $
+                  Fixture.defaultOptions
+                    { Fixture.stringifyNumbers = True,
+                      Fixture.skipTests = Just "Cockroach disabled pending prepared args fix https://github.com/cockroachdb/cockroach/issues/86375"
+                    }
+            }
+        ]
+    )
+    tests
+
+--------------------------------------------------------------------------------
+-- Schema
+
+jsonType :: Schema.ScalarType
+jsonType =
+  Schema.TCustomType $
+    Schema.defaultBackendScalarType
+      { Schema.bstPostgres = Just "JSON",
+        Schema.bstCitus = Just "JSON",
+        Schema.bstCockroach = Just "JSON"
+      }
+
+jsonbType :: Schema.ScalarType
+jsonbType =
+  Schema.TCustomType $
+    Schema.defaultBackendScalarType
+      { Schema.bstPostgres = Just "JSONB",
+        Schema.bstCitus = Just "JSONB",
+        Schema.bstCockroach = Just "JSONB"
+      }
+
+mkJsonValue :: Text -> Schema.ScalarValue
+mkJsonValue json =
+  Schema.VCustomValue $
+    Schema.defaultBackendScalarValue
+      { Schema.bsvPostgres = Just (Schema.Quoted json),
+        Schema.bsvCitus = Just (Schema.Quoted json),
+        Schema.bsvCockroach = Just (Schema.Quoted json)
+      }
+
+schema :: [Schema.Table]
+schema =
+  [ (Schema.table "test")
+      { Schema.tableColumns =
+          [ Schema.column "id" Schema.defaultSerialType,
+            Schema.column "json" jsonType,
+            Schema.column "jsonb" jsonbType
+          ],
+        Schema.tablePrimaryKey = ["id"],
+        Schema.tableData =
+          [ [Schema.VInt 1, mkJsonValue "[1,2,3]", mkJsonValue "[1,2,3]"],
+            [Schema.VInt 2, mkJsonValue "{\"dog\": true}", mkJsonValue "{\"dog\":true}"]
+          ]
+      }
+  ]
+
+--------------------------------------------------------------------------------
+-- Tests
+
+tests :: Fixture.Options -> SpecWith TestEnvironment
+tests opts = do
+  let shouldBe :: IO Value -> Value -> IO ()
+      shouldBe = shouldReturnYaml opts
+
+  describe "Retrieves JSON and JSONB values" do
+    it "Everything decodes as expected" \testEnvironment -> do
+      let expected :: Value
+          expected =
+            [yaml|
+              data:
+                hasura_test:
+                  - id: 1
+                    json: [1,2,3]
+                    jsonb: [1,2,3]
+                  - id: 2
+                    json: {"dog": true}
+                    jsonb: {"dog": true}
+            |]
+
+          actual :: IO Value
+          actual =
+            postGraphql
+              testEnvironment
+              [graphql|
+                query {
+                  hasura_test {
+                    id
+                    json
+                    jsonb
+                  }
+                }
+              |]
+
+      actual `shouldBe` expected
