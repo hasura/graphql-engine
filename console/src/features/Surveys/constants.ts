59f5fe4545b97ab84e5df56379066b3e2ebf4282
Abhijeet Khangarot <abhijeet40308@gmail.com>
2022-09-16 01:50:40 +0530

    console: hotfix hasura familiarity survey to v2.12
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/5902
    Co-authored-by: nevermore <31686586+OjasWadhwani@users.noreply.github.com>
    GitOrigin-RevId: 6336177d7eb61b4937008b0bf17ab9dad6a1d931

---
+++ console/src/features/Surveys/constants.ts
@@ -0,0 +1,47 @@
+import { convertDateTimeToUTC } from './utils';
+
+export const surveysQueryKey = 'surveysData';
+export const surveyName = 'Hasura familiarity survey';
+
+/**
+ * GraphQl query to fetch all surveys related data
+ */
+export const fetchAllSurveysDataQuery = `
+ query ($currentTime: timestamptz!) {
+   survey(where: {
+       _or: [
+         {ended_at: {_gte: $currentTime}}
+         {ended_at: {_is_null: true}},
+       ]
+     }) {
+     survey_name
+     survey_questions {
+       kind
+       question
+       id
+       survey_question_options {
+         option
+         id
+       }
+     }
+   }
+   survey_question_answers {
+    survey_question_id
+   }
+ }
+  `;
+
+export const fetchAllSurveysDataQueryVariables = {
+  currentTime: convertDateTimeToUTC(new Date()),
+};
+
+/**
+ * GraphQl mutation to save the survey answer
+ */
+export const addSurveyAnswerMutation = `
+ mutation ($responses: [QuestionAnswers]!, $surveyName: String!) {
+    saveSurveyAnswer(payload: {responses: $responses, surveyName: $surveyName}) {
+      status
+    }
+  }
+  `;
