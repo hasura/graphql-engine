df06d5cb0c27f85b69e4fba605a233dd133ee0bf
Daniele Cammareri <daniele.cammareri@hasura.io>
2022-09-19 09:46:56 +0200

    console: new remote schema preview tab
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/5896
    GitOrigin-RevId: 43c2a1b1331c58ad502c3da1ea87e29df6d9ab8c

---
+++ console/src/features/RemoteSchema/components/Details/RemoteSchemaDetailsHeaders.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { CardedTable } from '@/new-components/CardedTable';
+import { ServerHeader } from '@/features/MetadataAPI';
+
+interface RemoteSchemaDetailsHeadersProps {
+  headers?: ServerHeader[];
+}
+export const RemoteSchemaDetailsHeaders = (
+  props: RemoteSchemaDetailsHeadersProps
+) => {
+  const { headers } = props;
+
+  if (!headers) {
+    return null;
+  }
+
+  const filteredHeaders = headers.filter(h => !!h.name);
+
+  return (
+    <div className="mb-md">
+      <label className="block mb-xs font-semibold text-muted">Headers</label>
+      <CardedTable
+        columns={['Name', 'Type', 'Value']}
+        data={filteredHeaders.map(header => {
+          if ('value' in header) {
+            return [header.name, 'Static', header.value];
+          }
+          if ('value_from_env' in header) {
+            return [header.name, 'From env var', header.value_from_env];
+          }
+          return ['', '', ''];
+        })}
+      />
+    </div>
+  );
+};
