90543c99ae286dc14c05139f26122593ab7f0a56
Abhijeet Khangarot <abhijeet40308@gmail.com>
2022-09-26 16:03:04 +0530

    console: add onboarding wizard tests
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/6041
    GitOrigin-RevId: 72e5f6e0fc7c51c9a4db70665059771194be2730

--- console/src/features/OnboardingWizard/mocks/handlers.mock.ts
+++ console/src/features/OnboardingWizard/mocks/handlers.mock.ts
@@ -1,5 +1,7 @@
 import { graphql } from 'msw';
 import Endpoints from '@/Endpoints';
+import { SurveysResponseData } from '@/features/Surveys';
+import { fetchSurveysDataResponse } from './constants';
 
 type ResponseBodyOnSuccess = {
   status: 'success';
@@ -7,13 +9,50 @@ type ResponseBodyOnSuccess = {
 
 const controlPlaneApi = graphql.link(Endpoints.luxDataGraphql);
 
-export const handlers = () => [
-  controlPlaneApi.operation<any, ResponseBodyOnSuccess>((req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.data({
-        status: 'success',
-      })
-    );
-  }),
+export const baseHandlers = () => [
+  controlPlaneApi.query<ResponseBodyOnSuccess>(
+    'fetchAllExperimentsData',
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.data({
+          status: 'success',
+        })
+      );
+    }
+  ),
+  controlPlaneApi.mutation<ResponseBodyOnSuccess>(
+    'addSurveyAnswer',
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.data({
+          status: 'success',
+        })
+      );
+    }
+  ),
+  controlPlaneApi.mutation<ResponseBodyOnSuccess>(
+    'trackExperimentsCohortActivity',
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.data({
+          status: 'success',
+        })
+      );
+    }
+  ),
 ];
+
+export const fetchUnansweredSurveysHandler = controlPlaneApi.query<
+  SurveysResponseData['data']
+>('fetchAllSurveysData', (req, res, ctx) => {
+  return res(ctx.status(200), ctx.data(fetchSurveysDataResponse.unanswered));
+});
+
+export const fetchAnsweredSurveysHandler = controlPlaneApi.query<
+  SurveysResponseData['data']
+>('fetchAllSurveysData', (req, res, ctx) => {
+  return res(ctx.status(200), ctx.data(fetchSurveysDataResponse.answered));
+});
