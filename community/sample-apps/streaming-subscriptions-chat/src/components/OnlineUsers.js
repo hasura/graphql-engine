9fe25f840bc31fa985f306ba9c05fd200daa9130
Jigyasu Arya <arya.jigyasu6815@gmail.com>
2022-10-04 19:45:32 +0530

    community: update streaming-subscriptions-chat ui
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/6082
    Co-authored-by: arjunyel <11153289+arjunyel@users.noreply.github.com>
    Co-authored-by: Praveen Durairaju <14110316+praveenweb@users.noreply.github.com>
    GitOrigin-RevId: 48a02077900571686529c52e835569ad9dbc6b1c

--- community/sample-apps/streaming-subscriptions-chat/src/components/OnlineUsers.js
+++ community/sample-apps/streaming-subscriptions-chat/src/components/OnlineUsers.js
@@ -1,5 +1,12 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { gql, useSubscription } from '@apollo/client';
+
+import {
+  StyledLeftSection,
+  StyledOnlineUsers,
+  StyledOnlineUserCircle,
+} from '../styles/StyledChatApp';
+import { getUserBgColor } from './MessageList';
 
 const fetchOnlineUsersSubscription = gql`
   subscription {
@@ -10,8 +17,10 @@ const fetchOnlineUsersSubscription = gql`
   }
 `;
 
-function OnlineUsers() {
+function OnlineUsers({ dataStream }) {
   const [showMobileView, setMobileView] = useState(false);
+
+  const [showMobileMenu, toggleMobileMenu] = useState(false);
 
   const { data } = useSubscription(fetchOnlineUsersSubscription);
 
@@ -19,8 +28,31 @@ function OnlineUsers() {
     setMobileView(!showMobileView);
   };
 
+  const subscriptionStreamData = (isMobileView) => (
+    <StyledOnlineUsers>
+      <p
+        className={isMobileView ? 'mobileuserListHeading' : 'userListHeading'}
+        onClick={toggleMobileView}
+      >
+        Subscription Stream
+        {isMobileView && <i className="fa fa-angle-up"></i>}
+      </p>
+      {((isMobileView && showMobileView) || !isMobileView) && (
+        <ul
+          className={
+            isMobileView ? 'mobileUserList' : 'userList subscription-stream'
+          }
+        >
+          <p className="subscription-stream">
+            {JSON.stringify(dataStream, undefined, 4)}
+          </p>
+        </ul>
+      )}
+    </StyledOnlineUsers>
+  );
+
   const subscriptionData = (isMobileView) => (
-    <div>
+    <StyledOnlineUsers>
       <p
         className={isMobileView ? 'mobileuserListHeading' : 'userListHeading'}
         onClick={toggleMobileView}
@@ -31,20 +63,59 @@ function OnlineUsers() {
       {((isMobileView && showMobileView) || !isMobileView) && (
         <ul className={isMobileView ? 'mobileUserList' : 'userList'}>
           {data?.user_online.map((u) => {
-            return <li key={u.id}>{u.username}</li>;
+            return (
+              <StyledOnlineUserCircle
+                bgColor={getUserBgColor(u.username)}
+                key={u.id}
+              >
+                {u.username.substring(0, 2)}
+              </StyledOnlineUserCircle>
+            );
           })}
         </ul>
       )}
-    </div>
+    </StyledOnlineUsers>
   );
 
   return (
-    <div>
-      <div className="onlineUsers hidden-xs">{subscriptionData(false)}</div>
-      <div className="mobileonlineUsers visible-xs">
-        {subscriptionData(true)}
+    <StyledLeftSection showMobileMenu={showMobileMenu}>
+      {/* Mobile View */}
+      <div className="mobile-header hide-on-desk">
+        <a href="https://hasura.io/">
+          <img
+            loading="lazy"
+            alt="Hasura"
+            src="https://graphql-engine-cdn.hasura.io/assets/main-site/logo_primary_light.svg"
+            className="hasura-logo-img"
+          />
+        </a>
+        {!showMobileMenu && (
+          <div className="flex-div">
+            <p onClick={() => toggleMobileMenu(true)}>
+              Online Users&nbsp;(
+              {!data?.user_online ? 0 : data?.user_online.length}){' '}
+            </p>
+            <i
+              className={showMobileMenu ? 'fa fa-angle-down' : 'fa fa-angle-up'}
+            ></i>
+          </div>
+        )}
+        {showMobileMenu && (
+          <div className="mobile-data-wrapper">
+            <p className="close-btn" onClick={() => toggleMobileMenu(false)}>
+              <i className="fa fa-times"></i>
+            </p>
+            <div className="onlineUsers">{subscriptionData(false)}</div>
+            <div className="onlineUsers">{subscriptionStreamData(false)}</div>
+          </div>
+        )}
       </div>
-    </div>
+      {/* ************ */}
+      <div className="onlineUsers hideOnMobile">
+        {subscriptionStreamData(false)}
+      </div>
+      <div className="onlineUsers hideOnMobile">{subscriptionData(false)}</div>
+    </StyledLeftSection>
   );
 }
 
