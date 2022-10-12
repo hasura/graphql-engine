0bd013653a2cf03e4331b3b610d80e6e2086cc9e
Mohd Bilal <mohd.bilal1996@gmail.com>
2022-10-12 14:16:01 +0530

    cli: port pgdump package to use new errors package
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/6263
    GitOrigin-RevId: e410548ecba70e556e41a0377952555c3b40c8ef

--- cli/internal/hasura/pgdump/pgdump.go
+++ cli/internal/hasura/pgdump/pgdump.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/hasura/graphql-engine/cli/v2/internal/httpc"
 
+	"github.com/hasura/graphql-engine/cli/v2/internal/errors"
 	"github.com/hasura/graphql-engine/cli/v2/internal/hasura"
 )
 
@@ -22,25 +23,27 @@ func New(client *httpc.Client, path string) *Client {
 }
 
 func (c *Client) send(body interface{}, responseBodyWriter io.Writer) (*httpc.Response, error) {
+	var op errors.Op = "pgdump.Client.send"
 	req, err := c.NewRequest(http.MethodPost, c.path, body)
 	if err != nil {
-		return nil, err
+		return nil, errors.E(op, err)
 	}
 	resp, err := c.LockAndDo(context.Background(), req, responseBodyWriter)
 	if err != nil {
-		return nil, err
+		return nil, errors.E(op, err)
 	}
 	return resp, nil
 }
 
 func (c *Client) Send(request hasura.PGDumpRequest) (io.Reader, error) {
+	var op errors.Op = "pgdump.Client.Send"
 	responseBody := new(bytes.Buffer)
 	response, err := c.send(request, responseBody)
 	if err != nil {
-		return nil, err
+		return nil, errors.E(op, err)
 	}
 	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("pg_dump request: %d \n%s", response.StatusCode, responseBody.String())
+		return nil, errors.E(op, errors.KindHasuraAPI, fmt.Errorf("pg_dump request: %d \n%s", response.StatusCode, responseBody.String()))
 	}
 	return responseBody, nil
 }
