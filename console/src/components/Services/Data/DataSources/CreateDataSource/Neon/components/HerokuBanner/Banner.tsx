8cded6f08ca03b05703f9bf7730f29d08e702793
Abhijeet Khangarot <abhijeet40308@gmail.com>
2022-10-18 10:51:25 +0530

    console: hotfix neon UI changes to v2.13
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/6399
    GitOrigin-RevId: cc540649049bf24b79acc5c5ee391a3a632e8c24

--- console/src/components/Services/Data/DataSources/CreateDataSource/Neon/components/HerokuBanner/Banner.tsx
+++ console/src/components/Services/Data/DataSources/CreateDataSource/Neon/components/HerokuBanner/Banner.tsx
@@ -6,16 +6,16 @@ export function HerokuBanner() {
     <div className="flex items-center">
       <GrHeroku size={15} className="mr-xs" color="#430098" />
       <div className="text-sm text-gray-700">
-        Heroku free database integration support has been deprecated.
+        Heroku free database integration support has been deprecated.{' '}
+        <a
+          href="https://hasura.io/docs/latest/databases/connect-db/cloud-databases/heroku/"
+          className="text-secondary italic"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          (Know More)
+        </a>
       </div>
-      <a
-        href="https://hasura.io/docs/latest/databases/connect-db/cloud-databases/heroku/"
-        className="ml-xs font-normal text-secondary italic text-sm"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        (Know More)
-      </a>
     </div>
   );
 }
