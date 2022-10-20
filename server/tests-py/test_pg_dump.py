0a66d87305cda62186e90e91b9c622b6f7a29068
Samir Talwar <samir.talwar@hasura.io>
2022-10-20 07:56:11 +0200

    server/tests-py: Upgrade to `postgresql-client-15`.
    This means we can run `TestPGDump` against it.
    
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/6372
    GitOrigin-RevId: 2512a1f372342e5793b97aeb34f19b92be99b6b2

--- server/tests-py/test_pg_dump.py
+++ server/tests-py/test_pg_dump.py
@@ -9,41 +9,29 @@ class TestPGDump:
 
     def test_pg_dump_for_public_schema(self, hge_ctx):
         query_file = self.dir() + '/pg_dump_public.yaml'
-        PG_VERSION = os.getenv('PG_VERSION', 'latest')
-        if PG_VERSION == "15":
-            # we do not have a working version of pg_dump for Postgres 15
-            # yet, skip these tests
-            pytest.skip('pg_dump does not yet support PostgreSQL 15')
-        else:
-            with open(query_file, 'r') as stream:
-                q = yaml.load(stream)
-                headers = q['headers'] or {}
-                if hge_ctx.hge_key is not None:
-                    headers['x-hasura-admin-secret'] = hge_ctx.hge_key
-                resp = hge_ctx.http.post(hge_ctx.hge_url + q['url'], json=q['query'], headers=headers)
-                body = resp.text
-                assert resp.status_code == q['status']
-                print(body)
-                print(q['expected_response'])
-                assert body == q['expected_response']
+        with open(query_file, 'r') as stream:
+            q = yaml.load(stream)
+            headers = q['headers'] or {}
+            if hge_ctx.hge_key is not None:
+                headers['x-hasura-admin-secret'] = hge_ctx.hge_key
+            resp = hge_ctx.http.post(hge_ctx.hge_url + q['url'], json=q['query'], headers=headers)
+            body = resp.text
+            assert resp.status_code == q['status']
+            print(body)
+            print(q['expected_response'])
+            assert body == q['expected_response']
 
     def test_pg_dump_for_public_schema_for_user_role(self, hge_ctx):
-        PG_VERSION = os.getenv('PG_VERSION', 'latest')
-        if PG_VERSION == "15":
-            # we do not have a working version of pg_dump for Postgres 15
-            # yet, skip these tests
-            pytest.skip('pg_dump does not yet support PostgreSQL 15')
-        else:
-            query_file = self.dir() + '/pg_dump_public.yaml'
-            with open(query_file, 'r') as stream:
-                q = yaml.load(stream)
-                headers = q['headers'] or {}
-                if hge_ctx.hge_key is not None:
-                    headers['x-hasura-admin-secret'] = hge_ctx.hge_key
-                headers['X-Hasura-Role'] = 'user'
-                resp = hge_ctx.http.post(hge_ctx.hge_url + q['url'], json=q['query'], headers=headers)
-                body = resp.text
-                assert resp.status_code == 400, body
+        query_file = self.dir() + '/pg_dump_public.yaml'
+        with open(query_file, 'r') as stream:
+            q = yaml.load(stream)
+            headers = q['headers'] or {}
+            if hge_ctx.hge_key is not None:
+                headers['x-hasura-admin-secret'] = hge_ctx.hge_key
+            headers['X-Hasura-Role'] = 'user'
+            resp = hge_ctx.http.post(hge_ctx.hge_url + q['url'], json=q['query'], headers=headers)
+            body = resp.text
+            assert resp.status_code == 400, body
 
     @classmethod
     def dir(cls):
