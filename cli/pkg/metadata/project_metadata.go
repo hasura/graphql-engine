f82fc6d4205b771d082031fac3cb93337383209b
Aravind K P <aravindkp@hasura.io>
2022-10-28 12:16:52 +0530

    cli: port `pkg/metadata` to use `internal/errors`
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/6490
    Co-authored-by: Mohd Bilal <24944223+m-Bilal@users.noreply.github.com>
    GitOrigin-RevId: 10b2ddbd534895c28bc69a6cf1a68229f67e8e3d

--- cli/pkg/metadata/project_metadata.go
+++ cli/pkg/metadata/project_metadata.go
@@ -5,6 +5,7 @@ import (
 
 	"io"
 
+	"github.com/hasura/graphql-engine/cli/v2/internal/errors"
 	"github.com/hasura/graphql-engine/cli/v2/internal/projectmetadata"
 
 	"github.com/spf13/viper"
@@ -18,28 +19,53 @@ type ProjectMetadata struct {
 
 // Parse metadata in project as JSON
 func (p *ProjectMetadata) Parse() (io.Reader, error) {
-	return getModeHandler(p.ec.MetadataMode).Parse(p)
+	var op errors.Op = "metadata.ProjectMetadata.Parse"
+	r, err := getModeHandler(p.ec.MetadataMode).Parse(p)
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+	return r, nil
 }
 
 // Apply metadata from in the project and provide raw response from hge server
 func (p *ProjectMetadata) Apply() (io.Reader, error) {
-	return getModeHandler(p.ec.MetadataMode).Apply(p)
+	var op errors.Op = "metadata.ProjectMetadata.Apply"
+	r, err := getModeHandler(p.ec.MetadataMode).Apply(p)
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+	return r, nil
 }
 
 // Reload metadata on hge server and provides raw response from hge server
 func (p *ProjectMetadata) Reload() (io.Reader, error) {
+	var op errors.Op = "metadata.ProjectMetadata.Reload"
 	metadataHandler := projectmetadata.NewHandlerFromEC(p.ec)
-	return metadataHandler.ReloadMetadata()
+	r, err := metadataHandler.ReloadMetadata()
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+	return r, nil
 }
 
 // GetInconsistentMetadata objects from hge server
 func (p *ProjectMetadata) GetInconsistentMetadata() (io.Reader, error) {
-	return cli.GetCommonMetadataOps(p.ec).GetInconsistentMetadataRaw()
+	var op errors.Op = "metadata.ProjectMetadata.GetInconsistentMetadata"
+	r, err := cli.GetCommonMetadataOps(p.ec).GetInconsistentMetadataRaw()
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+	return r, nil
 }
 
 // Diff will return the differences between metadata in the project (in JSON) and on the server
 func (p *ProjectMetadata) Diff() (io.Reader, error) {
-	return getModeHandler(p.ec.MetadataMode).Diff(p)
+	var op errors.Op = "metadata.ProjectMetadata.Diff"
+	r, err := getModeHandler(p.ec.MetadataMode).Diff(p)
+	if err != nil {
+		return nil, errors.E(op, err)
+	}
+	return r, nil
 }
 
 type ProjectMetadataOption func(*ProjectMetadata)
@@ -63,6 +89,7 @@ func WithCliExtPath(path string) ProjectMetadataOption {
 }
 
 func NewProjectMetadata(projectDirectory string, opts ...ProjectMetadataOption) (*ProjectMetadata, error) {
+	var op errors.Op = "metadata.NewProjectMetadata"
 	ec := cli.NewExecutionContext()
 	ec.ExecutionDirectory = projectDirectory
 	ec.Viper = viper.New()
@@ -71,7 +98,7 @@ func NewProjectMetadata(projectDirectory string, opts ...ProjectMetadataOption) 
 	ec.Stderr = io.Discard
 
 	if err := ec.Prepare(); err != nil {
-		return nil, err
+		return nil, errors.E(op, err)
 	}
 	p := &ProjectMetadata{ec}
 	for _, opt := range opts {
@@ -79,10 +106,10 @@ func NewProjectMetadata(projectDirectory string, opts ...ProjectMetadataOption) 
 	}
 
 	if err := ec.Validate(); err != nil {
-		return nil, err
+		return nil, errors.E(op, err)
 	}
 	if ec.Config.Version <= cli.V1 {
-		return nil, fmt.Errorf("config %v is not supported", ec.Config.Version)
+		return nil, errors.E(op, fmt.Errorf("config %v is not supported", ec.Config.Version))
 	}
 	return p, nil
 }
