7ba2409fb8b5c1485e20877b853a9898f4d67aa5
Mohd Bilal <mohd.bilal1996@gmail.com>
2022-11-08 19:22:00 +0530

    cli: refactor `metadata inconsistency status` and `metadata reload` to use `internal/errors`
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/6722
    GitOrigin-RevId: 2dece53c8575627ffe98155261161a07f2195cef

--- cli/commands/metadata_reload.go
+++ cli/commands/metadata_reload.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/hasura/graphql-engine/cli/v2"
+	"github.com/hasura/graphql-engine/cli/v2/internal/errors"
 	"github.com/hasura/graphql-engine/cli/v2/internal/projectmetadata"
 
 	"github.com/spf13/cobra"
@@ -27,7 +28,11 @@ func newMetadataReloadCmd(ec *cli.ExecutionContext) *cobra.Command {
   hasura metadata export --endpoint "<endpoint>"`,
 		SilenceUsage: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return opts.runWithInfo()
+			op := genOpName(cmd, "RunE")
+			if err := opts.runWithInfo(); err != nil {
+				return errors.E(op, err)
+			}
+			return nil
 		},
 	}
 
@@ -39,11 +44,12 @@ type MetadataReloadOptions struct {
 }
 
 func (o *MetadataReloadOptions) runWithInfo() error {
+	var op errors.Op = "commands.MetadataReloadOptions.runWithInfo"
 	o.EC.Spin("Reloading metadata...")
 	err := o.run()
 	o.EC.Spinner.Stop()
 	if err != nil {
-		return fmt.Errorf("failed to reload metadata: %w", err)
+		return errors.E(op, fmt.Errorf("failed to reload metadata: %w", err))
 	}
 	o.EC.Logger.Info("Metadata reloaded")
 	icListOpts := &metadataInconsistencyListOptions{
@@ -51,7 +57,7 @@ func (o *MetadataReloadOptions) runWithInfo() error {
 	}
 	err = icListOpts.read(projectmetadata.NewHandlerFromEC(icListOpts.EC))
 	if err != nil {
-		return fmt.Errorf("failed to read metadata status: %w", err)
+		return errors.E(op, fmt.Errorf("failed to read metadata status: %w", err))
 	}
 	if icListOpts.isConsistent {
 		icListOpts.EC.Logger.Infoln("Metadata is consistent")
@@ -62,11 +68,12 @@ func (o *MetadataReloadOptions) runWithInfo() error {
 }
 
 func (o *MetadataReloadOptions) run() error {
+	var op errors.Op = "commands.MetadataReloadOptions.run"
 	var err error
 	metadataHandler := projectmetadata.NewHandlerFromEC(o.EC)
 	_, err = metadataHandler.ReloadMetadata()
 	if err != nil {
-		return fmt.Errorf("cannot reload metadata: %w", err)
+		return errors.E(op, fmt.Errorf("cannot reload metadata: %w", err))
 	}
 	return nil
 }
