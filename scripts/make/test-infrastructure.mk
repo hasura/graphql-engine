9fe793e828aef6b84460ee4789671068cd3461a5
Samir Talwar <samir.talwar@hasura.io>
2022-11-14 13:38:19 +0100

    Split the Docker Compose files into pieces.
    With the vague idea that we can eventually avoid publishing ports at all, at least in CI, while still having the flexibility to publish ports locally.
    
    If we can get there, it should help with the issues we're seeing on CI, where ports are already allocated from previous runs and containers aren't properly cleaned up.
    
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/6836
    GitOrigin-RevId: 1d636c73ae889e45e80ad52042c56daa1b9d5838

--- scripts/make/test-infrastructure.mk
+++ scripts/make/test-infrastructure.mk
@@ -1,10 +1,3 @@
-# the following variables are set up in Docker Compose
-# and are also defined in Harness.Constants for use in api tests
-MSSQL_PORT = 65003
-MSSQL_DBNAME = hasura
-MSSQL_DBUSER = hasura
-MSSQL_DBPASSWORD = Hasura1!
-
 # Use the Azure SQL Edge image instead of the SQL Server image on arm64.
 # The latter doesn't work yet.
 ifeq ($(shell uname -m),arm64)
@@ -13,11 +6,7 @@ else
 MSSQL_IMAGE=  # allow the Docker Compose file to set the image
 endif
 
-# Run `sqlcmd` in a separate image when waiting for SQL Server to start.
-MSSQL_SQLCMD = docker run --rm --platform=linux/amd64 --net=host mcr.microsoft.com/mssql-tools /opt/mssql-tools/bin/sqlcmd
-
 export MSSQL_IMAGE
-export MSSQL_SQLCMD
 
 define stop_after
 @ echo $1 >&2
@@ -30,22 +19,20 @@ else \
 fi
 endef
 
-.PHONY: start-sqlserver
-## start-sqlserver: start local MS SQL Server DB in Docker and wait for it to be ready
-start-sqlserver:
-	docker compose up -d --wait sqlserver
-	docker compose run sqlserver-init
+.PHONY: build-backends
+## build-backends: build Docker images for any backends that need them
+build-backends:
+	docker compose build
 
 .PHONY: start-backends
 ## start-backends: start all known backends in Docker and wait for them to be ready
-start-backends: start-sqlserver
-	docker compose up -d --wait postgres mariadb citus dc-reference-agent dc-sqlite-agent cockroach
+start-backends: build-backends
+	docker compose up --detach --wait
 
 .PHONY: stop-everything
 ## stop-everything: tear down test databases
 stop-everything:
-	# stop docker
-	docker compose down -v
+	docker compose down --volumes
 
 .PHONY: remove-tix-file
 remove-tix-file:
