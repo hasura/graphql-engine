a1df4770955df0ed36d71514e17241842d654b87
Rishichandra Wawhal <rishi@hasura.io>
2022-12-06 12:23:09 +0530

    console: [hotfix v2.16] hide onboarding for users who onboarded before onboarding was launched
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/7169
    GitOrigin-RevId: 33db215da96c802df6dd535224d16fbc49ccffab

--- console/src/features/OnboardingWizard/types.ts
+++ console/src/features/OnboardingWizard/types.ts
@@ -3,8 +3,14 @@ export type UserOnboarding = {
   target: string;
 };
 
+export type User = {
+  id: string;
+  created_at: string;
+};
+
 export type OnboardingResponseData = {
   data: {
     user_onboarding: UserOnboarding[];
+    users: User[];
   };
 };
