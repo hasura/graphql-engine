47eeace9a6106c8b26493ceba0826d13ef6ffe0c
Sean Park-Ross <94021366+seanparkross@users.noreply.github.com>
2022-12-16 17:52:42 +0200

    Docs: Switch to SWC
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/7304
    GitOrigin-RevId: c413cc5145b54ef89b16aef792332a85b681b3d7

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,7 +18,23 @@ const config = {
   organizationName: 'hasura',
   projectName: 'graphql-engine',
   staticDirectories: ['static', 'public'],
-
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('swc-loader'),
+      options: {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+          },
+          target: 'es2017',
+        },
+        module: {
+          type: isServer ? 'commonjs' : 'es6',
+        },
+      },
+    }),
+  },
   presets: [
     [
       'classic',
