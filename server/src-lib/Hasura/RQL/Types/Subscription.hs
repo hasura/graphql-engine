139aca3a25e884de854e158acd0b09e910a724b1
Anon Ray <ecthiender@users.noreply.github.com>
2023-01-17 19:23:53 +0530

    server: add kind field to livequery-poller-log (GS-329)
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/7394
    GitOrigin-RevId: 34663eb7c0c312b735c77e1855de32dd3a3fba5f

--- server/src-lib/Hasura/RQL/Types/Subscription.hs
+++ server/src-lib/Hasura/RQL/Types/Subscription.hs
@@ -4,6 +4,7 @@ module Hasura.RQL.Types.Subscription
   )
 where
 
+import Data.Aeson (ToJSON (..))
 import Hasura.Prelude
 
 -- | CursorOrdering is used in the streaming subscriptions to specify how to order the cursor.
@@ -12,3 +13,8 @@ data CursorOrdering = COAscending | CODescending deriving (Show, Eq, Generic)
 instance Hashable CursorOrdering
 
 data SubscriptionType = Streaming | LiveQuery deriving (Show, Eq, Generic)
+
+instance ToJSON SubscriptionType where
+  toJSON = \case
+    Streaming -> "streaming"
+    LiveQuery -> "live-query"
