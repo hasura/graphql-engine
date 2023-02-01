18b346bf44e7f231738d926f06268c58d99f0ead
Tom Harding <tomjharding@live.co.uk>
2023-02-01 12:23:55 +0000

    Extract `Hasura.Incremental` into `lib/hasura-incremental`
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/7534
    GitOrigin-RevId: 85863cee836d348e8e314f8af1b50bb5cd912b46

--- server/lib/monad-unique/src/Control/Monad/Unique.hs
+++ server/lib/monad-unique/src/Control/Monad/Unique.hs
@@ -5,8 +5,12 @@ module Control.Monad.Unique
   )
 where
 
+import Control.Monad.Except (ExceptT)
+import Control.Monad.Reader (ReaderT)
+import Control.Monad.State.Strict (StateT)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Writer.Strict (WriterT)
 import Data.Unique qualified as U
-import Hasura.Prelude
 
 class (Monad m) => MonadUnique m where
   newUnique :: m U.Unique
