aa39203e13a91ad1b3cb64d927c46cc75be56aeb
Matthew Goodwin <49927862+m4ttheweric@users.noreply.github.com>
2023-03-07 08:37:13 -0600

    console: fixes incorrect data type shown in GDC modify tab
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/8180
    GitOrigin-RevId: 0b4272f7161759541b28ae04bf0a517fb9e42823

--- frontend/libs/console/legacy-ce/src/lib/features/Data/ModifyTable/ModifyTable.stories.tsx
+++ frontend/libs/console/legacy-ce/src/lib/features/Data/ModifyTable/ModifyTable.stories.tsx
@@ -1,15 +1,11 @@
 // Button.stories.ts|tsx
-
+import { userEvent, waitFor, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 import React from 'react';
 import { ReactQueryDecorator } from '../../../storybook/decorators/react-query';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ModifyTable } from './ModifyTable';
-
-const props = {
-  table: ['Customer'],
-  dataSourceName: 'music_db',
-  tableName: 'Customer',
-};
+import { handlers } from './mock/handlers';
 
 export default {
   /* 👇 The title prop is optional.
@@ -17,9 +13,43 @@ export default {
    * to learn how to generate automatic titles
    */
   component: ModifyTable,
+
   decorators: [ReactQueryDecorator()],
 } as ComponentMeta<typeof ModifyTable>;
 
-export const Primary: ComponentStory<typeof ModifyTable> = () => (
-  <ModifyTable {...props} />
+export const Primary: ComponentStory<typeof ModifyTable> = args => (
+  <ModifyTable {...args} />
 );
+
+Primary.args = {
+  table: ['Customer'],
+  dataSourceName: 'sqlite',
+  tableName: 'Customer',
+};
+
+export const TestData = Primary.bind({});
+
+TestData.args = {
+  table: ['Customer'],
+  dataSourceName: 'sqlite',
+  tableName: 'Customer',
+};
+
+TestData.parameters = {
+  msw: {
+    handlers: handlers(),
+  },
+};
+
+TestData.play = async ({ canvasElement }) => {
+  const c = within(canvasElement);
+  const dataTypeBadge = await c.findByTestId(
+    `CustomerId-ui-data-type`,
+    {},
+    { timeout: 3000 }
+  );
+  const hiddenDataInput = await c.findByTestId('CustomerId-data');
+  const correctDataType = await hiddenDataInput.dataset['dataType'];
+  await expect(correctDataType).toBeTruthy();
+  await expect(dataTypeBadge).toHaveTextContent(correctDataType as string);
+};
