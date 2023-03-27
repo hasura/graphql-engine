26bb2b4030bf321fbc40dcf3495e2cd233f92a80
Matthew Goodwin <49927862+m4ttheweric@users.noreply.github.com>
2023-03-27 06:47:09 -0500

    console: Set the new relationships UI as the default UI
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/8423
    GitOrigin-RevId: a614df40b108c35f8aa8cde67557b117061b5591

--- frontend/libs/console/legacy-ce/src/lib/features/FeatureFlags/availableFeatureFlags.ts
+++ frontend/libs/console/legacy-ce/src/lib/features/FeatureFlags/availableFeatureFlags.ts
@@ -1,6 +1,6 @@
 import { FeatureFlagDefinition } from './types';
 
-const relationshipTabTablesId = '0bea35ff-d3e9-45e9-af1b-59923bf82fa9';
+const relationshipTabTablesId = 'f6c57c31-abd3-46d9-aae9-b97435793273';
 const importActionFromOpenApiId = '12e5aaf4-c794-4b8f-b762-5fda0bff946a';
 const enabledNewUIForBigQuery = 'e2d790ba-96fb-11ed-a8fc-0242ac120002';
 
@@ -15,10 +15,10 @@ export const availableFeatureFlags: FeatureFlagDefinition[] = [
     id: relationshipTabTablesId,
     title: 'New Relationship tab UI for tables/views',
     description:
-      'Try out the new UI for the Relationship tab of Tables/Views in Data section.',
+      'Use the new UI for the Relationship tab of Tables/Views in Data section.',
     section: 'data',
-    status: 'alpha',
-    defaultValue: false,
+    status: 'release candidate',
+    defaultValue: true,
     discussionUrl: '',
   },
   {
