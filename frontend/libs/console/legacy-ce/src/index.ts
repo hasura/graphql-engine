f2b76b7da7389c4f3794ac5426d8507cede0cd61
Nicolas Inchauspe <nicoinch@users.noreply.github.com>
2023-04-05 10:22:13 +0200

    console: replace bootstrap modal with radix one
    [PLAT-511]: https://hasurahq.atlassian.net/browse/PLAT-511?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/8622
    GitOrigin-RevId: 8387bad11c7529ded8a89c551f5858f3f1ee0c27

--- frontend/libs/console/legacy-ce/src/index.ts
+++ frontend/libs/console/legacy-ce/src/index.ts
@@ -12,7 +12,7 @@ import { IntrospectionOptions } from './lib/components/Services/ApiExplorer/Secu
 import * as EndpointNamedExps from './lib/Endpoints';
 import * as ControlPlane from './lib/features/ControlPlane';
 
-export * from './lib/utils/console-dev-tools/ConsoleDevTools';
+export * from './lib/utils/console-dev-tools';
 const CommonScss = require('./lib/components/Common/Common.module.scss');
 const filterQueryScss = require('./lib/components/Common/FilterQuery/FilterQuery.module.scss');
 const tableScss = require('./lib/components/Common/TableCommon/Table.module.scss');
