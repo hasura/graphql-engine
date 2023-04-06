6d79b7dfafe9aadc89c0c0178168c96fa1ec76dd
Samir Talwar <samir.talwar@hasura.io>
2023-04-06 10:51:45 +0200

    make: Add `run-oss-server`, and associated frontend targets.
    `make run-oss-server` will now build the Console for OSS ("CE"), start PostgreSQL, and run a simply-configured server.
    
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/8652
    GitOrigin-RevId: cada25e66fe216867ff331d597764e64aaf2dc19

--- scripts/make/frontend.mk
+++ scripts/make/frontend.mk
@@ -1,11 +1,35 @@
-.PHONY: build-console-assets
-## build-console-assets
-build-console-assets: frontend/node_modules
+CONSOLE_CE_ASSETS_PATH = frontend/dist/apps/server-assets-console-ce
+CONSOLE_EE_ASSETS_PATH = frontend/dist/apps/server-assets-console-ee
+
+.PHONY: build-console-ce-assets
+## build-console-ce-assets: Build Console assets required for the CE server
+build-console-ce-assets: $(CONSOLE_CE_ASSETS_PATH)
+
+.PHONY: build-console-ee-assets
+## build-console-ee-assets: Build Console assets required for the EE server
+build-console-ee-assets: $(CONSOLE_EE_ASSETS_PATH)
+
+# We split the named, phony targets from the path-based targets below so that
+# `make` has a chance of avoiding rework, by recognizing that the output path
+# is newer than the inputs.
+#
+# We use `touch` to ensure that the path is marked as newer, because modified
+# timestamps on directories can be quite unreliable.
+#
+# `$@` refers to the target.
+
+$(CONSOLE_CE_ASSETS_PATH): frontend/node_modules
 	cd frontend && npm run server-build:ce
+	touch $@
+
+$(CONSOLE_EE_ASSETS_PATH): frontend/node_modules
+	cd frontend && npm run server-build:ee
+	touch $@
 
 # Install node_modules if package-lock.json changes
 frontend/node_modules: frontend/package-lock.json
-	cd frontend && npm install && touch node_modules
+	cd frontend && npm install
+	touch $@
 
 # Cleanly install node_modules if package.json changes
 frontend/package-lock.json: frontend/package.json
--- scripts/make/run.mk
+++ scripts/make/run.mk
@@ -1,16 +1,23 @@
 # this connection should match postgres settings in `/docker-compose/databases.yaml`
-POSTGRES_URL=postgresql://hasura:hasura@localhost:65002/hasura
-CONSOLE_EE_ASSETS_PATH=./frontend/dist/apps/server-assets-console-ee
-ENABLED_LOG_TYPES=startup,http-log,webhook-log,websocket-log,query-log,execution-log
+POSTGRES_URL = postgresql://hasura:hasura@localhost:65002/hasura
+ENABLED_LOG_TYPES = startup,http-log,webhook-log,websocket-log,query-log,execution-log
 
+.PHONY: run-oss-server
+## run-oss-server: run an OSS server backend by a PostgreSQL database
+run-oss-server: $(CONSOLE_CE_ASSETS_PATH)
+	docker compose up --wait postgres
+	cabal run graphql-engine:exe:graphql-engine -- \
+		--database-url '$(POSTGRES_URL)' \
+		serve \
+		--enable-console --console-assets-dir '$(CONSOLE_CE_ASSETS_PATH)' \
+		--enabled-log-types '$(ENABLED_LOG_TYPES)'
 
 .PHONY: run-pro-server
-## run-pro-server: run a pro server backend by a postgres database
-run-pro-server:
-	cd frontend && npm install && npm run server-build:ee
-	docker compose up -d --wait postgres
-	cabal run graphql-engine-pro:exe:graphql-engine-pro serve -- \
+## run-pro-server: run a pro server backend by a PostgreSQL database
+run-pro-server: $(CONSOLE_EE_ASSETS_PATH)
+	docker compose up --wait postgres
+	cabal run graphql-engine-pro:exe:graphql-engine-pro -- \
+		--database-url '$(POSTGRES_URL)' \
+		serve \
 		--enable-console --console-assets-dir '$(CONSOLE_EE_ASSETS_PATH)' \
-		--enabled-log-types $(ENABLED_LOG_TYPES) \
-		--database-url '$(POSTGRES_URL)'
-
+		--enabled-log-types '$(ENABLED_LOG_TYPES)'
