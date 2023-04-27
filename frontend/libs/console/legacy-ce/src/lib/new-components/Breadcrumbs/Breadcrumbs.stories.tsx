20c61909a37f958acefd9a82b944fbaa4a3912b5
Matthew Goodwin <49927862+m4ttheweric@users.noreply.github.com>
2023-04-27 10:15:00 -0500

    console: add breadcrumbs component
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/8895
    GitOrigin-RevId: e111124adfa0df66fb255056dff05880d62e2d2f

---
+++ frontend/libs/console/legacy-ce/src/lib/new-components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -0,0 +1,68 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { expect } from '@storybook/jest';
+import { screen, within } from '@storybook/testing-library';
+import { action } from '@storybook/addon-actions';
+import { FaAddressBook, FaAirFreshener } from 'react-icons/fa';
+import { hasuraToast } from '../Toasts';
+import { BreadcrumbItem, Breadcrumbs } from './Breadcrumbs';
+
+export default {
+  title: 'components/Breadcrumbs 🧬',
+
+  decorators: [
+    Story => (
+      <div className="p-4 flex gap-5 items-center max-w-screen">{Story()}</div>
+    ),
+  ],
+} as ComponentMeta<typeof Breadcrumbs>;
+
+const autoByeToast = (message: string) => {
+  hasuraToast({ title: message, toastOptions: { duration: 1500 } });
+  action(message);
+};
+
+const breadcrumbs: BreadcrumbItem[] = [
+  {
+    title: 'clickable w/ icon',
+    icon: <FaAddressBook />,
+    onClick: () => autoByeToast('foo'),
+  },
+  {
+    title: 'clickable no icon',
+
+    onClick: () => autoByeToast('no icon'),
+  },
+  {
+    title: 'not clickable',
+  },
+  'string item',
+  {
+    title: 'last item',
+    icon: <FaAirFreshener />,
+  },
+];
+
+export const Basic: ComponentStory<typeof Breadcrumbs> = () => {
+  return (
+    <div>
+      <Breadcrumbs items={breadcrumbs} />
+      <div className="w-full h-full bg-slate-200 p-3">
+        Some other content to visualize spacing
+      </div>
+    </div>
+  );
+};
+
+Basic.storyName = '🧰 Basic Usage';
+Basic.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await expect(await screen.findByTestId('breadcrumbs')).toBeInTheDocument();
+  await expect(await canvas.getByText('clickable w/ icon')).toBeInTheDocument();
+  await expect(await canvas.getByText('clickable no icon')).toBeInTheDocument();
+  await expect(await canvas.getByText('not clickable')).toBeInTheDocument();
+  await expect(await canvas.getByText('string item')).toBeInTheDocument();
+  await expect(await canvas.getByText('last item')).toHaveClass(
+    'text-yellow-500'
+  );
+};
---
+++ frontend/libs/console/legacy-ce/src/lib/new-components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BreadcrumbItemView } from './BreadcrumbItemView';
+
+export type BreadcrumbItem =
+  | {
+      title: React.ReactText;
+      icon?: React.ReactElement;
+      onClick?: () => void;
+    }
+  | string;
+
+export type BreadcrumbsProps = {
+  items: BreadcrumbItem[];
+};
+
+export const Breadcrumbs = ({ items }: BreadcrumbsProps) => (
+  <div className="flex items-center gap-1" data-testid="breadcrumbs">
+    {items.map((item, i) => (
+      <BreadcrumbItemView item={item} isLastItem={i === items.length - 1} />
+    ))}
+  </div>
+);
