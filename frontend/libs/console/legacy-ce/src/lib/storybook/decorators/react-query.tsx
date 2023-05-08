54c6197ab75d3865d610002a246b4048d584f907
Matthew Goodwin <49927862+m4ttheweric@users.noreply.github.com>
2023-05-08 11:36:46 -0500

    console: fix stale react-query cache on story switching
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/9085
    GitOrigin-RevId: bf08d7ab68f1094ecfad1b5fb6e5a4625aa9e01f

--- frontend/libs/console/legacy-ce/src/lib/storybook/decorators/react-query.tsx
+++ frontend/libs/console/legacy-ce/src/lib/storybook/decorators/react-query.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
 import { DecoratorFn } from '@storybook/react';
+import { useEffect } from 'react';
 
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider, useQueryClient } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 export const ReactQueryDecorator = (): DecoratorFn => {
   const reactQueryClient = new QueryClient();
+
   return Story => (
     <QueryClientProvider client={reactQueryClient}>
+      <QueryInvalidator />
       <Story />
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
+};
+
+const QueryInvalidator = () => {
+  const client = useQueryClient();
+  useEffect(() => {
+    return () => {
+      client.invalidateQueries();
+    };
+  }, []);
+
+  return null;
 };
