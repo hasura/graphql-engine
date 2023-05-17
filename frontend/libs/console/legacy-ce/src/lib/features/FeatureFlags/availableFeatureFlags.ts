ee2258288eab16c3fa9644b6af2645f546c6abb4
Luca Restagno <59067245+lucarestagno@users.noreply.github.com>
2023-05-17 13:50:26 +0200

    Add feature flag to enable the new permissions UI for native sources
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/9167
    GitOrigin-RevId: 23a5ef0f6a0987312a85301f6f89af94422f009d

--- frontend/libs/console/legacy-ce/src/lib/features/FeatureFlags/availableFeatureFlags.ts
+++ frontend/libs/console/legacy-ce/src/lib/features/FeatureFlags/availableFeatureFlags.ts
@@ -6,6 +6,7 @@ const relationshipTabTablesId = 'f6c57c31-abd3-46d9-aae9-b97435793273';
 const importActionFromOpenApiId = '12e5aaf4-c794-4b8f-b762-5fda0bff946a';
 const enabledNewUIForBigQuery = 'e2d790ba-96fb-11ed-a8fc-0242ac120002';
 const connectDBRedesign = '532492b6-adca-11ed-afa1-0242ac120002';
+const permissionsNewUI = '5f7b1673-b2ef-4c98-89f7-f30cb64f0136';
 
 const importActionFromOpenApi: FeatureFlagDefinition = {
   id: importActionFromOpenApiId,
@@ -23,6 +24,7 @@ export const availableFeatureFlagIds = {
   importActionFromOpenApiId,
   enabledNewUIForBigQuery,
   connectDBRedesign,
+  permissionsNewUI,
 };
 
 export const availableFeatureFlags: FeatureFlagDefinition[] = [
@@ -52,6 +54,15 @@ export const availableFeatureFlags: FeatureFlagDefinition[] = [
     section: 'data',
     status: 'beta',
     defaultValue: true,
+    discussionUrl: '',
+  },
+  {
+    id: permissionsNewUI,
+    title: 'Enable the revamped UI for Permissions',
+    description: 'Try out the new UI experience for setting table permissions.',
+    section: 'data',
+    status: 'experimental',
+    defaultValue: false,
     discussionUrl: '',
   },
   // eslint-disable-next-line no-underscore-dangle
