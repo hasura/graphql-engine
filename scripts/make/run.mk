4b507040613274cb602895df22d9bec0d3ed1ff0
Samir Talwar <samir.talwar@hasura.io>
2023-05-22 15:52:13 +0200

    make: Set an admin secret for the Pro server.
    Otherwise it won't run at all with a license key.
    
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/9246
    GitOrigin-RevId: 2c80baac78d4e209dee15f86e93bc7f958e1d1e0

--- scripts/make/run.mk
+++ scripts/make/run.mk
@@ -1,3 +1,7 @@
+# This is the admin secret for the Pro server.
+# Override it with `make run-pro-server ADMIN_SECRET=my-override`.
+ADMIN_SECRET ?= top-secret
+
 # this connection should match postgres settings in `/docker-compose/databases.yaml`
 POSTGRES_URL = postgresql://hasura:hasura@localhost:65002/hasura
 ENABLED_LOG_TYPES = startup,http-log,webhook-log,websocket-log,query-log,execution-log
@@ -19,6 +23,7 @@ run-pro-server: $(CONSOLE_EE_ASSETS_PATH)
 	cabal run graphql-engine-pro:exe:graphql-engine-pro -- \
 		--database-url '$(POSTGRES_URL)' \
 		serve \
+		--admin-secret '$(ADMIN_SECRET)' \
 		--enable-console --console-assets-dir '$(CONSOLE_EE_ASSETS_PATH)' \
 		--enabled-log-types '$(ENABLED_LOG_TYPES)'
 
