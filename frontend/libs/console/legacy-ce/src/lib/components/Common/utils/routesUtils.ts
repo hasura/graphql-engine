800be3c915e53b3b7aa923e52a3e820abe36c1ea
Rishichandra Wawhal <rishi@hasura.io>
2023-06-01 20:24:42 +0530

    schema-registry: fix lux endpoint and misc fixes
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/9388
    GitOrigin-RevId: 883d0a59e5202c688e210eb624fd279f24ff1cfd

--- frontend/libs/console/legacy-ce/src/lib/components/Common/utils/routesUtils.ts
+++ frontend/libs/console/legacy-ce/src/lib/components/Common/utils/routesUtils.ts
@@ -161,7 +161,6 @@ export const eventsPrefix = 'events';
 export const scheduledEventsPrefix = 'cron';
 export const adhocEventsPrefix = 'one-off-scheduled-events';
 export const dataEventsPrefix = 'data';
-export const schemaRegsitryPrefix = 'sh';
 
 export const getSTRoute = (type: string | undefined, relativeRoute: string) => {
   if (type === 'relative') {
