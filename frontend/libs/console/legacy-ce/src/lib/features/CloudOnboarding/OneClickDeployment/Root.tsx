19556ce01554228863e0b7247a5366e9a9a5265b
Varun Dey <varundey20@gmail.com>
2023-06-06 16:42:39 +0530

    console: Refactor cloud console onboarding (cherry-pick v2.27)
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/9413
    GitOrigin-RevId: 7c7e4054969539fb594a009abab9213492ad0bdf

--- frontend/libs/console/legacy-ce/src/lib/features/CloudOnboarding/OneClickDeployment/Root.tsx
+++ frontend/libs/console/legacy-ce/src/lib/features/CloudOnboarding/OneClickDeployment/Root.tsx
@@ -3,7 +3,8 @@ import globals from '../../../Globals';
 import { FaGithub } from 'react-icons/fa';
 import { GraphiqlPopup, WorkflowProgress } from './components';
 import { stepperNavSteps } from './constants';
-import { DialogContainer } from '../OnboardingWizard';
+import { oneClickDeploymentOnboardingShown } from '../constants';
+import { DialogContainer } from '../components';
 import { useTriggerDeployment } from './hooks';
 import { GitRepoDetails, FallbackApp } from './types';
 import {
@@ -11,6 +12,7 @@ import {
   getGitRepoFullLinkFromDetails,
   getSampleQueriesUrl,
 } from './util';
+import { emitOnboardingEvent } from '../utils';
 
 /**
  * Parent container for the one click deployment wizard. Takes care of assembling and rendering all steps.
@@ -39,6 +41,7 @@ export function Root(props: {
   const transitionToQueryPopupSuccessState = () => {
     setGraphiQlPopupStatus('success');
     setState('graphiql-popup');
+    emitOnboardingEvent(oneClickDeploymentOnboardingShown);
   };
   const transitionToQueryPopupWithErrorState = () => {
     setGraphiQlPopupStatus('error');
