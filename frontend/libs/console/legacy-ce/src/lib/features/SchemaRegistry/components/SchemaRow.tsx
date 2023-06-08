db1b6cf424ab057ac8fb03e047064df37e2f4bd7
Rishichandra Wawhal <rishi@hasura.io>
2023-06-09 00:01:38 +0530

    frontend: Schema registry UI improvements
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/9479
    GitOrigin-RevId: 306df780cfb9cc39a335abd98706ae2fe4293700

--- frontend/libs/console/legacy-ce/src/lib/features/SchemaRegistry/components/SchemaRow.tsx
+++ frontend/libs/console/legacy-ce/src/lib/features/SchemaRegistry/components/SchemaRow.tsx
@@ -1,36 +1,45 @@
 import React from 'react';
 import { CountLabel } from './CountLabel';
-import { CapitalizeFirstLetter } from '../utils';
 import { SchemaChange } from '../types';
 import { FaChevronRight } from 'react-icons/fa';
 
 export const SchemaRow: React.VFC<{
   role: string;
-  changes: SchemaChange[];
+  changes?: SchemaChange[];
   onClick?: VoidFunction;
 }> = props => {
   const { role, changes, onClick } = props;
 
   const countBreakingChanges = changes?.filter(
     c => c.criticality.level === 'BREAKING'
-  ).length;
+  )?.length;
   const countDangerousChanges = changes?.filter(
     c => c.criticality.level === 'DANGEROUS'
-  ).length;
+  )?.length;
   const countSafeChanges = changes?.filter(
     c => c.criticality.level === 'NON_BREAKING'
-  ).length;
+  )?.length;
   return (
     <div className="w-full flex my-2">
       <div className="flex text-base w-[72%] justify-start">
         <span className="text-sm font-bold bg-gray-100 p-1 rounded">
-          {CapitalizeFirstLetter(role)}
+          {role}
         </span>
       </div>
       <div className="flex text-base w-[28%] justify-between">
-        <CountLabel count={countBreakingChanges} type="BREAKING" />
-        <CountLabel count={countDangerousChanges} type="DANGEROUS" />
-        <CountLabel count={countSafeChanges} type="NON_BREAKING" />
+        {changes ? (
+          <>
+            <CountLabel count={countBreakingChanges || 0} type="BREAKING" />
+            <CountLabel count={countDangerousChanges || 0} type="DANGEROUS" />
+            <CountLabel count={countSafeChanges || 0} type="NON_BREAKING" />
+          </>
+        ) : (
+          <>
+            <CountLabel count={countBreakingChanges} type="BREAKING" />
+            <CountLabel count={countDangerousChanges} type="DANGEROUS" />
+            <CountLabel count={countSafeChanges} type="NON_BREAKING" />
+          </>
+        )}
       </div>
       {onClick && (
         <div
