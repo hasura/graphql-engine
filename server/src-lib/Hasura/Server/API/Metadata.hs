20ffd73921fe72f4ceedfa8996e2a0295ddcc9b1
paritosh-08 <85472423+paritosh-08@users.noreply.github.com>
2023-06-26 23:49:06 +0530

    server: fix schema-registry inconsistent metadata bug
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/9670
    GitOrigin-RevId: 923b6111c44713956c3b6ee5499712e8b23effea

--- server/src-lib/Hasura/Server/API/Metadata.hs
+++ server/src-lib/Hasura/Server/API/Metadata.hs
@@ -176,7 +176,7 @@ runMetadataQuery appContext schemaCache closeWebsocketsOnMetadataChange RQLMetad
         Tracing.newSpan "runSchemaRegistryAction"
           $ for_ schemaRegistryAction
           $ \action -> do
-            liftIO $ action newResourceVersion
+            liftIO $ action newResourceVersion (scInconsistentObjs (lastBuiltSchemaCache modSchemaCache))
 
         -- notify schema cache sync
         Tracing.newSpan "notifySchemaCacheSync"
