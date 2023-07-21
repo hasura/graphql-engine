4c66d648c71161d7b5dfb3c2c22cbb8cf56cbe3b
Rikin Kachhia <54616969+rikinsk@users.noreply.github.com>
2023-07-21 18:20:50 +0530

    docs: add prettier
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/9923
    GitOrigin-RevId: 94038efe5768b63f38dfb447bc315ae5f7185c43

--- docs/src/theme/TOCInline/index.js
+++ docs/src/theme/TOCInline/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import styles from './styles.module.css';
 import TOCItems from '@theme/TOCItems';
 // In the event of re-swizzling, make sure to add the `filterTOC` and pass it down to TOCITems
-export default function TOCInline({toc, minHeadingLevel, maxHeadingLevel, filterTOC}) {
+export default function TOCInline({
+  toc,
+  minHeadingLevel,
+  maxHeadingLevel,
+  filterTOC,
+}) {
   return (
     <div className={styles.tableOfContentsInline}>
       <TOCItems
