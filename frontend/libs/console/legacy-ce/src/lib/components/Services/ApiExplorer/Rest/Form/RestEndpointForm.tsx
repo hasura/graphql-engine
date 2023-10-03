18452a18ef747a41a6e7c93d7c4262d75f816dba
Varun Choudhary <68095256+Varun-Choudhary@users.noreply.github.com>
2023-10-03 12:14:35 +0530

    console(hotfix): fix autofill name while editing rest endpoint
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/10350
    GitOrigin-RevId: fd8b9c73badfba89f07e71563db8203123a1e765

--- frontend/libs/console/legacy-ce/src/lib/components/Services/ApiExplorer/Rest/Form/RestEndpointForm.tsx
+++ frontend/libs/console/legacy-ce/src/lib/components/Services/ApiExplorer/Rest/Form/RestEndpointForm.tsx
@@ -122,7 +122,7 @@ export const RestEndpointForm: React.FC<RestEndpointFormProps> = ({
           if (parsedQuery?.definitions?.length > 0) {
             const operation = parsedQuery.definitions[0];
             if ('name' in operation) {
-              if (!userChangedName) {
+              if (!userChangedName && mode === 'create') {
                 setValue('name', operation.name?.value);
               }
               if (!userChangedUrl && mode === 'create') {
