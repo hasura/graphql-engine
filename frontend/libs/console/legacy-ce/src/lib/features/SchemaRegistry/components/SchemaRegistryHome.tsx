b66d04d581bf411f7bbdb6e66454071245c92f1d
Aaysha <109507451+aayshasura@users.noreply.github.com>
2023-10-17 15:46:09 +0530

    console: Schema Registry DX improvements(cherrypick v2.34)
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/10394
    Co-authored-by: ojas <31686586+OjasWadhwani@users.noreply.github.com>
    GitOrigin-RevId: 81c25fea3cd309097e5635efb2606aca249e5de9

--- frontend/libs/console/legacy-ce/src/lib/features/SchemaRegistry/components/SchemaRegistryHome.tsx
+++ frontend/libs/console/legacy-ce/src/lib/features/SchemaRegistry/components/SchemaRegistryHome.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Dispatch } from 'redux';
-import { useGetSchemaChangeList } from '../hooks/useGetSchemaChangeList';
+import { useGetSchemaRegistryList } from '../hooks/useGetSchemaRegistryList';
 import { Role, SchemaChangeCard, SchemaRegistryTag } from '../types';
 import globals from '../../../Globals';
 import {
@@ -29,32 +29,37 @@ import {
 import { IconTooltip } from '../../../new-components/Tooltip';
 import _push from '../../../components/Services/Data/push';
 import { useAppDispatch } from '../../../storeHooks';
+import { useGetURLSchema } from '../hooks/useGetURLSchema';
+
 interface SchemaRegistryHomeProps {
   schemaId: string | undefined;
+  v2Cursor: string;
+  v2Count: number;
 }
 export const SchemaRegistryHome: React.FC<SchemaRegistryHomeProps> = props => {
-  const { schemaId } = props;
+  const { schemaId, v2Cursor, v2Count } = props;
   const projectID = globals.hasuraCloudProjectId || '';
   const dispatch = useAppDispatch();
   const [pageNo, setPageno] = useState(
     parseInt(getSearchParam(window.location.search, 'page') || '0')
   );
   const schemaRoleID = schemaId ? schemaId : EMPTY_UUID_STRING;
-  const fetchSchemaResponse = useGetSchemaChangeList(
+
+  const fetchSchemasResponse = useGetSchemaRegistryList(
     projectID,
-    SCHEMA_LIST_FETCH_BATCH_SIZE,
-    pageNo * SCHEMA_LIST_FETCH_BATCH_SIZE,
-    schemaRoleID
+    pageNo,
+    v2Count,
+    v2Cursor
   );
 
-  const { kind } = fetchSchemaResponse;
+  const fetchURLSchemaResponse = useGetURLSchema(schemaRoleID);
 
-  //to check if fetchSchemaResponse changed
+  const { kind } = fetchSchemasResponse;
 
   let latestAdminRoleID: string | null = null;
   if (kind === 'success') {
     const schemaList = schemaChangeListTransformFn(
-      fetchSchemaResponse.response
+      fetchSchemasResponse.response
     );
     latestAdminRoleID =
       schemaList[0]?.roles?.find(item => item.role === ADMIN_ROLE)?.id || null;
@@ -67,40 +72,59 @@ export const SchemaRegistryHome: React.FC<SchemaRegistryHomeProps> = props => {
   const handlePrev = () => {
     setPageno(pageNo - 1);
   };
+
   //setting the selected role ID as admin role ID by default
   useEffect(() => {
     if (schemaRoleID === EMPTY_UUID_STRING) {
       if (latestAdminRoleID) {
         setSelectedRoleID(latestAdminRoleID);
-        dispatch(_push(`/settings/schema-registry/${latestAdminRoleID}`));
+        dispatch(_push(`/api/schema-registry/${latestAdminRoleID}`));
       }
     } else {
       setSelectedRoleID(schemaRoleID);
     }
   }, [schemaRoleID]);
+
   useEffect(() => {
     if (latestAdminRoleID) {
       setSelectedRoleID(latestAdminRoleID);
-      dispatch(_push(`/settings/schema-registry/${latestAdminRoleID}`));
+      dispatch(_push(`/api/schema-registry/${latestAdminRoleID}`));
     }
     setSearchParam(pageNo);
   }, [latestAdminRoleID, pageNo]);
+
   switch (kind) {
     case 'loading':
       return <p>Loading...</p>;
     case 'error':
-      return <p>Error: {fetchSchemaResponse.message}</p>;
+      return <p>Error: {fetchSchemasResponse.message}</p>;
     case 'success': {
+      let URLSchemaCard: SchemaChangeCard[] = [];
+      if (
+        fetchURLSchemaResponse.kind === 'success' &&
+        fetchURLSchemaResponse.response[0]
+      ) {
+        const URLAdminRoleID = URLSchemaCard[0]?.roles.find(
+          item => item.role === ADMIN_ROLE
+        )?.id;
+
+        if (URLAdminRoleID !== latestAdminRoleID) {
+          URLSchemaCard = schemaChangeListTransformFn(
+            fetchURLSchemaResponse.response
+          );
+        }
+      }
       const schemaList = schemaChangeListTransformFn(
-        fetchSchemaResponse.response
+        fetchSchemasResponse.response
       );
       return (
-        <div className="flex w-full">
-          <div className="w-1/4">
+        <div className="flex w-[80%] justify-center">
+          <div className="w-[20%]">
             <SchemaChangeList
               schemas={schemaList}
+              urlSchemaCard={URLSchemaCard}
               pageNumber={pageNo}
-              totalCount={fetchSchemaResponse.totalCount}
+              totalCount={fetchSchemasResponse.totalCount}
               selectedRoleID={selectedRoleID}
               dispatch={dispatch}
               handlePrev={handlePrev}
@@ -120,6 +144,7 @@ export const SchemaRegistryHome: React.FC<SchemaRegistryHomeProps> = props => {
 
 export const SchemaChangeList: React.VFC<{
   schemas: SchemaChangeCard[];
+  urlSchemaCard: SchemaChangeCard[];
   pageNumber: number;
   totalCount: number;
   selectedRoleID: string | null;
@@ -129,6 +154,7 @@ export const SchemaChangeList: React.VFC<{
 }> = props => {
   const {
     schemas,
+    urlSchemaCard,
     pageNumber,
     totalCount,
     selectedRoleID,
@@ -142,30 +168,37 @@ export const SchemaChangeList: React.VFC<{
     totalCount <= (pageNumber + 1) * SCHEMA_LIST_FETCH_BATCH_SIZE;
   const isSecondLastPage =
     totalCount <= (pageNumber + 2) * SCHEMA_LIST_FETCH_BATCH_SIZE;
+  const schemaList: SchemaChangeCard[] = [...urlSchemaCard, ...schemas];
   return (
     <div className="overflow-x-auto rounded-md border-neutral-200 bg-white border mr-sm">
       <div className="w-full flex bg-gray-100 px-4 py-2">
-        <div className="flex text-base w-[69%] justify-start">
+        <div className="flex text-base w-[50%] justify-start">
           <span className="text-sm font-bold">PUBLISHED SCHEMA</span>
         </div>
       </div>
+
       <div className="flex flex-col w-full">
         {schemas.length ? (
           <div className="mb-md">
-            {schemas.map((schema, index) => (
-              <SchemaCard
-                cardKey={index}
-                openSchemaCardIndex={openSchemaCardIndex}
-                selectedRoleID={selectedRoleID}
-                createdAt={schema.created_at}
-                roles={schema.roles}
-                entryHash={schema.entry_hash}
-                tags={schema.tags}
-                dispatch={dispatch}
-                handleClick={() => setIsOpenSchemaCardIndex(index)}
-              />
-            ))}
-            <div className="flex w-full justify-center items-center">
+            <div
+              style={{ maxHeight: '80vh' }}
+              className={`flex flex-col w-full max-h-400px overflow-y-scroll`}
+            >
+              {schemaList.map((schema, index) => (
+                <SchemaCard
+                  cardKey={index}
+                  openSchemaCardIndex={openSchemaCardIndex}
+                  selectedRoleID={selectedRoleID}
+                  createdAt={schema.created_at}
+                  roles={schema.roles}
+                  entryHash={schema.entry_hash}
+                  tags={schema.tags}
+                  dispatch={dispatch}
+                  handleClick={() => setIsOpenSchemaCardIndex(index)}
+                />
+              ))}
+            </div>
+            <div className="flex w-full justify-center items-center mt-2">
               <button
                 className="btn btn-primary items-center max-w-full justify-center inline-flex text-sm font-sans font-semibold border rounded shadow-sm focus-visible:outline-none h-btn px-sm mx-1 disabled:border-gray-300  disabled:opacity-60 disabled:cursor-not-allowed"
                 disabled={pageNumber === 0}
@@ -254,7 +287,8 @@ const SchemaCard: React.VFC<{
     handleClick,
   } = props;
   const [isTagModalOpen, setIsTagModalOpen] = React.useState(false);
-  const [isRolesMenuOpen, setIsRolesMenuOpen] = useState(false);
+  const [isCurrentCardOpen, setIsCurrentCardOpen] = useState(false);
+
   const [tagsList, setTagsList] = React.useState<SchemaRegistryTag[]>(tags);
   const onRemoveTag = (id: string) => {
     const filteredTags = tagsList.filter(
@@ -262,92 +296,128 @@ const SchemaCard: React.VFC<{
     );
     setTagsList(filteredTags);
   };
+  const adminIndex = roles.findIndex(role => role.role === 'admin');
+
+  // If "admin" is not at the 0 index, move it there
+  if (adminIndex !== 0 && adminIndex !== -1) {
+    const adminRole = roles.splice(adminIndex, 1)[0];
+    roles.unshift(adminRole);
+  }
+
   useEffect(() => {
     setTagsList(tags);
   }, [tags]);
   React.useEffect(() => {
-    setIsRolesMenuOpen(cardKey === openSchemaCardIndex);
+    setIsCurrentCardOpen(cardKey === openSchemaCardIndex);
   }, [cardKey, openSchemaCardIndex]);
   const handleRoleClick = (roleBasedChange: Role) => {
-    dispatch(_push(`/settings/schema-registry/${roleBasedChange.id}`));
+    console.log('click');
+    dispatch(_push(`/api/schema-registry/${roleBasedChange.id}`));
   };
+
+  const defaultShowAllRoles = isCurrentCardOpen || roles.length <= 3;
+  const RolesList = defaultShowAllRoles ? roles : roles.slice(0, 3);
   return (
     <div
-      className="w-full flex flex-col px-4 bg-white rounded"
-      onClick={handleClick}
+      className={`w-full flex flex-col px-4 bg-white rounded mb-1`}
+      onClick={() => {
+        if (!roles.some(role => role.id === selectedRoleID)) {
+          dispatch(_push(`/api/schema-registry/${roles[0].id}`));
+        }
+        handleClick();
+      }}
     >
-      <div className="flex flex-col mt-2 justify-between h-full">
-        <div className="flex text-gray-600 text-sm justify-start">
+      <div
+        className={`mt-2 ${
+          isCurrentCardOpen ? 'bg-gray-100' : ' bg-white'
+        } hover:bg-gray-100 `}
+      >
+        <div className="flex mt-1 mb-2 text-gray-600 text-sm items-center justify-start h-full px-2">
           <span>{getPublishTime(createdAt)}</span>
+          <span>
+            <Analytics name="schema-registry-add-tag-btn">
+              {isTagModalOpen && (
+                <AddSchemaRegistryTagDialog
+                  tagsList={tagsList}
+                  setTagsList={setTagsList}
+                  entryHash={entryHash}
+                  onClose={() => {
+                    setIsTagModalOpen(false);
+                  }}
+                />
+              )}
+              <div className="flex items-center">
+                <div className="flex flex-nowrap items-center justify-start ">
+                  {tagsList &&
+                    tagsList.map(schemaRegistryTag => (
+                      <div className="mx-1 ">
+                        <SchemaTag
+                          schemaRegistryTag={schemaRegistryTag}
+                          onRemove={onRemoveTag}
+                        />
+                      </div>
+                    ))}
+                  <div className="ml-1" onClick={() => setIsTagModalOpen(true)}>
+                    <span>
+                      <IconTooltip
+                        message="Add a Tag"
+                        icon={<FaPlusCircle />}
+                      />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </Analytics>
+          </span>
         </div>
         <div
-          className={`flex font-semibold  text-md justify-end hover:text-gray-600  ${
-            isRolesMenuOpen ? 'text-gray-600' : 'text-gray-400'
-          } cursor-pointer`}
+          className={`flex flex-col justify-start w-full pt-auto ${
+            isCurrentCardOpen ? 'bg-gray-100' : ''
+          } rounded `}
         >
-          {roles.length} {roles.length === 1 ? 'Role' : 'Roles'}
-          <span className="mr-1">{<FaChevronDown />}</span>
-        </div>
-      </div>
-      {isRolesMenuOpen && (
-        <div className="w-full pt-auto">
-          <div className="text-sm text-gray-500 font-bold mb-3">
-            {roles.length === 1 ? 'ROLE' : 'ROLES'}
+          <div className="text-sm text-gray-500 font-bold mb-1 px-2">
+            {roles.length} {roles.length === 1 ? 'ROLE' : 'ROLES'}
           </div>
-          {roles.map((roleBasedChange, index) => (
-            <div
-              className={`flex w-full p-2 ${
-                roleBasedChange.id === selectedRoleID ? 'bg-gray-100' : ''
-              } rounded hover:bg-gray-200`}
-              onClick={() => {
-                handleRoleClick(roleBasedChange);
-              }}
-              key={index}
-            >
-              <div className="flex items-center justify-between w-full rounded">
-                <div className="text-base rounded cursor-pointer">
-                  <p className="text-sm text-teal-800 font-bold bg-gray-200 px-1 rounded">
-                    {CapitalizeFirstLetter(roleBasedChange.role)}
-                  </p>
+          <div>
+            {RolesList.map((roleBasedChange, index) => (
+              <div
+                className={`flex w-full px-2 py-1 ${
+                  isCurrentCardOpen && roleBasedChange.id === selectedRoleID
+                    ? 'bg-gray-200'
+                    : ''
+                } rounded hover:bg-gray-200`}
+                onClick={() => {
+                  handleRoleClick(roleBasedChange);
+                }}
+                key={index}
+              >
+                <div className="flex items-center justify-between w-full rounded">
+                  <div className="text-base rounded cursor-pointer">
+                    <p className="text-sm text-teal-800 font-bold bg-gray-200 px-1 rounded">
+                      {CapitalizeFirstLetter(roleBasedChange.role)}
+                    </p>
+                  </div>
+                  <FaChevronRight />
                 </div>
-                <FaChevronRight />
               </div>
-            </div>
-          ))}
-        </div>
-      )}
-      {isTagModalOpen && (
-        <AddSchemaRegistryTagDialog
-          tagsList={tagsList}
-          setTagsList={setTagsList}
-          entryHash={entryHash}
-          onClose={() => {
-            setIsTagModalOpen(false);
-          }}
-        />
-      )}
-      <div className="flex flex-nowrap items-center justify-start mt-2">
-        {tagsList &&
-          tagsList.map(schemaRegistryTag => (
-            <div className="mr-2 ">
-              <SchemaTag
-                schemaRegistryTag={schemaRegistryTag}
-                onRemove={onRemoveTag}
-              />
-            </div>
-          ))}
-        <Analytics name="schema-registry-add-tag-btn">
-          <div
-            className="mt-[7px] ml-[-6px] pb-2"
-            onClick={() => setIsTagModalOpen(true)}
-          >
-            <span>
-              <IconTooltip message="Add a Tag" icon={<FaPlusCircle />} />
-            </span>
+            ))}
+            {!defaultShowAllRoles && (
+              <Analytics name="schema-registry-see-more-roles-btn">
+                <div
+                  className="flex justify-center items-center cursor-pointer text-base hover:bg-gray-200 pt-1"
+                  //here there is no need for onClick as clicking anywhere on a schemaCard sets the schemaCard as open and defaultShowAllRoles will be set to true
+                >
+                  <span className="text-gray font-semibold">
+                    See More Roles
+                  </span>
+                  <FaChevronDown />
+                </div>
+              </Analytics>
+            )}
           </div>
-        </Analytics>
+        </div>
       </div>
-      <div className="flex w-full border-b border-gray-300 my-2"></div>
+      <div className="flex w-full border-b border-gray-300 my-1"></div>
     </div>
   );
 };
