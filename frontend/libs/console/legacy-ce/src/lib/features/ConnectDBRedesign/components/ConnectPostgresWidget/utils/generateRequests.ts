3a49e7ae1a974b4e2588da6151fbcd52e7a8e1cc
Vijay Prasanna <vijayprasanna2896@gmail.com>
2023-12-02 01:37:33 +0530

    console: expose `max_connections` field on UI while connecting postgres
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/10524
    GitOrigin-RevId: c50b27b8dd83399b3da6048785eb698e0cf5c4bd

--- frontend/libs/console/legacy-ce/src/lib/features/ConnectDBRedesign/components/ConnectPostgresWidget/utils/generateRequests.ts
+++ frontend/libs/console/legacy-ce/src/lib/features/ConnectDBRedesign/components/ConnectPostgresWidget/utils/generateRequests.ts
@@ -24,6 +24,7 @@ export const generateConnectionInfo = (
         },
   pool_settings: {
     total_max_connections: values.poolSettings?.totalMaxConnections,
+    max_connections: values.poolSettings?.maxConnections,
     idle_timeout: values.poolSettings?.idleTimeout,
     retries: values.poolSettings?.retries,
     pool_timeout: values.poolSettings?.poolTimeout,
