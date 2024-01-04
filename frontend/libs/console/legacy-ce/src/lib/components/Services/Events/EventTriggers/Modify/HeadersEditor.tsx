9f1bed38336e45c8555989110dd02515b68fe04c
Ullas <ullasgowda42@gmail.com>
2024-01-04 23:35:22 +0530

    fix: add appropriate padding for table-data with respect to table-header for existng headers tables

--- frontend/libs/console/legacy-ce/src/lib/components/Services/Events/EventTriggers/Modify/HeadersEditor.tsx
+++ frontend/libs/console/legacy-ce/src/lib/components/Services/Events/EventTriggers/Modify/HeadersEditor.tsx
@@ -47,13 +47,13 @@ const HeadersEditor = (props: HeaderEditorProps) => {
                 .filter(h => !!h.name)
                 .map(header => (
                   <tr className="">
-                    <td className="px-3 py-3 whitespace-nowrap font-medium">
+                    <td className="px-md py-sm whitespace-nowrap font-medium">
                       {header.name}
                     </td>
-                    <td className="px-3 py-3 whitespace-nowrap text-gray-600">
+                    <td className="px-md py-sm whitespace-nowrap text-gray-600">
                       {header.type}
                     </td>
-                    <td className="px-3 py-3 whitespace-nowrap text-gray-600">
+                    <td className="px-md py-sm whitespace-nowrap text-gray-600">
                       {header.value}
                     </td>
                   </tr>
