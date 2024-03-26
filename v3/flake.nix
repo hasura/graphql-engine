50ff30ae5be3677ea891774de186a2e3421e6e8c
Samir Talwar <samir.talwar@hasura.io>
2024-03-26 16:37:50 +0100

    Nix: Switch to nixpkgs-unstable, rather than nixos-unstable. (#407)
    If we're going to be unstable, let's go all the way.
    
    This change was motivated by needing to upgrade cargo-machete to make it
    work with Rust v1.77, and not having the upgrade in nixos-unstable.
    
    V3_GIT_ORIGIN_REV_ID: 8191a376a6aeebc08787f973d58d373f8ab9ad0d

--- v3/flake.nix
+++ v3/flake.nix
@@ -2,7 +2,7 @@
   description = "DDN Engine";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = github:NixOS/nixpkgs;
     flake-utils.url = github:numtide/flake-utils;
 
     crane = {
