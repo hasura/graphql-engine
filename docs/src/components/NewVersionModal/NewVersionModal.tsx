199e69cb16f4d6a571ba8cc8a148d80d580b7a3d
Sean Park-Ross <94021366+seanparkross@users.noreply.github.com>
2024-08-19 15:22:55 +0100

    Docs: New version modal modularise CSS
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/11004
    GitOrigin-RevId: 2b28dcbe295765e228caec4e573886fb051cb0da

--- docs/src/components/NewVersionModal/NewVersionModal.tsx
+++ docs/src/components/NewVersionModal/NewVersionModal.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import './NewVersionModal.css';
+import styles from './NewVersionModal.module.css';
 import { useLocalStorage } from 'usehooks-ts';
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+// @ts-ignore
 import monsta from './monsta.png';
 
 export const NewVersionModal = () => {
   const {
-    siteConfig: { customFields },
+    siteConfig: {customFields},
   } = useDocusaurusContext();
 
   const [lastClosed, setLastClosed] = useLocalStorage<number | null>(
@@ -19,7 +20,7 @@ export const NewVersionModal = () => {
   useEffect(() => {
     const currentTime = Date.now();
     const oneWeek = 7 * 24 * 60 * 60 * 1000; // One week in milliseconds
-    // const oneWeek = 60 * 1000; // Temp one minute in milliseconds (for testing)
+    // const oneWeek = 10 * 1000; // Temp 10 seconds in milliseconds (for testing)
 
     if (!lastClosed || (currentTime - lastClosed > oneWeek)) {
       setShowModal(true);
@@ -31,23 +32,23 @@ export const NewVersionModal = () => {
     setShowModal(false);
   };
 
-  const showHideClassName = showModal ? "new-version-modal display-block" : "new-new-version-modal display-none";
-
   return (
-    <div className={showHideClassName} onClick={handleClose}>
-      <div className="modal-main" onClick={e => e.stopPropagation()}>
-        <div className="close-icon" onClick={handleClose}>
+    <div className={showModal ? styles.newVersionModal : styles.displayNone} onClick={handleClose}>
+      <div className={styles.modalMain} onClick={e => e.stopPropagation()}>
+        <div className={styles.closeIcon} onClick={handleClose}>
           <CloseButton/>
         </div>
-        <img className="monsta-image" src={monsta} alt="Hasura Logo"/>
-        <p className="above-headline">INTRODUCING</p>
-        <h1 className="headline">Hasura Data Delivery Network</h1>
-        <h5 className="subhead">The next-generation Hasura platform is here!</h5>
-        <p className="body-text">Powerful v3 engine. Reimagined console. Multi-team federation. Simplified custom logic
-          integration. Enhanced developer experience. And so much more to love!</p>
-        <div className="cta-thing"><a target="_blank" rel="noopener noreferrer" href="https://hasura.io/docs/3.0/index/">Check
-          out Hasura DDN
-          docs</a></div>
+        <img className={styles.monstaImage} src={monsta} alt="Hasura Logo"/>
+        <p className={styles.aboveHeadline}>INTRODUCING</p>
+        <h1 className={styles.headline}>Hasura Data Delivery Network</h1>
+        <h5 className={styles.subhead}>The next-generation Hasura platform is here!</h5>
+        <p className={styles.bodyText}>
+          Powerful v3 engine. Reimagined console. Multi-team federation. Simplified custom logic integration. Enhanced developer experience. And so much more to love!</p>
+        <div className={styles.ctaThing}>
+          <a target="_blank" rel="noopener noreferrer" href="https://hasura.io/docs/3.0/index/">
+            Check out Hasura DDN docs
+          </a>
+        </div>
       </div>
     </div>
   );
@@ -57,8 +58,8 @@ const CloseButton = () => {
   return (
     <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect width="32" height="32" rx="16" fill="#F3F4F6"></rect>
-      <path d="M21.714 10.286 10.285 21.714m0-11.428 11.429 11.428" stroke="#1F2A37" stroke-width="2"
-            stroke-linecap="round" stroke-linejoin="round"></path>
+      <path d="M21.714 10.286 10.285 21.714m0-11.428 11.429 11.428" stroke="#1F2A37" strokeWidth="2"
+            strokeLinecap="round" strokeLinejoin="round"></path>
     </svg>
   )
 }
