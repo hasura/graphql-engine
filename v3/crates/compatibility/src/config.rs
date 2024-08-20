a9d9055fad3b33367067f30e422dada7fe266ad0
Daniel Harvey <danieljamesharvey@gmail.com>
2024-08-20 14:45:00 +0100

    More compatibility validation types (#996)
    <!-- The PR description should answer 2 important questions: -->
    
    ### What
    
    Add more types for validating compatibility configuration. Functional
    no-op.
    
    V3_GIT_ORIGIN_REV_ID: 56239149762bc170ce40a7f5c526cb8f3f49ec61

--- v3/crates/compatibility/src/config.rs
+++ v3/crates/compatibility/src/config.rs
@@ -1,4 +1,5 @@
 use super::compatibility_date::CompatibilityDate;
+use chrono::NaiveDate;
 
 #[derive(Clone, Debug, PartialEq, serde::Serialize, opendds_derive::OpenDd)]
 #[serde(rename_all = "camelCase")]
@@ -20,4 +21,27 @@ pub struct CompatibilityConfigV2 {
     /// Any backwards incompatible changes made to Hasura DDN after this date won't impact the metadata.
     pub date: CompatibilityDate,
     // TODO: add flags.
+}
+
+pub const fn new_compatibility_date(year: i32, month: u32, day: u32) -> CompatibilityDate {
+    CompatibilityDate(match NaiveDate::from_ymd_opt(year, month, day) {
+        // Need to match instead of unwrap because unwrap is still unstable as const
+        Some(date) => date,
+        None => panic!("Invalid date"),
+    })
+}
+
+#[derive(Debug, PartialEq, thiserror::Error)]
+pub enum CompatibilityError {
+    #[error("no compatibility config found")]
+    NoCompatibilityConfigFound,
+    #[error("duplicate compatibility config found")]
+    DuplicateCompatibilityConfig,
+    #[error("compatibility date {specified} is too old, oldest supported date is {oldest}")]
+    DateTooOld {
+        specified: CompatibilityDate,
+        oldest: CompatibilityDate,
+    },
+    #[error("compatibility date {0} is in the future")]
+    DateInTheFuture(CompatibilityDate),
 }
--- v3/crates/compatibility/src/lib.rs
+++ v3/crates/compatibility/src/lib.rs
@@ -2,4 +2,6 @@ mod compatibility_date;
 pub use compatibility_date::CompatibilityDate;
 
 mod config;
-pub use config::{CompatibilityConfigV1, CompatibilityConfigV2};
+pub use config::{
+    new_compatibility_date, CompatibilityConfigV1, CompatibilityConfigV2, CompatibilityError,
+};
