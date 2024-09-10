a275966b7b4eaf52e28d20531edc57aaecfe976d
Daniel Harvey <danieljamesharvey@gmail.com>
2024-09-10 15:56:29 +0100

    Basic test framework for jsonapi (#1091)
    <!-- The PR description should answer 2 important questions: -->
    
    ### What
    
    Let's add a test!
    
    (and check it still works 👍 )
    
    <img width="405" alt="Screenshot 2024-09-10 at 13 05 01"
    src="https://github.com/user-attachments/assets/6a49feaa-bd0b-4137-a9d5-a1e1336d9fa6">
    
    ### How
    
    Glob a folder for test files, parse them and run them against the
    functions.
    
    V3_GIT_ORIGIN_REV_ID: cc3e0d8cfb6f5eaa58cf72dab97e7220a57e7673

--- v3/crates/engine/bin/engine/json_api.rs
+++ v3/crates/engine/bin/engine/json_api.rs
@@ -1,45 +1,15 @@
 use axum::{
-    extract::Query,
-    http::{HeaderMap, Method, Request, Uri},
-    middleware::Next,
+    http::{HeaderMap, Method, Uri},
     response::IntoResponse,
     routing::get,
     Extension, Json, Router,
 };
-use execute::{ExposeInternalErrors, HttpContext, ProjectId};
 use hasura_authn_core::Session;
-use indexmap::IndexMap;
-use serde::Serialize;
-use std::{collections::HashMap, fmt::Display, sync::Arc};
+use std::sync::Arc;
 use tower_http::trace::TraceLayer;
-use tracing_util::{
-    set_status_on_current_span, ErrorVisibility, SpanVisibility, Traceable, TraceableError,
-    TraceableHttpResponse,
-};
+use tracing_util::{set_status_on_current_span, SpanVisibility};
 
 use crate::{authentication_middleware, EngineState};
-
-#[derive(Debug)]
-pub struct State {
-    pub routes: Vec<String>,
-}
-
-impl State {
-    pub fn new(metadata: &metadata_resolve::Metadata) -> Self {
-        let routes = metadata
-            .models
-            .iter()
-            .map(|(model_name, _)| format!("/{}/{}", model_name.subgraph, model_name.name))
-            .collect::<Vec<String>>();
-        Self { routes }
-    }
-}
-
-#[derive(Debug, derive_more::Display)]
-pub enum RequestError {
-    NotFound,
-    BadRequest(String),
-}
 
 pub(crate) fn create_json_api_router(state: Arc<EngineState>) -> axum::Router {
     let router = Router::new()
@@ -54,7 +24,9 @@ pub(crate) fn create_json_api_router(state: Arc<EngineState>) -> axum::Router {
             state.clone(),
             authentication_middleware,
         ))
-        .layer(axum::middleware::from_fn(rest_request_tracing_middleware))
+        .layer(axum::middleware::from_fn(
+            jsonapi::rest_request_tracing_middleware,
+        ))
         // *PLEASE DO NOT ADD ANY MIDDLEWARE
         // BEFORE THE `explain_request_tracing_middleware`*
         // Refer to it for more details.
@@ -64,12 +36,12 @@ pub(crate) fn create_json_api_router(state: Arc<EngineState>) -> axum::Router {
 }
 
 async fn handle_request(
-    headers: HeaderMap,
+    _headers: HeaderMap,
     method: Method,
     uri: Uri,
-    Query(query_string): Query<HashMap<String, String>>,
+    axum::extract::RawQuery(raw_query): axum::extract::RawQuery,
     axum::extract::State(state): axum::extract::State<Arc<EngineState>>,
-    Extension(session): Extension<Session>,
+    Extension(_session): Extension<Session>,
 ) -> impl IntoResponse {
     let tracer = tracing_util::global_tracer();
     let response = tracer
@@ -79,16 +51,11 @@ async fn handle_request(
             SpanVisibility::User,
             || {
                 {
-                    Box::pin(handler_internal(
-                        state.expose_internal_errors,
-                        &state.http_context,
+                    Box::pin(jsonapi::handler_internal(
                         &state.jsonapi_state,
-                        &session,
-                        &headers,
                         method,
                         uri,
-                        query_string,
-                        None,
+                        jsonapi_library::query::Query::from_params(&raw_query.unwrap_or_default()),
                     ))
                 }
             },
@@ -99,132 +66,16 @@ async fn handle_request(
     match response {
         Ok(r) => (axum::http::StatusCode::OK, Json(r)).into_response(),
         Err(e) => match e {
-            RequestError::BadRequest(err) => (
+            jsonapi::RequestError::BadRequest(err) => (
                 axum::http::StatusCode::BAD_REQUEST,
                 Json(serde_json::json!({"error": err})),
             )
                 .into_response(),
-            RequestError::NotFound => (
+            jsonapi::RequestError::NotFound => (
                 axum::http::StatusCode::NOT_FOUND,
                 Json(serde_json::json!({"error": "invalid route or path"})),
             )
                 .into_response(),
         },
     }
-}
-
-#[allow(clippy::unused_async)]
-async fn handler_internal(
-    _expose_internal_errors: ExposeInternalErrors,
-    _http_context: &HttpContext,
-    state: &State,
-    _session: &Session,
-    _request_headers: &HeaderMap,
-    http_method: Method,
-    uri: Uri,
-    query_string: HashMap<String, String>,
-    _project_id: Option<&ProjectId>,
-) -> Result<JsonApiDocument, RequestError> {
-    // route matching/validation
-    if !is_valid_route(state, &uri) {
-        return Err(RequestError::NotFound);
-    }
-    // create the query IR
-    let query_ir = create_query_ir(&http_method, &uri, &query_string)?;
-    // execute the query with the query-engine
-    let result = query_engine_execute(&query_ir, state)?;
-    // process result to JSON:API compliant response
-    Ok(process_result(&result))
-}
-
-fn is_valid_route(state: &State, uri: &Uri) -> bool {
-    // TODO: to_string() maybe not optimal. Optimize later
-    let uri_s = uri.to_string();
-    for route in &state.routes {
-        if uri_s.starts_with(route) {
-            return true;
-        }
-    }
-    false
-}
-
-#[derive(Serialize, Debug)]
-struct JsonApiDocument {
-    name: String,
-    age: i32,
-}
-
-impl Display for JsonApiDocument {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Hello error")
-    }
-}
-
-/// Implement traceable for GraphQL Response
-impl Traceable for JsonApiDocument {
-    type ErrorType<'a> = RequestError;
-
-    fn get_error(&self) -> Option<RequestError> {
-        None
-    }
-}
-
-impl TraceableError for RequestError {
-    fn visibility(&self) -> ErrorVisibility {
-        ErrorVisibility::User
-    }
-}
-
-fn process_result(_result: &QueryResult) -> JsonApiDocument {
-    JsonApiDocument {
-        age: 1,
-        name: "foo".to_string(),
-    }
-}
-
-fn query_engine_execute(
-    _query_ir: &open_dds::query::QueryRequest,
-    _metadata: &State,
-) -> Result<QueryResult, RequestError> {
-    Err(RequestError::BadRequest("Not implemented".to_string()))
-}
-
-#[allow(clippy::unnecessary_wraps)]
-fn create_query_ir(
-    _http_method: &Method,
-    _uri: &Uri,
-    _query_string: &HashMap<String, String>,
-) -> Result<open_dds::query::QueryRequest, RequestError> {
-    let queries = IndexMap::new();
-    Ok(open_dds::query::QueryRequest::V1(
-        open_dds::query::QueryRequestV1 { queries },
-    ))
-}
-
-struct QueryResult;
-
-/// Middleware to start tracing of the `/v1/jsonapi` request. This middleware
-/// must be active for the entire duration of the request i.e. this middleware
-/// should be the entry point and the exit point of the JSON:API request.
-async fn rest_request_tracing_middleware<B: Send>(
-    request: Request<B>,
-    next: Next<B>,
-) -> axum::response::Response {
-    let tracer = tracing_util::global_tracer();
-    let path = "/v1/jsonapi";
-    tracer
-        .in_span_async_with_parent_context(
-            path,
-            path,
-            SpanVisibility::User,
-            &request.headers().clone(),
-            || {
-                Box::pin(async move {
-                    let response = next.run(request).await;
-                    TraceableHttpResponse::new(response, path)
-                })
-            },
-        )
-        .await
-        .response
 }
--- v3/crates/jsonapi/src/lib.rs
+++ v3/crates/jsonapi/src/lib.rs
@@ -1,1 +1,143 @@
+use axum::{
+    http::{Method, Request, Uri},
+    middleware::Next,
+};
+use indexmap::IndexMap;
+use serde::Serialize;
+use std::fmt::Display;
+use tracing_util::{
+    ErrorVisibility, SpanVisibility, Traceable, TraceableError, TraceableHttpResponse,
+};
 
+#[derive(Debug)]
+pub struct State {
+    pub routes: Vec<String>,
+}
+
+impl State {
+    pub fn new(metadata: &metadata_resolve::Metadata) -> Self {
+        let routes = metadata
+            .models
+            .iter()
+            .map(|(model_name, _)| format!("/{}/{}", model_name.subgraph, model_name.name))
+            .collect::<Vec<String>>();
+        Self { routes }
+    }
+}
+
+#[derive(Debug, derive_more::Display)]
+pub enum RequestError {
+    NotFound,
+    BadRequest(String),
+}
+
+#[allow(clippy::unused_async)]
+pub async fn handler_internal(
+    state: &State,
+    http_method: Method,
+    uri: Uri,
+    query_string: jsonapi_library::query::Query,
+) -> Result<JsonApiDocument, RequestError> {
+    // route matching/validation
+    if !is_valid_route(state, &uri) {
+        return Err(RequestError::NotFound);
+    }
+    // create the query IR
+    let query_ir = create_query_ir(&http_method, &uri, &query_string)?;
+    // execute the query with the query-engine
+    let result = query_engine_execute(&query_ir, state)?;
+    // process result to JSON:API compliant response
+    Ok(process_result(&result))
+}
+
+fn is_valid_route(state: &State, uri: &Uri) -> bool {
+    // TODO: to_string() maybe not optimal. Optimize later
+    let uri_s = uri.to_string();
+    for route in &state.routes {
+        if uri_s.starts_with(route) {
+            return true;
+        }
+    }
+    false
+}
+
+#[derive(Serialize, Debug)]
+pub struct JsonApiDocument {
+    name: String,
+    age: i32,
+}
+
+impl Display for JsonApiDocument {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Hello error")
+    }
+}
+
+/// Implement traceable for GraphQL Response
+impl Traceable for JsonApiDocument {
+    type ErrorType<'a> = RequestError;
+
+    fn get_error(&self) -> Option<RequestError> {
+        None
+    }
+}
+
+impl TraceableError for RequestError {
+    fn visibility(&self) -> ErrorVisibility {
+        ErrorVisibility::User
+    }
+}
+
+fn process_result(_result: &QueryResult) -> JsonApiDocument {
+    JsonApiDocument {
+        age: 1,
+        name: "foo".to_string(),
+    }
+}
+
+fn query_engine_execute(
+    _query_ir: &open_dds::query::QueryRequest,
+    _metadata: &State,
+) -> Result<QueryResult, RequestError> {
+    Err(RequestError::BadRequest("Not implemented".to_string()))
+}
+
+#[allow(clippy::unnecessary_wraps)]
+fn create_query_ir(
+    _http_method: &Method,
+    _uri: &Uri,
+    _query_string: &jsonapi_library::query::Query,
+) -> Result<open_dds::query::QueryRequest, RequestError> {
+    let queries = IndexMap::new();
+    Ok(open_dds::query::QueryRequest::V1(
+        open_dds::query::QueryRequestV1 { queries },
+    ))
+}
+
+struct QueryResult;
+
+/// Middleware to start tracing of the `/v1/jsonapi` request. This middleware
+/// must be active for the entire duration of the request i.e. this middleware
+/// should be the entry point and the exit point of the JSON:API request.
+pub async fn rest_request_tracing_middleware<B: Send>(
+    request: Request<B>,
+    next: Next<B>,
+) -> axum::response::Response {
+    let tracer = tracing_util::global_tracer();
+    let path = "/v1/jsonapi";
+    tracer
+        .in_span_async_with_parent_context(
+            path,
+            path,
+            SpanVisibility::User,
+            &request.headers().clone(),
+            || {
+                Box::pin(async move {
+                    let response = next.run(request).await;
+                    TraceableHttpResponse::new(response, path)
+                })
+            },
+        )
+        .await
+        .response
+}
