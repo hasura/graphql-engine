hasura/graphql-engine

commit c6e6ed80266dec72a29e7615e4e597a3bfc1cac2

Wrap metadata-resolve errors with path container (#1170)
<!-- The PR description should answer 2 important questions: -->

### What

Another incremental step towards metadata-resolve paths. This PR wraps
the current error type with `ErrorWithContext`, a type that can
optionally contain a path. The PR should be a functional no-op as we
still ignore these paths when displaying the error; the next PR will
surface them in the error responses.

### How

We wrap the type and then do a _lot_ of hand-holding to get Rust
inference to work.

V3_GIT_ORIGIN_REV_ID: c1384cb1e8700c9b7e3aa643b7d0fbd4c8471098

v3/crates/metadata-resolve/src/stages/mod.rs
@@ -27,13 +27,13 @@ use open_dds::flags;
 pub use types::Metadata;
 
 use crate::types::configuration::Configuration;
-use crate::types::error::{Error, SeparatedBy, ShouldBeAnError};
+use crate::types::error::{Error, ErrorWithContext, SeparatedBy, ShouldBeAnError};
 
 /// This is where we take the input metadata and attempt to resolve a working `Metadata` object.
 pub fn resolve(
     metadata: open_dds::Metadata,
     configuration: &Configuration,
-) -> Result<(Metadata, Vec<Warning>), Error> {
+) -> Result<(Metadata, Vec<Warning>), ErrorWithContext> {
     // all issues raised throughout metadata-resolve. These will be turned into `warnings` or
     // `errors` at the end of this function, depending on OpenDDS flags.
     let mut all_issues = vec![];
@@ -44,13 +44,14 @@ pub fn resolve(
     // The graphql config represents the shape of the Hasura features in the graphql schema,
     // and which features should be enabled or disabled. We check this structure is valid.
     let graphql_config =
-        graphql_config::resolve(&metadata_accessor.graphql_config, metadata_accessor.flags)?;
+        graphql_config::resolve(&metadata_accessor.graphql_config, metadata_accessor.flags)
+            .map_err(Error::from)?;
 
     // Fetch and check schema information for all our data connectors
     let data_connectors::DataConnectorsOutput {
         data_connectors,
         issues,
-    } = data_connectors::resolve(&metadata_accessor, configuration)?;
+    } = data_connectors::resolve(&metadata_accessor, configuration).map_err(Error::from)?;
 
     all_issues.extend(issues.into_iter().map(Warning::from));
 
@@ -60,14 +61,14 @@ pub fn resolve(
         global_id_enabled_types,
         apollo_federation_entity_enabled_types,
         object_types,
-    } = object_types::resolve(&metadata_accessor, &data_connectors)?;
+    } = object_types::resolve(&metadata_accessor, &data_connectors).map_err(Error::from)?;
 
     // Validate custom defined scalar types
     let scalar_types::ScalarTypesOutput {
         scalar_types,
         graphql_types,
         issues,
-    } = scalar_types::resolve(&metadata_accessor, graphql_types)?;
+    } = scalar_types::resolve(&metadata_accessor, graphql_types).map_err(Error::from)?;
 
     all_issues.extend(issues.into_iter().map(Warning::from));
 
@@ -81,7 +82,8 @@ pub fn resolve(
         &data_connectors,
         &object_types,
         &scalar_types,
-    )?;
+    )
+    .map_err(Error::from)?;
 
     // Validate `DataConnectorScalarType` metadata. This will soon be deprecated and subsumed by
     // `BooleanExpressionType`
@@ -93,14 +95,15 @@ pub fn resolve(
         &data_connectors,
         &scalar_types,
         graphql_types,
-    )?;
+    )
+    .map_err(Error::from)?;
 
     // Fetch and validate permissions, and attach them to the relevant object types
     let object_types_with_permissions =
-        type_permissions::resolve(&metadata_accessor, object_types)?;
+        type_permissions::resolve(&metadata_accessor, object_types).map_err(Error::from)?;
 
     // collect raw relationships information
-    let relationships = relationships::resolve(&metadata_accessor)?;
+    let relationships = relationships::resolve(&metadata_accessor).map_err(Error::from)?;
 
     // Resolve fancy new boolean expression types
     let boolean_expressions::BooleanExpressionsOutput {
@@ -113,7 +116,8 @@ pub fn resolve(
         &graphql_config,
         &object_types_with_permissions,
         &relationships,
-    )?;
+    )
+    .map_err(Error::from)?;
 
     let order_by_expressions::OrderByExpressionsOutput {
         order_by_expressions,
@@ -137,7 +141,8 @@ pub fn resolve(
         &scalar_types,
         graphql_types,
         &graphql_config,
-    )?;
+    )
+    .map_err(Error::from)?;
 
     all_issues.extend(issues.into_iter().map(Warning::from));
 
@@ -154,7 +159,8 @@ pub fn resolve(
         &object_types_with_permissions,
         graphql_types,
         &graphql_config,
-    )?;
+    )
+    .map_err(Error::from)?;
 
     all_issues.extend(issues.into_iter().map(Warning::from));
 
@@ -179,7 +185,8 @@ pub fn resolve(
         &aggregate_expressions,
         order_by_expressions,
         graphql_types,
-    )?;
+    )
+    .map_err(Error::from)?;
 
     all_issues.extend(issues.into_iter().map(Warning::from));
 
@@ -191,13 +198,14 @@ pub fn resolve(
         &scalar_types,
         &object_boolean_expression_types,
         &boolean_expression_types,
-    )?;
+    )
+    .map_err(Error::from)?;
 
     all_issues.extend(issues.into_iter().map(Warning::from));
 
-    apollo::resolve(apollo_federation_entity_enabled_types)?;
+    apollo::resolve(apollo_federation_entity_enabled_types).map_err(Error::from)?;
 
-    relay::resolve(global_id_enabled_types)?;
+    relay::resolve(global_id_enabled_types).map_err(Error::from)?;
 
     let object_types_with_relationships = object_relationships::resolve(
         &metadata_accessor,

v3/crates/metadata-resolve/src/stages/models/source.rs
@@ -53,7 +53,6 @@ pub(crate) fn resolve_model_source(
         .0
         .get(&qualified_data_connector_name)
         .ok_or_else(|| ModelsError::UnknownModelDataConnector {
-            path: model.path.clone(),
             model_name: model.name.clone(),
             data_connector: qualified_data_connector_name.clone(),
         })?;

v3/crates/metadata-resolve/src/types/error.rs
@@ -23,6 +23,27 @@ use crate::helpers::{
     ndc_validation::NDCValidationError, type_mappings::TypeMappingCollectionError, typecheck,
 };
 
+// Eventually, we'll just delete the `Raw` variant and this will become a regular struct when all
+// errors have all the relevant path information.
+#[derive(Debug, thiserror::Error)]
+pub enum ErrorWithContext {
+    Raw(#[from] Error),
+    Contextualised {
+        error: Error,
+        path: jsonpath::JSONPath,
+    },
+}
+
+impl Display for ErrorWithContext {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ErrorWithContext::Contextualised { error, .. } | ErrorWithContext::Raw(error) => {
+                error.fmt(f)
+            }
+        }
+    }
+}
+
 // TODO: This enum really needs structuring
 #[derive(Debug, thiserror::Error)]
 pub enum Error {

v3/crates/metadata-resolve/src/stages/models/error.rs
@@ -28,7 +28,6 @@ pub enum ModelsError {
         "the source data connector {data_connector:} for model {model_name:} has not been defined"
     )]
     UnknownModelDataConnector {
-        path: jsonpath::JSONPath,
         model_name: Qualified<ModelName>,
         data_connector: Qualified<DataConnectorName>,
     },

v3/crates/metadata-resolve/src/stages/object_relationships/mod.rs
@@ -4,7 +4,6 @@ mod types;
 use std::collections::{BTreeMap, BTreeSet, HashSet};
 
 use indexmap::IndexMap;
-use jsonpath::JSONPath as Path;
 
 use lang_graphql::ast::common::{self as ast};
 use open_dds::aggregates::AggregateExpressionName;
@@ -389,7 +388,6 @@ fn get_relationship_capabilities(
             .ok_or_else(|| match target_name {
                 RelationshipTargetName::Model(model_name) => {
                     Error::from(models::ModelsError::UnknownModelDataConnector {
-                        path: Path::new(),
                         model_name: model_name.clone(),
                         data_connector: data_connector.name.clone(),
                     })

v3/crates/engine/src/build.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 #[derive(Debug, thiserror::Error)]
 pub enum BuildError {
     #[error("invalid metadata: {0}")]
-    InvalidMetadata(#[from] metadata_resolve::Error),
+    InvalidMetadata(#[from] metadata_resolve::ErrorWithContext),
     #[error("unable to build schema: {0}")]
     UnableToBuildSchema(#[from] graphql_schema::Error),
 }

v3/crates/engine/tests/validate_metadata.rs
@@ -230,9 +230,9 @@ fn test_disallow_object_mapped_to_scalar() -> anyhow::Result<()> {
         matches!(
             gds,
             Err(SchemaError::ResolveError {
-                error: ResolveError::ModelsError(
+                error: metadata_resolve::ErrorWithContext::Raw(ResolveError::ModelsError(
                     metadata_resolve::ModelsError::ModelTypeMappingCollectionError { .. }
-                )
+                ))
             })
         ),
         "actual: {gds:?}"
@@ -252,7 +252,9 @@ fn test_disallow_filter_expression_without_source() -> anyhow::Result<()> {
         matches!(
             gds,
             Err(SchemaError::ResolveError {
-                error: ResolveError::CannotUseFilterExpressionsWithoutSource { .. }
+                error: metadata_resolve::ErrorWithContext::Raw(
+                    ResolveError::CannotUseFilterExpressionsWithoutSource { .. }
+                )
             })
         ),
         "actual: {gds:?}"
@@ -270,8 +272,10 @@ fn test_disallow_filter_expression_with_object_type_mismatch() -> anyhow::Result
         matches!(
             gds,
             Err(SchemaError::ResolveError {
-                error: ResolveError::BooleanExpressionError(
-                    BooleanExpressionError::BooleanExpressionTypeForInvalidObjectTypeInModel { .. }
+                error: metadata_resolve::ErrorWithContext::Raw(
+                    ResolveError::BooleanExpressionError(
+                        BooleanExpressionError::BooleanExpressionTypeForInvalidObjectTypeInModel { .. }
+                    )
                 )
             })
         ),
@@ -291,7 +295,8 @@ fn test_disallow_boolean_expression_without_mapping() -> anyhow::Result<()> {
         matches!(
             gds,
             Err(SchemaError::ResolveError {
-                error: ResolveError::BooleanExpressionError (BooleanExpressionError::NoDataConnectorTypeMappingForObjectTypeInBooleanExpression { .. })
+                error: metadata_resolve::ErrorWithContext::Raw(
+                           ResolveError::BooleanExpressionError (BooleanExpressionError::NoDataConnectorTypeMappingForObjectTypeInBooleanExpression { .. }))
             })
         ),
         "actual: {gds:?}"

v3/crates/graphql/schema/src/aggregates.rs
@@ -137,7 +137,8 @@ fn add_aggregatable_fields(
             })?;
 
         let field_graphql_name = mk_name(aggregatable_field_info.field_name.as_str())
-            .map_err(metadata_resolve::Error::from)?;
+            .map_err(metadata_resolve::Error::from)
+            .map_err(metadata_resolve::ErrorWithContext::from)?;
 
         let field = gql_schema::Field::<GDS>::new(
             field_graphql_name.clone(),
@@ -272,7 +273,8 @@ fn add_aggregation_functions(
 ) -> Result<(), Error> {
     for aggregatable_function_info in &aggregate_expression.operand.aggregation_functions {
         let field_graphql_name = mk_name(aggregatable_function_info.name.as_str())
-            .map_err(metadata_resolve::Error::from)?;
+            .map_err(metadata_resolve::Error::from)
+            .map_err(metadata_resolve::ErrorWithContext::from)?;
 
         let field = gql_schema::Field::<GDS>::new(
             field_graphql_name.clone(),

commit b2ed771d46a0634797e60e7d37cebf0b15cac85b

Wire up an error path to appear in the MBS API responses (#1180)
### What

This PR completes step 2 on the plan in #1153. With this PR, an attempt
to call MBS to build metadata containing a model referencing an unknown
data connector should result in a path to the model.

### How

#1179 introduced the polymorphic version of the error type introduced by
#1170. Unfortunately, due to Rust not having an apartness constraint, we
can't write `impl<T, S: From<T> From<WithContext<T>> for
WithContext<S>`, so we introduce the `coerce` function to allow us to
cast between `WithContext<ChildError>` and `WithContext<ParentError>`.

This means that we can return a `WithContext<ModelsError>` from the
models stage, coerce it to a `WithContext<Error>`, and propagate that
path throughout the system. This PR also extends the `path` function
inside MBS to detect these paths if present, so MBS API errors will
contain any available path.

V3_GIT_ORIGIN_REV_ID: a47c565b792fefb99294c9e21ba526424ddf7cb7

v3/crates/metadata-resolve/src/stages/mod.rs
@@ -191,7 +191,7 @@ pub fn resolve(
         order_by_expressions,
         graphql_types,
     )
-    .map_err(Error::from)?;
+    .map_err(WithContext::coerce)?;
 
     all_issues.extend(issues.into_iter().map(Warning::from));
 

v3/crates/metadata-resolve/src/stages/models/mod.rs
@@ -65,7 +65,7 @@ pub fn resolve(
     >,
     mut order_by_expressions: OrderByExpressions,
     mut graphql_types: BTreeSet<ast::TypeName>,
-) -> Result<ModelsOutput, ModelsError> {
+) -> Result<ModelsOutput, crate::types::error::WithContext<ModelsError>> {
     // resolve models
     // TODO: validate types
     let mut models = IndexMap::new();
@@ -110,13 +110,13 @@ pub fn resolve(
             ) {
                 None => {}
                 Some(duplicate_model_name) => {
-                    return Err(ModelsError::from(
+                    Err(ModelsError::from(
                         relay::RelayError::DuplicateModelGlobalIdSource {
-                            model_1: resolved_model.name,
+                            model_1: resolved_model.name.clone(),
                             model_2: duplicate_model_name,
-                            object_type: resolved_model.data_type,
+                            object_type: resolved_model.data_type.clone(),
                         },
-                    ))
+                    ))?;
                 }
             }
         }
@@ -158,9 +158,9 @@ pub fn resolve(
             .insert(qualified_model_name.clone(), resolved_model)
             .is_some()
         {
-            return Err(ModelsError::DuplicateModelDefinition {
+            Err(ModelsError::DuplicateModelDefinition {
                 name: qualified_model_name,
-            });
+            })?;
         }
     }
     Ok(ModelsOutput {
@@ -191,7 +191,7 @@ fn resolve_model(
     >,
     order_by_expressions: &mut OrderByExpressions,
     graphql_types: &mut BTreeSet<ast::TypeName>,
-) -> Result<Model, ModelsError> {
+) -> Result<Model, crate::types::error::WithContext<ModelsError>> {
     let qualified_object_type_name = Qualified::new(subgraph.clone(), model.object_type().clone());
     let qualified_model_name = Qualified::new(subgraph.clone(), model.name().clone());
     let object_type_representation = source::get_model_object_type_representation(
@@ -208,19 +208,19 @@ fn resolve_model(
             .global_id_fields
             .is_empty()
         {
-            return Err(ModelsError::from(
+            Err(ModelsError::from(
                 relay::RelayError::NoGlobalFieldsPresentInGlobalIdSource {
-                    type_name: qualified_object_type_name,
+                    type_name: qualified_object_type_name.clone(),
                     model_name: model.name().clone(),
                 },
-            ));
+            ))?;
         }
         if !model.arguments().is_empty() {
-            return Err(ModelsError::from(
+            Err(ModelsError::from(
                 relay::RelayError::ModelWithArgumentsAsGlobalIdSource {
-                    model_name: qualified_model_name,
+                    model_name: qualified_model_name.clone(),
                 },
-            ));
+            ))?;
         }
         // model has `global_id_source`; insert into the BTreeMap of `global_id_enabled_types`
         match global_id_enabled_types.get_mut(&qualified_object_type_name) {
@@ -255,11 +255,11 @@ fn resolve_model(
             .is_some()
         {
             if !model.arguments().is_empty() {
-                return Err(ModelsError::from(
+                Err(ModelsError::from(
                     apollo::ApolloError::ModelWithArgumentsAsApolloFederationEntitySource {
-                        model_name: qualified_model_name,
+                        model_name: qualified_model_name.clone(),
                     },
-                ));
+                ))?;
             }
             // model has `apollo_federation_entity_source`; insert into the BTreeMap of
             // `apollo_federation_entity_enabled_types`
@@ -267,12 +267,12 @@ fn resolve_model(
                 None => {
                     // the model's graphql configuration has `apollo_federation.entitySource` but the object type
                     // of the model doesn't have any apollo federation keys
-                    return Err(ModelsError::from(
+                    Err(ModelsError::from(
                         apollo::ApolloError::NoKeysFieldsPresentInEntitySource {
-                            type_name: qualified_object_type_name,
+                            type_name: qualified_object_type_name.clone(),
                             model_name: model.name().clone(),
                         },
-                    ));
+                    ))?;
                 }
                 Some(type_name) => {
                     match type_name {
@@ -281,11 +281,11 @@ fn resolve_model(
                         }
                         // Multiple models are marked as apollo federation entity source
                         Some(_) => {
-                            return Err(ModelsError::from(
+                            Err(ModelsError::from(
                                 apollo::ApolloError::MultipleEntitySourcesForType {
-                                    type_name: qualified_object_type_name,
+                                    type_name: qualified_object_type_name.clone(),
                                 },
-                            ));
+                            ))?;
                         }
                     }
                 }
@@ -317,10 +317,10 @@ fn resolve_model(
             )
             .is_some()
         {
-            return Err(ModelsError::DuplicateModelArgumentDefinition {
-                model_name: qualified_model_name,
+            Err(ModelsError::DuplicateModelArgumentDefinition {
+                model_name: qualified_model_name.clone(),
                 argument_name: argument.name.clone(),
-            });
+            })?;
         }
     }
 

v3/crates/metadata-resolve/src/stages/models/source.rs
@@ -40,21 +40,26 @@ pub(crate) fn resolve_model_source(
         object_boolean_expressions::ObjectBooleanExpressionType,
     >,
     boolean_expression_types: &boolean_expressions::BooleanExpressionTypes,
-) -> Result<(ModelSource, Vec<ModelsIssue>), ModelsError> {
+) -> Result<(ModelSource, Vec<ModelsIssue>), crate::WithContext<ModelsError>> {
     if model.source.is_some() {
-        return Err(ModelsError::DuplicateModelSourceDefinition {
+        Err(ModelsError::DuplicateModelSourceDefinition {
             model_name: model.name.clone(),
-        });
+        })?;
     }
-    let qualified_data_connector_name =
-        Qualified::new(subgraph.clone(), model_source.data_connector_name.clone());
+    let qualified_data_connector_name = Qualified::new(
+        subgraph.clone(),
+        model_source.data_connector_name.value.clone(),
+    );
 
     let data_connector_context = data_connectors
         .0
         .get(&qualified_data_connector_name)
-        .ok_or_else(|| ModelsError::UnknownModelDataConnector {
-            model_name: model.name.clone(),
-            data_connector: qualified_data_connector_name.clone(),
+        .ok_or_else(|| crate::WithContext::Contextualised {
+            error: ModelsError::UnknownModelDataConnector {
+                model_name: model.name.clone(),
+                data_connector: qualified_data_connector_name.clone(),
+            },
+            path: model_source.data_connector_name.path.clone(),
         })?;
 
     let source_collection = data_connector_context
@@ -137,7 +142,8 @@ pub(crate) fn resolve_model_source(
             qualified_data_connector_name,
             data_connector_context,
         )
-        .map(Arc::new)?,
+        .map(Arc::new)
+        .map_err(ModelsError::from)?,
         collection: model_source.collection.clone(),
         collection_type: source_collection_type,
         type_mappings,
@@ -192,7 +198,8 @@ pub(crate) fn resolve_model_source(
         }
     }
 
-    ndc_validation::validate_ndc(&model.name, model, &data_connector_context.schema)?;
+    ndc_validation::validate_ndc(&model.name, model, &data_connector_context.schema)
+        .map_err(ModelsError::from)?;
     Ok((resolved_model_source, issues))
 }
 

v3/crates/metadata-resolve/src/types/error.rs
@@ -32,6 +32,25 @@ pub enum WithContext<T> {
     Contextualised { error: T, path: jsonpath::JSONPath },
 }
 
+impl<T> WithContext<T> {
+    pub fn path(&self) -> Option<jsonpath::JSONPath> {
+        match self {
+            WithContext::Contextualised { path, .. } => Some(path.clone()),
+            WithContext::Raw(_) => None,
+        }
+    }
+
+    pub fn coerce<S: From<T>>(self) -> WithContext<S> {
+        match self {
+            WithContext::Raw(err) => WithContext::Raw(S::from(err)),
+            WithContext::Contextualised { error, path } => WithContext::Contextualised {
+                error: S::from(error),
+                path,
+            },
+        }
+    }
+}
+
 impl<T: Display> Display for WithContext<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {

v3/crates/open-dds/metadata.jsonschema
@@ -2731,12 +2731,11 @@
       ],
       "properties": {
         "dataConnectorName": {
+          "$id": "https://hasura.io/jsonschemas/metadata/DataConnectorName",
+          "title": "DataConnectorName",
           "description": "The name of the data connector backing this model.",
-          "allOf": [
-            {
-              "$ref": "#/definitions/DataConnectorName"
-            }
-          ]
+          "type": "string",
+          "pattern": "^[_a-zA-Z][_a-zA-Z0-9]*$"
         },
         "collection": {
           "description": "The collection in the data connector that backs this model.",

v3/crates/open-dds/src/models.rs
@@ -11,6 +11,7 @@ use crate::{
     str_newtype,
     traits::{OpenDd, OpenDdDeserializeError},
     types::{CustomTypeName, Deprecated, FieldName, GraphQlFieldName, GraphQlTypeName},
+    Spanned,
 };
 
 str_newtype!(ModelName over Identifier | doc "The name of data model.");
@@ -231,7 +232,7 @@ pub struct ModelV2 {
 /// Description of how a model maps to a particular data connector
 pub struct ModelSource {
     /// The name of the data connector backing this model.
-    pub data_connector_name: DataConnectorName,
+    pub data_connector_name: Spanned<DataConnectorName>,
 
     /// The collection in the data connector that backs this model.
     pub collection: CollectionName,