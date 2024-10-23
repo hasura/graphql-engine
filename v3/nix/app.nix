cff49205529b22efe69d61e77f46b2f250975359
Daniel Harvey <danieljamesharvey@gmail.com>
2024-10-23 16:11:24 +0100

    More general nix build (#1265)
    <!-- The PR description should answer 2 important questions: -->
    
    ### What
    
    This was building more often than I'd expected - looking at the crane
    docs we'll get more cache sharing if we build all the deps.
    
    https://crane.dev/examples/quick-start.html?highlight=cargoArtifacts#
    
    V3_GIT_ORIGIN_REV_ID: b6e29d8daa8b77b76324819607f0c50c1a00df70

--- v3/nix/app.nix
+++ v3/nix/app.nix
@@ -42,14 +42,11 @@ let
     ];
   };
 
-  # only build the binary we care about
-  cargoExtraArgs = "--package ${packageName}";
-
   # Build the dependencies first.
   cargoArtifacts = craneLib.buildDepsOnly (buildArgs //
     {
       # without this we'll build deps for the entire workspace every time
-      buildPhaseCargoCommand = "cargo build --profile $CARGO_PROFILE --package ${packageName}";
+      buildPhaseCargoCommand = "cargo build --profile $CARGO_PROFILE";
       doCheck = false;
     }
 
@@ -58,7 +55,7 @@ in
 # Then build the crate.
 craneLib.buildPackage
   (buildArgs // {
-    inherit cargoArtifacts cargoExtraArgs;
+    inherit cargoArtifacts;
     doCheck = false;
     RELEASE_VERSION = version;
   })
