b5a0b3d68f64bcc0ed13138ebc29a0bbeb4815a3
Varun Choudhary <68095256+Varun-Choudhary@users.noreply.github.com>
2025-01-23 19:00:50 +0530

    Console[hotifx]: fix model summary on data tab
    PR-URL: https://github.com/hasura/graphql-engine-mono/pull/11127
    GitOrigin-RevId: 6930ffcc6212dd8761036164badbe6aff7b4caf8

--- frontend/libs/console/legacy-ce/src/lib/components/Services/Data/ModelSummary/useModelCountSummary.ts
+++ frontend/libs/console/legacy-ce/src/lib/components/Services/Data/ModelSummary/useModelCountSummary.ts
@@ -2,23 +2,25 @@ import { useMetadata } from '../../../../features/hasura-metadata-api';
 import { Metadata } from '../../../../features/hasura-metadata-types';
 import { SourceLevelSummary } from './ModelSummary';
 
+const MONGO_DB_TYPE = 'mongodb';
+
 export const selectModelSummaryData = (m: Metadata) => {
   const tablesAndViews: SourceLevelSummary[] = m.metadata.sources
-    .filter(source => source.kind !== 'mongo')
+    .filter(source => source.kind !== MONGO_DB_TYPE)
     .map(source => ({
       dataSourceName: source.name,
       totalCount: source.tables.length,
     }));
 
   const collections: SourceLevelSummary[] = m.metadata.sources
-    .filter(source => source.kind === 'mongo')
+    .filter(source => source.kind === MONGO_DB_TYPE)
     .map(source => ({
       dataSourceName: source.name,
       totalCount: source.tables.length,
     }));
 
   const logicalModels: SourceLevelSummary[] = m.metadata.sources
-    .filter(source => source.kind !== 'mongo')
+    .filter(source => source.kind !== MONGO_DB_TYPE)
     .map(source => ({
       dataSourceName: source.name,
       totalCount: source?.logical_models?.length ?? 0,
