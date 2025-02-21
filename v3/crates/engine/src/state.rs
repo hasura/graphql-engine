95cf3af990b26aa18a8eab84ca7251d9703832a0
Daniel Harvey <danieljamesharvey@gmail.com>
2025-02-21 15:17:43 +0000

    Do MBS build at startup in `ddn-engine-local-dev` (#1632)
    <!-- The PR description should answer 2 important questions: -->
    
    ### What
    
    This adds a new runtime flag to `ddn-engine-local-dev`,
    `--metadata-kind`, which can set to `open-dd` (the default, current
    behaviour) or `hasura` which takes metadata in the form Metadata Build
    Service expects it.
    
    When it's enabled, we build the metadata with MBS, and interpolate any
    environment variables directly in.
    
    To test:
    
    - Create a DDN project following the [DDN Quickstart](url).
    
    - Create a local build with `ddn supergraph build local`.
    
    - Open the generated metadata in `engine/build/metadata.json` and change
    the `url` section in `DataConnectorLink` to use environment variables:
    
    ```json
    "url": {
      "readWriteUrls": {
        "read": { "valueFromEnv": "WHERE_IS_NDC_POSTGRES" },
        "write": { "valueFromEnv": "WHERE_IS_NDC_POSTGRES" }
      }
    }
    ```
    
    - Run the engine with the flag:
    
    ```bash
     cargo run \
       --bin ddn-engine-local-dev -- \
       --authn-config-path static/auth/auth_config.json \
       --metadata-path ../test-project/supergraph/engine/build/metadata.json \
       --metadata-kind hasura
    ```
    
    - See an error because of the missing env var:
    
    ```bash
    Error while starting up the engine: failed to build engine state - unable to resolve environment_variables: environment_variable WHERE_IS_NDC_POSTGRES not found
    ```
    
    - Export the environment variable
    
    ```bash
    export WHERE_IS_NDC_POSTGRES=http://local.hasura.dev:8407
    ```
    
    - Run it again (with command above)
    
    - Make a request in the console
    
    - Success!
    
    <img width="1309" alt="Screenshot 2025-02-21 at 13 08 28"
    src="https://github.com/user-attachments/assets/7f7217eb-afbc-4c4b-a7f3-02e4a8de0ad5"
    />
    V3_GIT_ORIGIN_REV_ID: d1c6a2e0beeb933f3997fe22b1e0c400089c680a

--- v3/crates/engine/src/state.rs
+++ v3/crates/engine/src/state.rs
@@ -3,39 +3,44 @@ use engine_types::{ExposeInternalErrors, HttpContext};
 use graphql_ir::GraphqlRequestPipeline;
 use hasura_authn::resolve_auth_config;
 use std::fmt::Display;
-use std::path::PathBuf;
 use std::sync::Arc;
 
 #[allow(clippy::print_stdout)]
 /// Print any build warnings to stdout
-fn print_warnings<T: Display>(warnings: Vec<T>) {
+pub fn print_warnings<T: Display>(warnings: Vec<T>) {
     for warning in warnings {
         println!("Warning: {warning}");
     }
+}
+
+pub fn resolve_metadata(
+    raw_metadata: &str,
+    raw_auth_config: &str,
+    metadata_resolve_configuration: &metadata_resolve::configuration::Configuration,
+) -> Result<(metadata_resolve::Metadata, hasura_authn::AuthConfig), anyhow::Error> {
+    // Auth Config
+    let (auth_config, auth_warnings) =
+        resolve_auth_config(raw_auth_config).map_err(StartupError::ReadAuth)?;
+
+    // Metadata
+    let metadata = open_dds::Metadata::from_json_str(raw_metadata)?;
+    let (resolved_metadata, warnings) =
+        metadata_resolve::resolve(metadata, metadata_resolve_configuration)?;
+
+    print_warnings(auth_warnings);
+    print_warnings(warnings);
+
+    Ok((resolved_metadata, auth_config))
 }
 
 /// Build the engine state - include auth, metadata, and jsonapi context.
 pub fn build_state(
     request_pipeline: GraphqlRequestPipeline,
     expose_internal_errors: ExposeInternalErrors,
-    authn_config_path: &PathBuf,
-    metadata_path: &PathBuf,
-    metadata_resolve_configuration: &metadata_resolve::configuration::Configuration,
+    auth_config: hasura_authn::AuthConfig,
+    resolved_metadata: metadata_resolve::Metadata,
 ) -> Result<EngineState, anyhow::Error> {
-    // Auth Config
-    let raw_auth_config = std::fs::read_to_string(authn_config_path)?;
-    let (auth_config, auth_warnings) =
-        resolve_auth_config(&raw_auth_config).map_err(StartupError::ReadAuth)?;
-
-    // Metadata
-    let raw_metadata = std::fs::read_to_string(metadata_path)?;
-    let metadata = open_dds::Metadata::from_json_str(&raw_metadata)?;
-    let (resolved_metadata, warnings) =
-        metadata_resolve::resolve(metadata, metadata_resolve_configuration)?;
     let resolved_metadata = Arc::new(resolved_metadata);
-
-    print_warnings(auth_warnings);
-    print_warnings(warnings);
 
     let http_context = HttpContext {
         client: reqwest::Client::new(),
