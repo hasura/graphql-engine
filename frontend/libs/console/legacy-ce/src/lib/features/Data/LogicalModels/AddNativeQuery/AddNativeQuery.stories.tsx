9a37e451bf50521f837427c513628ebb5f835225
jack-chan-123 <wing.chan@student.uclouvain.be>
2025-08-14 01:00:09 +0800

    Fix typo on Native Query page

--- frontend/libs/console/legacy-ce/src/lib/features/Data/LogicalModels/AddNativeQuery/AddNativeQuery.stories.tsx
+++ frontend/libs/console/legacy-ce/src/lib/features/Data/LogicalModels/AddNativeQuery/AddNativeQuery.stories.tsx
@@ -58,7 +58,7 @@ const fillAndSubmitForm: Story['play'] = async (
     'my_native_query'
   );
   await userEvent.type(
-    c.getByPlaceholderText('A description of this logical model'),
+    c.getByPlaceholderText('A description of this Native query'),
     'a description'
   );
 
